a5dface136005a13a4686363f8bcbf467eb5cc49
Dmitry Filimonov <me@dfilimonov.com>
2020-12-18 21:39:27 -0800

    making changes

--- third_party/pyspy/pyspy.h
+++
@@ -1,5 +0,0 @@
-#include <sys/types.h>
-
-int pyspy_init(pid_t pid);
-int pyspy_cleanup(pid_t pid);
-int pyspy_snapshot(pid_t pid, void* pointer, int len);
--- third_party/rbspy/rbspy.h
+++
@@ -1,5 +0,0 @@
-#include <sys/types.h>
-
-int rbspy_init(pid_t pid);
-int rbspy_cleanup(pid_t pid);
-int rbspy_snapshot(pid_t pid, void* pointer, int len);
