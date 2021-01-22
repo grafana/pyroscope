ae6f34ded2aa557121688fdca810e65a7d8b54de
Louis Lo <louislo@gmail.com>
2021-01-21 22:54:22 -0800

    Lint MaxNodesSelector.jsx

--- webapp/javascript/components/MaxNodesSelector.jsx
+++ webapp/javascript/components/MaxNodesSelector.jsx
@@ -1,42 +1,34 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 import { setMaxNodes } from "../redux/actions";
 
 class MaxNodesSelector extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  select = (event) =>{
+  select = (event) => {
+    // eslint-disable-next-line radix, react/destructuring-assignment
     this.props.setMaxNodes(parseInt(event.target.value));
-  }
+  };
 
   render() {
-    let options = [
-      1024,
-      2048,
-      4096,
-      8192,
-      16384,
-      32768,
-      65536,
-    ];
-    let selected = this.props.maxNodes;
-    return <span>
-      Max Nodes:&nbsp;
-      <select className="max-nodes-select" value={selected} onChange={this.select}>
-        {options.map(function(name) {
-          return <option
-            key={name}
-            value={name}
-          >{name}</option>;
-        })}
-      </select>
-    </span>
+    const options = [1024, 2048, 4096, 8192, 16384, 32768, 65536];
+    // eslint-disable-next-line react/destructuring-assignment
+    const selected = this.props.maxNodes;
+    return (
+      <span>
+        Max Nodes:&nbsp;
+        <select
+          className="max-nodes-select"
+          value={selected}
+          onChange={this.select}
+        >
+          {options.map((name) => (
+            <option key={name} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </span>
+    );
   }
 }
 
-export default connect(
-  (x) => x,
-  { setMaxNodes }
-)(MaxNodesSelector);
+export default connect((x) => x, { setMaxNodes })(MaxNodesSelector);
