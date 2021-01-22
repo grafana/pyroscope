05fd92b436e9e0b28f13f944634c0cec6c0cef22
Louis Lo <louislo@gmail.com>
2021-01-21 22:09:31 -0800

    Lint & Modernize RefreshButton.jsx

--- webapp/javascript/components/RefreshButton.jsx
+++ webapp/javascript/components/RefreshButton.jsx
@@ -1,29 +1,18 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 import { refresh } from "../redux/actions";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
-
-class RefreshButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  refresh = () => {
-    this.props.refresh();
-  };
-
-  render() {
-    return <div>
-      <button className="btn refresh-btn" onClick={this.refresh}>
+function RefreshButton() {
+  return (
+    <div>
+      <button className="btn refresh-btn" onClick={() => refresh()}>
         <FontAwesomeIcon icon={faSyncAlt} />
       </button>
     </div>
-  }
+  );
 }
 
-export default connect(
-  (x) => x,
-  { refresh }
-)(RefreshButton);
+export default connect((x) => x, { refresh })(RefreshButton);
