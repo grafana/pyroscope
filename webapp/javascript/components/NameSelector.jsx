fb27aeadeef3efaf84f74930afbb5f74f25bbaf3
Louis Lo <louislo@gmail.com>
2021-01-21 22:08:11 -0800

    Lint & Modernize NameSelector.jsx

--- webapp/javascript/components/NameSelector.jsx
+++ webapp/javascript/components/NameSelector.jsx
@@ -1,57 +1,50 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import { addLabel, receiveJSON} from "../redux/actions";
 import { bindActionCreators } from "redux";
+import { addLabel, receiveJSON } from "../redux/actions";
 
+function NameSelector(props) {
+  const { actions, names, labels } = props;
+  const selectName = (event) => {
+    actions.addLabel("__name__", event.target.value);
+  };
 
-class NameSelector extends React.Component {
-  constructor() {
-    super();
-
-  }
-
-  selectName = (event) =>{
-    this.props.actions.addLabel("__name__", event.target.value)
-  }
-
-  render() {
-    let names = this.props.names || [];
-    let selectedName = this.props.labels.filter(x => x.name == "__name__")[0];
-    selectedName = selectedName ? selectedName.value : "none";
-    return <span>
+  let selectedName = labels.filter((x) => x.name === "__name__")[0];
+  selectedName = selectedName ? selectedName.value : "none";
+  return (
+    <span>
       Application:&nbsp;
-      <select className="label-select" value={selectedName} onChange={this.selectName}>
-        <option
-          disabled
-          key="Select an app..."
-          value="Select an app..."
-        >Select an app...</option>
-        {names.map(function(name) {
-          return <option
-            key={name}
-            value={name}
-          >{name}</option>;
-        })}
+      <select
+        className="label-select"
+        value={selectedName}
+        onChange={selectName}
+      >
+        <option disabled key="Select an app..." value="Select an app...">
+          Select an app...
+        </option>
+        {names &&
+          names.map((name) => (
+            <option key={name} value={name}>
+              {name}
+            </option>
+          ))}
       </select>
     </span>
-  }
+  );
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   ...state,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   actions: bindActionCreators(
     {
       receiveJSON,
       addLabel,
     },
-    dispatch,
+    dispatch
   ),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(NameSelector);
+export default connect(mapStateToProps, mapDispatchToProps)(NameSelector);
