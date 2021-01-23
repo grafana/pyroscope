dabc10180df6e910c6808a0ca440a01085a32c32
Louis Lo <louislo@gmail.com>
2021-01-22 22:44:05 -0800

    Fix refresh button

--- webapp/javascript/components/RefreshButton.jsx
+++ webapp/javascript/components/RefreshButton.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 import { refresh } from "../redux/actions";
 
 function RefreshButton() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button className="btn refresh-btn" onClick={() => refresh()}>
+      <button className="btn refresh-btn" onClick={() => dispatch(refresh())}>
         <FontAwesomeIcon icon={faSyncAlt} />
       </button>
     </div>
   );
 }
 
-export default connect((x) => x, { refresh })(RefreshButton);
+export default RefreshButton;
