9fa9e34314e0e33ee9127e0c2f4d17cbd782ffa3
Dmitry Filimonov <dmitry@pyroscope.io>
2021-01-31 10:55:36 -0800

    reverts examples/golang/main.go changes

--- examples/golang/main.go
+++ examples/golang/main.go
@@ -7,7 +7,11 @@ import (
 )
 
 func work(int) {
+	// revive:disable:empty-block this is fine because this is a example app, not real production code
+	for i := 0; i < n; i++ {
 
+	}
+	// revive:enable:empty-block
 }
 
 func fastFunction() {
