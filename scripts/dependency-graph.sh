e4c216810277eeba9676abaf2fb1e847c801aeff
Dmitry Filimonov <dmitry@pyroscope.io>
2021-02-26 00:24:56 -0800

    fixes dependency graph task

--- scripts/dependency-graph.sh
+++ scripts/dependency-graph.sh
@@ -6,10 +6,9 @@ godepgraph -nostdlib -s ./cmd/pyroscope/ > tmp/deps.dot
   cat tmp/deps.dot | head -n 7 | grep -v 'splines=ortho'
   cat tmp/deps.dot | \
     grep pyroscope | \
-    sed 's/github.com\/petethepig\/pyroscope\///g' | \
+    sed 's/github.com\/pyroscope-io\/pyroscope\///g' | \
     grep -v 'pkg/config' | \
     grep -v 'pkg/build' | \
-    grep -v 'pkg/log' | \
     grep -v .com
   echo "}"
 ) | dot -Tsvg -o tmp/go-deps-graph.svg
