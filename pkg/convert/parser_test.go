1ae506dae8e2820d0157c0b00b6cca50697fad1a
Dmitry Filimonov <dmitry@pyroscope.io>
2021-04-24 20:45:12 -0700

    adds more go tests

--- pkg/convert/parser_test.go
+++ pkg/convert/parser_test.go
@@ -2,7 +2,9 @@ package convert
 
 import (
 	"bytes"
+	"compress/gzip"
 	"fmt"
+	"io/ioutil"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -19,6 +21,26 @@ var _ = Describe("convert", func() {
 			Expect(result).To(ConsistOf("foo;bar 1", "foo;baz 1"))
 		})
 	})
+
+	Describe("ParsePprof", func() {
+		It("parses data correctly", func() {
+			result := []string{}
+
+			b, err := ioutil.ReadFile("fixtures/cpu.pprof")
+			Expect(err).ToNot(HaveOccurred())
+			r := bytes.NewReader(b)
+			g, err := gzip.NewReader(r)
+			Expect(err).ToNot(HaveOccurred())
+			p, err := ParsePprof(g)
+			Expect(err).ToNot(HaveOccurred())
+
+			p.Get("samples", func(name []byte, val int) {
+				result = append(result, fmt.Sprintf("%s %d", name, val))
+			})
+			Expect(result).To(ContainElement("runtime.main;main.work 1"))
+		})
+	})
+
 	Describe("ParseGroups", func() {
 		It("parses data correctly", func() {
 			r := bytes.NewReader([]byte("foo;bar 10\nfoo;baz 20\n"))
@@ -29,6 +51,7 @@ var _ = Describe("convert", func() {
 			Expect(result).To(ConsistOf("foo;bar 10", "foo;baz 20"))
 		})
 	})
+
 	Describe("ParseIndividualLines", func() {
 		It("parses data correctly", func() {
 			r := bytes.NewReader([]byte("foo;bar\nfoo;baz\n"))
