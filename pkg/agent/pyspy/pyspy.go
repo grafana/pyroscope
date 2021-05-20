743d317fec4f219d8a302b9e629fc23402ea01e1
alonlong <alonlong@163.com>
2021-05-21 07:01:41 +0800

    support the compile on linux system, like ubuntu

--- pkg/agent/pyspy/pyspy.go
+++ pkg/agent/pyspy/pyspy.go
@@ -4,7 +4,7 @@
 package pyspy
 
 // #cgo darwin LDFLAGS: -L../../../third_party/rustdeps/target/release -lrustdeps
-// #cgo linux LDFLAGS: -L../../../third_party/rustdeps/target/release -lrustdeps
+// #cgo linux LDFLAGS: -L../../../third_party/rustdeps/target/release -lrustdeps -ldl -lm -lrt -lunwind-x86_64 -lunwind-ptrace
 // #include "../../../third_party/rustdeps/pyspy.h"
 import "C"
 
