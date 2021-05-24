7a1482c613dcf19b05d0127f671d35780f6ebfb0
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2021-05-24 17:47:11 +0200

    Disable dotnet profiling options for Windows

--- pkg/agent/dotnetspy/placeholder.go
+++ pkg/agent/dotnetspy/placeholder.go
@@ -1,3 +1,1 @@
-// +build !dotnetspy
-
 package dotnetspy
---
+++ pkg/agent/dotnetspy/session_windows.go
@@ -0,0 +1,7 @@
+// +build dotnetspy
+
+package dotnetspy
+
+import "github.com/pyroscope-io/dotnetdiag/nettrace/profiler"
+
+var profilerOptions []profiler.Option
