e70ba293520bceee4cd422668e5e034cb72e4ad6
Dmitry Filimonov <dmitry@pyroscope.io>
2021-06-01 23:56:51 -0700

    eslint fix

--- benchmark/take-screenshot.js
+++ benchmark/take-screenshot.js
@@ -1,26 +1,29 @@
-console.log('puppeteer start');
+console.log("puppeteer start");
 
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const args = process.argv.slice(2);
 
 const from = args[1];
 
-(async() => {
+(async () => {
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
-  await page.setViewport({width: 1600, height: 1280})
-  await page.goto('http://localhost:8080/d/65gjqY3Mk/main?orgId=1&from='+from, {waitUntil: 'networkidle2'});
+  await page.setViewport({ width: 1600, height: 1280 });
+  await page.goto(
+    `http://localhost:8080/d/65gjqY3Mk/main?orgId=1&from=${from}`,
+    { waitUntil: "networkidle2" }
+  );
   await timeout(2000);
   await page.screenshot({
     captureBeyondViewport: false,
-    path: args[0]
+    path: args[0],
   });
   browser.close();
 })();
 
-console.log('puppeteer stop');
+console.log("puppeteer stop");
