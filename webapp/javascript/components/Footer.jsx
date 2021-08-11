0c89ea5f15ff7e5a3b6edcc429ca16f3c8869a3d
Dmitry Filimonov <dmitry@pyroscope.io>
2021-08-11 00:49:32 -0700

    js lint fixes

--- webapp/javascript/components/Footer.jsx
+++ webapp/javascript/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { version } from "../../../package.json";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faDownload,
-} from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { version } from "../../../package.json";
 
 const START_YEAR = 2020;
 const PYROSCOPE_VERSION = version;
@@ -32,7 +30,8 @@ function buildInfo() {
 
 function Footer() {
   const latestVersion = window.latestVersionInfo.latest_version;
-  const newVersionAvailable = latestVersion && window.buildInfo.version !== latestVersion;
+  const newVersionAvailable =
+    latestVersion && window.buildInfo.version !== latestVersion;
 
   return (
     <div className="footer" title={buildInfo()}>
@@ -40,17 +39,19 @@ function Footer() {
         {`© Pyroscope ${copyrightYears(START_YEAR, new Date().getFullYear())}`}
       </span>
       &nbsp;&nbsp;|&nbsp;&nbsp;
-      <span>
-        {window.buildInfo.version}
-      </span>
-      {
-        newVersionAvailable && <span>
+      <span>{window.buildInfo.version}</span>
+      {newVersionAvailable && (
+        <span>
           &nbsp;&nbsp;|&nbsp;&nbsp;
-          <a href="https://pyroscope.io/downloads?utm_source=pyroscope_footer" target="_blank">
+          <a
+            href="https://pyroscope.io/downloads?utm_source=pyroscope_footer"
+            rel="noreferrer"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faDownload} />&nbsp;<span>Newer Version Available ({latestVersion})</span>
           </a>
         </span>
-      }
+      )}
     </div>
   );
 }
