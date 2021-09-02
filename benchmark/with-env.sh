f966660dd94ef4ac3662fc307ec97a8edcc37fca
eduardo <eduardoaleixomartins@gmail.com>
2021-09-02 12:24:10 -0300

    allow script to be run from anywhere

--- benchmark/with-env.sh
+++ benchmark/with-env.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Each variable or function that is created or modified is given the export attribute
 # and marked for export to the environment of subsequent commands. 
 set -o allexport
-source ./run-parameters.env
+source "$SCRIPT_DIR/run-parameters.env"
 set +o allexport
 
 $@
