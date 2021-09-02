4d249edb52efb1077431a4a2a03ee7ac950f8141
eduardo <eduardoaleixomartins@gmail.com>
2021-09-02 17:42:13 -0300

    .

---
+++ benchmark/cireport/cireport.go
@@ -0,0 +1,57 @@
+package cireport
+
+import (
+	"bytes"
+	"text/template"
+	"time"
+
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+)
+
+type ciReport struct {
+	q Querier
+}
+type Querier interface {
+	Instant(query string, t time.Time) (model.Value, v1.Warnings, error)
+}
+
+func New(q Querier) *ciReport {
+	return &ciReport{
+		q,
+	}
+}
+
+// Report reports benchmarking results in markdown format
+func (r *ciReport) Report() (string, error) {
+	query := `rate(pyroscope_http_request_duration_seconds_count{handler="/ingest", code="200"}[5m])`
+	now := time.Now()
+
+	result, _, err := r.q.Instant(query, now)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO(eh-am): mount markdown
+	var tpl bytes.Buffer
+
+	data := struct {
+		Result model.Value
+	}{
+		Result: result,
+	}
+
+	t, err := template.New("tw").Parse(`
+# benchmarking results
+string {{ .Result }}
+	`)
+	if err != nil {
+		return "", err
+	}
+
+	if err := t.Execute(&tpl, data); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
