3f5091fd6a50ac13be7ee34b1f2ef6ee32d58b89
eduardo <eduardoaleixomartins@gmail.com>
2021-09-03 19:05:41 -0300

    .

--- benchmark/pr-screenshots.dangerfile.js
+++ benchmark/pr-screenshots.dangerfile.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const filenames = fs.readdirSync("./dashboard-screenshots");
 const bucketAddress = process.env.BUCKET_ADDRESS;
 
-const img = (name, url) => `[${name}](${url})`
+const img = (name, url) => `![${name}](${url})`
 
 const md = filenames.map(name => img(name, `${bucketAddress}/${name}`)).join("\n"); 
 
