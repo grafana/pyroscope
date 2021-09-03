f66c203123e6177f37a2a75305ff187c574e5f37
eduardo <eduardoaleixomartins@gmail.com>
2021-09-03 18:28:30 -0300

    .

--- benchmark/pr-screenshots.dangerfile.js
+++ benchmark/pr-screenshots.dangerfile.js
@@ -3,7 +3,12 @@ const fs = require('fs')
 
 
 const filenames = fs.readdirSync("./dashboard-screenshots");
+const bucketAddress = process.env.BUCKET_ADDRESS;
+
+const img = (name, url) => `[${name}](${url})`
+
+const md = filenames.map(name => img(name, `${bucketAddress}/${name}`)).join("\n"); 
+
 markdown(`
-# report
-generated files ${filenames}
-`);
+# screenshots
+${md}`);
