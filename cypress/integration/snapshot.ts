1df31f31b0ede230a757dab20c9089905cd13163
eduardo aleixo <eduardo@pyroscope.io>
2021-09-23 16:53:45 -0300

    add basic visual testing test

---
+++ cypress/integration/snapshot.ts
@@ -0,0 +1,10 @@
+
+// just testing
+it('matches snapshot', () => {
+  cy.intercept('**/render*', {
+    fixture: 'simple-golang-app-cpu.json',
+  }).as('render')
+
+  cy.visit('/');
+  cy.findByTestId('flamegraph-view').toMatchImageSnapshot();
+});
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 /**
  * @type {Cypress.PluginConfig}
@@ -19,4 +20,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  //
+ initPlugin(on, config);
+  return config;
 };
