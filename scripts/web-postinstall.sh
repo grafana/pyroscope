5f4e0e306869995521ba1d2a532d5c7e6732e226
Dmitry Filimonov <dmitry@pyroscope.io>
2021-09-24 12:57:36 -0700

    fixes mac builds

--- scripts/web-postinstall.sh
+++ scripts/web-postinstall.sh
@@ -3,8 +3,11 @@
 # web-postinstall.sh
 # to be run by yarn/npm after installing
 
-# don't run where git is not present (probably CI)
-if command -v git; then
-  # makes git blame ignore commits that are purely reformatting code
-  git config blame.ignoreRevsFile .git-blame-ignore-revs
+# don't run where .git directory is not present (probably mac builds / CI)
+if [ -d ".git" ]; then
+  # don't run where git is not present (probably CI)
+  if command -v git; then
+    # makes git blame ignore commits that are purely reformatting code
+    git config blame.ignoreRevsFile .git-blame-ignore-revs
+  fi
 fi
