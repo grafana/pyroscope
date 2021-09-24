063f5ee6070bd6e0e1350fadfd2cbad0f6b58ae3
eduardo aleixo <eduardo@pyroscope.io>
2021-09-24 11:05:32 -0300

    set a fixed viewport

--- cypress/integration/snapshot.ts
+++ cypress/integration/snapshot.ts
@@ -1,12 +1,13 @@
-
 // just testing
 it('matches snapshot', () => {
+  cy.viewport(1024, 768);
+
   cy.intercept('**/render*', {
     fixture: 'simple-golang-app-cpu.json',
-  }).as('render')
+  }).as('render');
 
   cy.visit('/');
-//  cy.findByTestId('flamegraph-view').toMatchImageSnapshot();
+  //  cy.findByTestId('flamegraph-view').toMatchImageSnapshot();
 
   cy.findByTestId('flamegraph-canvas').matchImageSnapshot();
 });
