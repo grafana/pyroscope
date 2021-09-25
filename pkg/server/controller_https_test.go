1c138b321a50fccd8f2f76ae6336356da0f8b2cd
gawicks <gawicks@gmail.com>
2021-09-25 13:59:21 +0530

    split http and gzip tests

---
+++ pkg/server/controller_https_test.go
@@ -0,0 +1,94 @@
+package server
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+
+	"github.com/pyroscope-io/pyroscope/pkg/config"
+	"github.com/pyroscope-io/pyroscope/pkg/storage"
+	"github.com/pyroscope-io/pyroscope/pkg/testing"
+)
+
+var _ = Describe("server", func() {
+	testing.WithConfig(func(cfg **config.Config) {
+		const test_data_dir string = "testdata"
+		const tlsCertificateFile string = "server.crt"
+		const tlsKeyFile string = "server.key"
+
+		Describe("HTTPS", func() {
+			It("Should serve HTTPS when TLSCertificateFile and TLSKeyFile is defined",
+				func() {
+					defer GinkgoRecover()
+					const addr = ":10046"
+					(*cfg).Server.APIBindAddr = addr
+					(*cfg).Server.TLSCertificateFile = filepath.Join(test_data_dir, tlsCertificateFile)
+					(*cfg).Server.TLSKeyFile = filepath.Join(test_data_dir, tlsKeyFile)
+
+					s, err := storage.New(&(*cfg).Server, prometheus.NewRegistry())
+					Expect(err).ToNot(HaveOccurred())
+
+					c, _ := New(&(*cfg).Server, s, s, logrus.New(), prometheus.NewRegistry())
+					c.dir = http.Dir(test_data_dir)
+
+					go c.Start()
+					// TODO: Wait for start .There's possibly a better way of doing this
+					time.Sleep(50 * time.Millisecond)
+					tr := &http.Transport{
+						TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+					}
+
+					client := &http.Client{Transport: tr}
+					resHTTPS, _ := client.Get(fmt.Sprintf("https://localhost%s", addr))
+					resHTTP, _ := client.Get(fmt.Sprintf("http://localhost%s", addr))
+
+					defer resHTTPS.Body.Close()
+					defer resHTTP.Body.Close()
+					defer c.Stop()
+
+					Expect(resHTTPS.StatusCode).To(Equal(http.StatusOK))
+					Expect(resHTTP.StatusCode).To(Equal(http.StatusBadRequest))
+
+				},
+			)
+			It("Should serve HTTP when TLSCertificateFile & TLSKeyFile is undefined",
+				func() {
+					defer GinkgoRecover()
+					const addr = ":10046"
+					(*cfg).Server.APIBindAddr = addr
+
+					s, err := storage.New(&(*cfg).Server, prometheus.NewRegistry())
+					Expect(err).ToNot(HaveOccurred())
+
+					c, _ := New(&(*cfg).Server, s, s, logrus.New(), prometheus.NewRegistry())
+					c.dir = http.Dir(test_data_dir)
+
+					go c.Start()
+					time.Sleep(50 * time.Millisecond)
+					tr := &http.Transport{
+						TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+					}
+
+					client := &http.Client{Transport: tr}
+					resHTTP, _ := client.Get(fmt.Sprintf("http://localhost%s", addr))
+					_, errHTTPS := client.Get(fmt.Sprintf("https://localhost%s", addr))
+
+					defer resHTTP.Body.Close()
+					defer c.Stop()
+
+					Expect(resHTTP.StatusCode).To(Equal(http.StatusOK))
+					Expect(errHTTPS).To(HaveOccurred())
+
+				},
+			)
+
+		})
+	})
+})
