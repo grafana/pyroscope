089a469c5540e7191342defa23c8620eca5bb36c
eduardo aleixo <eduardo@pyroscope.io>
2021-09-30 12:00:38 -0300

    cmd: clean up lint errors

--- cmd/pyroscope/main_unix.go
+++ cmd/pyroscope/main_unix.go
@@ -9,5 +9,6 @@ import (
 
 func fatalf(format string, args ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, format, args...)
+	// revive:disable-next-line:deep-exit different impls exit at different points
 	os.Exit(1)
 }
