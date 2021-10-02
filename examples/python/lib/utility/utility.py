44f04357c059853482adcc4d29d95bb1f2a62931
Ryan Perry <rperry2174@gmail.com>
2021-10-02 11:37:05 -0700

    switch to using tag_wrapper

--- examples/python/lib/utility/utility.py
+++ examples/python/lib/utility/utility.py
@@ -2,9 +2,8 @@ import time
 import pyroscope_io as pyroscope
 
 def find_nearest_vehicle(n, vehicle):
-    pyroscope.tag({ "vehicle": vehicle})
-    i = 0
-    start_time = time.time()
-    while time.time() - start_time < n:
-        i += 1
-    pyroscope.remove_tags("vehicle")
+    with pyroscope.tag_wrapper({ "vehicle": vehicle}):
+        i = 0
+        start_time = time.time()
+        while time.time() - start_time < n:
+            i += 1
