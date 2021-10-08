77c570468814499ff40d6058c8873304bb8b5b45
eduardo aleixo <eduardo@pyroscope.io>
2021-10-08 12:46:36 -0300

    chore: reimplement ObjectsFormatter in ts

--- webapp/javascript/util/format.ts
+++ webapp/javascript/util/format.ts
@@ -58,64 +58,30 @@ export function numberWithCommas(x: number): string {
 //  }
 //}
 //
-//const objects = [
-//  [1000, 'K'],
-//  [1000, 'M'],
-//  [1000, 'G'],
-//  [1000, 'T'],
-//  [1000, 'P'],
-//];
 //
-//export class ObjectsFormatter {
-//  constructor(maxObjects) {
-//    this.divider = 1;
-//    this.suffix = '';
-//    for (var i = 0; i < objects.length; i++) {
-//      if (maxObjects >= objects[i][0]) {
-//        this.divider *= objects[i][0];
-//        maxObjects /= objects[i][0];
-//        this.suffix = objects[i][1];
-//      } else {
-//        break;
-//      }
-//    }
-//  }
 //
-//  format(samples, sampleRate) {
-//    let number = samples / this.divider;
-//    if (number >= 0 && number < 0.01) {
-//      number = '< 0.01';
-//    } else if (number <= 0 && number > -0.01) {
-//      number = '< 0.01';
-//    } else {
-//      number = number.toFixed(2);
-//    }
-//    return `${number} ${this.suffix}`;
-//  }
-//}
-//
-//export function getPackageNameFromStackTrace(spyName, stackTrace) {
-//  // TODO: actually make sure these make sense and add tests
-//  const regexpLookup = {
-//    default: /^(?<packageName>(.*\/)*)(?<filename>.*)(?<line_info>.*)$/,
-//    dotnetspy: /^(?<packageName>.+)\.(.+)\.(.+)\(.*\)$/,
-//    ebpfspy: /^(?<packageName>.+)$/,
-//    gospy: /^(?<packageName>(.*\/)*)(?<filename>.*)(?<line_info>.*)$/,
-//    phpspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.php+)(?<line_info>.*)$/,
-//    pyspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.py+)(?<line_info>.*)$/,
-//    rbspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.rb+)(?<line_info>.*)$/,
-//  };
-//
-//  if (stackTrace.length === 0) {
-//    return stackTrace;
-//  }
-//  const regexp = regexpLookup[spyName] || regexpLookup.default;
-//  const fullStackGroups = stackTrace.match(regexp);
-//  if (fullStackGroups) {
-//    return fullStackGroups.groups.packageName;
-//  }
-//  return stackTrace;
-//}
+export function getPackageNameFromStackTrace(spyName, stackTrace) {
+  // TODO: actually make sure these make sense and add tests
+  const regexpLookup = {
+    default: /^(?<packageName>(.*\/)*)(?<filename>.*)(?<line_info>.*)$/,
+    dotnetspy: /^(?<packageName>.+)\.(.+)\.(.+)\(.*\)$/,
+    ebpfspy: /^(?<packageName>.+)$/,
+    gospy: /^(?<packageName>(.*\/)*)(?<filename>.*)(?<line_info>.*)$/,
+    phpspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.php+)(?<line_info>.*)$/,
+    pyspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.py+)(?<line_info>.*)$/,
+    rbspy: /^(?<packageName>(.*\/)*)(?<filename>.*\.rb+)(?<line_info>.*)$/,
+  };
+
+  if (stackTrace.length === 0) {
+    return stackTrace;
+  }
+  const regexp = regexpLookup[spyName] || regexpLookup.default;
+  const fullStackGroups = stackTrace.match(regexp);
+  if (fullStackGroups) {
+    return fullStackGroups.groups.packageName;
+  }
+  return stackTrace;
+}
 //
 
 // TODO add an enum for the units
@@ -123,13 +89,13 @@ export function getFormatter(max: number, sampleRate: number, units: string) {
   switch (units) {
     case 'samples':
       return new DurationFormatter(max / sampleRate);
-    //    case 'objects':
-    //      return new ObjectsFormatter(max);
+    case 'objects':
+      return new ObjectsFormatter(max);
     //    case 'bytes':
     //      return new BytesFormatter(max);
     default:
-      throw new Error(`Unsupported unit: ${units}`);
-    //      return new DurationFormatter(max / sampleRate);
+      //  throw new Error(`Unsupported unit: ${units}`);
+      return new DurationFormatter(max / sampleRate);
   }
 }
 
@@ -168,5 +134,42 @@ class DurationFormatter {
     }
 
     return `${nStr} ${this.suffix}` + (n == 1 ? '' : 's');
+  }
+}
+
+export class ObjectsFormatter {
+  divider = 1;
+  suffix = '';
+  objects: [number, string][] = [
+    [1000, 'K'],
+    [1000, 'M'],
+    [1000, 'G'],
+    [1000, 'T'],
+    [1000, 'P'],
+  ];
+  constructor(maxObjects: number) {
+    for (var i = 0; i < this.objects.length; i++) {
+      if (maxObjects >= this.objects[i][0]) {
+        this.divider *= this.objects[i][0];
+        maxObjects /= this.objects[i][0];
+        this.suffix = this.objects[i][1];
+      } else {
+        break;
+      }
+    }
+  }
+
+  // TODO:
+  // how to indicate that sampleRate doesn't matter?
+  format(samples: number, sampleRate: number) {
+    let n = samples / this.divider;
+    let nStr = n.toFixed(2);
+
+    if (n >= 0 && n < 0.01) {
+      nStr = '< 0.01';
+    } else if (n <= 0 && n > -0.01) {
+      nStr = '< 0.01';
+    }
+    return `${nStr} ${this.suffix}`;
   }
 }
