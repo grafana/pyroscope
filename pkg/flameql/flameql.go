ec5b5fada943eb01ff172071094ee9f7266ce3c6
Mikhail Fesenko <proggga@gmail.com>
2021-10-11 18:12:37 +0300

    chore: FlameQL code refactoring (#453)

--- pkg/flameql/flameql.go
+++ pkg/flameql/flameql.go
@@ -101,7 +101,7 @@ func IsTagKeyRuneAllowed(r rune) bool {
 }
 
 func IsAppNameRuneAllowed(r rune) bool {
-	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '_' || r == '-' || r == '.'
+	return r == '-' || r == '.' || IsTagKeyRuneAllowed(r)
 }
 
 func IsTagKeyReserved(k string) bool {
