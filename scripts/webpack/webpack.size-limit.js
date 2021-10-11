69696aaa80be923a0fb5355676168f3546598a47
eduardo aleixo <eduardo@pyroscope.io>
2021-10-11 13:35:17 -0300

    chore: add another webpack config to be used by size limit only

---
+++ scripts/webpack/webpack.size-limit.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+
+const prod = require('./webpack.prod');
+
+module.exports = merge(prod, {
+  output: {
+    publicPath: '',
+    path: path.resolve(__dirname, '../../webapp/public/assets'),
+
+    // build WITHOUT hash
+    // otherwise size-limit won't know what to compare to
+    // https://github.com/andresz1/size-limit-action/issues/47
+    filename: '[name].js',
+    clean: true,
+  },
+});
