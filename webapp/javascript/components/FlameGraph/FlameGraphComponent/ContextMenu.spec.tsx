5d8ca6ed95141034bb4aed375b9f161b5796ab03
eduardo aleixo <eduardo@pyroscope.io>
2021-10-11 16:40:29 -0300

    feat: add basic context menu

---
+++ webapp/javascript/components/FlameGraph/FlameGraphComponent/ContextMenu.spec.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MenuItem } from '@szhsin/react-menu';
+import userEvent from '@testing-library/user-event';
+
+import ContextMenu, { ContextMenuProps } from './ContextMenu';
+
+const { queryByRole, queryAllByRole } = screen;
+
+function TestCanvas(props: Omit<ContextMenuProps, 'canvasRef'>) {
+  const canvasRef = React.useRef();
+
+  return (
+    <>
+      <canvas data-testid="canvas" ref={canvasRef} />
+      <ContextMenu data-testid="contextmenu" canvasRef={canvasRef} {...props} />
+    </>
+  );
+}
+
+describe('ContextMenu', () => {
+  it('works', () => {
+    let hasBeenClicked = false;
+
+    const xyToMenuItems = () => {
+      return [
+        <MenuItem
+          key="test"
+          onClick={() => {
+            hasBeenClicked = true;
+          }}
+        >
+          Test
+        </MenuItem>,
+      ] as unknown[] as typeof MenuItem[]; // nasty conversion
+    };
+
+    render(<TestCanvas xyToMenuItems={xyToMenuItems} />);
+
+    expect(queryByRole('menu')).not.toBeInTheDocument();
+
+    // trigger a right click
+    userEvent.click(screen.getByTestId('canvas'), { buttons: 2 });
+
+    expect(queryByRole('menu')).toBeVisible();
+    expect(queryAllByRole('menuitem')).toHaveLength(1);
+
+    userEvent.click(queryByRole('menuitem'));
+    expect(hasBeenClicked).toBe(true);
+  });
+});
