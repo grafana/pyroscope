d599b1658dc050dc7032321a5fbe19e3b5b8ab0c
eduardo aleixo <eduardo@pyroscope.io>
2021-10-15 18:15:20 -0300

    chore: install fonts in jest snapshot test container

--- scripts/jest-snapshots/run-snapshots.sh
+++ scripts/jest-snapshots/run-snapshots.sh
@@ -9,6 +9,9 @@ if [ "$UPDATE_SNAPSHOTS" = true ]; then
   updateArg="-u"
 fi
 
+apt update -y
+apt install fontconfig -y
+
 yarn install
 RUN_SNAPSHOTS=true yarn test --testNamePattern='group:snapshot' --verbose "$updateArg"
 
