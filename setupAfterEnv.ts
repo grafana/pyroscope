e032b95f611bd75f33096d6279d19c252c099ce1
eduardo aleixo <eduardo@pyroscope.io>
2021-10-17 18:04:02 -0300

    chore: always run snapshot tests, except we don't try to match image

--- setupAfterEnv.ts
+++ setupAfterEnv.ts
@@ -1,38 +1,14 @@
 import '@testing-library/jest-dom';
-// import 'jest-canvas-mock';
 
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
-expect.extend({ toMatchImageSnapshot });
-/**
- * Takes a JSDOM image and returns a Node.js buffer to use
- * with jest-image-snapshot.
- * source: https://github.com/americanexpress/jest-image-snapshot/issues/279
- */
-// function imageToBuffer(image: HTMLImageElement): Buffer {
-//  const canvas = document.createElement('canvas');
-//  canvas.width = image.width;
-//  canvas.height = image.height;
-//
-//  const ctx = canvas.getContext('2d');
-//
-//  ctx.drawImage(image, 0, 0);
-//
-//  const base64 = canvas.toDataURL().split(',')[1];
-//  return Buffer.from(base64, 'base64');
-// }
-//
-// expect.extend({
-//  toMatchImageSnapshot: function (received, options) {
-//    // If these checks pass, assume we're in a JSDOM environment with the 'canvas' package.
-//    if (
-//      received &&
-//      received.constructor &&
-//      received.constructor.name === 'HTMLImageElement'
-//    ) {
-//      received = imageToBuffer(received);
-//    }
-//
-//    return toMatchImageSnapshot.call(this, received, options);
-//  },
-// });
+expect.extend({
+  toMatchImageSnapshot(received: any, options: any) {
+    // If these checks pass, assume we're in a JSDOM environment with the 'canvas' package.
+    if (process.env.RUN_SNAPSHOTS) {
+      return toMatchImageSnapshot.call(this, received, options);
+    }
+
+    return { pass: true };
+  },
+});
--- webapp/javascript/components/FlameGraph/FlameGraphComponent/CanvasRenderer.spec.tsx
+++ webapp/javascript/components/FlameGraph/FlameGraphComponent/CanvasRenderer.spec.tsx
@@ -7,11 +7,7 @@ import TestData from './testData';
 
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
-const desc = process.env.RUN_SNAPSHOTS ? describe : describe.skip;
-
-// We skip this suite by default
-// So that's only run in docker (using 'group:snapshot' as a tag)
-desc('CanvasRenderer -- group:snapshot', () => {
+describe('CanvasRenderer -- group:snapshot', () => {
   it('works with normal flamegraph', () => {
     const canvas = createCanvas(800, 0) as unknown as HTMLCanvasElement;
 
@@ -181,44 +177,11 @@ desc('CanvasRenderer -- group:snapshot', () => {
     });
 
     expect(canvasToBuffer(canvas)).toMatchImageSnapshot();
-    // const canvas = createCanvas(800, 600) as unknown as HTMLCanvasElement;
-    // RenderCanvas({
-    //  canvas,
-    //  //      topLevel: 0,
-    //  rangeMin: 0,
-    //  viewType: 'single',
-    //  numTicks: 988,
-    //  sampleRate: 100,
-    //  names: [
-    //    'total',
-    //    'runtime.main',
-    //    'main.slowFunction',
-    //    'main.work',
-    //    'main.main',
-    //    'main.fastFunction',
-    //  ],
-    //  levels: [
-    //    [0, 988, 0, 0],
-    //    [0, 988, 0, 1],
-    //    [0, 214, 0, 5, 214, 3, 2, 4, 217, 771, 0, 2],
-    //    [0, 214, 214, 3, 216, 1, 1, 5, 217, 771, 771, 3],
-    //  ],
-    //  units: Units.Samples,
-    //  fitMode: 'HEAD',
-    //  spyName: 'gospy',
-    //  font: 'monospace',
-    //  selectedLevel: 2,
-    //  topLevel: 0,
-    //  // horrible api
-    //  // TODO, receive the i/j ?
-    //  rangeMax: 0.2165991902834008,
-    // });
-    // expect(canvasToBuffer(canvas)).toMatchImageSnapshot();
   });
 });
 
 // since this is the only test that uses snapshot testing
-expect.extend({ toMatchImageSnapshot });
+// expect.extend({ toMatchImageSnapshot });
 function canvasToBuffer(canvas: HTMLCanvasElement) {
   const converter = new CanvasConverter(canvas, {
     image: { types: ['png'] },
