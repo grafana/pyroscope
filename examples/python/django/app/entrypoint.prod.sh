0dab59904e104eac4fef5b9f8b151bb2625c66bf
Ryan Perry <Rperry2174@gmail.com>
2021-10-19 16:21:48 -0700

    docs: Add Django Example to Python Examples Folder (#472)
    * Add django example

---
+++ examples/python/django/app/entrypoint.prod.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
