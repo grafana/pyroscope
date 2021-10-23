12aba7a7410057816f34662c87ac2522ca42bdf1
Dmitry Filimonov <dmitry@pyroscope.io>
2021-10-23 01:29:02 -0700

    unifies to google.golang.org/protobuf/proto

--- pkg/agent/pprof/profile.pb.go
+++ pkg/agent/pprof/profile.pb.go
@@ -45,7 +45,7 @@
 package pprof
 
 import (
-	proto "github.com/golang/protobuf/proto"
+	proto "google.golang.org/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
