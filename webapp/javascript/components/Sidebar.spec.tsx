7c7366bb2a0ab1e0f7c0c801a1d62996e1bc4a13
clovis <clovis-@live.com>
2021-10-31 19:00:13 -0400

    Make sidebar links

--- webapp/javascript/components/Sidebar.spec.tsx
+++ webapp/javascript/components/Sidebar.spec.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ShortcutProvider } from 'react-keybind';
 import Sidebar from './Sidebar';
-import history from '../util/history';
 
 jest.mock('react-redux', () => ({
   connect: () => (Component: any) => Component,
@@ -12,7 +12,9 @@ describe('Sidebar', () => {
   it('Successfully changes location when clicked', () => {
     render(
       <ShortcutProvider>
-        <Sidebar />
+        <MemoryRouter>
+          <Sidebar />
+        </MemoryRouter>
       </ShortcutProvider>
     );
     const singleView = screen.getByTestId('sidebar-root');
@@ -25,33 +27,5 @@ describe('Sidebar', () => {
     fireEvent.click(singleView);
     expect(singleView).toHaveClass('active-route');
     expect(comparisonView).not.toHaveClass('active-route');
-  });
-  it('Successfully changes location when it changes externally', () => {
-    // Replace the listener implementation with a mock.
-    let registeredListener;
-    history.listen = (fn) => {
-      registeredListener = fn;
-      return () => {};
-    };
-
-    render(
-      <ShortcutProvider>
-        <Sidebar />
-      </ShortcutProvider>
-    );
-    expect(registeredListener).not.toBeUndefined();
-    const singleView = screen.getByTestId('sidebar-root');
-    const comparisonView = screen.getByTestId('sidebar-comparison');
-    fireEvent.click(comparisonView);
-    fireEvent.click(singleView);
-    expect(singleView).toHaveClass('active-route');
-    expect(comparisonView).not.toHaveClass('active-route');
-
-    // Changes the location on the history object
-    act(() => {
-      registeredListener({ pathname: '/comparison' });
-    });
-    expect(singleView).not.toHaveClass('active-route');
-    expect(comparisonView).toHaveClass('active-route');
   });
 });
