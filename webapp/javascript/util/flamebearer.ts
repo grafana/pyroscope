95767e3ea3983746a6ae781c0f14969905d4f540
gabrielzezze <gabriel@zezze.dev>
2021-11-01 12:08:16 -0300

    Added `deltaDiffWrapper` to flamebearer utils

--- webapp/javascript/util/flamebearer.ts
+++ webapp/javascript/util/flamebearer.ts
@@ -38,4 +38,27 @@ function createFF(viewType: 'single' | 'double') {
   }
 }
 
-export { createFF };
+function deltaDiffWrapper(format, levels) {
+  let mutable_levels = levels;
+
+  function deltaDiff(levels, start, step) {
+    for (const level of levels) {
+      let prev = 0;
+      for (let i = start; i < level.length; i += step) {
+        level[i] += prev;
+        prev = level[i] + level[i + 1];
+      }
+    }
+  }
+
+  if (format === 'double') {
+    deltaDiff(mutable_levels, 0, 7);
+    deltaDiff(mutable_levels, 3, 7);
+  } else {
+    deltaDiff(mutable_levels, 0, 4);
+  }
+
+  return mutable_levels;
+}
+
+export { createFF, deltaDiffWrapper };
