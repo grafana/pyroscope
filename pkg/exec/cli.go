16c082188da56a72effb241931bc2ebfec0d113b
Dmitry Filimonov <dmitry@pyroscope.io>
2021-11-01 00:56:41 -0700

    feat: improves ebpf integration

--- pkg/exec/cli.go
+++ pkg/exec/cli.go
@@ -38,7 +38,7 @@ func Cli(cfg *config.Exec, args []string) error {
 		if len(args) == 0 {
 			return errors.New("no arguments passed")
 		}
-	} else if !processExists(cfg.Pid) {
+	} else if (cfg.Pid != -1 && cfg.SpyName == "ebpfspy") && !processExists(cfg.Pid) {
 		return errors.New("process not found")
 	}
 
