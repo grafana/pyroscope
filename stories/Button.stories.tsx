1af93cf32b08146c09e511745c4e2ef950f317d7
eduardo aleixo <eduardo@pyroscope.io>
2021-11-04 17:30:51 -0300

    improve simple button

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,22 +2,27 @@
 import React from 'react';
 import Button from '@ui/Button';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { faAlignLeft } from '@fortawesome/free-solid-svg-icons/faAlignLeft';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons/faSyncAlt';
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Button</Button>
+);
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Pyroscope/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  //  argTypes: {
-  //    backgroundColor: { control: 'color' },
-  //  },
 } as ComponentMeta<typeof Button>;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
 };
+
+export const DefaultWithIcon = () => (
+  <Button disabled icon={faAlignLeft}>
+    Button with icon
+  </Button>
+);
+
+export const IconOnly = () => <Button icon={faSyncAlt} />;
--- webapp/javascript/ui/Button.tsx
+++ webapp/javascript/ui/Button.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAlignLeft } from '@fortawesome/free-solid-svg-icons/faAlignLeft';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import styles from './Button.module.scss';
 
-export default function Button() {
-  return <button className={styles.button}>test</button>;
+export interface ButtonProps {
+  /** Whether the button is disabled or not */
+  disabled?: boolean;
+  /** Whether the button is disabled or not */
+  primary?: boolean;
+  icon: IconDefinition;
+  children: React.ReactNode;
+}
+
+export default function Button({
+  disabled = false,
+  primary = false,
+  icon,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button disabled={disabled} className={styles.button}>
+      {icon ? <FontAwesomeIcon icon={icon} /> : null}
+      {children}
+    </button>
+  );
 }
