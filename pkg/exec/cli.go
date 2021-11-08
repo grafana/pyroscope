0616ef7328a9b880797f2f737503da20162a2143
Dmitry Filimonov <dmitry@pyroscope.io>
2021-11-08 13:32:00 -0800

    Update cli.go

--- pkg/exec/cli.go
+++ pkg/exec/cli.go
@@ -34,7 +34,7 @@ func Cli(cfg *Config, args []string) error {
 		if len(args) == 0 {
 			return errors.New("no arguments passed")
 		}
-	} else if (cfg.Pid != -1 && cfg.SpyName == "ebpfspy") && !process.Exists(cfg.Pid) {
+	} else if (cfg.pid != -1 && cfg.spyName == "ebpfspy") && !process.Exists(cfg.Pid) {
 		return errors.New("process not found")
 	}
 
