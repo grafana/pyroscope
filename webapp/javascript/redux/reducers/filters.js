17bfc424d25f35a5d540af7a7e29008fda49aac8
Gabriel Zezze <gabrielfz@al.insper.edu.br>
2021-11-09 19:23:29 -0300

    Added logic to each view type fetch action, populating appropriate store keys.

--- webapp/javascript/redux/reducers/filters.js
+++ webapp/javascript/redux/reducers/filters.js
@@ -5,8 +5,6 @@ import {
   SET_UNTIL,
   SET_MAX_NODES,
   REFRESH,
-  RECEIVE_TIMELINE,
-  REQUEST_TIMELINE,
   REQUEST_TAGS,
   RECEIVE_TAGS,
   REQUEST_TAG_VALUES,
@@ -20,6 +18,12 @@ import {
   SET_LEFT_UNTIL,
   SET_RIGHT_UNTIL,
   SET_QUERY,
+  RECEIVE_COMPARISON_APP_DATA,
+  RECEIVE_PYRESCOPE_APP_DATA,
+  REQUEST_PYRESCOPE_APP_DATA,
+  REQUEST_COMPARISON_APP_DATA,
+  REQUEST_COMPARISON_DIFF_APP_DATA,
+  RECEIVE_COMPARISON_DIFF_APP_DATA,
 } from '../actionTypes';
 
 const defaultName = window.initialState.appNames.find(
@@ -69,6 +73,10 @@ function decodeTimelineData(timelineData) {
 }
 
 export default function (state = initialState, action) {
+  let flamebearer;
+  let timeline;
+  let data;
+  let viewSide;
   switch (action.type) {
     case SET_DATE_RANGE:
       return {
@@ -128,14 +136,78 @@ export default function (state = initialState, action) {
         ...state,
         refreshToken: Math.random(),
       };
-    case REQUEST_TIMELINE:
+
+    case REQUEST_PYRESCOPE_APP_DATA:
       return {
         ...state,
         isJSONLoading: true,
       };
-    case RECEIVE_TIMELINE:
-      const { data, viewType, viewSide } = action.payload;
-      const { flamebearer, leftTicks, rightTicks, timeline } = data;
+    case RECEIVE_PYRESCOPE_APP_DATA:
+      data = action.payload.data;
+      timeline = data.timeline;
+      flamebearer = data.flamebearer;
+
+      return {
+        ...state,
+        timeline: decodeTimelineData(timeline),
+        single: { flamebearer },
+        isJSONLoading: false,
+      };
+
+    case REQUEST_COMPARISON_APP_DATA:
+      return {
+        ...state,
+        isJSONLoading: true,
+      };
+    case RECEIVE_COMPARISON_APP_DATA:
+      data = action.payload.data;
+      viewSide = action.payload.viewSide;
+      timeline = data.timeline;
+      flamebearer = data.flamebearer;
+
+      let left;
+      let right;
+      switch (viewSide) {
+        case 'left':
+          left = { flamebearer };
+          right = state.comparison.right;
+          break;
+
+        case 'right': {
+          left = state.comparison.left;
+          right = { flamebearer };
+          break;
+        }
+        case 'both': {
+          left = { flamebearer };
+          right = { flamebearer };
+          break;
+        }
+        default:
+          throw new Error(`Invalid viewSide: '${viewSide}'`);
+      }
+
+      return {
+        ...state,
+        timeline: decodeTimelineData(timeline),
+        comparison: {
+          left,
+          right,
+        },
+        isJSONLoading: false,
+      };
+
+    case REQUEST_COMPARISON_DIFF_APP_DATA:
+      return {
+        ...state,
+        isJSONLoading: true,
+      };
+    case RECEIVE_COMPARISON_DIFF_APP_DATA:
+      data = action.payload.data;
+      timeline = data.timeline;
+      flamebearer = data.flamebearer;
+      const { leftTicks, rightTicks } = data;
+
       const calculatedLevels = deltaDiffWrapper(
         flamebearer.format,
         flamebearer.levels
@@ -150,28 +222,13 @@ export default function (state = initialState, action) {
         },
       };
 
-      // When viewType = 'comparison' and no viewSide is given
-      // then both left and right keys are populated.
-      // This happens only on initialization as seen on components/ComparisonApp.jsx
-      const viewData = () => {
-        if (viewType === 'comparison' && !viewSide)
-          return {
-            left: flamebearerData,
-            right: flamebearerData,
-          };
-        return viewSide
-          ? {
-              [viewSide]: flamebearerData,
-            }
-          : flamebearerData;
-      };
-
       return {
         ...state,
         timeline: decodeTimelineData(timeline),
-        [viewType]: viewData(),
+        diff: flamebearerData,
         isJSONLoading: false,
       };
+
     case REQUEST_TAGS:
       return {
         ...state,
