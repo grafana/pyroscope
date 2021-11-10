45314dad6fc6bceadcd6f1f0bdccdb820af81512
eduardo aleixo <eduardo@pyroscope.io>
2021-11-10 14:54:12 -0300

    add tests for GET

--- pkg/admin/controller.go
+++ pkg/admin/controller.go
@@ -15,9 +15,62 @@ type Controller struct {
 	SocketAddr string
 }
 
+type AdminServer struct {
+	log        *logrus.Logger
+	svc        *AdminService
+	SocketAddr string
+	http.Handler
+}
+
 type Config struct {
 	SocketAddr string
 	*logrus.Logger
+}
+
+func NewServer(c Config, svc *AdminService) (*AdminServer, error) {
+	if c.SocketAddr == "" {
+		return nil, fmt.Errorf("A socket path must be defined")
+	}
+
+	as := &AdminServer{
+		log:        c.Logger,
+		svc:        svc,
+		SocketAddr: c.SocketAddr,
+	}
+
+	mux := http.NewServeMux()
+
+	// Routes
+	mux.HandleFunc("/v1/apps", as.GetApps)
+
+	as.Handler = mux
+
+	return as, nil
+}
+
+func (as *AdminServer) Start() error {
+	adminServer := http.Server{Handler: as.Handler}
+	adminListener, err := net.Listen("unix", as.SocketAddr)
+	if err != nil {
+		return err
+	}
+
+	return adminServer.Serve(adminListener)
+}
+
+func (as *AdminServer) GetApps(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		{
+			appNames := as.svc.GetAppNames()
+
+			w.WriteHeader(200)
+			as.writeResponseJSON(w, appNames)
+		}
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
 }
 
 func NewController(c Config, svc *AdminService) (*Controller, error) {
@@ -56,5 +109,6 @@ func (ctrl *Controller) Start() error {
 func (ctrl *Controller) GetApps(w http.ResponseWriter, r *http.Request) {
 	appNames := ctrl.svc.GetAppNames()
 
+	w.WriteHeader(200)
 	ctrl.writeResponseJSON(w, appNames)
 }
--- pkg/admin/controller_test.go
+++ pkg/admin/controller_test.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	"github.com/pyroscope-io/pyroscope/pkg/admin"
@@ -20,24 +22,34 @@ func (mockAppsGetter) GetAppNames() []string {
 	return []string{"app1", "app2"}
 }
 
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 var _ = Describe("controller", func() {
+	// TODO
+	// since this talks with socket and stuff
+	// this can become an integration test
 	Context("/v1/apps", func() {
-		It("returns app names", func() {
+		var (
+			httpC http.Client
+
+			socketAddr string
+		)
+
+		BeforeEach(func() {
 			file, err := ioutil.TempFile("", "pyroscope.sock")
-			if err != nil {
-				panic(err)
-			}
-			socketAddr := file.Name()
-			defer os.Remove(file.Name())
+			must(err)
+			socketAddr = file.Name()
 
 			cfg := admin.Config{
 				SocketAddr: socketAddr,
 			}
 			svc := admin.NewService(mockAppsGetter{})
 			ctrl, err := admin.NewController(cfg, svc)
-			if err != nil {
-				panic(err)
-			}
+			must(err)
 
 			go func() {
 				err := ctrl.Start()
@@ -46,24 +58,74 @@ var _ = Describe("controller", func() {
 				}
 			}()
 
-			httpC := newHttpClient(socketAddr)
-
+			httpC = newHttpClient(socketAddr)
 			// TODO
-			// for some reason it takes some until server is ready
-			time.Sleep(time.Second * 1)
+			// for some reason it takes some time until server is ready
+			// we could add retries?
+			time.Sleep(time.Millisecond * 10)
+		})
 
+		AfterEach(func() {
+			os.Remove(socketAddr)
+		})
+
+		It("returns app names", func() {
 			req, err := httpC.Get("http://dummy/v1/apps")
 			Expect(err).To(BeNil())
 
 			body, err := ioutil.ReadAll(req.Body)
-			if err != nil {
-				panic(err)
-			}
 			Expect(err).To(BeNil())
 
 			Expect(string(body)).To(Equal(`["app1","app2"]
 `))
 		})
+
+		It("only accepts GET requests", func() {
+
+			Expect(true).To(Equal(true))
+		})
+	})
+
+	Context("/v1/apps", func() {
+		var svr *admin.AdminServer
+		var response *httptest.ResponseRecorder
+
+		// create a server
+		BeforeEach(func() {
+			cfg := admin.Config{SocketAddr: "foo"}
+
+			svc := admin.NewService(mockAppsGetter{})
+			server, err := admin.NewServer(cfg, svc)
+
+			must(err)
+			svr = server
+			response = httptest.NewRecorder()
+		})
+
+		It("returns list of apps", func() {
+			request, _ := http.NewRequest(http.MethodGet, "/v1/apps", nil)
+
+			svr.Handler.ServeHTTP(response, request)
+
+			body, err := ioutil.ReadAll(response.Body)
+			Expect(err).To(BeNil())
+
+			Expect(response.Code).To(Equal(200))
+			Expect(string(body)).To(Equal(`["app1","app2"]
+`))
+		})
+
+		DescribeTable("Non GET requests return 405 (method not allowed)",
+			func(method string) {
+				request, _ := http.NewRequest(method, "/v1/apps", nil)
+				svr.Handler.ServeHTTP(response, request)
+				Expect(response.Code).To(Equal(405))
+			},
+			Entry("POST", http.MethodPost),
+			Entry("PUT", http.MethodPost),
+			Entry("DELETE", http.MethodPost),
+			Entry("NON_VALID_METHOD", http.MethodPost),
+		)
 	})
 })
 
