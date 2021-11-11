d7601ecdab42a4131740976516208cc8c2b2c37f
eduardo aleixo <eduardo@pyroscope.io>
2021-11-11 11:49:53 -0300

    add more tests for http over uds

--- pkg/admin/http_over_uds.go
+++ pkg/admin/http_over_uds.go
@@ -9,6 +9,20 @@ import (
 	"syscall"
 )
 
+var (
+	// ErrSocketStillResponding refers to when
+	// a) an instance of the server is still running normally; or
+	// b) server was not closed properly
+	ErrSocketStillResponding = errors.New("a server is still running and responding to socket.")
+	// ErrInvalidSocketPathname refers to when the socket filepath is obviously invalid (eg empty string)
+	ErrInvalidSocketPathname = errors.New("the socket filepath is invalid")
+	// ErrListenerBind refers to generic errors
+	ErrListenerBind = errors.New("could not listen on socket")
+
+	// Anything works here
+	SocketHTTPAddress = "http://pyroscope"
+)
+
 type UdsHTTPServer struct {
 	server     *http.Server
 	listener   net.Listener
@@ -16,13 +30,13 @@ type UdsHTTPServer struct {
 }
 
 func NewUdsHTTPServer(socketAddr string) (*UdsHTTPServer, error) {
-	if socketAddr == "" {
-		return nil, errors.New("socket address must be defined")
+	if err := validateSocketAddress(socketAddr); err != nil {
+		return nil, err
 	}
 
 	listener, err := createListener(socketAddr)
 	if err != nil {
-		return nil, fmt.Errorf("error while creating listener: %w", err)
+		return nil, err
 	}
 
 	return &UdsHTTPServer{
@@ -32,6 +46,8 @@ func NewUdsHTTPServer(socketAddr string) (*UdsHTTPServer, error) {
 }
 
 func (u *UdsHTTPServer) Start(handler *http.ServeMux) error {
+	// enrich with an additional endpoint
+	// that we will use to probe when starting a new instance
 	handler.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		writeMessage(w, 200, "it works!")
 	})
@@ -41,6 +57,15 @@ func (u *UdsHTTPServer) Start(handler *http.ServeMux) error {
 	return u.server.Serve(u.listener)
 }
 
+// createListener creates a listener on socketAddr UDS
+// it tries to bind to socketAddr
+// if it fails, it also tries to consume that socket
+// if it's able to, it fails with ErrSocketStillResponding
+// if it not able to, then it assumes it's a dangling socket and takes over it
+//
+// keep in mind there's a slight chance for a race condition there
+// where a socket is verified to be not responding
+// but the moment it's taken over, it starts to respond (probably because it was taken over by a different instance)
 func createListener(socketAddr string) (net.Listener, error) {
 	takeOver := func(socketAddr string) (net.Listener, error) {
 		err := os.Remove(socketAddr)
@@ -52,33 +77,36 @@ func createListener(socketAddr string) (net.Listener, error) {
 	}
 
 	// we listen on a unix domain socket
-	// which will be created by the os
+	// which will be created by the bind syscall
 	// https://man7.org/linux/man-pages/man2/bind.2.html
 	listener, err := net.Listen("unix", socketAddr)
 
 	if err != nil {
 		if isErrorAddressAlreadyInUse(err) {
 			// that socket is already being used
-
 			// let's check if the server is also responding
 			httpClient := newHttpClient(socketAddr)
-			resp, err := httpClient.Get("http://pyroscope/health")
-			// if not, then we take over
+			resp, err := httpClient.Get(SocketHTTPAddress + "/health")
+
+			// the httpclient failed
+			// let's take over
 			// TODO identify what kind of error happened
 			if err != nil {
 				return takeOver(socketAddr)
 			}
 
-			// if yes, then we fail
+			// httpclient responded
+			// let's check the status code
 			if resp.StatusCode == http.StatusOK {
-				return nil, errors.New(fmt.Sprintf("a server is still running and responding to socket %s. please stop that server first", socketAddr))
+				return nil, ErrSocketStillResponding
 			}
 
-			// if any other kind of error, we also take over
+			// httpclient responded, but with a non 200 status code
+			// let's be optimistic and try to take over
 			return takeOver(socketAddr)
 		}
 
-		return nil, fmt.Errorf("another error happened %w", err)
+		return nil, fmt.Errorf("could not bind to socket due to unrecoverable error: %w", err)
 	}
 
 	// no errors happened
@@ -96,36 +124,6 @@ func (u *UdsHTTPServer) Stop() error {
 	return os.Remove(u.socketAddr)
 }
 
-func handleListenError(socketAddr string, err error) error {
-	if isErrorAddressAlreadyInUse(err) {
-		// that socket is already being used
-
-		// let's check if the server is also responding
-		httpClient := newHttpClient(socketAddr)
-		resp, err := httpClient.Get("http://pyroscope/health")
-		// if not, then we take over
-		// TODO identify what kind of error happened
-		if err != nil {
-			return nil
-		}
-
-		// if yes, then we fail
-		if resp.StatusCode == http.StatusOK {
-			return errors.New(fmt.Sprintf("a server is still running and responding to socket %s. please stop that server first", socketAddr))
-		}
-
-		// if not, then we take over
-		return nil
-	}
-
-	// it's some other error
-	if err != nil {
-		return fmt.Errorf("another error happened %w", err)
-	}
-
-	return nil
-}
-
 // https://stackoverflow.com/a/65865898
 func isErrorAddressAlreadyInUse(err error) bool {
 	var eOsSyscall *os.SyscallError
@@ -141,4 +139,16 @@ func isErrorAddressAlreadyInUse(err error) bool {
 	}
 
 	return false
+}
+
+func validateSocketAddress(socketAddr string) error {
+	if socketAddr == "" {
+		return ErrInvalidSocketPathname
+	}
+
+	// TODO
+	// check for the filepath size?
+	// https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_un.h.html#tag_13_67_04
+
+	return nil
 }
---
+++ pkg/admin/http_over_uds_test.go
@@ -0,0 +1,77 @@
+package admin_test
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pyroscope-io/pyroscope/pkg/admin"
+)
+
+type mockHandler struct{}
+
+func (m mockHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+var _ = Describe("HTTP Over UDS", func() {
+	var (
+		socketAddr string
+		dir        string
+		cleanup    func()
+	)
+
+	When("passed an empty socket address", func() {
+		It("should give an error", func() {
+			_, err := admin.NewUdsHTTPServer("")
+
+			Expect(err).To(MatchError(admin.ErrInvalidSocketPathname))
+		})
+	})
+
+	When("passed a non existing socket address", func() {
+		It("should give an error", func() {
+			_, err := admin.NewUdsHTTPServer("/non_existing_path")
+
+			// TODO how to test for wrapped errors?
+			// Expect(err).To(MatchError(fmt.Errorf("could not bind to socket")))
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("passed an already bound socket address", func() {
+		BeforeEach(func() {
+			cleanup, dir = genRandomDir()
+			socketAddr = dir + "/pyroscope.sock"
+		})
+		AfterEach(func() {
+			cleanup()
+		})
+
+		When("that socket does not respond", func() {
+			It("should take over that socket", func() {
+				// create server 1
+				_, err := admin.NewUdsHTTPServer(socketAddr)
+				must(err)
+
+				// create server 2
+				_, err = admin.NewUdsHTTPServer(socketAddr)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		When("that socket is still responding", func() {
+			It("should error", func() {
+				// create server 1
+				server, err := admin.NewUdsHTTPServer(socketAddr)
+				must(err)
+
+				go func() {
+					server.Start(http.NewServeMux())
+				}()
+
+				// create server 2
+				_, err = admin.NewUdsHTTPServer(socketAddr)
+				Expect(err).To(MatchError(admin.ErrSocketStillResponding))
+			})
+		})
+	})
+})
