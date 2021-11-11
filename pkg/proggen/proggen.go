d6f30e17a4bb37476b08f07239f4926a9e2499a0
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2021-11-11 14:29:22 +0200

    Introduce cpuUtilizationTarget

--- pkg/proggen/proggen.go
+++ pkg/proggen/proggen.go
@@ -12,6 +12,7 @@ type Function struct {
 	name string
 	workAmount uint64
 	calls []string
+	cpuUtilizationTarget float64
 }
 
 type Program struct {
@@ -24,10 +25,16 @@ func hash(key []byte) string {
 	return hex.EncodeToString(checksum[:])
 }
 
+// To be used with Tree.IterateWithChildKeys.
+// Essentially converts a treeNode into a Function.
+// Notice the hashing of treeNode keys. The treeNode key is considered a
+// universal identifier of every callable in the program to be generated.
+// Keys are hashed into alphanumeric strings to be used as function names.
 func nodeWithChildKeysToFunction(
 	key []byte,
 	workAmount uint64,
 	childKeys [][]byte,
+	cpuUtilizationTarget float64,
 ) Function {
 	name := hash(key)
 	calls := make([]string, 0)
@@ -35,28 +42,37 @@ func nodeWithChildKeysToFunction(
 		callName := hash(callKey)
 		calls = append(calls, callName)
 	}
-	function := Function{ name: name, workAmount: workAmount, calls: calls }
+	function := Function{
+			name: name,
+			workAmount: workAmount,
+			calls: calls,
+			cpuUtilizationTarget: cpuUtilizationTarget,
+		}
 	return function
 }
 
-func treeToFunctions(tree *tree.Tree) []Function {
+func treeToFunctions(tree *tree.Tree, cpuUtilizationTarget float64) []Function {
 	functions := make([]Function, 0)
 	tree.IterateWithChildKeys(func(key []byte, workAmount uint64, childKeys [][]byte) {
-		function := nodeWithChildKeysToFunction(key, workAmount, childKeys)
+		function := nodeWithChildKeysToFunction(key, workAmount, childKeys, cpuUtilizationTarget)
 		functions = append(functions, function)
 	})
 	return functions
 }
 
-func treeToProgram(tree *tree.Tree) Program {
+func treeToProgram(tree *tree.Tree, cpuUtilizationTarget float64) Program {
 	entryFunctionKey := tree.RootKey()
 	entryFunctionName := hash(entryFunctionKey)
-	return Program { functions: treeToFunctions(tree), entryFunctionName: entryFunctionName }
+	return Program {
+		functions: treeToFunctions(tree, cpuUtilizationTarget),
+		entryFunctionName: entryFunctionName,
+	}
 }
 
+// TODO Accomodate Function.cpuUtilizationTarget
 func (f *Function) toBlock() string {
 	callBlock := strings.Join(f.calls, "()\n") + "()"
-	return fmt.Sprintf(`
+	untrimmed := fmt.Sprintf(`
 func %s() {
 	for i := 0; i < %d; i++ {
 		// noop
@@ -68,13 +84,15 @@ func %s() {
 		f.workAmount,
 		callBlock,
 	)
+	return strings.TrimSpace(untrimmed)
 }
 
 func generateGoCode(program Program) string {
 	blocks := make([]string, 0)
 
 	for _, function := range program.functions {
-		blocks = append(blocks, function.toBlock())
+		block := function.toBlock()
+		blocks = append(blocks, block)
 	}
 
 	entryBlock := program.entryFunctionName + "()"
