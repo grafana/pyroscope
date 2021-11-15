42d174c388534d2a1371d57ff26ed6a84a7d9e8e
eduardo aleixo <eh-am@users.noreply.github.com>
2021-11-15 18:16:33 -0300

    Feat/delete apps (#515)
    feat: delete apps using the cli

---
+++ pkg/admin/admin_test.go
@@ -0,0 +1,13 @@
+package admin_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "admin suite")
+}
