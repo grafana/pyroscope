eca4b13dbaf913bba19a2977176e6083215f06b8
eduardo aleixo <eduardo@pyroscope.io>
2021-11-18 11:16:03 -0300

    feat: allow to --skip-verification when deleting an app

--- pkg/admin/cli.go
+++ pkg/admin/cli.go
@@ -37,28 +37,31 @@ func (c *CLI) GetAppsNames() error {
 }
 
 // DeleteApp deletes an app if a matching app exists
-func (c *CLI) DeleteApp(appname string) error {
-	// since this is a very destructive action
-	// we ask the user to type it out the app name as a form of validation
-	fmt.Println(fmt.Sprintf("Are you sure you want to delete the app '%s'? This action can not be reversed.", appname))
-	fmt.Println("")
-	fmt.Println("Keep in mind the following:")
-	fmt.Println("a) If an agent is still running, the app will be recreated.")
-	fmt.Println("b) The API is idempotent, ie. if the app already does NOT exist, this command will run just fine.")
-	fmt.Println("")
-	fmt.Println(fmt.Sprintf("Type '%s' to confirm (without quotes).", appname))
-	reader := bufio.NewReader(os.Stdin)
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		return err
-	}
-	trimmed := strings.TrimRight(text, "\n")
-	if trimmed != appname {
-		return fmt.Errorf("The app typed does not match. Want '%s' but got '%s'", appname, trimmed)
+func (c *CLI) DeleteApp(appname string, skipVerification bool) error {
+
+	if !skipVerification {
+		// since this is a very destructive action
+		// we ask the user to type it out the app name as a form of validation
+		fmt.Println(fmt.Sprintf("Are you sure you want to delete the app '%s'? This action can not be reversed.", appname))
+		fmt.Println("")
+		fmt.Println("Keep in mind the following:")
+		fmt.Println("a) If an agent is still running, the app will be recreated.")
+		fmt.Println("b) The API is idempotent, ie. if the app already does NOT exist, this command will run just fine.")
+		fmt.Println("")
+		fmt.Println(fmt.Sprintf("Type '%s' to confirm (without quotes).", appname))
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		trimmed := strings.TrimRight(text, "\n")
+		if trimmed != appname {
+			return fmt.Errorf("The app typed does not match. Want '%s' but got '%s'", appname, trimmed)
+		}
 	}
 
 	// finally delete the app
-	err = c.client.DeleteApp(appname)
+	err := c.client.DeleteApp(appname)
 	if err != nil {
 		return fmt.Errorf("failed to delete app: %w", err)
 	}
