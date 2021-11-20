d6238c6693ff0e0b18d89134a1d8b883a35657a9
Dominykas Mostauskis <dominykas.mostauskis@gmail.com>
2021-11-20 13:44:43 +0200

    Implement specific CPU utilization targeting
    Still needs refactoring

--- pkg/proggen/proggen.go
+++ pkg/proggen/proggen.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"strings"
 	"errors"
+	"log"
+	mapset "github.com/deckarep/golang-set"
 )
 
 type Function struct {
@@ -14,6 +16,7 @@ type Function struct {
 	workAmount uint64
 	calls []string
 	cpuUtilizationTarget float64
+	loopCalls bool
 }
 
 type Program struct {
@@ -21,10 +24,19 @@ type Program struct {
 	entryFunctionName string
 }
 
-// TODO introduce name generation routine that uses hash(...)
 func hash(key []byte) string {
 	checksum := sha256.Sum224(key)
-	return hex.EncodeToString(checksum[:])
+	hexString := hex.EncodeToString(checksum[:])
+	return hexString
+}
+
+func generateFunctionName(key []byte) string {
+	log.Printf("string(key) %s", string(key))
+	hashString := hash(key)
+	prefix := "f"
+	functionName := prefix + hashString
+	log.Printf("functionName %s", functionName)
+	return functionName
 }
 
 // To be used with Tree.IterateWithChildKeys.
@@ -38,10 +50,10 @@ func nodeWithChildKeysToFunction(
 	childKeys [][]byte,
 	cpuUtilizationTarget float64,
 ) Function {
-	name := hash(key)
+	name := generateFunctionName(copiedByteSlice(key))
 	calls := make([]string, 0)
 	for _, callKey := range childKeys {
-		callName := hash(callKey)
+		callName := generateFunctionName(copiedByteSlice(callKey))
 		calls = append(calls, callName)
 	}
 	function := Function{
@@ -53,18 +65,41 @@ func nodeWithChildKeysToFunction(
 	return function
 }
 
+func copiedByteSlice(oldSlice []byte) []byte {
+	newSlice := make([]byte, len(oldSlice))
+	copy(newSlice, oldSlice)
+	return newSlice
+}
+
 func treeToFunctions(tree *tree.Tree, cpuUtilizationTarget float64) []Function {
 	functions := make([]Function, 0)
+	i := 0
+	k := 0
+	seenChildKeys := mapset.NewSet()
+	seenNodeKeys := mapset.NewSet()
 	tree.IterateWithChildKeys(func(key []byte, workAmount uint64, childKeys [][]byte) {
-		function := nodeWithChildKeysToFunction(key, workAmount, childKeys, cpuUtilizationTarget)
+		i = i + 1
+		k = k + len(childKeys)
+		for _, childKey := range childKeys {
+			seenChildKeys.Add(string(copiedByteSlice(childKey)))
+		}
+		seenNodeKeys.Add(string(copiedByteSlice(key)))
+		function := nodeWithChildKeysToFunction(copiedByteSlice(key), workAmount, childKeys, cpuUtilizationTarget)
 		functions = append(functions, function)
 	})
+	log.Printf("parents visited %v", i)
+	log.Printf("sum of len(childKeys) %v", k)
+	log.Printf("seenChildKeys.Cardinality() %v", seenChildKeys.Cardinality())
+	log.Printf("seenChildKeys.Difference(seenNodeKeys).Cardinality() %v", seenChildKeys.Difference(seenNodeKeys).Cardinality())
+	log.Printf("seenChildKeys.Difference(seenNodeKeys) %v", seenChildKeys.Difference(seenNodeKeys))
+	log.Printf("seenNodeKeys.Difference(seenChildKeys).Cardinality() %v", seenNodeKeys.Difference(seenChildKeys).Cardinality())
+	log.Printf("seenNodeKeys.Difference(seenChildKeys) %v", seenNodeKeys.Difference(seenChildKeys))
 	return functions
 }
 
 func TreeToProgram(tree *tree.Tree, cpuUtilizationTarget float64) Program {
 	entryFunctionKey := tree.RootKey()
-	entryFunctionName := hash(entryFunctionKey)
+	entryFunctionName := generateFunctionName(entryFunctionKey)
 	return Program {
 		functions: treeToFunctions(tree, cpuUtilizationTarget),
 		entryFunctionName: entryFunctionName,
@@ -80,12 +115,21 @@ func (f *Function) toBlock() string {
 	callBlock := ""
 	if len(f.calls) != 0 {
 		callBlock = strings.Join(f.calls, "()\n" + indentation()) + "()"
+		if f.loopCalls {
+			callBlock = "for {" + "\n" + indentation() + callBlock + "\n" + indentation() + "}"
+		}
 	}
 	workBlock := ""
 	if f.workAmount != 0 {
 		workBlock = fmt.Sprintf(`
 	for i := 0; i < %d; i++ {
-		// noop
+		select {
+			case durationToSleep, shouldSleep := <-durationToSleepChannel:
+				if shouldSleep {
+					time.Sleep(durationToSleep)
+				}
+			default:
+		}
 	}
 			`, f.workAmount)
 		workBlock = strings.TrimSpace(workBlock)
@@ -106,14 +150,59 @@ func (f *Function) toBlock() string {
 func generateGoCode(program *Program) string {
 	blocks := make([]string, 0)
 
+	packageBlock := "package main"
+	blocks = append(blocks, packageBlock)
+
+	importBlock := "import \"time\""
+	//importBlock = importBlock + "\n" + "import \"fmt\""
+	blocks = append(blocks, importBlock)
+
+
+	fullCycleInMillis := 100
+	// TODO currently cpuUtilizationTarget is a property of Function; refactor to make it Program's property
+	cpuUtilizationTarget := 0.80
+	setupFunctionName := "setupFunction"
+	timerInitBlock := fmt.Sprintf(`
+var fullCycleInMillis = %d
+var cpuUtilizationTarget = %f
+var durationToWork = time.Duration(int(cpuUtilizationTarget * float64(fullCycleInMillis))) * time.Millisecond
+var durationToSleep = time.Duration(int((1 - cpuUtilizationTarget) * float64(fullCycleInMillis))) * time.Millisecond
+var durationToSleepChannel = make(chan time.Duration, 1)
+func switchToSleep() {
+	durationToSleepChannel <- durationToSleep
+	time.AfterFunc(durationToWork, switchToSleep)
+}
+var shouldDoSetup = true
+func %s() {
+	if shouldDoSetup {
+		time.AfterFunc(durationToWork, switchToSleep)
+		shouldDoSetup = false
+	}
+}
+		`, fullCycleInMillis, cpuUtilizationTarget, setupFunctionName)
+
+	blocks = append(blocks, timerInitBlock)
+
+	functionBlocks := make([]string, 0)
+
 	for _, function := range program.functions {
-		block := function.toBlock()
-		blocks = append(blocks, block)
+		functionBlock := function.toBlock()
+		functionBlocks = append(functionBlocks, functionBlock)
 	}
 
-	entryBlock := program.entryFunctionName + "()"
+	blocks = append(blocks, functionBlocks...)
 
-	blocks = append(blocks, entryBlock)
+	mainFunction := Function {
+		name: "main",
+		calls: []string{program.entryFunctionName, setupFunctionName},
+		workAmount: 0,
+		cpuUtilizationTarget: 0.75,
+		loopCalls: true,
+	}
+
+	mainBlock := mainFunction.toBlock()
+
+	blocks = append(blocks, mainBlock)
 
 	code := strings.Join(blocks, "\n")
 
