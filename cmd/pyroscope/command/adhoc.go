4f786271ec1aa63a27e2896c9460442d115d8563
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2021-11-24 11:27:06 +0100

    Add a long description of how the adhoc subcommand should work.
    This is an attempt to give a relatively easy way to integrate and
    document all the possible ways in which profiling can work.

--- cmd/pyroscope/command/adhoc.go
+++ cmd/pyroscope/command/adhoc.go
@@ -12,8 +12,40 @@ import (
 
 func newAdhocCmd(cfg *config.Adhoc) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "adhoc",
-		Short: "adhoc mode commands",
+		Use:   "adhoc [flags] [args]",
+		Short: "Profile a process and save the results to be used in adhoc mode",
+		Long: `adhoc command is a complete toolset to profile a process and save the profiling
+results.
+
+These results are then available to be visualized through the 'Adhoc Profiling'
+section in the UI (available through 'pyroscope server').
+
+There are multiple ways to gather the profiling data, and not all of them are
+available for all the languages.
+Which way is better depends on several factors: what the language supports,
+how the profiled process is launched, and how the profiled process provides
+the profiled data.
+
+The different supported ways are:
+- exec. In this case, pyroscope creates a different process for the profiled
+program and uses a spy to directly gather profiling data. It's a useful way
+to profile a whole execution of some program that has no other pyroscope
+integration or way of exposing profiling data.
+It's the default mode for languages with a supported spy when either the spy
+name is specified (through '--spy-name' flag) or when the spyname is
+autodetected.
+- connect. Similar to exec, pyroscope uses a spy to gather profiling data,
+but instead of creating a new profiled process, it spies an already running
+process, indicated through '--pid' flag.
+- push. In this case, pyroscope creates a different process for the profiled
+program and launches an HTTP server with an ingestion endpoint. It's useful
+to profile programs already integrated with Pyroscope using its HTTP API.
+Push mode is used by default when no spy is detected and no '--url' flag is
+provided. It can also be override the default exec mode with the '--push' flag.
+- pull. In this case, pyroscope periodically connects to the URL specified
+thorugh '--url' where it tries to retrieve profiling data in any of the
+supported formats. In this case arguments are optional, and if provided,
+they are used to launch a new process before polling the URL.`,
 	}
 	cmd.AddCommand(newAdhocRecordCmd(&cfg.AdhocRecord))
 	cmd.AddCommand(newAdhocServerCmd(&cfg.AdhocServer))
