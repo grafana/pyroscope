2f74c3bebea9e3e2dc6420eeebd58d766d0946fd
eduardo aleixo <eduardo@pyroscope.io>
2021-11-25 12:31:26 -0300

    WIP failing to delete dimensions where labels come into play

--- pkg/storage/storage_delete_test.go
+++ pkg/storage/storage_delete_test.go
@@ -177,5 +177,120 @@ var _ = Describe("storage package", func() {
 				checkLabelsPresence(appname, false)
 			})
 		})
+
+		Context("app with labels", func() {
+			It("works correctly", func() {
+
+				appname := "my.app.cpu"
+
+				// We insert info for an app
+				tree1 := tree.New()
+				tree1.Insert([]byte("a;b"), uint64(1))
+
+				// We are mirroring this on the simple.golang.cpu example
+				labels := []string{
+					"",
+					"{foo=bar,function=fast}",
+					"{foo=bar,function=slow}",
+				}
+
+				st := testing.SimpleTime(10)
+				et := testing.SimpleTime(19)
+				for _, l := range labels {
+					key, _ := segment.ParseKey(appname + l)
+					err := s.Put(&PutInput{
+						StartTime:  st,
+						EndTime:    et,
+						Key:        key,
+						Val:        tree1,
+						SpyName:    "testspy",
+						SampleRate: 100,
+					})
+					Expect(err).ToNot(HaveOccurred())
+				}
+
+				// Since the DeleteApp also removes dictionaries
+				// therefore we need to create them manually here
+				// (they are normally created when TODO)
+				d := dict.New()
+				s.dicts.Put(appname, d)
+
+				/*******************************/
+				/*  S a n i t y   C h e c k s  */
+				/*******************************/
+
+				By("checking dimensions were created")
+				// Dimensions
+				// 4 dimensions
+				// i:__name__:my.app.cpu
+				// i:foo:bar
+				// i:function:fast
+				// i:function:slow
+				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(4)))
+				checkDimensionsPresence(appname, true)
+
+				By("checking trees were created")
+				// Trees
+				// 3 trees
+				// t:my.app.cpu{foo=bar,function=fast}:0:1637853390
+				// t:my.app.cpu{foo=bar,function=slow}:0:1637853390
+				// t:my.app.cpu{}:0:1637853390
+				Expect(s.trees.Cache.Size()).To(Equal(uint64(3)))
+				checkTreesPresence(appname, st, 0, true)
+
+				By("checking segments were created")
+				// Segments
+				// 3 segments
+				// s:my.app.app3.cpu{foo=bar,function=fast}
+				// s:my.app.app3.cpu{foo=bar,function=slow}
+				// s:my.app.app3.cpu{}
+				Expect(s.segments.Cache.Size()).To(Equal(uint64(3)))
+				checkSegmentsPresence(appname, true)
+
+				By("checking dicts were created")
+				// Dicts
+				// I manually inserted a dictionary so it should be fine?
+				Expect(s.dicts.Cache.Size()).To(Equal(uint64(1)))
+				checkDictsPresence(appname, true)
+
+				// Labels
+				checkLabelsPresence(appname, true)
+
+				/*************************/
+				/*  D e l e t e   a p p  */
+				/*************************/
+				By("deleting the app")
+				err := s.DeleteApp(appname)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("checking trees were deleted")
+				// Trees
+				// should've been deleted from CACHE
+				Expect(s.trees.Cache.Size()).To(Equal(uint64(0)))
+				checkTreesPresence(appname, st, 0, false)
+				// Trees should've been also deleted from DISK
+				// TODO: how to check for that?
+
+				// Dimensions
+				By("checking dimensions were deleted")
+				s.dimensions.Dump()
+				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(0)))
+				checkDimensionsPresence(appname, false)
+
+				// Dicts
+				By("checking dicts were deleted")
+				Expect(s.dicts.Cache.Size()).To(Equal(uint64(0)))
+				checkDictsPresence(appname, false)
+
+				// Segments
+				By("checking segments were deleted")
+				Expect(s.segments.Cache.Size()).To(Equal(uint64(0)))
+				checkSegmentsPresence(appname, false)
+
+				// Labels
+				By("checking labels were deleted")
+				checkLabelsPresence(appname, false)
+			})
+		})
 	})
 })
