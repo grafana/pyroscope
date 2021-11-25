9b1af78be5d21b32a6265c43a452009cd3a2f7e2
eduardo aleixo <eduardo@pyroscope.io>
2021-11-25 18:41:54 -0300

    testing when there's 2 apps with same labels

--- pkg/storage/storage_delete_test.go
+++ pkg/storage/storage_delete_test.go
@@ -28,6 +28,7 @@ var _ = Describe("storage package", func() {
 	})
 
 	// Just a sanity check
+	// I may delete this later
 	Context("basic delete", func() {
 		It("deletes trees", func() {
 			Expect(s.trees.Cache.Size()).To(Equal(uint64(0)))
@@ -258,30 +259,14 @@ var _ = Describe("storage package", func() {
 				/*******************************/
 
 				By("checking dimensions were created")
-				// Dimensions
-				// 4 dimensions
-				// i:__name__:my.app.cpu
-				// i:foo:bar
-				// i:function:fast
-				// i:function:slow
 				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(4)))
 				checkDimensionsPresence(appname, true)
 
 				By("checking trees were created")
-				// Trees
-				// 3 trees
-				// t:my.app.cpu{foo=bar,function=fast}:0:1637853390
-				// t:my.app.cpu{foo=bar,function=slow}:0:1637853390
-				// t:my.app.cpu{}:0:1637853390
 				Expect(s.trees.Cache.Size()).To(Equal(uint64(3)))
 				checkTreesPresence(appname, st, 0, true)
 
 				By("checking segments were created")
-				// Segments
-				// 3 segments
-				// s:my.app.app3.cpu{foo=bar,function=fast}
-				// s:my.app.app3.cpu{foo=bar,function=slow}
-				// s:my.app.app3.cpu{}
 				Expect(s.segments.Cache.Size()).To(Equal(uint64(3)))
 				checkSegmentsPresence(appname, true)
 
@@ -311,7 +296,6 @@ var _ = Describe("storage package", func() {
 
 				// Dimensions
 				By("checking dimensions were deleted")
-				s.dimensions.Dump()
 				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(0)))
 				checkDimensionsPresence(appname, false)
 
@@ -334,7 +318,108 @@ var _ = Describe("storage package", func() {
 		// In this test we have 2 apps with the same label
 		// And deleting one app should not interfer with the labels of the other app
 		Context("multiple apps with labels", func() {
-			It("works correctly", func() {
+			It("deletes the correct data", func() {
+
+				st := testing.SimpleTime(10)
+				insert := func(appname string) {
+					// We insert info for an app
+					tree1 := tree.New()
+					tree1.Insert([]byte("a;b"), uint64(1))
+
+					// We are mirroring this on the simple.golang.cpu example
+					labels := []string{
+						"",
+						"{foo=bar,function=fast}",
+						"{foo=bar,function=slow}",
+					}
+
+					et := testing.SimpleTime(19)
+					for _, l := range labels {
+						key, _ := segment.ParseKey(appname + l)
+						err := s.Put(&PutInput{
+							StartTime:  st,
+							EndTime:    et,
+							Key:        key,
+							Val:        tree1,
+							SpyName:    "testspy",
+							SampleRate: 100,
+						})
+						Expect(err).ToNot(HaveOccurred())
+					}
+
+					// Since the DeleteApp also removes dictionaries
+					// therefore we need to create them manually here
+					// (they are normally created when TODO)
+					d := dict.New()
+					s.dicts.Put(appname, d)
+				}
+
+				app1name := "myapp1.cpu"
+				app2name := "myapp2.cpu"
+
+				insert(app1name)
+				insert(app2name)
+
+				/*******************************/
+				/*  S a n i t y   C h e c k s  */
+				/*******************************/
+				By("checking dimensions were created")
+				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(5)))
+				checkDimensionsPresence(app1name, true)
+				checkDimensionsPresence(app2name, true)
+
+				By("checking trees were created")
+				Expect(s.trees.Cache.Size()).To(Equal(uint64(6)))
+				checkTreesPresence(app1name, st, 0, true)
+				checkTreesPresence(app2name, st, 0, true)
+
+				By("checking segments were created")
+				Expect(s.segments.Cache.Size()).To(Equal(uint64(6)))
+				checkSegmentsPresence(app1name, true)
+				checkSegmentsPresence(app2name, true)
+
+				By("checking dicts were created")
+				Expect(s.dicts.Cache.Size()).To(Equal(uint64(2)))
+				checkDictsPresence(app1name, true)
+				checkDictsPresence(app2name, true)
+
+				By("checking labels were created")
+				checkLabelsPresence(app1name, true)
+				checkLabelsPresence(app2name, true)
+
+				/*************************/
+				/*  D e l e t e   a p p  */
+				/*************************/
+				By("deleting the app")
+				err := s.DeleteApp(app1name)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("checking trees were deleted")
+				Expect(s.trees.Cache.Size()).To(Equal(uint64(3)))
+				checkTreesPresence(app1name, st, 0, false)
+				checkTreesPresence(app2name, st, 0, true)
+				// Trees should've been also deleted from DISK
+				// TODO: how to check for that?
+
+				// Dimensions
+				By("checking dimensions were deleted")
+				Expect(s.dimensions.Cache.Size()).To(Equal(uint64(4)))
+				checkDimensionsPresence(app1name, false)
+				checkDimensionsPresence(app2name, true)
+
+				By("checking dicts were deleted")
+				Expect(s.dicts.Cache.Size()).To(Equal(uint64(1)))
+				checkDictsPresence(app1name, false)
+				checkDimensionsPresence(app2name, true)
+
+				By("checking segments were deleted")
+				Expect(s.segments.Cache.Size()).To(Equal(uint64(3)))
+				checkSegmentsPresence(app1name, false)
+				checkSegmentsPresence(app2name, true)
+
+				By("checking labels were deleted")
+				checkLabelsPresence(app1name, false)
+				checkSegmentsPresence(app2name, true)
 			})
 		})
 	})
