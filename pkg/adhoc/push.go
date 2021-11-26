0d7814ac48ba122a299467a282e953866ec6b562
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2021-11-26 15:23:49 +0100

    Move the initialization part to the constructor.

--- pkg/adhoc/push.go
+++ pkg/adhoc/push.go
@@ -11,10 +11,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
-	"github.com/valyala/bytebufferpool"
 
 	"github.com/pyroscope-io/pyroscope/pkg/config"
-	"github.com/pyroscope-io/pyroscope/pkg/exec"
 	"github.com/pyroscope-io/pyroscope/pkg/exporter"
 	"github.com/pyroscope-io/pyroscope/pkg/server"
 	"github.com/pyroscope-io/pyroscope/pkg/storage"
@@ -22,24 +20,25 @@ import (
 )
 
 type push struct {
-	args       []string
-	storage    *storage.Storage
-	logger     *logrus.Logger
-	bufferPool bytebufferpool.Pool
+	args    []string
+	handler http.Handler
+	logger  *logrus.Logger
 }
 
-func newPush(_cfg *config.Adhoc, args []string, storage *storage.Storage, logger *logrus.Logger) (runner, error) {
+func newPush(_cfg *config.Adhoc, args []string, st *storage.Storage, logger *logrus.Logger) (runner, error) {
+	exporter, err := exporter.NewExporter(config.MetricsExportRules{}, prometheus.DefaultRegisterer)
+	if err != nil {
+		return nil, err
+	}
 	return push{
 		args:    args,
-		storage: storage,
+		handler: server.NewIngestHandler(logger, st, exporter, func(_ *storage.PutInput) {}),
 		logger:  logger,
 	}, nil
 }
 
 func (p push) Run() error {
-	exporter, err := exporter.NewExporter(config.MetricsExportRules{}, prometheus.DefaultRegisterer)
-	handler := server.NewIngestHandler(p.logger, p.storage, exporter, func(_ *storage.PutInput) {})
-	http.Handle("/ingest", handler)
+	http.Handle("/ingest", p.handler)
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		return err
@@ -53,11 +52,10 @@ func (p push) Run() error {
 
 	// start command
 	c := make(chan os.Signal, 10)
-	var cmd *exec.Cmd
 	// Note that we don't specify which signals to be sent: any signal to be
 	// relayed to the child process (including SIGINT and SIGTERM).
 	signal.Notify(c)
-	cmd = exec.Command(p.args[0], p.args[1:]...)
+	cmd := exec.Command(p.args[0], p.args[1:]...)
 	cmd.Env = append(os.Environ(), fmt.Sprintf("PYROSCOPE_SERVER_ADDRESS=http://localhost:%d", listener.Addr().(*net.TCPAddr).Port))
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
