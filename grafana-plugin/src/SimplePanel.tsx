c1a3635ce372fa66c35b9d657b40b6bf9de14ad6
eduardo aleixo <eduardo@pyroscope.io>
2021-11-30 16:26:23 -0300

    wip, compiling plugin stuff

---
+++ grafana-plugin/src/SimplePanel.tsx
@@ -0,0 +1,60 @@
+// @ts-nocheck
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SimpleOptions } from 'types';
+import { css } from 'emotion';
+import { stylesFactory, useTheme } from '@grafana/ui';
+//import FlameGraph, { parseFlamebearerFormat } from 'flamegraphComponent.js';
+//import Flamegraph from '../../../webapp/javascript/components/FlameGraph/FlameGraphComponent/index';
+//import Flamegraph from '../build/flamegraphComponent.js';
+//
+//function Flamegraph() {
+//  return <h1>hello!!</h1>;
+//}
+
+interface Props extends PanelProps<SimpleOptions> {}
+
+export const SimplePanel: React.FC<Props> = ({
+  options,
+  data,
+  width,
+  height,
+}) => {
+  const theme = useTheme();
+  const styles = getStyles();
+  return (
+    <>
+      <div className={styles.app}>
+        <div className={`${styles.appContainer} flamegraph-wrapper`}>
+          <h1>Hi</h1>
+          {/*
+          <FlameGraph
+            flamebearer={data.series[data.series.length - 1].fields[0].values.buffer[0]}
+            format={parseFlamebearerFormat()}
+            width={width}
+            height={height}
+          />
+            */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const getStyles = stylesFactory(() => {
+  return {
+    app: css`
+      height: 100%;
+      min-height: 100%;
+      display: flex;
+      flex-direction: column;
+      .flamegraph-tooltip {
+        position: fixed;
+      }
+    `,
+    appContainer: css`
+      flex: 1 0 auto;
+      position: relative;
+    `,
+  };
+});
---
+++ grafana-plugin/src/module.ts
@@ -0,0 +1,42 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SimplePanel } from './SimplePanel';
+
+export const plugin = new PanelPlugin<SimpleOptions>(
+  SimplePanel
+).setPanelOptions((builder) => {
+  return builder
+    .addTextInput({
+      path: 'text',
+      name: 'Simple text option',
+      description: 'Description of panel option',
+      defaultValue: 'Default value of text input option',
+    })
+    .addBooleanSwitch({
+      path: 'showSeriesCount',
+      name: 'Show series counter',
+      defaultValue: false,
+    })
+    .addRadio({
+      path: 'seriesCountSize',
+      defaultValue: 'sm',
+      name: 'Series counter size',
+      settings: {
+        options: [
+          {
+            value: 'sm',
+            label: 'Small',
+          },
+          {
+            value: 'md',
+            label: 'Medium',
+          },
+          {
+            value: 'lg',
+            label: 'Large',
+          },
+        ],
+      },
+      showIf: (config) => config.showSeriesCount,
+    });
+});
