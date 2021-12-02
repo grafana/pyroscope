3c12f83f5dfc1a708fa0febd717ba88146f79717
eduardo aleixo <eduardo@pyroscope.io>
2021-12-02 15:39:24 -0300

    chore(sidebar): show active link

--- webapp/javascript/components/Sidebar2.tsx
+++ webapp/javascript/components/Sidebar2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { faWindowMaximize } from '@fortawesome/free-regular-svg-icons';
 import { faChartBar } from '@fortawesome/free-solid-svg-icons/faChartBar';
 import { faColumns } from '@fortawesome/free-solid-svg-icons/faColumns';
@@ -13,25 +13,69 @@ import Sidebar, {
   SubMenu,
   Menu,
 } from '@ui/Sidebar';
+import { useLocation, NavLink } from 'react-router-dom';
+import { isExperimentalAdhocUIEnabled } from '@utils/features';
 import styles from './Sidebar.module.css';
 
 export default function Sidebar2() {
+  const { search, pathname } = useLocation();
+
+  // the component doesn't seem to support setting up an active item
+  // so we must set it up manually
+  // https://github.com/azouaoui-med/react-pro-sidebar/issues/84
+  const isRouteActive = function (route: string) {
+    return pathname === route;
+  };
+
+  // notice how there's no SubMenu here
+  // since that's only rendered when Adhoc is enabled
+  const continuousOnly = (
+    <>
+      <MenuItem active={isRouteActive('/')} icon={faWindowMaximize}>
+        Single View
+        <NavLink
+          activeClassName="active-route"
+          data-testid="sidebar-root"
+          to={{ pathname: '/', search }}
+          exact
+        />
+      </MenuItem>
+      <MenuItem active={isRouteActive('/comparison')} icon={faColumns}>
+        Comparison View
+        <NavLink to={{ pathname: '/comparison', search }} exact />
+      </MenuItem>
+      <MenuItem active={isRouteActive('/comparison-diff')} icon={faChartBar}>
+        Diff View
+        <NavLink to={{ pathname: '/comparison-diff', search }} exact />
+      </MenuItem>
+    </>
+  );
+
+  const continuousAndAdhoc = (
+    <>
+      <SubMenu title="Continuous Profiling">{continuousOnly}</SubMenu>
+      <SubMenu title="Adhoc Profiling">
+        <MenuItem
+          active={isRouteActive('/adhoc-single')}
+          icon={faWindowMaximize}
+        >
+          Single View
+        </MenuItem>
+        <MenuItem active={isRouteActive('/adhoc-comparison')} icon={faColumns}>
+          Comparison View
+        </MenuItem>
+        <MenuItem active={isRouteActive('/adhoc-diff')} icon={faChartBar}>
+          Diff View
+        </MenuItem>
+      </SubMenu>
+    </>
+  );
+
   return (
     <Sidebar>
       <SidebarContent>
         <Menu iconShape="square">
-          <SubMenu title="Continuous Profiling" open>
-            <MenuItem active icon={faWindowMaximize}>
-              Single View
-            </MenuItem>
-            <MenuItem icon={faColumns}>Comparison View</MenuItem>
-            <MenuItem icon={faChartBar}>Diff View</MenuItem>
-          </SubMenu>
-          <SubMenu title="Adhoc Profiling">
-            <MenuItem icon={faWindowMaximize}>Single View</MenuItem>
-            <MenuItem icon={faColumns}>Comparison View</MenuItem>
-            <MenuItem icon={faChartBar}>Diff View</MenuItem>
-          </SubMenu>
+          {isExperimentalAdhocUIEnabled ? continuousAndAdhoc : continuousOnly}
         </Menu>
       </SidebarContent>
       <SidebarFooter>
