0d022d2e653957d0da5b1c96c4d3fbb9ce788acd
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2021-12-07 09:35:13 +0100

    Remove debug output
    Co-authored-by: eduardo aleixo <eh-am@users.noreply.github.com>

--- webapp/javascript/components/FileUploader.tsx
+++ webapp/javascript/components/FileUploader.tsx
@@ -39,7 +39,6 @@ export default function FileUploader({ file, setFile }: Props) {
           flamebearer.levels = calculatedLevels;
           setFile(file, flamebearer);
         } catch (e) {
-          console.log(e);
           alert(e);
         }
       };
