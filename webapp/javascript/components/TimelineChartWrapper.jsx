c9163d74b3280fbd10ec9b5f0712bb0709716491
eduardo aleixo <eh-am@users.noreply.github.com>
2021-12-13 13:29:49 -0300

    chore (frontend): make flamegraphrenderer render children between toolbar and canvas (#604)

--- webapp/javascript/components/TimelineChartWrapper.jsx
+++ webapp/javascript/components/TimelineChartWrapper.jsx
@@ -166,6 +166,7 @@ class TimelineChartWrapper extends React.Component {
 
     return (
       <TimelineChart
+        data-testid={this.props['data-testid']}
         id={this.props.id}
         options={this.state.flotOptions}
         viewSide={this.props.viewSide}
