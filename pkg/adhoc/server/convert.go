baaa85a4bd490d9a322952e195df3975f5e219c2
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2021-12-23 19:13:08 +0100

    feat: add (partial) server side support for adhoc diff.
    The conversion from flamebearer profile to tree is still pending.

---
+++ pkg/adhoc/server/convert.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"errors"
+
+	"github.com/pyroscope-io/pyroscope/pkg/storage/tree"
+	"github.com/pyroscope-io/pyroscope/pkg/structs/flamebearer"
+)
+
+func profileToTree(fb flamebearer.FlamebearerProfile) (*tree.Tree, error) {
+	return nil, errors.New("Not implemented")
+}
--- pkg/adhoc/server/server.go
+++ pkg/adhoc/server/server.go
@@ -16,6 +16,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/pyroscope-io/pyroscope/pkg/adhoc/util"
+	"github.com/pyroscope-io/pyroscope/pkg/storage"
 	"github.com/pyroscope-io/pyroscope/pkg/structs/flamebearer"
 )
 
@@ -44,7 +45,7 @@ func (s *server) AddRoutes(r *mux.Router) http.HandlerFunc {
 	if s.enabled {
 		r.HandleFunc("/v1/profiles", s.Profiles)
 		r.HandleFunc("/v1/profile/{id:[0-9a-f]+}", s.Profile)
-		r.HandleFunc("/v1/diff/{id1[0-9a-f]+}/{id2[0-9a-f]+}", s.Diff)
+		r.HandleFunc("/v1/diff/{left[0-9a-f]+}/{right[0-9a-f]+}", s.Diff)
 	}
 	return r.ServeHTTP
 }
@@ -129,6 +130,92 @@ func (s *server) Profile(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// TODO(abeaumont)
-func (*server) Diff(_ http.ResponseWriter, _ *http.Request) {
+func (s *server) Diff(w http.ResponseWriter, r *http.Request) {
+	lid := mux.Vars(r)["left"]
+	rid := mux.Vars(r)["right"]
+	s.mtx.RLock()
+	lp, lok := s.profiles[lid]
+	rp, rok := s.profiles[rid]
+	s.mtx.RUnlock()
+	if !lok {
+		s.log.WithField("id", lid).Warning("Profile does not exist")
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+	if !rok {
+		s.log.WithField("id", rid).Warning("Profile does not exist")
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
+	}
+	lf, err := os.Open(filepath.Join(util.DataDirectory(), lp.Name))
+	if err != nil {
+		s.log.WithError(err).Error("Unable to open profile")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer lf.Close()
+	rf, err := os.Open(filepath.Join(util.DataDirectory(), rp.Name))
+	if err != nil {
+		s.log.WithError(err).Error("Unable to open profile")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer lf.Close()
+
+	// TODO(abeaumont): Support for other formats
+	lb, err := io.ReadAll(lf)
+	if err != nil {
+		s.log.WithError(err).Error("Unable to read profile")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rb, err := io.ReadAll(rf)
+	if err != nil {
+		s.log.WithError(err).Error("Unable to read profile")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var lfb, rfb flamebearer.FlamebearerProfile
+	if err := json.Unmarshal(lb, &lfb); err != nil {
+		s.log.WithError(err).Error("Invalid file format")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.Unmarshal(rb, &rfb); err != nil {
+		s.log.WithError(err).Error("Invalid file format")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO(abeaumont): Validate that profiles are comparable
+	// TODO(abeaumont): Simplify profile generation
+	out := &storage.GetOutput{
+		Tree:       nil,
+		Units:      lfb.Metadata.Units,
+		SpyName:    lfb.Metadata.SpyName,
+		SampleRate: lfb.Metadata.SampleRate,
+	}
+	lt, err := profileToTree(lfb)
+	if err != nil {
+		s.log.WithError(err).Error("Unable to convert profile to tree")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rt, err := profileToTree(rfb)
+	if err != nil {
+		s.log.WithError(err).Error("Unable to convert profile to tree")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	lOut := &storage.GetOutput{Tree: lt}
+	rOut := &storage.GetOutput{Tree: rt}
+
+	// FIXME(abeaumont): Use maxNodes once https://github.com/pyroscope-io/pyroscope/pull/649 is merged
+	fb := flamebearer.NewCombinedProfile(out, lOut, rOut, 2048)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(fb); err != nil {
+		s.log.WithError(err).Error("Unable to encode the profile diff")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
