747b3c994ab3a50659c805c93941b7554a689524
Ryan Perry <rperry2174@gmail.com>
2021-12-23 11:40:20 -0500

    docs: fix rideshare example

--- examples/golang-push/rideshare/bike/bike.go
+++ examples/golang-push/rideshare/bike/bike.go
@@ -1,6 +1,6 @@
 package bike
 
-import "github.com/pyroscope-io/pyroscope/tree/main/examples/golang/utility"
+import "github.com/tree/main/examples/golang-push/rideshare/utility"
 
 func OrderBike(search_radius int64) {
 	utility.FindNearestVehicle(search_radius, "bike")
--- examples/golang-push/rideshare/car/car.go
+++ examples/golang-push/rideshare/car/car.go
@@ -1,6 +1,6 @@
 package car
 
-import "github.com/pyroscope-io/pyroscope/tree/main/examples/golang/utility"
+import "github.com/tree/main/examples/golang-push/rideshare/utility"
 
 func OrderCar(search_radius int64) {
 	utility.FindNearestVehicle(search_radius, "car")
