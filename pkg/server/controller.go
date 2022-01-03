f377cf3c455cd7231ba1527ada933696fb9c1495
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2022-01-03 23:16:01 +0100

    fix: register all pprof http handlers (#672)

--- pkg/server/controller.go
+++ pkg/server/controller.go
@@ -184,6 +184,12 @@ func (ctrl *Controller) mux() (http.Handler, error) {
 			{"/debug/pprof/profile", pprof.Profile},
 			{"/debug/pprof/symbol", pprof.Symbol},
 			{"/debug/pprof/trace", pprof.Trace},
+			{"/debug/pprof/allocs", pprof.Index},
+			{"/debug/pprof/goroutine", pprof.Index},
+			{"/debug/pprof/heap", pprof.Index},
+			{"/debug/pprof/threadcreate", pprof.Index},
+			{"/debug/pprof/block", pprof.Index},
+			{"/debug/pprof/mutex", pprof.Index},
 		}...)
 	}
 
