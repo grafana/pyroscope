c180cd495bed27b9d82b344dc56e90bc1d710c50
eduardo aleixo <eduardo@pyroscope.io>
2022-01-04 15:09:11 -0300

    more lint fixes

--- setupAfterEnv.ts
+++ setupAfterEnv.ts
@@ -1,14 +1,11 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 import timezoneMock from 'timezone-mock';
-
-const {
-  toMatchImageSnapshot,
-  configureToMatchImageSnapshot,
-} = require('jest-image-snapshot');
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import type { MatchImageSnapshotOptions } from 'jest-image-snapshot';
 
 expect.extend({
-  toMatchImageSnapshot(received: any, options: any) {
+  toMatchImageSnapshot(received: string, options: MatchImageSnapshotOptions) {
     // If these checks pass, assume we're in a JSDOM environment with the 'canvas' package.
     if (process.env.RUN_SNAPSHOTS) {
       const customConfig = { threshold: 0.02 };
@@ -19,9 +16,12 @@ expect.extend({
       return toMatchImageSnapshot.call(this, received, options);
     }
 
+    // This is running in node
+    // eslint-disable-next-line no-console
     console.info(
       `Skipping 'toMatchImageSnapshot' assertion since env var 'RUN_SNAPSHOTS' is not set.`
     );
+
     return { pass: true };
   },
 });
