4d2e53a3a4c6564516b1385a22335f22235b9b07
eduardo aleixo <eduardo@pyroscope.io>
2022-01-06 15:10:06 -0300

    test baseService

--- webapp/javascript/services/base.spec.ts
+++ webapp/javascript/services/base.spec.ts
@@ -2,33 +2,133 @@ import { Result } from '@utils/fp';
 import { get } from './base';
 import { setupServer, rest } from './testUtils';
 
-const baseURL = 'http://localhost';
-const server = setupServer(
-  rest.get(`${baseURL}/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(500),
-      ctx.json({ message: 'Deliberately broken request' })
-    );
-  })
-);
-
 describe('Base HTTP', () => {
-  beforeAll(() => {
-    (window as any).baseURL = baseURL;
-    server.listen();
-  });
-  afterAll(() => {
-    server.close();
+  let server: ReturnType<typeof setupServer>;
+
+  afterEach(() => {
+    if (server) {
+      server.close();
+    }
+    server = null;
   });
 
-  it('works', async () => {
-    const res = await get<any>('/test');
+  describe('Server responds', () => {
+    it('with valid JSON data', async () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              foo: 'bar',
+            })
+          );
+        })
+      );
+      server.listen();
+      const res = await get('/test');
 
-    expect(res).toMatchObject(
-      Result.err({
-        statusCode: 500,
-        message: 'Request failed',
-      })
-    );
+      expect(res).toMatchObject(
+        Result.ok({
+          foo: 'bar',
+        })
+      );
+    });
+
+    it('with invalid JSON data', async () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(ctx.status(200), ctx.text('bla'));
+        })
+      );
+      server.listen();
+      const res = await get('/test');
+
+      expect(res).toMatchObject(
+        Result.err({
+          message: 'Failed to parse JSON',
+          data: 'bla',
+        })
+      );
+    });
+  });
+
+  describe('Server never responded', () => {
+    it('fails', async () => {
+      const res = await get('/test');
+
+      expect(res).toMatchObject(
+        Result.err({
+          message:
+            'request to http://localhost/test failed, reason: connect ECONNREFUSED 127.0.0.1:80',
+        })
+      );
+    });
+  });
+
+  describe('Server responded with 2xx and data', () => {
+    it('works', () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(ctx.status(200), ctx.json({}));
+        })
+      );
+      server.listen();
+    });
+  });
+
+  describe('Server responded with statusCode outside 2xx range', () => {
+    it(`Returns a default message if theres no body`, async () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(ctx.status(500));
+        })
+      );
+      server.listen();
+
+      const res = await get('/test');
+
+      expect(res).toMatchObject(
+        Result.err({
+          statusCode: 500,
+          message: 'Request failed',
+        })
+      );
+    });
+
+    it(`Returns the body when there's a body in JSON format`, async () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(ctx.status(500), ctx.json({ message: 'server error' }));
+        })
+      );
+      server.listen();
+
+      const res = await get('/test');
+
+      expect(res).toMatchObject(
+        Result.err({
+          statusCode: 500,
+          message: 'server error',
+        })
+      );
+    });
+
+    it(`Returns the text body as message when there's response NOT in JSON format`, async () => {
+      server = setupServer(
+        rest.get(`http://localhost/test`, (req, res, ctx) => {
+          return res(ctx.status(500), ctx.text('text error'));
+        })
+      );
+      server.listen();
+
+      const res = await get('/test');
+
+      expect(res).toMatchObject(
+        Result.err({
+          statusCode: 500,
+          message: 'text error',
+        })
+      );
+    });
   });
 });
--- webapp/javascript/services/base.ts
+++ webapp/javascript/services/base.ts
@@ -2,15 +2,32 @@
 import { Result } from '@utils/fp';
 import basename from '../util/baseurl';
 
-interface RequestError {
+// RequestNotOkError refers to when the Response is not within the 2xx range
+interface RequestNotOkError {
   statusCode: number;
   message: string;
 }
 
-export async function get<T>(
+// RequestError refers to when the request is not completed
+// For example CORS errors or timeouts
+interface RequestIncompleteError {
+  message: string;
+}
+
+interface ResponseInvalidJSONError {
+  message: string;
+  data: any;
+}
+
+type RequestError =
+  | RequestNotOkError
+  | RequestIncompleteError
+  | ResponseInvalidJSONError;
+
+export async function get(
   path: string,
   config?: RequestInit
-): Promise<Result<T, RequestError>> {
+): Promise<Result<unknown, RequestError>> {
   let baseURL = basename();
 
   // There's no explicit baseURL configured
@@ -22,21 +39,72 @@ export async function get<T>(
 
   const address = new URL(path, baseURL).href;
 
-  const response = await fetch(address, config);
-  // TODO 1
-  // Response could fail (request never returns)
-  //
-  // TODO 2
-  // response can fail with a status code for eg
-  if (!response.ok) {
-    // Check if there's a body
-    // If so, use it
-
+  let response;
+  try {
+    response = await fetch(address, config);
+  } catch (e) {
+    // Fetch failed
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
     return Result.err({
-      statusCode: response.status,
-      message: 'Request failed',
+      message: e.message,
     });
   }
 
-  return Result.ok('hey' as unknown as T);
+  if (!response.ok) {
+    const textBody = await response.text();
+
+    // There's nothing in the body, so let's use a default message
+    if (!textBody || !textBody.length) {
+      return Result.err({
+        statusCode: response.status,
+        message: 'Request failed',
+      });
+    }
+
+    // We know there's data, so let's check if it's in JSON format
+    try {
+      const data = JSON.parse(textBody);
+
+      // We could parse the response
+      return Result.err({
+        statusCode: response.status,
+        ...data,
+      });
+    } catch (e) {
+      // We couldn't parse, but there's definitly some data
+      // We must handle this case since the go server sometimes responds with plain text
+      return Result.err({
+        statusCode: response.status,
+        message: textBody,
+      });
+    }
+  }
+
+  // Server responded with 2xx
+  const textBody = await response.text();
+
+  // There's nothing in the body
+  if (!textBody || !textBody.length) {
+    return Result.ok({
+      statusCode: response.status,
+    });
+  }
+
+  // We know there's data, so let's check if it's in JSON format
+  try {
+    const data = JSON.parse(textBody);
+
+    // We could parse the response
+    return Result.ok({
+      statusCode: response.status,
+      ...data,
+    });
+  } catch (e) {
+    // We couldn't parse, but there's definitly some data
+    return Result.err({
+      statusCode: response.status,
+      message: 'Failed to parse JSON',
+      data: textBody,
+    });
+  }
 }
