e1e57482113c06b344b7d50de1a237fa9e852488
eduardo aleixo <eduardo@pyroscope.io>
2022-01-09 11:48:56 -0300

    standalone webpack

--- scripts/webpack/webpack.adhoc.ts
+++ scripts/webpack/webpack.adhoc.ts
@@ -2,23 +2,45 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import InlineChunkHtmlPlugin from 'react-dev-utils/InlineChunkHtmlPlugin';
 import HTMLInlineCSSWebpackPlugin from 'html-inline-css-webpack-plugin';
 import path from 'path';
-import { merge } from 'webpack-merge';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { getAlias, getJsLoader, getStyleLoaders } from './shared';
 
-import common from './webpack.common';
-
-const config = merge(common, {
+const config = {
   mode: 'production',
   entry: {
     app: './webapp/javascript/adhoc.tsx',
-    styles: './webapp/sass/profile.scss',
+    styles: './webapp/sass/standalone.scss',
   },
 
   output: {
+    publicPath: '',
     // Emit to another directory other than webapp/public/assets to not have any conflicts
     path: path.resolve(__dirname, '../../dist/standalone'),
     filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    // Note: order is bottom-to-top and/or right-to-left
+    rules: [
+      ...getJsLoader(),
+      ...getStyleLoaders(),
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.svg'],
+    alias: getAlias(),
+    modules: [path.resolve('webapp'), path.resolve('node_modules')],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
     new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, `../../webapp/public/adhoc.html`),
       template: path.resolve(__dirname, `../../webapp/templates/adhoc.html`),
@@ -26,6 +48,6 @@ const config = merge(common, {
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/.*/]),
     new HTMLInlineCSSWebpackPlugin(),
   ],
-} as any);
+};
 
 export default config;
