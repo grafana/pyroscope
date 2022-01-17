d0a721dd61297a319f1847a0274accacf5a56f32
eduardo aleixo <eduardo@pyroscope.io>
2022-01-17 11:42:02 -0300

    simplify datasource test

--- cypress/integration/datasource/basic.ts
+++ cypress/integration/datasource/basic.ts
@@ -1,16 +1,17 @@
-// For these tests we can mock the requests
-// Since we are only testing the panel itself
 describe('smoke', () => {
-  it('renders the panel correctly', () => {
+  // Since we are testing the datasource plugin only
+  // We don't do any assertions against the panel
+  it('makes requests to the datasource', () => {
     cy.intercept('**/render*', {
       fixture: 'simple-golang-app-cpu.json',
     }).as('render');
 
     cy.visit('http://localhost:3000/d/single-panel/pyroscope-demo?orgId=1');
 
-    // Hopefully there should not be any visual changes here
-    cy.findByTestId('flamegraph-canvas').matchImageSnapshot(
-      'grafana-simple-golang-app-cpu'
-    );
+    cy.intercept(
+      'http://localhost:3000/api/datasources/proxy/1/render/render?format=json&from=now-5m&until=now&queryType=randomWalk&refId=A&datasource=Pyroscope&query=pyroscope.server.cpu'
+    ).as('renderRequest');
+
+    cy.wait('@renderRequest');
   });
 });
