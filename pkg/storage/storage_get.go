d108207268057df0f1fc7956dadf550c0eeb2bee
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2022-01-26 16:26:55 +0100

    fix: profile fetching when no query specified

--- pkg/storage/storage_get.go
+++ pkg/storage/storage_get.go
@@ -67,9 +67,19 @@ func (s *Storage) GetContext(ctx context.Context, gi *GetInput) (*GetOutput, err
 		return nil, fmt.Errorf("key or query must be specified")
 	}
 
-	// If a query includes 'profile_id' matcher others are ignored.
-	for _, m := range gi.Query.Matchers {
-		if m.Key == segment.ProfileIDLabelName && m.Op == flameql.OpEqual {
+	s.getTotal.Inc()
+	logger.Debug("storage.Get")
+	trace.Logf(ctx, traceCatGetKey, "%+v", gi)
+
+	if gi.Query != nil {
+		// If a query includes 'profile_id' matcher others are ignored.
+		for _, m := range gi.Query.Matchers {
+			if m.Key != segment.ProfileIDLabelName {
+				continue
+			}
+			if m.Op != flameql.OpEqual {
+				return nil, fmt.Errorf("only '=' operator is allowed for %q label", segment.ProfileIDLabelName)
+			}
 			o := GetOutput{
 				SpyName:    "unknown",
 				Units:      "samples",
@@ -84,10 +94,6 @@ func (s *Storage) GetContext(ctx context.Context, gi *GetInput) (*GetOutput, err
 			return &o, nil
 		}
 	}
-
-	s.getTotal.Inc()
-	logger.Debug("storage.Get")
-	trace.Logf(ctx, traceCatGetKey, "%+v", gi)
 
 	var (
 		resultTrie  *tree.Tree
