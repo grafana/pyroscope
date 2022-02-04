42f9adfd7db58a70ee92ca0bb03849fafec2c883
Antony Shaleynikov <shaleynikov@gmail.com>
2022-02-04 14:05:04 +0300

    fix(frontend): styled add api key form

--- webapp/javascript/components/Settings/APIKeys/APIKeyAddForm.tsx
+++ webapp/javascript/components/Settings/APIKeys/APIKeyAddForm.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@ui/Button';
 import { useHistory } from 'react-router-dom';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { createAPIKey } from '@pyroscope/redux/reducers/settings';
 import { useAppDispatch } from '@pyroscope/redux/hooks';
 import { type APIKey } from '@models/apikeys';
@@ -18,7 +18,6 @@ function APIKeyAddForm() {
   });
   const [key, setKey] = useState(undefined);
   const dispatch = useAppDispatch();
-  const history = useHistory();
 
   const handleFormChange = (event) => {
     const { name } = event.target;
@@ -30,7 +29,7 @@ function APIKeyAddForm() {
     setForm({ ...form, role: value });
   };
 
-  const handleFormSubmit = () => {
+  const handleFormSubmit = (event) => {
     const data = {
       name: form.name,
       role: form.role,
@@ -51,53 +50,61 @@ function APIKeyAddForm() {
   return (
     <>
       <h4>Add API Key</h4>
+
       <div>{form.errors.join(', ')}</div>
-      {key ? (
-        <div>
-          <div className={styles.success}>
-            Key has been successfully added. You may copy and save it somewhere
-          </div>
+      <form onSubmit={handleFormSubmit}>
+        {key ? (
           <div>
-            <input type="text" value={key} />
-            <Button icon={faCopy} />
+            <div className={styles.success}>
+              Key has been successfully added. You may copy and save it
+              somewhere
+            </div>
+            <div>
+              <input type="text" value={key} />
+              <Button icon={faCopy} />
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className={styles.addForm}>
-          <div>
-            <label htmlFor="keyName">Name:</label>
-            <input
-              id="keyName"
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleFormChange}
-            />
+        ) : (
+          <div className={styles.addForm}>
+            <div>
+              <h4>Name</h4>
+              <input
+                id="keyName"
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div>
+              <h4>Role</h4>
+              <Dropdown
+                onItemClick={(i) => handleRoleChange(i.value)}
+                value={form.role}
+                label="Role"
+              >
+                <MenuItem value="Admin">Admin</MenuItem>
+                <MenuItem value="ReadOnly">ReadOnly</MenuItem>
+                <MenuItem value="Agent">Agent</MenuItem>
+              </Dropdown>
+            </div>
+            <div>
+              <h4>Valid For (seconds):</h4>
+              <input
+                id="keyTTL"
+                name="ttlSeconds"
+                value={form.ttlSeconds}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div>
+              <Button icon={faCheck} type="submit" kind="primary">
+                Add API Key
+              </Button>
+            </div>
           </div>
-          <div>
-            <label htmlFor="keyRole">Role:</label>
-            <Dropdown
-              onItemClick={(i) => handleRoleChange(i.value)}
-              value={form.role}
-            >
-              <MenuItem value="Admin">Admin</MenuItem>
-              <MenuItem value="ReadOnly">ReadOnly</MenuItem>
-            </Dropdown>
-          </div>
-          <div>
-            <label htmlFor="keyTTL">Valid For (seconds):</label>
-            <input
-              id="keyTTL"
-              name="ttlSeconds"
-              value={form.ttlSeconds}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <button onClick={handleFormSubmit}>Add API Key</button>
-          </div>
-        </div>
-      )}
+        )}
+      </form>
     </>
   );
 }
