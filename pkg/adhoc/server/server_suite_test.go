8551df1973f3dfacfda7d2b33055e994e5ffad57
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-02-08 16:11:04 +0100

    feat: add upload support in adhoc server. (#801)
    This is an initial step towards improving the upload functionality in
    adhoc. This allows the frontend to upload profiles in other supported
    formats (frontend work still pending).
    
    This still won't work for diff uploads, that will come later.

---
+++ pkg/adhoc/server/server_suite_test.go
@@ -0,0 +1,13 @@
+package server_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestServer(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Server Suite")
+}
