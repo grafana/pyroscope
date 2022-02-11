747eccfc151ae1a343f1c96044512b64e0ec761b
eduardo aleixo <eduardo@pyroscope.io>
2022-02-11 14:41:16 -0300

    export Palette

--- webapp/lib/index.node.ts
+++ webapp/lib/index.node.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 import Flamegraph from '../javascript/components/FlameGraph/FlameGraphComponent/Flamegraph';
+import { DefaultPalette } from '../javascript/components/FlameGraph/FlameGraphComponent/colorPalette';
 
-export { Flamegraph };
+export { Flamegraph, DefaultPalette };
