59de29f80e4243736e1ed53d63ff7d6282e397ae
Serhii Samoilenko <s.samoilenko@gmail.com>
2022-02-16 09:17:04 +0200

    replace the php script with more complex logic

--- examples/php-fpm/php/index.php
+++ examples/php-fpm/php/index.php
@@ -1,9 +1,31 @@
 <?php
-$x = 1;
 
-while(true) {
-  echo "The number is: $x\n";
-  $x++;
-  sleep(1);
+function dummy()
+{
+    sleep(1);
 }
-?>
+
+function work(int $n)
+{
+    for ($i = 0; $i < $n; $i++) {
+    }
+
+    if (time() % 2 === 0) {
+        dummy();
+    }
+}
+
+function fastFunction()
+{
+    work(20000000);
+}
+
+function slowFunction()
+{
+    work(80000000);
+}
+
+for (; ;) {
+    fastFunction();
+    slowFunction();
+}
