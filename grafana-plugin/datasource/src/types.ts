87124048c194ae27c975ebffd0589ef4241f2601
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2022-02-16 20:55:46 +0100

    feat: add support for auth to grafana datasource plugin (#844)

--- grafana-plugin/datasource/src/types.ts
+++ grafana-plugin/datasource/src/types.ts
@@ -25,6 +25,6 @@ export interface MyDataSourceOptions extends DataSourceJsonData {
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
-// export interface MySecureJsonData {
-//   apiKey?: string;
-// }
+export interface MySecureJsonData {
+  apiKey?: string;
+}
