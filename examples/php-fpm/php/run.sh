d0567d31ce569511435ce146b10c348f5edd278b
Serhii Samoilenko <s.samoilenko@gmail.com>
2022-02-16 09:18:37 +0200

    run the php script on starting the container

--- examples/php-fpm/php/run.sh
+++ examples/php-fpm/php/run.sh
@@ -2,6 +2,8 @@
 
 php-fpm -D
 
+screen -d -m sh -c "SCRIPT_FILENAME=./index.php REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000"
+
 pid=$(pgrep "php-fpm: master process")
 
 pyroscope connect --pid $pid --spy-name phpspy
