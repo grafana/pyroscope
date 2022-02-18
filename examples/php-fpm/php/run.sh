12ba9e3ce9f73fde638196bfa80853cbc8abe212
Serhii Samoilenko <s.samoilenko@gmail.com>
2022-02-18 10:12:07 +0200

    use exec instead of connect

--- examples/php-fpm/php/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-php-fpm -D
-
-screen -d -m sh -c "SCRIPT_FILENAME=./index.php REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000"
-
-pid=$(pgrep "php-fpm: master process")
-
-pyroscope connect --pid $pid --spy-name phpspy
