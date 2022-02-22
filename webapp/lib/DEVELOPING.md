4d72b4d5b2bee94ad9b25e65f1424663473e9bbe
eduardo aleixo <eduardo@pyroscope.io>
2022-02-22 15:11:39 -0300

    remove webapp/lib now that it has been moved to packages

--- webapp/lib/DEVELOPING.md
+++
@@ -1,5 +0,0 @@
-# development
-
-If you are just updating this the `FlamegraphRenderer`, we recommend running `yarn run storybook`.
-
-Then access `http://localhost:6006`
--- webapp/lib/index.node.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import Flamegraph from '../../../webapp/javascript/components/FlameGraph/FlameGraphComponent/Flamegraph';
-import { DefaultPalette } from '../../../webapp/javascript/components/FlameGraph/FlameGraphComponent/colorPalette';
-
-export { Flamegraph, DefaultPalette };
