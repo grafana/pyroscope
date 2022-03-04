af91be65e7fd6860025a1db3384f8f42b4ea6b1e
eduardo aleixo <eduardo@pyroscope.io>
2022-03-04 14:31:36 -0300

    fix build

--- scripts/jest-snapshots/run-snapshots.sh
+++ scripts/jest-snapshots/run-snapshots.sh
@@ -17,5 +17,6 @@ apt install fontconfig -y
 # error eslint-import-resolver-webpack@0.13.1: The engine "node" is incompatible with this module. Expected version "^16 || ^15 || ^14 || ^13 || ^12 || ^11 || ^10 || ^9 || ^8 || ^7 || ^6". Got "17.0.1"
 # error Found incompatible module.
 yarn install --ignore-engines
+yarn build
 RUN_SNAPSHOTS=true yarn test --testNamePattern='group:snapshot' --verbose "$updateArg"
 
