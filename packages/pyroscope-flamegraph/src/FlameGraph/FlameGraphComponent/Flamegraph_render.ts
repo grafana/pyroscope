c5b4e246a9ece9fab5393d41e26c14ef013caf38
eduardo aleixo <eduardo@pyroscope.io>
2022-03-04 11:38:01 -0300

    remove unnecessary files

--- packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphComponent/Flamegraph_render.ts
+++ packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphComponent/Flamegraph_render.ts
@@ -31,7 +31,7 @@ import {
   getFormatter,
   ratioToPercent,
 } from '../../../../../webapp/javascript/util/format';
-import { fitToCanvasRect } from '../../fitMode/fitMode';
+import { fitToCanvasRect } from '../../../../../webapp/javascript/util/fitMode';
 import { createFF } from '../../../../../webapp/javascript/util/flamebearer';
 import { getRatios } from './utils';
 import {
