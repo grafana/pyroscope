361f8ffd99df204856a9c6d85dd14b3603914edb
Egor Mozheiko <egormozheiko@gmail.com>
2022-03-10 16:34:30 +0300

    feat(frontend) add confirmation modals stories

--- webapp/javascript/ui/Modals/ConfirmDelete.tsx
+++ webapp/javascript/ui/Modals/ConfirmDelete.tsx
@@ -2,7 +2,7 @@ import ShowModal from '.';
 
 function confirmDelete(object: string, onConfirm) {
   ShowModal({
-    title: `Are you sure you want to delete ${object}`,
+    title: `Are you sure you want to delete ${object}?`,
     confirmButtonText: 'Delete',
     danger: true,
     onConfirm,
