d52a94cbf509edc78a323d7acfb79e9706790290
eduardo aleixo <eduardo@pyroscope.io>
2022-03-28 14:10:44 -0300

    Revert "chore: delete unused code"
    This reverts commit cbc952d38ad312859f1d99080de2a40593cc9879.

--- packages/pyroscope-flamegraph/src/index.tsx
+++ packages/pyroscope-flamegraph/src/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-props-no-spreading */
+
 import Flamegraph from './FlameGraph/FlameGraphComponent/Flamegraph';
 import { FlamegraphRenderer } from './FlamegraphRenderer';
 
