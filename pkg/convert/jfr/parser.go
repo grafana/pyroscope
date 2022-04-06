6162ce19e29b0f828d8a238b44ebec31bf342860
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2022-04-06 11:02:05 +0200

    chore: move chunk parsing to its own function.
    That should make the linter happy.

--- pkg/convert/jfr/parser.go
+++ pkg/convert/jfr/parser.go
@@ -19,107 +19,114 @@ func ParseJFR(ctx context.Context, r io.Reader, s storage.Putter, pi *storage.Pu
 		return fmt.Errorf("unable to parse JFR format: %w", err)
 	}
 	for _, c := range chunks {
-		var event, alloc string
-		cpu := tree.New()
-		wall := tree.New()
-		inTLABObjects := tree.New()
-		inTLABBytes := tree.New()
-		outTLABObjects := tree.New()
-		outTLABBytes := tree.New()
-		for _, e := range c.Events {
-			switch e.(type) {
-			case *parser.ExecutionSample:
-				es := e.(*parser.ExecutionSample)
-				if fs := frames(es.StackTrace); fs != nil {
-					if es.State.Name == "STATE_RUNNABLE" {
-						cpu.InsertStackString(fs, 1)
-					}
-					wall.InsertStackString(fs, 1)
-				}
-			case *parser.ObjectAllocationInNewTLAB:
-				oa := e.(*parser.ObjectAllocationInNewTLAB)
-				if fs := frames(oa.StackTrace); fs != nil {
-					inTLABObjects.InsertStackString(fs, 1)
-					inTLABBytes.InsertStackString(fs, uint64(oa.TLABSize))
-				}
-			case *parser.ObjectAllocationOutsideTLAB:
-				oa := e.(*parser.ObjectAllocationOutsideTLAB)
-				if fs := frames(oa.StackTrace); fs != nil {
-					outTLABObjects.InsertStackString(fs, 1)
-					outTLABBytes.InsertStackString(fs, uint64(oa.AllocationSize))
-				}
-			case *parser.ActiveSetting:
-				as := e.(*parser.ActiveSetting)
-				switch as.Name {
-				case "event":
-					event = as.Value
-				case "alloc":
-					alloc = as.Value
-				}
-			}
-		}
-		labels := pi.Key.Labels()
-		prefix := labels["__name__"]
-		if event == "cpu" || event == "itimer" || event == "wall" {
-			profile := event
-			if event == "wall" {
-				profile = "cpu"
-			}
-			labels["__name__"] = prefix + "." + profile
-			pi.Key = segment.NewKey(labels)
-			pi.Val = cpu
-			pi.Units = "samples"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
-		}
-		if event == "wall" {
-			labels["__name__"] = prefix + "." + event
-			pi.Key = segment.NewKey(labels)
-			pi.Val = wall
-			pi.Units = "samples"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
-		}
-		if alloc != "" {
-			labels["__name__"] = prefix + ".alloc_in_new_tlab_objects"
-			pi.Key = segment.NewKey(labels)
-			pi.Val = inTLABObjects
-			pi.Units = "objects"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
-			labels["__name__"] = prefix + ".alloc_in_new_tlab_bytes"
-			pi.Key = segment.NewKey(labels)
-			pi.Val = inTLABObjects
-			pi.Units = "bytes"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
-			labels["__name__"] = prefix + ".alloc_outside_tlab_objects"
-			pi.Key = segment.NewKey(labels)
-			pi.Val = inTLABObjects
-			pi.Units = "objects"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
-			labels["__name__"] = prefix + ".alloc_outside_tlab_bytes"
-			pi.Key = segment.NewKey(labels)
-			pi.Val = inTLABObjects
-			pi.Units = "bytes"
-			pi.AggregationType = "sum"
-			if putErr := s.Put(ctx, pi); err != nil {
-				err = multierror.Append(err, putErr)
-			}
+		if pErr := parse(ctx, c, s, pi); pErr != nil {
+			err = multierror.Append(err, pErr)
 		}
 	}
 	pi.Val = nil
+	return err
+}
+
+func parse(ctx context.Context, c parser.Chunk, s storage.Putter, pi *storage.PutInput) (err error) {
+	var event, alloc string
+	cpu := tree.New()
+	wall := tree.New()
+	inTLABObjects := tree.New()
+	inTLABBytes := tree.New()
+	outTLABObjects := tree.New()
+	outTLABBytes := tree.New()
+	for _, e := range c.Events {
+		switch e.(type) {
+		case *parser.ExecutionSample:
+			es := e.(*parser.ExecutionSample)
+			if fs := frames(es.StackTrace); fs != nil {
+				if es.State.Name == "STATE_RUNNABLE" {
+					cpu.InsertStackString(fs, 1)
+				}
+				wall.InsertStackString(fs, 1)
+			}
+		case *parser.ObjectAllocationInNewTLAB:
+			oa := e.(*parser.ObjectAllocationInNewTLAB)
+			if fs := frames(oa.StackTrace); fs != nil {
+				inTLABObjects.InsertStackString(fs, 1)
+				inTLABBytes.InsertStackString(fs, uint64(oa.TLABSize))
+			}
+		case *parser.ObjectAllocationOutsideTLAB:
+			oa := e.(*parser.ObjectAllocationOutsideTLAB)
+			if fs := frames(oa.StackTrace); fs != nil {
+				outTLABObjects.InsertStackString(fs, 1)
+				outTLABBytes.InsertStackString(fs, uint64(oa.AllocationSize))
+			}
+		case *parser.ActiveSetting:
+			as := e.(*parser.ActiveSetting)
+			switch as.Name {
+			case "event":
+				event = as.Value
+			case "alloc":
+				alloc = as.Value
+			}
+		}
+	}
+	labels := pi.Key.Labels()
+	prefix := labels["__name__"]
+	if event == "cpu" || event == "itimer" || event == "wall" {
+		profile := event
+		if event == "wall" {
+			profile = "cpu"
+		}
+		labels["__name__"] = prefix + "." + profile
+		pi.Key = segment.NewKey(labels)
+		pi.Val = cpu
+		pi.Units = "samples"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+	}
+	if event == "wall" {
+		labels["__name__"] = prefix + "." + event
+		pi.Key = segment.NewKey(labels)
+		pi.Val = wall
+		pi.Units = "samples"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+	}
+	if alloc != "" {
+		labels["__name__"] = prefix + ".alloc_in_new_tlab_objects"
+		pi.Key = segment.NewKey(labels)
+		pi.Val = inTLABObjects
+		pi.Units = "objects"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+		labels["__name__"] = prefix + ".alloc_in_new_tlab_bytes"
+		pi.Key = segment.NewKey(labels)
+		pi.Val = inTLABObjects
+		pi.Units = "bytes"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+		labels["__name__"] = prefix + ".alloc_outside_tlab_objects"
+		pi.Key = segment.NewKey(labels)
+		pi.Val = inTLABObjects
+		pi.Units = "objects"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+		labels["__name__"] = prefix + ".alloc_outside_tlab_bytes"
+		pi.Key = segment.NewKey(labels)
+		pi.Val = inTLABObjects
+		pi.Units = "bytes"
+		pi.AggregationType = "sum"
+		if putErr := s.Put(ctx, pi); err != nil {
+			err = multierror.Append(err, putErr)
+		}
+	}
 	return err
 }
 
