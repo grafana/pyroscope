54842ef1cfb0474a0c10570a8adb430c75f92291
eduardo aleixo <eduardo@pyroscope.io>
2022-04-07 20:48:56 -0300

    chore(examples): basic POC of nodejs pull mode

---
+++ examples/nodejs/pull/express/index.js
@@ -0,0 +1,78 @@
+/* eslint-disable */
+const Pyroscope = require('@pyroscope/nodejs');
+
+const port = process.env['PORT'] || 3000;
+
+const region = process.env['REGION'] || 'default';
+
+const express = require('express');
+const morgan = require('morgan');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Available routes are: /bike, /car, /scooter');
+});
+
+const genericSearchHandler = (p) => (req, res) => {
+  const time = +new Date() + p * 1000;
+  let i = 0;
+  while (+new Date() < time) {
+    i = i + Math.random();
+  }
+  res.send('Vehicle found');
+};
+
+function carSearchHandler() {
+  return genericSearchHandler(1);
+}
+
+function scooterSearchHandler() {
+  return genericSearchHandler(0.25);
+}
+
+Pyroscope.init({
+  autoStart: false,
+  server: 'http://pyroscope:4040',
+  tags: { region },
+});
+
+app.get('/debug/pprof/profile', async function handler(req, res) {
+  console.log('Collecting Cpu for', req.query.seconds);
+  try {
+    const p = await Pyroscope.collectCpu(req.query.seconds);
+    res.send(p);
+  } catch (e) {
+    console.error('Error collecting cpu', e);
+    res.sendStatus(500);
+  }
+});
+
+/***********************/
+/*      Rideshare      */
+/***********************/
+app.get('/bike', function bikeSearchHandler(req, res) {
+  return genericSearchHandler(0.5)(req, res);
+});
+app.get('/car', carSearchHandler());
+app.get('/scooter', scooterSearchHandler());
+
+setInterval(() => {
+  fetch(`http://localhost:${port}/car`);
+}, 1800);
+
+setInterval(() => {
+  fetch(`http://localhost:${port}/bike`);
+}, 5000);
+
+setInterval(() => {
+  fetch(`http://localhost:${port}/scooter`);
+}, 1000);
+
+app.listen(port, () => {
+  console.log(
+    `Server has started on port ${port}, use http://localhost:${port}`
+  );
+});
