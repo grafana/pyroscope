0f0023a27c7092ec42f35cff9e6fba07eba6102c
eduardo aleixo <eduardo@pyroscope.io>
2022-04-13 12:40:27 -0300

    .

--- webapp/javascript/redux/async-thunk.ts
+++ webapp/javascript/redux/async-thunk.ts
@@ -1,20 +1,17 @@
+/* eslint-disable import/prefer-default-export */
 import {
   createAsyncThunk as libCreateAsyncThunk,
-  AsyncThunkPayloadCreator,
-  AsyncThunk,
   SerializedError,
 } from '@reduxjs/toolkit';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ThunkAPIConfig {}
-
-// https://github.com/reduxjs/redux-toolkit/issues/486
-// eslint-disable-next-line import/prefer-default-export
-export const createAsyncThunk = <Returned, ThunkArg = any>(
-  type: string,
-  thunk: AsyncThunkPayloadCreator<Returned, ThunkArg>
-): AsyncThunk<Returned, ThunkArg, ThunkAPIConfig> => {
-  return libCreateAsyncThunk<Returned, ThunkArg, ThunkAPIConfig>(type, thunk, {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const createAsyncThunk: typeof libCreateAsyncThunk = (
+  ...args: Parameters<typeof libCreateAsyncThunk>
+) => {
+  const [typePrefix, payloadCreator, options] = args;
+  return libCreateAsyncThunk(typePrefix, payloadCreator, {
+    ...options,
     // Return the error as is (without)
     // So that the components can use features like instanceof, and accessing other fields that would otherwise be ignored
     // https://github.com/reduxjs/redux-toolkit/blob/db0d7dc20939b62f8c59631cc030575b78642296/packages/toolkit/src/createAsyncThunk.ts#L94
