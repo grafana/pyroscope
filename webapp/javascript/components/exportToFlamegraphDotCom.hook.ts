422ea82fd20a64eda44ae62821f67a89c129a594
EgorMozheiko <90687109+EgorMozheiko@users.noreply.github.com>
2022-04-14 19:01:31 +0300

    feat(webapp): add modal for custom export name (#965)
    * feat(webapp): add modal for custom export name

--- webapp/javascript/components/exportToFlamegraphDotCom.hook.ts
+++ webapp/javascript/components/exportToFlamegraphDotCom.hook.ts
@@ -6,12 +6,15 @@ import handleError from '@webapp/util/handleError';
 export default function useExportToFlamegraphDotCom(flamebearer?: Profile) {
   const dispatch = useAppDispatch();
 
-  return async () => {
+  return async (name?: string) => {
     if (!flamebearer) {
       return '';
     }
 
-    const res = await shareWithFlamegraphDotcom({ flamebearer });
+    const res = await shareWithFlamegraphDotcom({
+      flamebearer,
+      name,
+    });
 
     if (res.isErr) {
       handleError(dispatch, 'Failed to export to flamegraph.com', res.error);
--- webapp/javascript/ui/Modals/index.ts
+++ webapp/javascript/ui/Modals/index.ts
@@ -1,34 +1,74 @@
-import Swal, { SweetAlertOptions } from 'sweetalert2';
+import Swal, { SweetAlertInput, SweetAlertOptions } from 'sweetalert2';
+
+import styles from './Modal.module.css';
 
 const defaultParams: Partial<SweetAlertOptions> = {
   showCancelButton: true,
   allowOutsideClick: true,
   backdrop: true,
+  focusConfirm: false,
+  customClass: {
+    popup: styles.popup,
+    title: styles.title,
+    input: styles.input,
+    confirmButton: styles.button,
+    denyButton: styles.button,
+    cancelButton: styles.button,
+    validationMessage: styles.validationMessage,
+  },
+  inputAttributes: {
+    required: 'true',
+  },
 };
 
-type ShowModalParams = {
+export type ShowModalParams = {
   title: string;
   confirmButtonText: string;
-  danger: boolean;
-  onConfirm: ShamefulAny;
+  type: 'danger' | 'normal';
+  onConfirm?: ShamefulAny;
+  input?: SweetAlertInput;
+  inputValue?: string;
+  inputLabel?: string;
+  inputPlaceholder?: string;
+  validationMessage?: string;
 };
 
-const ShowModal = ({
+const ShowModal = async ({
   title,
   confirmButtonText,
-  danger,
+  type,
   onConfirm,
+  input,
+  inputValue,
+  inputLabel,
+  inputPlaceholder,
+  validationMessage,
 }: ShowModalParams) => {
-  Swal.fire({
+  const { isConfirmed, value } = await Swal.fire({
     title,
     confirmButtonText,
-    confirmButtonColor: danger ? '#dc3545' : '#0074d9',
+    input,
+    inputLabel,
+    inputPlaceholder,
+    inputValue,
+    validationMessage,
+    confirmButtonColor: getButtonStyleFromType(type),
     ...defaultParams,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      onConfirm();
-    }
   });
+
+  if (isConfirmed) {
+    onConfirm(value);
+  }
+
+  return value;
 };
+
+function getButtonStyleFromType(type: 'danger' | 'normal') {
+  if (type === 'danger') {
+    return '#dc3545'; // red
+  }
+
+  return '#0074d9'; // blue
+}
 
 export default ShowModal;
