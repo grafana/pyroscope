e8fa9cb817a3d5b02630ed3aa9bc203edb001b30
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-04-15 17:24:17 +0300

    fix(maybe): highlight cy test

--- cypress/integration/webapp/basic.ts
+++ cypress/integration/webapp/basic.ts
@@ -321,6 +321,8 @@ describe('basic test', () => {
 
       cy.findByTestId('flamegraph-highlight').should('not.be.visible');
 
+      cy.wait(500);
+
       cy.waitForFlamegraphToRender().trigger('mousemove', 0, 0);
       cy.findByTestId('flamegraph-highlight').should('be.visible');
     });
