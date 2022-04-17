210cca7eb256b7b097408788bdb72c31a57adfda
Dmitry Filimonov <dmitry@pyroscope.io>
2022-04-16 23:03:28 -0700

    comments

--- pkg/server/httputils/iface.go
+++ pkg/server/httputils/iface.go
@@ -6,13 +6,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// TODO(petethepig): this interface is pretty large, we can probably simplify it
 type Helper interface {
+	// these methods were originally extracted from api package
 	MustJSON(w http.ResponseWriter, v interface{})
 	HandleError(w http.ResponseWriter, r *http.Request, logger logrus.FieldLogger, err error)
 	Error(w http.ResponseWriter, logger logrus.FieldLogger, err error)
 	IdFromRequest(r *http.Request) (uint, error)
 	Logger(r *http.Request, logger logrus.FieldLogger) logrus.FieldLogger
 
+	// these methods were originally extracted from server package
 	WriteResponseJSON(logger logrus.FieldLogger, w http.ResponseWriter, res interface{})
 	WriteResponseFile(logger logrus.FieldLogger, w http.ResponseWriter, filename string, content []byte)
 	WriteInvalidMethodError(logger logrus.FieldLogger, w http.ResponseWriter)
