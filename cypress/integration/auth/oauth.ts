1a9684626c8cb0097fcc163a685a705f2d0b09ac
Antony Shaleynikov <shaleynikov@gmail.com>
2022-04-25 17:06:07 +0300

    feat: added test on password tab in oauth scenario

--- cypress/integration/auth/oauth.ts
+++ cypress/integration/auth/oauth.ts
@@ -6,6 +6,8 @@ describe('oauth with mock enabled', () => {
     cy.visit('/login');
 
     cy.get('#gitlab-link').should('be.visible');
+    cy.get('#google-link').should('not.exist');
+    cy.get('#github-link').should('not.exist');
 
     cy.get('#gitlab-link').click();
     cy.url().should('contain', '/?query=');
@@ -15,6 +17,11 @@ describe('oauth with mock enabled', () => {
     // cy.get('.spinner-container.loading').should('exist');
     cy.get('.spinner-container').should('exist');
     cy.intercept('/api/user');
+
+    cy.findByTestId('sidebar-settings').click();
+
+    cy.findByText('Change Password').should('not.exist');
+
     cy.get('li.pro-menu-item').contains('Sign out').click({ force: true });
     cy.url().should('contain', '/login');
   });
