bc950f122f73b53075d5bdffca34391c49ca993c
Antony Shaleynikov <shaleynikov@gmail.com>
2022-05-17 18:34:48 +0300

    chore: Added debounce to refresh button

--- webapp/javascript/components/RefreshButton.tsx
+++ webapp/javascript/components/RefreshButton.tsx
@@ -4,15 +4,15 @@ import Button from '@webapp/ui/Button';
 import { faSyncAlt } from '@fortawesome/free-solid-svg-icons/faSyncAlt';
 import { useAppDispatch } from '@webapp/redux/hooks';
 import { actions } from '@webapp/redux/reducers/continuous';
+import debounce from 'lodash.debounce';
 
 function RefreshButton() {
   const dispatch = useAppDispatch();
+  const refresh = debounce(() => {
+    dispatch(actions.refresh());
+  }, 500);
   return (
-    <Button
-      data-testid="refresh-btn"
-      icon={faSyncAlt}
-      onClick={() => dispatch(actions.refresh())}
-    />
+    <Button data-testid="refresh-btn" icon={faSyncAlt} onClick={refresh} />
   );
 }
 
