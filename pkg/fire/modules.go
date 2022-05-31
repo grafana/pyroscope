a7ed0b8dbdc36d150e2e3dc0d68d6cee0fda6daf
Cyril Tovena <cyril.tovena@gmail.com>
2022-05-31 17:05:28 +0200

    Add go build and the first skeleton

---
+++ pkg/fire/modules.go
@@ -0,0 +1,30 @@
+package fire
+
+// The various modules that make up Fire.
+const (
+	All         string = "all"
+	Agent       string = "agent"
+	Distributor string = "distributor"
+
+	// Ring                     string = "ring"
+	// RuntimeConfig            string = "runtime-config"
+	// Overrides                string = "overrides"
+	// OverridesExporter        string = "overrides-exporter"
+	// TenantConfigs            string = "tenant-configs"
+	// Server                   string = "server"
+	// Ingester                 string = "ingester"
+	// Querier                  string = "querier"
+	// IngesterQuerier          string = "ingester-querier"
+	// QueryFrontend            string = "query-frontend"
+	// QueryFrontendTripperware string = "query-frontend-tripperware"
+	// RulerStorage             string = "ruler-storage"
+	// Ruler                    string = "ruler"
+	// Store                    string = "store"
+	// TableManager             string = "table-manager"
+	// MemberlistKV             string = "memberlist-kv"
+	// Compactor                string = "compactor"
+	// IndexGateway             string = "index-gateway"
+	// IndexGatewayRing         string = "index-gateway-ring"
+	// QueryScheduler           string = "query-scheduler"
+	// UsageReport              string = "usage-report"
+)
