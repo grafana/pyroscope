f8c0814f089a6d9321a0f1fc7cbf6c678a9d4438
Cyril Tovena <cyril.tovena@gmail.com>
2022-06-09 16:23:06 +0200

    Remove vendor and render flamegraph

--- vendor/github.com/opentracing-contrib/go-grpc/package.go
+++
@@ -1,5 +0,0 @@
-// Package otgrpc provides OpenTracing support for any gRPC client or server.
-//
-// See the README for simple usage examples:
-// https://github.com/opentracing-contrib/go-grpc/README.md
-package otgrpc
