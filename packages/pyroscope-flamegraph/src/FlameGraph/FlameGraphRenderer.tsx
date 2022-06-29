949052d6db23daedde589a6eaa7c06a4db527cab
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-06-29 17:15:48 +0300

    fix: zoom/focus reset on changing selected node [refactored] (#1184)
    * fix: reset focus/zoom when the zoomed/selected node has changed
    
    Co-authored-by: eduardo aleixo <eduardo@pyroscope.io>

--- packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphRenderer.tsx
+++ packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphRenderer.tsx
@@ -8,7 +8,7 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import clsx from 'clsx';
 import { Maybe } from 'true-myth';
-import { Flamebearer, Profile } from '@pyroscope/models';
+import { createFF, Flamebearer, Profile } from '@pyroscope/models';
 import Graph from './FlameGraphComponent';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: let's move this to typescript some time in the future
@@ -114,10 +114,14 @@ class FlameGraphRenderer extends React.Component<
   FlamegraphRendererProps,
   FlamegraphRendererState
 > {
-  initialFlamegraphState = {
+  resetFlamegraphState = {
     focusedNode: Maybe.nothing<Node>(),
     zoom: Maybe.nothing<Node>(),
   };
+
+  // TODO: At some point the initial state may be set via the user
+  // Eg when sharing a specific node
+  initialFlamegraphState = this.resetFlamegraphState;
 
   constructor(props: FlamegraphRendererProps) {
     super(props);
@@ -150,15 +154,25 @@ class FlameGraphRenderer extends React.Component<
     prevProps: FlamegraphRendererProps,
     prevState: FlamegraphRendererState
   ) {
-    if (prevProps.profile !== this.props.profile) {
-      this.updateFlamebearerData();
-      return;
-    }
+    // TODO: this is a slow operation
+    const prevFlame = mountFlamebearer(prevProps);
+    const currFlame = mountFlamebearer(this.props);
 
-    const previousFlamebearer = prevProps.flamebearer;
-    const actualFlamebearer = this.props.flamebearer;
-    if (previousFlamebearer !== actualFlamebearer) {
-      this.updateFlamebearerData();
+    // This is a poor heuristic, but it should work most of the times
+    if (prevFlame.numTicks !== currFlame.numTicks) {
+      const newConfigs = this.calcNewConfigs(prevFlame, currFlame);
+
+      // Batch these updates to not do unnecessary work
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState({
+        flamebearer: currFlame,
+
+        flamegraphConfigs: {
+          ...this.state.flamegraphConfigs,
+          ...newConfigs,
+        },
+      });
+      return;
     }
 
     // flamegraph configs changed
@@ -166,6 +180,46 @@ class FlameGraphRenderer extends React.Component<
       this.updateFlamegraphDirtiness();
     }
   }
+
+  // Calculate what should be the new configs
+  // It checks if the zoom/selectNode still points to the same node
+  // If not, it resets to the resetFlamegraphState
+  calcNewConfigs = (prevFlame: Flamebearer, currFlame: Flamebearer) => {
+    const newConfigs = this.state.flamegraphConfigs;
+
+    // This is a simple heuristic based on the name
+    // It does not account for eg recursive calls
+    const isSameNode = (f: Flamebearer, f2: Flamebearer, s: Maybe<Node>) => {
+      // TODO: don't use createFF directly
+      const getBarName = (f: Flamebearer, i: number, j: number) => {
+        return f.names[createFF(f.format).getBarName(f.levels[i], j)];
+      };
+
+      // No node is technically the same node
+      if (s.isNothing) {
+        return true;
+      }
+
+      return (
+        getBarName(f, s.value.i, s.value.j) ===
+        getBarName(f2, s.value.i, s.value.j)
+      );
+    };
+
+    // Reset zoom
+    const currZoom = this.state.flamegraphConfigs.zoom;
+    if (!isSameNode(prevFlame, currFlame, currZoom)) {
+      newConfigs.zoom = this.resetFlamegraphState.zoom;
+    }
+
+    // Reset focused node
+    const currFocusedNode = this.state.flamegraphConfigs.focusedNode;
+    if (!isSameNode(prevFlame, currFlame, currFocusedNode)) {
+      newConfigs.focusedNode = this.resetFlamegraphState.focusedNode;
+    }
+
+    return newConfigs;
+  };
 
   handleSearchChange = (e: string) => {
     this.setState({
@@ -293,11 +347,6 @@ class FlameGraphRenderer extends React.Component<
     return this.props.showToolbar !== undefined ? this.props.showToolbar : true;
   }
 
-  updateFlamebearerData() {
-    const flamebearer = mountFlamebearer(this.props);
-    this.setState({ flamebearer });
-  }
-
   render = () => {
     // This is necessary because the order switches depending on single vs comparison view
     const tablePane = (
@@ -394,9 +443,7 @@ class FlameGraphRenderer extends React.Component<
               isFlamegraphDirty={this.state.isFlamegraphDirty}
               selectedNode={this.state.flamegraphConfigs.zoom}
               highlightQuery={this.state.highlightQuery}
-              onFocusOnSubtree={(i, j) => {
-                this.onFocusOnNode(i, j);
-              }}
+              onFocusOnSubtree={this.onFocusOnNode}
             />
           )}
           {this.props.children}
