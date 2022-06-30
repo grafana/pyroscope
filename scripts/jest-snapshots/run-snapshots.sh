9b5b8b51dfbfa5a2d4679b41691997d62cda9a73
eduardo aleixo <eduardo@pyroscope.io>
2022-06-30 19:42:53 -0300

    .

--- scripts/jest-snapshots/run-snapshots.sh
+++ scripts/jest-snapshots/run-snapshots.sh
@@ -9,15 +9,19 @@ if [ "$UPDATE_SNAPSHOTS" = true ]; then
   updateArg="-u"
 fi
 
+
 apt update -y
 apt install fontconfig -y
 
-# TODO conditionally install these
+if [ $(dpkg --print-architecture) = "arm64" ]; then
 # The chromium binary is not available for arm64.
-apt install chromium -y
-# To build canvas from source
-apt install python pkg-config -y
-apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+  apt install chromium -y
+
+  # To build canvas from source
+  apt install python pkg-config -y
+  apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+fi
+
 # ignore-engines due to
 # warning webpack-plugin-serve@1.5.0: Invalid bin field for "webpack-plugin-serve".
 # error eslint-import-resolver-webpack@0.13.1: The engine "node" is incompatible with this module. Expected version "^16 || ^15 || ^14 || ^13 || ^12 || ^11 || ^10 || ^9 || ^8 || ^7 || ^6". Got "17.0.1"
