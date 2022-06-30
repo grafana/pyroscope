43794b9915e5b6e6beeab5de731a81ad88ac5fe2
eduardo aleixo <eduardo@pyroscope.io>
2022-06-30 19:33:10 -0300

    chore: support running jest snapshots in apple sillicon

--- scripts/jest-snapshots/run-snapshots.sh
+++ scripts/jest-snapshots/run-snapshots.sh
@@ -12,6 +12,12 @@ fi
 apt update -y
 apt install fontconfig -y
 
+# TODO conditionally install these
+# The chromium binary is not available for arm64.
+apt install chromium -y
+# To build canvas from source
+apt install python pkg-config -y
+apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 # ignore-engines due to
 # warning webpack-plugin-serve@1.5.0: Invalid bin field for "webpack-plugin-serve".
 # error eslint-import-resolver-webpack@0.13.1: The engine "node" is incompatible with this module. Expected version "^16 || ^15 || ^14 || ^13 || ^12 || ^11 || ^10 || ^9 || ^8 || ^7 || ^6". Got "17.0.1"
