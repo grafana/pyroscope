406a60395edacc528784b1fec168eed6873c2260
Christian Simon <simon@swine.de>
2022-07-05 08:40:09 +0000

    Add sample deduplication in memory

--- pkg/firedb/head_test.go
+++ pkg/firedb/head_test.go
@@ -3,8 +3,10 @@ package firedb
 import (
 	"context"
 	"testing"
+	"time"
 
 	profilev1 "github.com/grafana/fire/pkg/gen/google/v1"
+	"github.com/segmentio/parquet-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -72,7 +74,7 @@ func TestHeadIngestFunctions(t *testing.T) {
 	require.NoError(t, head.Ingest(context.Background(), newProfileBaz()))
 
 	require.Equal(t, 3, len(head.functions.slice))
-	helper := &functionHelper{}
+	helper := &functionsHelper{}
 	assert.Equal(t, functionsKey{Name: 1}, helper.key(head.functions.slice[0]))
 	assert.Equal(t, functionsKey{Name: 2}, helper.key(head.functions.slice[1]))
 	assert.Equal(t, functionsKey{Name: 3}, helper.key(head.functions.slice[2]))
@@ -98,4 +100,72 @@ func TestHeadIngestStrings(t *testing.T) {
 	require.NoError(t, head.strings.ingest(ctx, newProfileBaz().StringTable, r))
 	require.Equal(t, []string{"", "func_a", "func_b", "func_c"}, head.strings.slice)
 	require.Equal(t, stringConversionTable{0, 3}, r.strings)
+}
+
+func TestHeadIngestRealProfiles(t *testing.T) {
+	var (
+		profilePaths = []string{
+			"testdata/heap",
+			"testdata/profile",
+		}
+	)
+
+	head := NewHead()
+	ctx := context.Background()
+
+	for range make([]struct{}, 100) {
+		for pos := range profilePaths {
+			profile := parseProfile(t, profilePaths[pos])
+			require.NoError(t, head.Ingest(ctx, profile))
+		}
+	}
+
+	require.NoError(t, head.WriteTo(ctx, t.TempDir()))
+
+	time.Sleep(time.Hour)
+
+	t.Logf("strings=%d samples=%d", len(head.strings.slice), len(head.samples.slice[0].Values))
+}
+
+func TestDelta(t *testing.T) {
+
+	sc := parquet.SchemaOf(struct {
+		Values []struct {
+			Value int `parquet:",delta"`
+		}
+	}{})
+
+	c, err := sc.Lookup("values", "value")
+
+	t.Logf("%+#v %+#v", c, err)
+
+	sc2 := parquet.SchemaOf(struct {
+		Values []int `parquet:","`
+	}{})
+	t.Logf("%s", sc2)
+
+}
+
+func BenchmarkHeadIngestProfiles(t *testing.B) {
+	var (
+		profilePaths = []string{
+			"testdata/heap",
+			"testdata/profile",
+		}
+		profileCount = 0
+	)
+
+	head := NewHead()
+	ctx := context.Background()
+
+	t.ReportAllocs()
+
+	for n := 0; n < t.N; n++ {
+		for pos := range profilePaths {
+			p := parseProfile(t, profilePaths[pos])
+			head.Ingest(ctx, p)
+			profileCount++
+		}
+	}
+
 }
