e834e9a6f317f9776102d4e786f9efa607e72d27
Christian Simon <simon@swine.de>
2022-07-08 08:59:24 +0100

    Move Sample, Profile, Stacktrace to schema

--- pkg/firedb/schemas/v1/schema.go
+++ pkg/firedb/schemas/v1/schema.go
@@ -1,10 +1,47 @@
 package v1
 
 import (
+	"github.com/google/uuid"
 	"github.com/segmentio/parquet-go"
 
+	"github.com/prometheus/common/model"
+
+	profilev1 "github.com/grafana/fire/pkg/gen/google/v1"
 	fireparquet "github.com/grafana/fire/pkg/parquet"
 )
+
+type Sample struct {
+	StacktraceID uint64             `parquet:","`
+	Values       []int64            `parquet:","`
+	Labels       []*profilev1.Label `parquet:","`
+}
+
+type Profile struct {
+	// A unique UUID per ingested profile
+	ID uuid.UUID `parquet:",uuid"`
+
+	// SeriesRefs reference the underlying series in the TSDB index
+	SeriesRefs []model.Fingerprint `parquet:","`
+
+	// The set of samples recorded in this profile.
+	Samples []*Sample `parquet:","`
+
+	// frames with Function.function_name fully matching the following
+	// regexp will be dropped from the samples, along with their successors.
+	DropFrames int64 `parquet:","` // Index into string table.
+	// frames with Function.function_name fully matching the following
+	// regexp will be kept, even if it matches drop_frames.
+	KeepFrames int64 `parquet:","` // Index into string table.
+	// Time of collection (UTC) represented as nanoseconds past the epoch.
+	TimeNanos int64 `parquet:",delta"`
+	// Duration of the profile, if a duration makes sense.
+	DurationNanos int64 `parquet:",delta"`
+	// Freeform text associated to the profile.
+	Comment []int64 `parquet:"Comments,"` // Indices into string table.
+	// Index into the string table of the type of the preferred sample
+	// value. If unset, clients should default to the last sample value.
+	DefaultSampleType int64 `parquet:","`
+}
 
 func ProfilesSchema() *parquet.Schema {
 	var (
@@ -41,4 +78,8 @@ func ProfilesSchema() *parquet.Schema {
 		fireparquet.NewGroupField("Comments", parquet.Repeated(stringRef)),
 		fireparquet.NewGroupField("DefaultSampleType", parquet.Int(64)),
 	})
+}
+
+type Stacktrace struct {
+	LocationIDs []uint64 `parquet:","`
 }
