a243e558cb7abfb1c3c2eb1075a30b3f0307eb77
Cyril Tovena <cyril.tovena@gmail.com>
2022-07-12 13:13:05 +0200

    Intern pprof labels and avoid allocations.

---
+++ pkg/firedb/profile_labels.go
@@ -0,0 +1,61 @@
+package firedb
+
+import (
+	"sync"
+
+	profilev1 "github.com/grafana/fire/pkg/gen/google/v1"
+)
+
+type labelCache struct {
+	labels map[labelKey]*profilev1.Label
+	rw     sync.RWMutex
+}
+
+func (lc *labelCache) init() {
+	lc.labels = make(map[labelKey]*profilev1.Label)
+}
+
+func (lc *labelCache) rewriteLabels(t stringConversionTable, in []*profilev1.Label) []*profilev1.Label {
+	lc.rw.RLock()
+	defer lc.rw.RUnlock()
+	for i, l := range in {
+		k := labelKey{
+			Key:     t[l.Key],
+			NumUnit: t[l.NumUnit],
+			Str:     t[l.Str],
+			Num:     l.Num,
+		}
+		l, ok := lc.labels[k]
+		if ok {
+			in[i] = l
+			continue
+		}
+		lc.rw.RUnlock()
+		lc.rw.Lock()
+		l, ok = lc.labels[k]
+		if !ok {
+			l = &profilev1.Label{
+				Key:     k.Key,
+				NumUnit: k.NumUnit,
+				Str:     k.Str,
+				Num:     k.Num,
+			}
+			lc.labels[k] = l
+			in[i] = l
+			lc.rw.Unlock()
+			lc.rw.RLock()
+			continue
+		}
+		lc.rw.Unlock()
+		lc.rw.RLock()
+		in[i] = l
+	}
+	return in
+}
+
+type labelKey struct {
+	Key     int64
+	Str     int64
+	Num     int64
+	NumUnit int64
+}
