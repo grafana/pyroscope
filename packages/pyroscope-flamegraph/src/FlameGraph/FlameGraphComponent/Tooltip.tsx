47fecc86f9c2443cd394f7a072282efdf4184d5c
dogfrogfog <maksim.hodasevich@gmail.com>
2022-07-12 14:34:03 +0200

    add new tooltip design

--- packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphComponent/Tooltip.tsx
+++ packages/pyroscope-flamegraph/src/FlameGraph/FlameGraphComponent/Tooltip.tsx
@@ -12,6 +12,8 @@ import {
 
 import { DefaultPalette, FlamegraphPalette } from './colorPalette';
 import styles from './Tooltip.module.scss';
+import Icon from '@webapp/ui/Icon';
+import { faHandPointRight } from '@fortawesome/free-solid-svg-icons/faHandPointRight';
 
 type xyToDataSingle = (
   x: number,
