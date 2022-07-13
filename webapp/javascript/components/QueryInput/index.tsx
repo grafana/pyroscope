a3d8cbf4360f9c036472c69b82fd052cee436ffe
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-07-13 14:01:15 +0300

    feat: restyling pills

--- webapp/javascript/components/QueryInput/index.tsx
+++ webapp/javascript/components/QueryInput/index.tsx
@@ -14,7 +14,7 @@ import { Query, brandQuery } from '@webapp/models/query';
 import Menu from './Menu';
 import styles from './QueryInput.module.scss';
 
-const inlineSvg = `<svg viewPort="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg"> <line x1="1" y1="11" x2="11" y2="1" stroke="white" stroke-width="2"/> <line x1="1" y1="1" x2="11" y2="11" stroke="white" stroke-width="2"/> </svg>`;
+const inlineSvg = `<svg viewPort="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg"> <line x1="1" y1="11" x2="11" y2="1" stroke-width="2"/> <line x1="1" y1="1" x2="11" y2="11" stroke-width="2"/></svg>`;
 
 interface QueryInputProps {
   initialQuery: Query;
