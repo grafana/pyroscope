060accf765ae191fb5d8dcb9074d9e4cbd730110
Dmitry Filimonov <dmitry@pyroscope.io>
2022-07-13 22:14:40 -0700

    wip diff code

--- webapp/javascript/components/Settings/index.tsx
+++ webapp/javascript/components/Settings/index.tsx
@@ -20,6 +20,91 @@ import ApiKeys from './APIKeys';
 import styles from './Settings.module.css';
 import UserAddForm from './Users/UserAddForm';
 import APIKeyAddForm from './APIKeys/APIKeyAddForm';
+import { FlamegraphRenderer } from '@pyroscope/flamegraph';
+
+import f1 from '../../../../cypress/fixtures/simple-golang-app-cpu.json';
+import f2 from '../../../../cypress/fixtures/simple-golang-app-cpu2.json';
+
+function flamebearersToTree(f1: Flamebearer, f2: Flamebearer) {
+  const lookup = {};
+  let root;
+  [f1, f2].forEach((f, fi) => {
+    for (let i = 0; i < f.levels.length; i += 1) {
+      for (let j = 0; j < f.levels[i].length; j += 4) {
+        const name = f.names[f.levels[i][j + 3]];
+        const key = i + name;
+        lookup[key] ||= { name: name, children: [], self: [], total: [] };
+        const obj = lookup[key];
+        obj.total[fi] = f.levels[i][j + 1];
+        obj.self[fi] = f.levels[i][j + 2];
+        const offset = f.levels[i][j + 0];
+        if (i === 0) {
+          root = obj;
+        } else {
+          for (let k = 0; k < f.levels[i - 1].length; k += 4) {
+            const parentName = f.names[f.levels[i - 1][k + 3]];
+            const parentKey = i - 1 + parentName;
+            const parentOffset = f.levels[i - 1][k + 0];
+            const total = f.levels[i - 1][k + 1];
+            if (offset >= parentOffset && offset < parentOffset + total) {
+              lookup[parentKey].children.push(obj);
+              break;
+            }
+          }
+        }
+      }
+    }
+  });
+
+  return root;
+}
+
+function diffFlamebearer(f1: Flamebearer, f2: Flamebearer): Flamebearer {
+  const result: Flamebearer = {
+    format: 'double',
+    numTicks: f1.numTicks + f2.numTicks,
+    leftTicks: f1.numTicks,
+    rightTicks: f2.numTicks,
+    maxSelf: 100,
+    sampleRate: 1000000,
+    names: [],
+    levels: [],
+    units: f1.units,
+    spyName: f1.spyName,
+  };
+
+  const tree = flamebearersToTree(f1, f2);
+  const processNode = (node, level, offsetLeft, offsetRight) => {
+    const { name, children, self, total } = node;
+    result.names.push(name);
+    result.levels[level] ||= [];
+    result.maxSelf = Math.max(result.maxSelf, self[0] || 0, self[1] || 0);
+    result.levels[level] = result.levels[level].concat([
+      offsetLeft,
+      total[0] || 0,
+      self[0] || 0,
+      offsetRight,
+      total[1] || 0,
+      self[1] || 0,
+      result.names.length - 1,
+    ]);
+    for (let i = 0; i < children.length; i += 1) {
+      const [ol, or] = processNode(
+        children[i],
+        level + 1,
+        offsetLeft,
+        offsetRight
+      );
+      offsetLeft += ol;
+      offsetRight += or;
+    }
+    return [total[0] || 0, total[1] || 0];
+  };
+
+  processNode(tree, 0, 0, 0);
+
+  return result;
+}
 
 function Settings() {
   const { path, url } = useRouteMatch();
@@ -101,6 +186,13 @@ function Settings() {
           <Switch>
             <Route exact path={path}>
               <Preferences />
+            </Route>
+            <Route exact path={`${path}/diff`}>
+              <FlamegraphRenderer
+                flamebearer={diffFlamebearer(f1.flamebearer, f2.flamebearer)}
+                onlyDisplay="flamegraph"
+                viewType="double"
+              />
             </Route>
             <Route path={`${path}/security`}>
               <Security />
