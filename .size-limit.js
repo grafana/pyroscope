4daceb5e75ee5b67cb344d9f17fbc55fc3bdb42f
eduardo aleixo <eduardo@pyroscope.io>
2022-07-15 14:52:12 -0300

    .

--- .size-limit.js
+++ .size-limit.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     path: ['webapp/public/assets/app.js'],
     // ugly
-    limit: '15000ms',
+    //    limit: '15000ms',
   },
   {
     path: ['webapp/public/assets/index.css'],
