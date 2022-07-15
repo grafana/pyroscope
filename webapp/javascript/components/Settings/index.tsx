df82ebc4e21729fb1d4161f533277d2bb2ded13c
Dmitry Filimonov <dmitry@pyroscope.io>
2022-07-15 14:52:59 -0700

    wip

--- webapp/javascript/components/Settings/index.tsx
+++ webapp/javascript/components/Settings/index.tsx
@@ -20,37 +20,61 @@ import ApiKeys from './APIKeys';
 import styles from './Settings.module.css';
 import UserAddForm from './Users/UserAddForm';
 import APIKeyAddForm from './APIKeys/APIKeyAddForm';
-import { FlamegraphRenderer } from '@pyroscope/flamegraph';
+import {
+  FlamegraphRenderer,
+  convertJaegerTraceToProfile,
+} from '@pyroscope/flamegraph';
 
 import f1 from '../../../../cypress/fixtures/simple-golang-app-cpu.json';
 import f2 from '../../../../cypress/fixtures/simple-golang-app-cpu2.json';
+import trace from './trace.json';
 
 function flamebearersToTree(f1: Flamebearer, f2: Flamebearer) {
   const lookup = {};
+  const lookup2 = {};
   let root;
   [f1, f2].forEach((f, fi) => {
     for (let i = 0; i < f.levels.length; i += 1) {
       for (let j = 0; j < f.levels[i].length; j += 4) {
+        const key2 = [fi, i, j].join('/');
         const name = f.names[f.levels[i][j + 3]];
-        const key = i + name;
-        lookup[key] ||= { name: name, children: [], self: [], total: [] };
-        const obj = lookup[key];
-        obj.total[fi] = f.levels[i][j + 1];
-        obj.self[fi] = f.levels[i][j + 2];
         const offset = f.levels[i][j + 0];
-        if (i === 0) {
-          root = obj;
-        } else {
-          for (let k = 0; k < f.levels[i - 1].length; k += 4) {
-            const parentName = f.names[f.levels[i - 1][k + 3]];
-            const parentKey = i - 1 + parentName;
-            const parentOffset = f.levels[i - 1][k + 0];
-            const total = f.levels[i - 1][k + 1];
+
+        let parentKey;
+        if (i !== 0) {
+          const pi = i - 1;
+          for (let k = 0; k < f.levels[pi].length; k += 4) {
+            const parentOffset = f.levels[pi][k + 0];
+            const total = f.levels[pi][k + 1];
             if (offset >= parentOffset && offset < parentOffset + total) {
-              lookup[parentKey].children.push(obj);
+              const parentKey2 = [fi, pi, k].join('/');
+              const parentObj = lookup2[parentKey2];
+              parentKey = parentObj.key;
               break;
             }
           }
+        }
+
+        const key = [parentKey || '', name].join('/');
+        console.log('key', key);
+        lookup[key] ||= {
+          name: name,
+          children: [],
+          self: [],
+          total: [],
+          key: key,
+        };
+        const obj = lookup[key];
+        obj.total[fi] ||= 0;
+        obj.total[fi] += f.levels[i][j + 1];
+        obj.self[fi] ||= 0;
+        obj.self[fi] += f.levels[i][j + 2];
+        lookup2[key2] = obj;
+        if (parentKey) {
+          lookup[parentKey].children.push(obj);
+        }
+        if (i === 0) {
+          root = obj;
         }
       }
     }
@@ -59,6 +83,8 @@ function flamebearersToTree(f1: Flamebearer, f2: Flamebearer) {
   return root;
 }
 
+console.log(trace[0]);
+
 function diffFlamebearer(f1: Flamebearer, f2: Flamebearer): Flamebearer {
   const result: Flamebearer = {
     format: 'double',
@@ -66,7 +92,7 @@ function diffFlamebearer(f1: Flamebearer, f2: Flamebearer): Flamebearer {
     leftTicks: f1.numTicks,
     rightTicks: f2.numTicks,
     maxSelf: 100,
-    sampleRate: 1000000,
+    sampleRate: f1.sampleRate,
     names: [],
     levels: [],
     units: f1.units,
@@ -188,6 +214,23 @@ function Settings() {
               <Preferences />
             </Route>
             <Route exact path={`${path}/diff`}>
+              trace
+              <FlamegraphRenderer
+                profile={convertJaegerTraceToProfile(trace[0])}
+              />
+              f1
+              <FlamegraphRenderer
+                flamebearer={f1.flamebearer}
+                onlyDisplay="flamegraph"
+                viewType="single"
+              />
+              f2
+              <FlamegraphRenderer
+                flamebearer={f2.flamebearer}
+                onlyDisplay="flamegraph"
+                viewType="single"
+              />
+              f3
               <FlamegraphRenderer
                 flamebearer={diffFlamebearer(f1.flamebearer, f2.flamebearer)}
                 onlyDisplay="flamegraph"
