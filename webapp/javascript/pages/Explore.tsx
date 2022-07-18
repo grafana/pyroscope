5cd565d659864131112305c2873841ed25863a26
dogfrogfog <maksim.hodasevich@gmail.com>
2022-07-18 17:13:20 +0200

    add tags dropdown

--- webapp/javascript/pages/Explore.tsx
+++ webapp/javascript/pages/Explore.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState, SetStateAction, Dispatch } from 'react';
+import type { Maybe } from 'true-myth';
+import type { ClickEvent } from '@szhsin/react-menu';
 
 import Box from '@webapp/ui/Box';
 import Toolbar from '@webapp/components/Toolbar';
 import TimelineChartWrapper from '@webapp/components/TimelineChartWrapper';
 import { FlamegraphRenderer } from '@pyroscope/flamegraph/src/FlamegraphRenderer';
+import Dropdown, { MenuItem } from '@webapp/ui/Dropdown';
 import useColorMode from '@webapp/hooks/colorMode.hook';
 import useTimeZone from '@webapp/hooks/timeZone.hook';
+import { useAppDispatch, useAppSelector } from '@webapp/redux/hooks';
+import {
+  fetchTags,
+  selectQueries,
+  selectAppTags,
+  TagsState,
+} from '@webapp/redux/reducers/continuous';
+import { queryToAppName } from '@webapp/models/query';
+
+import styles from './Explore.module.scss';
 
 function Explore() {
   const { offset } = useTimeZone();
   const { colorMode } = useColorMode();
+  const dispatch = useAppDispatch();
+
+  // should we save selection while switching views without changing application name ?
+  const [selectedTag, setSelectedTag] = useState('');
+
+  const { query } = useAppSelector(selectQueries);
+  const tags = useAppSelector(selectAppTags(query));
+  const appName = queryToAppName(query);
+
+  useEffect(() => {
+    if (query) {
+      dispatch(fetchTags(query));
+
+      setSelectedTag('');
+    }
+  }, [query]);
 
   return (
-    <>
-      <Toolbar />
+    <div className={styles.explorePage}>
+      <Toolbar hideTagsBar={true} />
       <Box>
-        selectors
+        <ExploreHeader
+          appName={appName}
+          tags={tags}
+          selectedTag={selectedTag}
+          setSelectedTag={setSelectedTag}
+        />
         <TimelineChartWrapper
           timezone={offset === 0 ? 'utc' : 'browser'}
           data-testid="timeline-explore-page"
@@ -40,7 +74,47 @@ function Explore() {
           colorMode={colorMode}
         />
       </Box>
-    </>
+    </div>
+  );
+}
+
+function ExploreHeader({
+  appName,
+  tags,
+  selectedTag,
+  setSelectedTag,
+}: {
+  appName: Maybe<string>;
+  tags: TagsState;
+  selectedTag: string;
+  setSelectedTag: Dispatch<SetStateAction<string>>;
+}) {
+  const tagKeys = Object.keys(tags.tags);
+
+  const handleClick = (e: ClickEvent) => {
+    setSelectedTag(e.value);
+  };
+
+  return (
+    <div className={styles.header}>
+      <span className={styles.appName}>
+        {appName.isJust ? appName.value : ''}
+      </span>
+      <div className={styles.query}>
+        <span className={styles.selectName}>grouped by</span>
+        <Dropdown
+          label="tags"
+          value={selectedTag ? `tag: ${selectedTag}` : 'select tag'}
+          onItemClick={handleClick}
+        >
+          {tagKeys.map((tagName) => (
+            <MenuItem key={tagName} value={tagName}>
+              {tagName}
+            </MenuItem>
+          ))}
+        </Dropdown>
+      </div>
+    </div>
   );
 }
 
