a53c758eecbcb81e0611f4c2d220c0e3264cfd92
eduardo aleixo <eduardo@pyroscope.io>
2022-07-20 16:06:22 -0300

    .

--- scripts/ci-skip-husky.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-// https://github.com/yarnpkg/yarn/issues/7212#issuecomment-1136443313
-if (!process.env.CI) {
-  console.log('Executing husky install...');
-  execSync('husky install');
-}
