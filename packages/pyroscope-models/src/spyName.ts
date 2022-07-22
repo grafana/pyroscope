be1f61b442ea9783ea7a9e65df92048b97e71829
eduardo aleixo <eduardo@pyroscope.io>
2022-07-22 12:38:39 -0300

    chore(models): export spynames at runtime

--- packages/pyroscope-models/src/spyName.ts
+++ packages/pyroscope-models/src/spyName.ts
@@ -1,21 +1,25 @@
 import { z } from 'zod';
 
-export const SpyNameSchema = z
-  .enum([
-    'dotnetspy',
-    'ebpfspy',
-    'gospy',
-    'phpspy',
-    'pyspy',
-    'rbspy',
-    'nodespy',
-    'javaspy',
-    'pyroscope-rs',
+export const SpyNameFirstClass = [
+  'dotnetspy',
+  'ebpfspy',
+  'gospy',
+  'phpspy',
+  'pyspy',
+  'rbspy',
+  'nodespy',
+  'javaspy',
+  'pyroscope-rs',
+];
 
-    'scrape', // for compability purposes, it should be golang
-    'tracing',
-    'unknown',
-  ])
+export const SpyNameOther = [
+  'scrape', // for compability purposes, it should be golang
+  'tracing',
+  'unknown',
+];
+type EnumParam = Parameters<typeof z.enum>[0];
+export const SpyNameSchema = z
+  .enum(SpyNameFirstClass.concat(SpyNameOther) as EnumParam)
   .default('unknown');
 
 export type SpyName = z.infer<typeof SpyNameSchema>;
