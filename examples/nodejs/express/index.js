1ddf73bea8e609d4e4de702d61d84f6e0e28782b
Antony Shaleynikov <shaleynikov@gmail.com>
2022-07-25 12:38:38 +0300

    feat: updated examples to meet new tag API. You need to install lastest pyroscope node module from git first

--- examples/nodejs/express/index.js
+++ examples/nodejs/express/index.js
@@ -14,32 +14,38 @@ app.get('/', (req, res) => {
   res.send('Available routes are: /bike, /car, /scooter');
 });
 
-const genericSearchHandler = (p) => (req, res) => {
-  const time = +new Date() + p * 1000;
-  let i = 0;
-  while (+new Date() < time) {
-    i = i + Math.random();
-  }
-  res.send('Vehicle found');
-};
+function genericSearchHandler(p) {
+  return function (req, res) {
+    const time = +new Date() + p * 1000;
+    let i = 0;
+    while (+new Date() < time) {
+      i = i + Math.random();
+    }
+    res.send('Vehicle found');
+  };
+}
 
 Pyroscope.init({
   appName: 'nodejs',
   serverAddress: process.env['PYROSCOPE_SERVER'] || 'http://pyroscope:4040',
-  tags: { region },
 });
 
 Pyroscope.startCpuProfiling();
-Pyroscope.startHeapProfiling();
 
 app.get('/bike', function bikeSearchHandler(req, res) {
-  return genericSearchHandler(0.5)(req, res);
+  Pyroscope.tagCall('vehicle', 'bike', () =>
+    genericSearchHandler(0.5)(req, res)
+  );
 });
 app.get('/car', function carSearchHandler(req, res) {
-  return genericSearchHandler(1)(req, res);
+  Pyroscope.tagCall('vehicle', 'car', () =>
+    genericSearchHandler(0.2)(req, res)
+  );
 });
 app.get('/scooter', function scooterSearchHandler(req, res) {
-  return genericSearchHandler(0.25)(req, res);
+  Pyroscope.tagCall('vehicle', 'scooter', () =>
+    genericSearchHandler(0.1)(req, res)
+  );
 });
 
 app.listen(port, () => {
