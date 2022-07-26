ed7f729e947f9a84c1814dc20a81d04453f92e4c
Cyril Tovena <cyril.tovena@gmail.com>
2022-07-26 17:31:33 +0200

    Diff alloc type of profiles

---
+++ pkg/firedb/delta.go
@@ -0,0 +1,136 @@
+package firedb
+
+import (
+	"sort"
+	"sync"
+
+	schemav1 "github.com/grafana/fire/pkg/firedb/schemas/v1"
+	profilev1 "github.com/grafana/fire/pkg/gen/google/v1"
+	firemodel "github.com/grafana/fire/pkg/model"
+	"github.com/prometheus/common/model"
+	"github.com/samber/lo"
+)
+
+var memoryProfileName = "memory"
+
+type deltaProfiles struct {
+	mtx      sync.Mutex
+	profiles map[model.Fingerprint]*schemav1.Profile
+}
+
+func newDeltaProfiles() *deltaProfiles {
+	return &deltaProfiles{
+		profiles: make(map[model.Fingerprint]*schemav1.Profile),
+	}
+}
+
+func isDelta(lbs firemodel.Labels) bool {
+	if lbs.Get(model.MetricNameLabel) == memoryProfileName {
+		ty := lbs.Get(firemodel.LabelNameType)
+		if ty == "alloc_objects" || ty == "alloc_space" {
+			return true
+		}
+	}
+	return false
+}
+
+func (d *deltaProfiles) computeDelta(ps *schemav1.Profile, lbss []firemodel.Labels) (*schemav1.Profile, []firemodel.Labels) {
+	deltaIdx := lo.FilterMap(lbss, func(lbs firemodel.Labels, i int) (int, bool) {
+		// right now we only support one delta profile on memory
+		return i, isDelta(lbs)
+	})
+	// there's no delta to compute for those profiles.
+	if len(deltaIdx) == 0 {
+		return ps, lbss
+	}
+	d.mtx.Lock()
+	defer d.mtx.Unlock()
+	// sort samples by stacktrace id
+	sort.Slice(ps.Samples, func(i, j int) bool {
+		return ps.Samples[i].StacktraceID < ps.Samples[j].StacktraceID
+	})
+	// we store all ref so fetching with one should work.
+	lastProfile, ok := d.profiles[ps.SeriesRefs[deltaIdx[0]]]
+	if !ok {
+		// if we don't have the last profile, we can't compute the delta.
+		// so we remove the delta from the list of labels and profiles.
+		for _, i := range deltaIdx {
+			d.profiles[ps.SeriesRefs[i]] = ps
+		}
+		keepIdx := lo.FilterMap(ps.SeriesRefs, func(_ model.Fingerprint, i int) (int, bool) {
+			return i, !lo.Contains(deltaIdx, i)
+		})
+		newProfile := *ps
+		newProfile.SeriesRefs = make([]model.Fingerprint, len(keepIdx))
+		for i, j := range keepIdx {
+			newProfile.SeriesRefs[i] = ps.SeriesRefs[j]
+		}
+
+		newProfile.Samples = make([]*schemav1.Sample, 0, len(ps.Samples))
+
+		for _, s := range ps.Samples {
+			newValues := make([]int64, len(keepIdx))
+			newProfileLbs := make([]*profilev1.Label, len(keepIdx))
+			allZero := true
+			for i, j := range keepIdx {
+				if s.Values[j] != 0 {
+					allZero = false
+				}
+				newValues[i] = s.Values[j]
+				newProfileLbs = copySlice(s.Labels)
+			}
+			if allZero {
+				// if we end up with remaining values all to zero skip the sample.
+				continue
+			}
+			newProfile.Samples = append(newProfile.Samples, &schemav1.Sample{
+				StacktraceID: s.StacktraceID,
+				Values:       newValues,
+				Labels:       newProfileLbs,
+			})
+		}
+
+		newLbss := make([]firemodel.Labels, len(keepIdx))
+		for i, j := range keepIdx {
+			newLbss[i] = lbss[j]
+		}
+		return &newProfile, newLbss
+	}
+
+	// we have the last profile, we can compute the delta.
+	// samples are sorted by stacktrace id.
+	// we need to compute the delta for each stacktrace.
+	for _, i := range deltaIdx {
+		d.profiles[ps.SeriesRefs[i]] = ps
+	}
+	if len(lastProfile.Samples) == 0 {
+		return ps, lbss
+	}
+	idx := 0
+	currSample := lastProfile.Samples[idx]
+	for _, s := range ps.Samples {
+		for currSample.StacktraceID < s.StacktraceID && idx+1 < len(lastProfile.Samples) {
+			idx++
+			currSample = lastProfile.Samples[idx]
+		}
+		if s.StacktraceID == currSample.StacktraceID {
+			for _, j := range deltaIdx {
+				s.Values[j] -= currSample.Values[j]
+			}
+		}
+	}
+	// remove samples that are all zero
+	i := 0
+	for _, x := range ps.Samples {
+		for _, v := range x.Values {
+			if v != 0 {
+				ps.Samples[i] = x
+				i++
+				break
+			}
+		}
+	}
+	ps.Samples = ps.Samples[:i]
+
+	return ps, lbss
+}
--- pkg/firedb/head_test.go
+++ pkg/firedb/head_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/klauspost/compress/gzip"
 	"github.com/prometheus/common/model"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -386,4 +387,138 @@ func BenchmarkHeadIngestProfiles(t *testing.B) {
 			profileCount++
 		}
 	}
+}
+
+func TestIngestDiff(t *testing.T) {
+	ctx := context.Background()
+	head, err := NewHead(t.TempDir())
+	require.NoError(t, err)
+
+	profile := newMemoryProfileBuilder(1)
+	profile.ForStacktrace("a", "b", "c").AddMemorySample(1, 10, 1, 1)
+	profile.ForStacktrace("a", "b", "c", "d").AddMemorySample(1, 10, 1, 1)
+
+	err = head.Ingest(ctx, profile.Profile, profile.UUID, profile.labels...)
+	require.NoError(t, err)
+
+	profile = newMemoryProfileBuilder(2)
+	profile.ForStacktrace("a", "b", "c").AddMemorySample(2, 20, 2, 2)
+	profile.ForStacktrace("a", "b", "c", "d").AddMemorySample(3, 30, 2, 2)
+
+	err = head.Ingest(ctx, profile.Profile, profile.UUID, profile.labels...)
+	require.NoError(t, err)
+}
+
+type memoryProfileBuilder struct {
+	*profilev1.Profile
+	uuid.UUID
+	labels []*commonv1.LabelPair
+}
+
+func newMemoryProfileBuilder(ts int64) *memoryProfileBuilder {
+	return &memoryProfileBuilder{
+		Profile: &profilev1.Profile{
+			TimeNanos: ts,
+			SampleType: []*profilev1.ValueType{
+				{
+					Unit: 4,
+					Type: 3,
+				},
+				{
+					Unit: 2,
+					Type: 5,
+				},
+				{
+					Unit: 4,
+					Type: 6,
+				},
+				{
+					Unit: 2,
+					Type: 7,
+				},
+			},
+			Mapping: []*profilev1.Mapping{
+				{Id: 1, HasFunctions: true},
+			},
+			StringTable:       []string{"", "space", "bytes", "alloc_objects", "count", "alloc_space", "inuse_objects", "inuse_space"},
+			DefaultSampleType: 5,
+			PeriodType: &profilev1.ValueType{
+				Unit: 2,
+				Type: 1,
+			},
+		},
+		UUID: uuid.New(),
+		labels: []*commonv1.LabelPair{
+			{
+				Name:  "job",
+				Value: "foo",
+			},
+			{
+				Name:  model.MetricNameLabel,
+				Value: "memory",
+			},
+		},
+	}
+}
+
+func (m *memoryProfileBuilder) ForStacktrace(stacktraces ...string) *stacktraceBuilder {
+	namePositions := lo.Map(stacktraces, func(stacktrace string, i int) int64 {
+		for i, n := range m.StringTable {
+			if n == stacktrace {
+				return int64(i)
+			}
+		}
+		m.StringTable = append(m.StringTable, stacktrace)
+		return int64(len(m.StringTable) - 1)
+	})
+
+	// search functions
+	functionIds := lo.Map(namePositions, func(namePos int64, i int) uint64 {
+		for _, f := range m.Function {
+			if f.Name == namePos {
+				return f.Id
+			}
+		}
+		f := &profilev1.Function{
+			Name: namePos,
+			Id:   uint64(len(m.Function)) + 1,
+		}
+		m.Function = append(m.Function, f)
+		return f.Id
+	})
+	// search locations
+	locationIDs := lo.Map(functionIds, func(functionId uint64, i int) uint64 {
+		for _, l := range m.Location {
+			if l.Line[0].FunctionId == functionId {
+				return l.Id
+			}
+		}
+		l := &profilev1.Location{
+			MappingId: uint64(1),
+			Line: []*profilev1.Line{
+				{
+					FunctionId: functionId,
+				},
+			},
+			Id: uint64(len(m.Location)) + 1,
+		}
+		m.Location = append(m.Location, l)
+		return l.Id
+	})
+	return &stacktraceBuilder{
+		locationID:           locationIDs,
+		memoryProfileBuilder: m,
+	}
+}
+
+type stacktraceBuilder struct {
+	locationID []uint64
+	*memoryProfileBuilder
+}
+
+func (s *stacktraceBuilder) AddMemorySample(allocObjs int64, allocSpace int64, inuseObjs int64, inuseSpace int64) {
+	s.Profile.Sample = append(s.Profile.Sample, &profilev1.Sample{
+		LocationId: s.locationID,
+		Value:      []int64{allocObjs, allocSpace, inuseObjs, inuseSpace},
+	})
 }
