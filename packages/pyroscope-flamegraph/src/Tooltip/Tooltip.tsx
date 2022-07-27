782b49dcf5c50281b789e625af0d733b6e5f89f1
Dmitry Filimonov <dmitry@pyroscope.io>
2022-07-26 18:14:41 -0700

    changes

--- packages/pyroscope-flamegraph/src/Tooltip/Tooltip.tsx
+++ packages/pyroscope-flamegraph/src/Tooltip/Tooltip.tsx
@@ -184,17 +184,17 @@ const tooltipTitles: Record<
 > = {
   objects: {
     percent: '% of objects in RAM',
-    formattedValue: 'RAM amount',
+    formattedValue: 'Objects in RAM',
     total: '% of total RAM',
   },
   goroutines: {
     percent: '% of goroutines',
-    formattedValue: 'goroutines',
+    formattedValue: 'Goroutines',
     total: '% of total goroutines',
   },
   bytes: {
     percent: '% of RAM',
-    formattedValue: 'bytes',
+    formattedValue: 'RAM',
     total: '% of total bytes',
   },
   samples: {
@@ -204,12 +204,12 @@ const tooltipTitles: Record<
   },
   lock_nanoseconds: {
     percent: '% of Time spent',
-    formattedValue: 'seconds',
+    formattedValue: 'Time',
     total: '% of total seconds',
   },
   lock_samples: {
     percent: '% of contended locks',
-    formattedValue: 'locks',
+    formattedValue: 'Contended locks',
     total: '% of total locks',
   },
   trace_samples: {
