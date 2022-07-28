9ae6b39c4673acde4e59d7c2351839391b117e67
Cyril Tovena <cyril.tovena@gmail.com>
2022-07-29 01:36:22 +0200

    Merge duplicate stacktraces in the distributor.

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -23,7 +23,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	commonv1 "github.com/grafana/fire/pkg/gen/common/v1"
-	profilev1 "github.com/grafana/fire/pkg/gen/google/v1"
 	pushv1 "github.com/grafana/fire/pkg/gen/push/v1"
 	"github.com/grafana/fire/pkg/gen/push/v1/pushv1connect"
 	"github.com/grafana/fire/pkg/ingester/clientpool"
@@ -163,75 +162,4 @@ func TestBuckets(t *testing.T) {
 	for _, r := range prometheus.ExponentialBucketsRange(10*1024, 15*1024*1024, 30) {
 		t.Log(humanize.Bytes(uint64(r)))
 	}
-}
-
-func TestSanitizeProfile(t *testing.T) {
-	p := &profilev1.Profile{
-		SampleType: []*profilev1.ValueType{
-			{Type: 2, Unit: 1},
-			{Type: 3, Unit: 4},
-		},
-		Sample: []*profilev1.Sample{
-			{LocationId: []uint64{2, 3}, Value: []int64{0, 1}, Label: []*profilev1.Label{{Num: 10, Key: 1}, {Num: 11, Key: 1}}},
-			// Those samples should be dropped.
-			{LocationId: []uint64{1, 2, 3}, Value: []int64{0, 0}, Label: []*profilev1.Label{{Num: 10, Key: 1}}},
-			{LocationId: []uint64{4}, Value: []int64{0, 0}, Label: []*profilev1.Label{{Num: 10, Key: 1}}},
-		},
-		Mapping: []*profilev1.Mapping{},
-		Location: []*profilev1.Location{
-			{Id: 1, Line: []*profilev1.Line{{FunctionId: 1, Line: 1}, {FunctionId: 2, Line: 3}}},
-			{Id: 2, Line: []*profilev1.Line{{FunctionId: 2, Line: 1}, {FunctionId: 3, Line: 3}}},
-			{Id: 3, Line: []*profilev1.Line{{FunctionId: 3, Line: 1}, {FunctionId: 4, Line: 3}}},
-			{Id: 4, Line: []*profilev1.Line{{FunctionId: 5, Line: 1}}},
-		},
-		Function: []*profilev1.Function{
-			{Id: 1, Name: 5, SystemName: 6, Filename: 7, StartLine: 1},
-			{Id: 2, Name: 8, SystemName: 9, Filename: 10, StartLine: 1},
-			{Id: 3, Name: 11, SystemName: 12, Filename: 13, StartLine: 1},
-			{Id: 4, Name: 14, SystemName: 15, Filename: 7, StartLine: 1},
-			{Id: 5, Name: 16, SystemName: 17, Filename: 18, StartLine: 1},
-		},
-		StringTable: []string{
-			"memory", "bytes", "in_used", "allocs", "count",
-			"main", "runtime.main", "main.go", // fn1
-			"foo", "runtime.foo", "foo.go", // fn2
-			"bar", "runtime.bar", "bar.go", // fn3
-			"buzz", "runtime.buzz", // fn4
-			"bla", "runtime.bla", "bla.go", // fn5
-		},
-		PeriodType:        &profilev1.ValueType{Type: 0, Unit: 1},
-		Comment:           []int64{},
-		DefaultSampleType: 0,
-	}
-
-	h := sanitizeProfile(p)
-	require.Equal(t, h, &profilev1.Profile{
-		SampleType: []*profilev1.ValueType{
-			{Type: 2, Unit: 1},
-			{Type: 3, Unit: 4},
-		},
-		Sample: []*profilev1.Sample{
-			{LocationId: []uint64{2, 3}, Value: []int64{0, 1}, Label: []*profilev1.Label{}},
-		},
-		Mapping: []*profilev1.Mapping{},
-		Location: []*profilev1.Location{
-			{Id: 2, Line: []*profilev1.Line{{FunctionId: 2, Line: 1}, {FunctionId: 3, Line: 3}}},
-			{Id: 3, Line: []*profilev1.Line{{FunctionId: 3, Line: 1}, {FunctionId: 4, Line: 3}}},
-		},
-		Function: []*profilev1.Function{
-			{Id: 2, Name: 6, SystemName: 7, Filename: 8, StartLine: 1},
-			{Id: 3, Name: 9, SystemName: 10, Filename: 11, StartLine: 1},
-			{Id: 4, Name: 12, SystemName: 13, Filename: 5, StartLine: 1},
-		},
-		StringTable: []string{
-			"memory", "bytes", "in_used", "allocs", "count",
-			"main.go",
-			"foo", "runtime.foo", "foo.go",
-			"bar", "runtime.bar", "bar.go",
-			"buzz", "runtime.buzz",
-		},
-		PeriodType:        &profilev1.ValueType{Type: 0, Unit: 1},
-		Comment:           []int64{},
-		DefaultSampleType: 0,
-	})
 }
