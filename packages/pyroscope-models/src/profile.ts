ff160b72ef31cfc13fbf756b26e620e01d817cab
Dmitry Filimonov <dmitry@pyroscope.io>
2022-07-28 20:13:51 -0700

    chore: allows empty names in names array

--- packages/pyroscope-models/src/profile.ts
+++ packages/pyroscope-models/src/profile.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const FlamebearerSchema = z.object({
-  names: z.array(z.string().nonempty()),
+  names: z.array(z.string()),
   levels: z.array(z.array(z.number())),
   numTicks: z.number(),
   maxSelf: z.number(),
