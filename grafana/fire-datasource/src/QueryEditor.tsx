3348aea1271edb7dd1bb0b579a580c2210e50c4f
Andrej Ocenas <mr.ocenas@gmail.com>
2022-08-03 18:17:18 +0200

    Add monaco editor and some basic language config

--- grafana/fire-datasource/src/QueryEditor.tsx
+++ grafana/fire-datasource/src/QueryEditor.tsx
@@ -1,70 +1,112 @@
+import { css } from '@emotion/css';
+import { languageDefinition } from './fireql';
 import { defaults } from 'lodash';
-import React, { PureComponent, FormEvent } from 'react';
-import { Input, ButtonCascader, CascaderOption } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import React, { useState } from 'react';
+import { ButtonCascader, CascaderOption, CodeEditor, Monaco, useStyles2 } from '@grafana/ui';
+import { GrafanaTheme2, QueryEditorProps } from '@grafana/data';
 import { DataSource } from './datasource';
 import { defaultQuery, MyDataSourceOptions, ProfileType, Query } from './types';
+import { useMount } from 'react-use';
+import type { languages } from 'monaco-editor';
 
+type Props = QueryEditorProps<DataSource, Query, MyDataSourceOptions>;
 
-type Props = QueryEditorProps<DataSource, Query, MyDataSourceOptions>
+export function QueryEditor(props: Props) {
+  const [profileTypes, setProfileTypes] = useState<CascaderOption[]>([]);
 
-interface State {
-  profileTypes: CascaderOption[]
-}
-
-export class QueryEditor extends PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      profileTypes: [],
-    };
-  }
-  onProfileTypeChange = (value: string[], selectedOptions: CascaderOption[]) => {
+  function onProfileTypeChange(value: string[], selectedOptions: CascaderOption[]) {
     if (selectedOptions.length === 0) {
-      return
+      return;
     }
     let type = selectedOptions[selectedOptions.length - 1].value as ProfileType;
-    this.props.onChange({ ...this.props.query, ProfileType: type });
-  };
-  onLabelSelectorChange = (value: FormEvent<HTMLInputElement>) => {
-    this.props.onChange({ ...this.props.query, LabelSelector: value.currentTarget.value });
-  };
-  componentDidMount() {
-    this.props.datasource.getProfileTypes().then(profileTypes => {
-      let mainTypes = new Map<string, CascaderOption>();
+    props.onChange({ ...props.query, ProfileType: type });
+  }
 
-      // Classify profile types by name then sample type.
-      for (let profileType of profileTypes) {
-        if (!mainTypes.has(profileType.name)) {
-          mainTypes.set(profileType.name, {
-            label: profileType.name,
-            value: profileType,
-            children: [],
-          });
-        }
-        mainTypes.get(profileType.name)?.children?.push({
-          label: profileType.sampleType,
+  function onLabelSelectorChange(value: string) {
+    props.onChange({ ...props.query, LabelSelector: value });
+  }
+
+  useMount(async () => {
+    const profileTypes = await props.datasource.getProfileTypes();
+    let mainTypes = new Map<string, CascaderOption>();
+
+    // Classify profile types by name then sample type.
+    for (let profileType of profileTypes) {
+      if (!mainTypes.has(profileType.name)) {
+        mainTypes.set(profileType.name, {
+          label: profileType.name,
           value: profileType,
-        })
+          children: [],
+        });
       }
-      let types = Array.from(mainTypes.values());
-      let state = { ...this.state, profileTypes: types };
-      this.setState(state);
-    });
-  };
+      mainTypes.get(profileType.name)?.children?.push({
+        label: profileType.sampleType,
+        value: profileType,
+      });
+    }
+    let types = Array.from(mainTypes.values());
+    setProfileTypes(types);
+  });
 
-  render() {
-    let query = defaults(this.props.query, defaultQuery);
-    const selectedProfileName = this.props.query.ProfileType ? this.props.query.ProfileType.name + " - " + this.props.query.ProfileType.sampleType : 'Select a profile type'
-    return (
-      <div className="gf-form">
-        <ButtonCascader
-          onChange={this.onProfileTypeChange}
-          options={this.state.profileTypes}
-          icon='process'
-        >{selectedProfileName}</ButtonCascader>
-        <Input onChange={this.onLabelSelectorChange} value={query.LabelSelector} />
-      </div>
-    );
+  let query = defaults(props.query, defaultQuery);
+  const selectedProfileName = props.query.ProfileType
+    ? props.query.ProfileType.name + ' - ' + props.query.ProfileType.sampleType
+    : 'Select a profile type';
+
+  const styles = useStyles2(getStyles);
+  return (
+    <div className="gf-form">
+      <ButtonCascader onChange={onProfileTypeChange} options={profileTypes} icon="process">
+        {selectedProfileName}
+      </ButtonCascader>
+      {/*<Input onChange={this.onLabelSelectorChange} value={query.LabelSelector} />*/}
+      <CodeEditor
+        value={query.LabelSelector}
+        language={langId}
+        onBlur={onLabelSelectorChange}
+        height={'30px'}
+        containerStyles={styles.queryField}
+        monacoOptions={{
+          folding: false,
+          fontSize: 14,
+          lineNumbers: 'off',
+          overviewRulerLanes: 0,
+          renderLineHighlight: 'none',
+          scrollbar: {
+            vertical: 'hidden',
+            verticalScrollbarSize: 8, // used as "padding-right"
+            horizontal: 'hidden',
+            horizontalScrollbarSize: 0,
+          },
+          scrollBeyondLastLine: false,
+          wordWrap: 'on',
+        }}
+        onBeforeEditorMount={ensureFireQL}
+      />
+    </div>
+  );
+}
+
+// we must only run the setup code once
+let fireqlSetupDone = false;
+const langId = 'fireql';
+
+function ensureFireQL(monaco: Monaco) {
+  if (fireqlSetupDone === false) {
+    fireqlSetupDone = true;
+    const { aliases, extensions, mimetypes, def } = languageDefinition;
+    monaco.languages.register({ id: langId, aliases, extensions, mimetypes });
+    monaco.languages.setMonarchTokensProvider(langId, def.language as languages.IMonarchLanguage);
+    monaco.languages.setLanguageConfiguration(langId, def.languageConfiguration as languages.LanguageConfiguration);
   }
 }
+
+const getStyles = (theme: GrafanaTheme2) => {
+  return {
+    queryField: css`
+      border-radius: ${theme.shape.borderRadius()};
+      border: 1px solid ${theme.components.input.borderColor};
+      flex: 1;
+    `,
+  };
+};
