cd9391313693fa1213b02a87486e931a6df60c10
Ryan Perry <rperry2174@gmail.com>
2022-08-11 09:28:13 -0700

    fix test

--- packages/pyroscope-flamegraph/src/format/format.spec.ts
+++ packages/pyroscope-flamegraph/src/format/format.spec.ts
@@ -42,7 +42,7 @@ describe('format', () => {
 
         expect(df.format(0, 100)).toBe('0.00 seconds');
         expect(df.format(0.001, 100)).toBe('< 0.01 seconds');
-        expect(df.format(100, 100)).toBe('1.00 seconds');
+        expect(df.format(100, 100)).toBe('1.00 second');
         expect(df.format(2000, 100)).toBe('20.00 seconds');
         expect(df.format(2012.3, 100)).toBe('20.12 seconds');
         expect(df.format(8000, 100)).toBe('80.00 seconds');
