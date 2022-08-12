48c5f651aeba909fcdfc52eb12e92de57427fca2
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-08-12 12:49:26 +0300

    feat: fade unselected tags

--- webapp/javascript/pages/tagExplorer/components/Legend.tsx
+++ webapp/javascript/pages/tagExplorer/components/Legend.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import type { TimelineGroupData } from '@webapp/components/TimelineChart/TimelineChartWrapper';
 import { appWithoutTagsWhereDropdownOptionName } from '@webapp/redux/reducers/continuous';
+import classNames from 'classnames/bind';
 import styles from './Legend.module.scss';
 
+const cx = classNames.bind(styles);
 interface LegendProps {
   groups: TimelineGroupData[];
   handleGroupByTagValueChange: (groupByTagValue: string) => void;
@@ -17,28 +18,32 @@ function Legend({
 }: LegendProps) {
   return (
     <div data-testid="legend" className={styles.legend}>
-      {groups.map(({ tagName, color }) => (
-        <div
-          aria-hidden
-          data-testid="legend-item"
-          className={styles.tagName}
-          key={tagName}
-          onClick={() =>
-            handleGroupByTagValueChange(
-              tagName === activeGroup
-                ? appWithoutTagsWhereDropdownOptionName
-                : tagName
-            )
-          }
-        >
-          <span
-            data-testid="legend-item-color"
-            className={styles.tagColor}
-            style={{ backgroundColor: color?.toString() }}
-          />
-          <span>{tagName}</span>
-        </div>
-      ))}
+      {groups.map(({ tagName, color }) => {
+        const isSelected = tagName === activeGroup;
+        return (
+          <div
+            aria-hidden
+            data-testid="legend-item"
+            className={cx({
+              [styles.tagName]: true,
+              [styles.faded]: activeGroup && !isSelected,
+            })}
+            key={tagName}
+            onClick={() =>
+              handleGroupByTagValueChange(
+                isSelected ? appWithoutTagsWhereDropdownOptionName : tagName
+              )
+            }
+          >
+            <span
+              data-testid="legend-item-color"
+              className={styles.tagColor}
+              style={{ backgroundColor: color?.toString() }}
+            />
+            <span>{tagName}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
