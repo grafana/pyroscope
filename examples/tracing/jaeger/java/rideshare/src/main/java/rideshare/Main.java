c0f6ba458bffbb8893bc3fc0e834e56617e9f4e6
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-08-15 17:12:47 +0700

    fix: add trace_id span_id fields to java jaeger example

--- examples/tracing/jaeger/java/rideshare/src/main/java/rideshare/Main.java
+++ examples/tracing/jaeger/java/rideshare/src/main/java/rideshare/Main.java
@@ -15,6 +15,7 @@ import io.pyroscope.javaagent.api.Logger;
 import io.pyroscope.javaagent.config.Config;
 import io.pyroscope.labels.Pyroscope;
 import io.pyroscope.otel.PyroscopeTelemetry;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -32,15 +33,41 @@ public class Main {
     }
 
     private static void configurePyroscope() {
+        Config config = new Config.Builder()
+                .setApplicationName(APP_NAME)
+                .setProfilingEvent(EventType.ITIMER)
+                .setFormat(Format.JFR)
+                .setLogLevel(Logger.Level.DEBUG)
+                .setServerAddress(System.getenv("PYROSCOPE_SERVER_ADDRESS"))
+                .build();
         PyroscopeAgent.start(
-                new Config.Builder()
-                        .setApplicationName(APP_NAME)
-                        .setProfilingEvent(EventType.ITIMER)
-                        .setFormat(Format.JFR)
-                        .setLogLevel(Logger.Level.DEBUG)
-                        .setServerAddress(System.getenv("PYROSCOPE_SERVER_ADDRESS"))
+                new PyroscopeAgent.Options.Builder(config)
+                        .setLogger(new Logger() {
+                            org.slf4j.Logger pyroscopeLogger = LoggerFactory.getLogger("pyroscope");
+
+                            @Override
+                            public void log(Level l, String msg, Object... args) {
+                                String m = String.format(msg, args);
+                                switch (l) {
+                                    case INFO:
+                                        pyroscopeLogger.info(m);
+                                        break;
+                                    case DEBUG:
+                                        pyroscopeLogger.debug(m);
+                                        break;
+                                    case WARN:
+                                        pyroscopeLogger.warn(m);
+                                        break;
+                                    case ERROR:
+                                        pyroscopeLogger.error(m);
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        })
                         .build()
         );
-        Pyroscope.setStaticLabels(Map.of("region", System.getenv("REGION")));
+        Pyroscope.setStaticLabels(Map.of("region",System.getenv("REGION")));
+                }
     }
-}
