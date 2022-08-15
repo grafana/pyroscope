82a49847f5be6debcd79299d31ee44df21d34e05
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-08-16 03:07:18 +0700

    docs: add trace_id span_id fields to java jaeger example (#1402)
    * fix: add trace_id span_id fields to java jaeger example
    Co-authored-by: Ryan Perry <Rperry2174@gmail.com>

---
+++ examples/tracing/jaeger/java/rideshare/src/main/java/rideshare/PyroscopeLog4jLogger.java
@@ -0,0 +1,29 @@
+package rideshare;
+
+import io.pyroscope.javaagent.api.Logger;
+import org.slf4j.LoggerFactory;
+
+class PyroscopeLog4jLogger implements Logger {
+    org.slf4j.Logger pyroscopeLogger = LoggerFactory.getLogger("pyroscope");
+
+    @Override
+    public void log(Level l, String msg, Object... args) {
+        String m = String.format(msg, args);
+        switch (l) {
+            case INFO:
+                pyroscopeLogger.info(m);
+                break;
+            case DEBUG:
+                pyroscopeLogger.debug(m);
+                break;
+            case WARN:
+                pyroscopeLogger.warn(m);
+                break;
+            case ERROR:
+                pyroscopeLogger.error(m);
+                break;
+            default:
+                break;
+        }
+    }
+}
