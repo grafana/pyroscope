e4c414000e077d1d5fcbc9dac0851b3ec1be33fc
eduardo aleixo <eduardo@pyroscope.io>
2022-08-16 14:17:11 -0300

    fix lint

--- webapp/javascript/components/TagsBar.tsx
+++ webapp/javascript/components/TagsBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import 'react-dom';
 import { TagsState } from '@webapp/redux/reducers/continuous';
 import Dropdown, {
