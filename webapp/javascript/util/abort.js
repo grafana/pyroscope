ef9cb89dfa75013419eea4828ed29b32d60f5e0c
eduardo aleixo <eduardo@pyroscope.io>
2022-08-18 12:04:09 -0300

    remove unused js files

--- webapp/javascript/util/abort.js
+++
@@ -1,8 +0,0 @@
-export function isAbortError(err) {
-  if (!err) {
-    return false;
-  }
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/DOMException
-  return err.name === 'AbortError' || err.code === 20;
-}
