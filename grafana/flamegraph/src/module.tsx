6be8a3c71247e941ae58eb0a73ed3cd7e0f207cf
Andrej Ocenas <mr.ocenas@gmail.com>
2022-08-19 19:23:48 +0200

    Add ts-ignore for grafana/data APIs

--- grafana/flamegraph/src/module.tsx
+++ grafana/flamegraph/src/module.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// @ts-ignore
 import { ExplorePanelProps, PanelPlugin, PanelProps } from '@grafana/data';
 import FlameGraphContainer from './components/FlameGraphContainer';
 
@@ -10,4 +11,9 @@ export const FlameExploreGraphPanel: React.FunctionComponent<ExplorePanelProps> 
   return <FlameGraphContainer data={props.data[0]} />;
 };
 
+// We use ts-ignore here because setExplorePanel and ExplorePanelProps are part of a draft PR that isn't yet merged.
+// We could solve this by linking but that has quite a bit of issues with regard of resolving dependencies downstream
+// in grafana/data and also needs some custom modification in grafana repo so for now this seems to be easier as the
+// there is not that much to the API.
+// @ts-ignore
 export const plugin = new PanelPlugin(FlameGraphPanel).setExplorePanel(FlameExploreGraphPanel, ['profile']);
