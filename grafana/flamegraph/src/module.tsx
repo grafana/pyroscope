e2a0704d902f79f2469ee895f1a23836fcd6ba4e
Andrej Ocenas <mr.ocenas@gmail.com>
2022-08-19 15:40:34 +0200

    Use data from server

--- grafana/flamegraph/src/module.tsx
+++ grafana/flamegraph/src/module.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { PanelPlugin, PanelProps } from '@grafana/data';
+import { ExplorePanelProps, PanelPlugin, PanelProps } from '@grafana/data';
 import FlameGraphContainer from './components/FlameGraphContainer';
 
-export const FlameGraphPanel: React.FunctionComponent<PanelProps> = () => {
-  return (
-    <FlameGraphContainer/>
-  );
+export const FlameGraphPanel: React.FunctionComponent<PanelProps> = (props) => {
+  return <FlameGraphContainer data={props.data.series[0]} />;
 };
 
-export const plugin = new PanelPlugin(FlameGraphPanel).setExplorePanel(FlameGraphPanel, ['profile']);
+export const FlameExploreGraphPanel: React.FunctionComponent<ExplorePanelProps> = (props) => {
+  return <FlameGraphContainer data={props.data[0]} />;
+};
+
+export const plugin = new PanelPlugin(FlameGraphPanel).setExplorePanel(FlameExploreGraphPanel, ['profile']);
