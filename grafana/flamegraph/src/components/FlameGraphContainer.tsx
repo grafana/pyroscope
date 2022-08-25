758b7921d308669fccc352429a841b8e5ef68c65
Joey Tawadrous <90795735+joey-grafana@users.noreply.github.com>
2022-08-25 16:57:48 +0100

    Fuzzy search (#159)
    * Search
    
    * Optimizations
    
    * Tests
    
    * Prettier
    
    * Fixed test
    
    * Prettier
    
    * Update test
    
    * Ignore case when searching
    
    * Update test
    
    * Self review
    
    * Update comments
    
    * go mod tidy

--- grafana/flamegraph/src/components/FlameGraphContainer.tsx
+++ grafana/flamegraph/src/components/FlameGraphContainer.tsx
@@ -12,16 +12,24 @@ const FlameGraphContainer = (props: Props) => {
   const [topLevelIndex, setTopLevelIndex] = useState(0);
   const [rangeMin, setRangeMin] = useState(0);
   const [rangeMax, setRangeMax] = useState(1);
+  const [query, setQuery] = useState('');
 
   return (
     <>
-      <FlameGraphHeader setTopLevelIndex={setTopLevelIndex} setRangeMin={setRangeMin} setRangeMax={setRangeMax} />
+      <FlameGraphHeader
+        setTopLevelIndex={setTopLevelIndex}
+        setRangeMin={setRangeMin}
+        setRangeMax={setRangeMax}
+        query={query}
+        setQuery={setQuery}
+      />
 
       <FlameGraph
         data={props.data}
         topLevelIndex={topLevelIndex}
         rangeMin={rangeMin}
         rangeMax={rangeMax}
+        query={query}
         setTopLevelIndex={setTopLevelIndex}
         setRangeMin={setRangeMin}
         setRangeMax={setRangeMax}
