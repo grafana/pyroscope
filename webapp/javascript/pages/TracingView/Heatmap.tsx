3552aea70f479ad4aeeef4bc6cd5f8717f5e7ff8
dogfrogfog <maksim.hodasevich@gmail.com>
2022-08-29 11:27:47 +0300

    feat: add useHeatmapSelect Hook

--- webapp/javascript/pages/TracingView/Heatmap.tsx
+++ webapp/javascript/pages/TracingView/Heatmap.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, RefObject } from 'react';
 import Color from 'color';
-// import globalTemperatureJSON from './global-temperature.json';
 
 import styles from './Heatmap.module.scss';
 
@@ -31,11 +30,11 @@ type SelectedAreaCoords = Record<'x' | 'y', number> | null;
 let startCoords: SelectedAreaCoords = null;
 let endCoords: SelectedAreaCoords = null;
 
-function HeatMap() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const heatmapRef = useRef<HTMLDivElement>(null);
+const useHeatmapSelect = (
+  canvasRef: RefObject<HTMLCanvasElement>,
+  heatmapWidth: number
+) => {
   const [isSelecting, setSelectingStatus] = useState(false);
-  const [heatmapWidth, setHeatmapWith] = useState(0);
 
   const startDrawing = (e: MouseEvent) => {
     if (canvasRef.current) {
@@ -55,8 +54,7 @@ function HeatMap() {
     }
   };
 
-  // draw selection area
-  const drawRect = (e: MouseEvent) => {
+  const drawSelectionAreaRect = (e: MouseEvent) => {
     if (isSelecting && canvasRef.current && startCoords) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -76,26 +74,52 @@ function HeatMap() {
     }
   };
 
-  // add events to draw selected area
-  // todo: add area selection when cursor goes over heatmap
   useEffect(() => {
     const canvas = document.getElementById('selectionCanvas');
     if (canvas) {
       canvas.addEventListener('mousedown', startDrawing);
-      canvas.addEventListener('mousemove', drawRect);
+      canvas.addEventListener('mousemove', drawSelectionAreaRect);
       canvas.addEventListener('mouseup', endDrawing);
-      canvas.addEventListener('mouseout', endDrawing);
+      canvas.addEventListener('mouseout', endDrawing); // ?
     }
 
     return () => {
       canvas?.removeEventListener('mousedown', startDrawing);
-      canvas?.removeEventListener('mousemove', drawRect);
+      canvas?.removeEventListener('mousemove', drawSelectionAreaRect);
       canvas?.addEventListener('mouseup', endDrawing);
-      canvas?.addEventListener('mouseout', endDrawing);
+      canvas?.addEventListener('mouseout', endDrawing); // ?
     };
-  }, [startDrawing, drawRect, endDrawing]);
+  }, [startDrawing, drawSelectionAreaRect, endDrawing]);
 
-  // to resize canvas
+  useEffect(() => {
+    if (startCoords && endCoords) {
+      const cellW = heatmapWidth / TIME_BUCKETS;
+      const cellH = HEATMAP_HEIGHT / VALUE_BUCKETS;
+
+      // if selection area hits element - it's starting point
+      const matrixCoordinates = {
+        xStart: Math.trunc(startCoords.x / cellW),
+        yStart: Math.trunc(startCoords.y / cellH),
+        xEnd: Math.trunc(endCoords.x / cellW),
+        yEnd: Math.trunc(endCoords.y / cellH),
+      };
+    }
+
+    // todo: redraw selected area after resize
+    // useEffect(() => {
+    // }, []);
+
+    // todo escape heatmapWidth dep to redraw selected area after resize
+  }, [startCoords, endCoords, heatmapWidth]);
+};
+
+function HeatMap() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const heatmapRef = useRef<HTMLDivElement>(null);
+  const [heatmapWidth, setHeatmapWith] = useState(0);
+
+  useHeatmapSelect(canvasRef, heatmapWidth);
+
   useEffect(() => {
     if (heatmapRef.current) {
       const resizeObserver = new ResizeObserver((entries) => {
@@ -120,28 +144,6 @@ function HeatMap() {
     }
   }, []);
 
-  // todo: redraw selected area after resize
-  // useEffect(() => {
-  // }, []);
-
-  useEffect(() => {
-    if (startCoords && endCoords) {
-      const cellW = heatmapWidth / TIME_BUCKETS;
-      const cellH = HEATMAP_HEIGHT / VALUE_BUCKETS;
-
-      // if selection area hits element - it's starting point
-      const matrixCoordinates = {
-        xStart: Math.trunc(startCoords.x / cellW),
-        yStart: Math.trunc(startCoords.y / cellH),
-        xEnd: Math.trunc(endCoords.x / cellW),
-        yEnd: Math.trunc(endCoords.y / cellH),
-      };
-
-      console.log(matrixCoordinates);
-    }
-    // escape heatmapWidth dep to redraw selected area after resize
-  }, [startCoords, endCoords, heatmapWidth]);
-
   const generateHeatmapGrid = () =>
     COLUMNS.map((column, colIndex) => (
       <g key={colIndex}>
@@ -149,6 +151,7 @@ function HeatMap() {
           <rect
             data-x={x}
             data-y={y}
+            // todo: add bucket click handler
             onClick={() => console.log(x, y)}
             fill={fill}
             key={cellIndex}
