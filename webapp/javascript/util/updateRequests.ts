6c8698b7b6fba02123ec98697a8ba136af9ef1c8
Dmitry Filimonov <dmitry@pyroscope.io>
2022-08-30 21:16:49 -0700

    deletes old function

--- webapp/javascript/util/updateRequests.ts
+++ webapp/javascript/util/updateRequests.ts
@@ -13,9 +13,7 @@ export function buildRenderURL(
   fromOverride?: string,
   untilOverride?: string
 ) {
-  const url = new URL('render', location.href);
-
-  const params = url.searchParams;
+  const params = new URLSearchParams();
   params.set('query', state.query);
   params.set('from', fromOverride || state.from);
   params.set('until', untilOverride || state.until);
@@ -24,7 +22,7 @@ export function buildRenderURL(
   state.groupBy && params.set('groupBy', state.groupBy);
   state.groupByValue && params.set('groupByValue', state.groupByValue);
 
-  return url.toString();
+  return `/render?${params}`;
 }
 
 export function buildMergeURLWithQueryID(state: {
@@ -32,39 +30,12 @@ export function buildMergeURLWithQueryID(state: {
   refreshToken?: string;
   maxNodes?: string | number;
 }) {
-  const url = new URL('merge', location.href);
+  const url = new URL('/merge');
 
   const params = url.searchParams;
   params.set('queryID', state.queryID);
   state.refreshToken && params.set('refreshToken', state.refreshToken);
   state.maxNodes && params.set('max-nodes', String(state.maxNodes));
-
-  return url.toString();
-}
-
-// TODO: merge buildRenderURL and buildDiffRenderURL
-export function buildDiffRenderURL(state: {
-  from: string;
-  until: string;
-  leftFrom: string;
-  leftUntil: string;
-  rightFrom: string;
-  rightUntil: string;
-  refreshToken?: string;
-  maxNodes: string;
-  query: string;
-}) {
-  const { from, until, leftFrom, leftUntil, rightFrom, rightUntil } = state;
-  const urlStr = buildRenderURL(state, from, until);
-  const url = new URL(urlStr, location.href);
-
-  url.pathname = url.pathname.replace('render', 'render-diff');
-
-  const params = url.searchParams;
-  params.set('leftFrom', leftFrom);
-  params.set('leftUntil', leftUntil);
-  params.set('rightFrom', rightFrom);
-  params.set('rightUntil', rightUntil);
 
   return url.toString();
 }
