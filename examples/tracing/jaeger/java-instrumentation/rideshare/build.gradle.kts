7e138d8233d423515f1396811f27ed8cba1cc93a
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-09-07 20:58:17 +0700

    cleanup buildscript example

--- examples/tracing/jaeger/java-instrumentation/rideshare/build.gradle.kts
+++ examples/tracing/jaeger/java-instrumentation/rideshare/build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") //todo replace when otel is not shnapshot
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") //todo replace when otel is not shnapshot
     maven("https://repo.spring.io/snapshot")
     maven("https://repo.spring.io/milestone")
     maven("https://repo.spring.io/release")
@@ -17,16 +17,7 @@ repositories {
 }
 
 dependencies {
-//    implementation("io.pyroscope:otel:0.9.3-SNAPSHOT")
-
-//    runtimeOnly(files("/home/korniltsev/Downloads/qweqwe/inst"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.springframework.cloud:spring-cloud-starter-sleuth") {
-//        exclude(group = "org.springframework.cloud", module = "spring-cloud-sleuth-brave")
-//    }
-//    implementation("org.springframework.cloud:spring-cloud-sleuth-otel-autoconfigure")
-
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
