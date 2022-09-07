7ac4d9cf4e3fd2cf177f25fc74ee1c2550f051aa
eduardo aleixo <eduardo@pyroscope.io>
2022-09-07 17:01:17 -0300

    basic annotation service

---
+++ pkg/service/annotation.go
@@ -0,0 +1,51 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/pyroscope-io/pyroscope/pkg/model"
+	"gorm.io/gorm"
+)
+
+type AnnotationsService struct{ db *gorm.DB }
+
+func NewAnnotationsService(db *gorm.DB) AnnotationsService {
+	return AnnotationsService{db: db}
+}
+
+type CreateAnnotationParams struct {
+	AppName   string
+	Content   string
+	Timestamp time.Time
+}
+
+// CreateAnnotation creates a single annotation for a given application
+// It does not check if the application has consumed any data
+func (svc AnnotationsService) CreateAnnotation(ctx context.Context, params CreateAnnotationParams) (*model.Annotation, error) {
+	var u model.Annotation
+
+	u.AppName = params.AppName
+	u.Content = params.Content
+	u.Timestamp = params.Timestamp
+
+	tx := svc.db.WithContext(ctx)
+	if err := tx.Create(&u).Error; err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+// FindAnnotationsByTimeRange finds all annotations for an app in a time range
+func (svc AnnotationsService) FindAnnotationsByTimeRange(ctx context.Context, appName string, startTime time.Time, endTime time.Time) ([]model.Annotation, error) {
+	tx := svc.db.WithContext(ctx)
+	var u []model.Annotation
+
+	// TODO(eh-am): find timerange
+	if err := tx.Where("app_name = ?", appName).Where("timestamp between ? and ?", startTime, endTime).Find(&u).Error; err != nil {
+		return u, err
+	}
+
+	return u, nil
+}
---
+++ pkg/service/annotation_test.go
@@ -0,0 +1,89 @@
+package service_test
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/pyroscope-io/pyroscope/pkg/service"
+)
+
+var _ = Describe("AnnotationsService", func() {
+	s := new(testSuite)
+	BeforeEach(s.BeforeEach)
+	AfterEach(s.AfterEach)
+
+	var svc service.AnnotationsService
+	BeforeEach(func() {
+		svc = service.NewAnnotationsService(s.DB())
+	})
+
+	Describe("create annotation", func() {
+		It("works", func() {
+			now := time.Now()
+
+			annotation, err := svc.CreateAnnotation(context.Background(), service.CreateAnnotationParams{
+				AppName:   "myapp",
+				Content:   "mycontent",
+				Timestamp: now,
+			})
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(annotation).ToNot(BeNil())
+			Expect(annotation.AppName).To(Equal("myapp"))
+			Expect(annotation.Content).To(Equal("mycontent"))
+			Expect(annotation.Timestamp).To(Equal(now))
+
+			Expect(annotation.CreatedAt).ToNot(BeZero())
+			Expect(annotation.UpdatedAt).ToNot(BeZero())
+		})
+	})
+
+	Describe("find annotations", func() {
+		When("there's no annotation", func() {
+			It("returns empty array", func() {
+				now := time.Now()
+
+				annotations, err := svc.FindAnnotationsByTimeRange(context.Background(), "myapp", now, now)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(annotations).To(BeEmpty())
+			})
+		})
+
+		When("annotation exists", func() {
+			var now time.Time
+
+			BeforeEach(func() {
+				now = time.Now()
+				_, err := svc.CreateAnnotation(context.Background(), service.CreateAnnotationParams{
+					AppName:   "myapp",
+					Content:   "mycontent",
+					Timestamp: now,
+				})
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			When("finding within interval", func() {
+				It("finds correctly", func() {
+					annotations, err := svc.FindAnnotationsByTimeRange(context.Background(), "myapp", now, now)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(annotations).ToNot(BeEmpty())
+					Expect(len(annotations)).To(Equal(1))
+				})
+			})
+
+			When("finding outside the interval", func() {
+				It("returns empty array", func() {
+					annotations, err := svc.FindAnnotationsByTimeRange(context.Background(), "myapp",
+						time.Now().Add(time.Hour),
+						time.Now().Add(time.Hour*2),
+					)
+
+					Expect(err).ToNot(HaveOccurred())
+					Expect(annotations).To(BeEmpty())
+				})
+			})
+		})
+	})
+})
