89904adb67ae0b04fb91b925c91d21940f60b4ed
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-09-08 14:15:32 +0300

    refactor: filtering moved to preprocess fn

--- webapp/javascript/redux/reducers/continuous.ts
+++ webapp/javascript/redux/reducers/continuous.ts
@@ -926,28 +926,9 @@ export const continuousSlice = createSlice({
     });
 
     builder.addCase(fetchTagExplorerView.fulfilled, (state, action) => {
-      let data = action.payload;
-      const { groups } = action.payload;
-      const groupNames = Object.keys(groups);
-
-      if (groupNames?.length) {
-        data = {
-          ...data,
-          groups: groupNames
-            .filter((g) => !!g.trim())
-            .reduce(
-              (acc, current) => ({
-                ...acc,
-                [current]: groups[current],
-              }),
-              {}
-            ),
-        };
-      }
-
       state.tagExplorerView = {
         ...state.tagExplorerView,
-        ...data,
+        ...action.payload,
         activeTagProfile: action.payload.profile,
         type: 'loaded',
       };
