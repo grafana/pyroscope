95c4f2bb17c36dfd12584a301258d12c44bceb8a
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-09-09 12:39:03 +0700

    fix: add pprof format during ingestion to RawProfile

--- pkg/server/ingest.go
+++ pkg/server/ingest.go
@@ -147,6 +147,7 @@ func (h ingestHandler) ingestInputFromRequest(r *http.Request) (*ingestion.Inges
 
 	case format == "pprof":
 		input.Profile = &pprof.RawProfile{RawData: b}
+		input.Format = ingestion.FormatPprof
 	case strings.Contains(contentType, "multipart/form-data"):
 		input.Profile = &pprof.RawProfile{
 			FormDataContentType: contentType,
