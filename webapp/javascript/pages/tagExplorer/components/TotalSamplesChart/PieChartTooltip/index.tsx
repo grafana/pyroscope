96a821921bb7a172ed899688b28cf1d51dcc1d64
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-09-13 13:56:54 +0300

    chore: pieChart, tooltip adjustments

--- webapp/javascript/pages/tagExplorer/components/TotalSamplesChart/PieChartTooltip/index.tsx
+++ webapp/javascript/pages/tagExplorer/components/TotalSamplesChart/PieChartTooltip/index.tsx
@@ -10,16 +10,21 @@ export interface PieChartTooltipProps {
 const PieChartTooltip = (props: PieChartTooltipProps) => {
   return (
     <div className={styles.wrapper}>
-      <div>{props?.label || ' '}</div>
-      <div>
-        Total samples: <span className={styles.bold}>{props?.value}</span>
-      </div>
-      <div>
-        Percentage:{' '}
-        <span className={styles.bold}>
-          {Number(props?.percent).toFixed(2)}%
-        </span>
-      </div>
+      <div className={styles.title}>{props?.label || ' '}</div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Total samples</td>
+            <td>Percentage</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props?.value}</td>
+            <td>{Number(props?.percent).toFixed(2)}%</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
