5f317298235c7e5de6fd2aa06dfdebc73928eb1f
Cyril Tovena <cyril.tovena@gmail.com>
2022-09-23 13:53:37 +0200

    Fixes stacktraces symbolization

--- pkg/firedb/block_querier_test.go
+++ pkg/firedb/block_querier_test.go
@@ -2,6 +2,7 @@ package firedb
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"testing"
 
@@ -9,11 +10,14 @@ import (
 	"github.com/go-kit/log"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	schemav1 "github.com/grafana/fire/pkg/firedb/schemas/v1"
 	commonv1 "github.com/grafana/fire/pkg/gen/common/v1"
 	ingestv1 "github.com/grafana/fire/pkg/gen/ingester/v1"
+	"github.com/grafana/fire/pkg/iter"
 	"github.com/grafana/fire/pkg/objstore/providers/filesystem"
 	pprofth "github.com/grafana/fire/pkg/pprof/testhelper"
 )
@@ -166,5 +170,44 @@ func Test_mergeSelectProfilesResponse(t *testing.T) {
 
 	if diff := cmp.Diff(exp, act, cmpopts.IgnoreUnexported(ingestv1.SelectProfilesResponse{}, ingestv1.Profile{}, ingestv1.StacktraceSample{})); diff != "" {
 		t.Errorf("Unexpected mergeSelectProfilesResponse result(-expect +actual):\n%s", diff)
+	}
+}
+
+func TestInMemoryReader(t *testing.T) {
+	path := t.TempDir()
+	st := deduplicatingSlice[string, string, *stringsHelper, *schemav1.StringPersister]{}
+	require.NoError(t, st.Init(path))
+	rewrites := &rewriter{}
+	rgCount := 5
+	for i := 0; i < rgCount*maxBufferRowCount; i++ {
+		require.NoError(t, st.ingest(context.Background(), []string{fmt.Sprintf("foobar %d", i)}, rewrites))
+	}
+	numRows, numRg, err := st.Flush()
+	require.NoError(t, err)
+	require.Equal(t, uint64(rgCount*maxBufferRowCount), numRows)
+	require.Equal(t, uint64(rgCount), numRg)
+	require.NoError(t, st.Close())
+	reader := inMemoryparquetReader[*schemav1.StoredString, *schemav1.StringPersister]{}
+	fs, err := filesystem.NewBucket(path)
+	require.NoError(t, err)
+
+	require.NoError(t, reader.open(context.Background(), fs))
+	it := reader.retrieveRows(context.Background(), iter.NewSliceIterator(lo.Times(int(numRows), func(i int) int64 { return int64(i) })))
+	var j int
+	for it.Next() {
+		require.Equal(t, it.At().Result.String, fmt.Sprintf("foobar %d", j))
+		require.Equal(t, it.At().RowNum, int64(j))
+		require.Equal(t, it.At().Result.ID, uint64(j))
+		j++
+	}
+
+	rows := []int64{0, 1000, 2000}
+	it = reader.retrieveRows(context.Background(), iter.NewSliceIterator(rows))
+	j = 0
+	for it.Next() {
+		require.Equal(t, it.At().Result.String, fmt.Sprintf("foobar %d", rows[j]))
+		require.Equal(t, it.At().RowNum, rows[j])
+		require.Equal(t, it.At().Result.ID, uint64(rows[j]))
+		j++
 	}
 }
