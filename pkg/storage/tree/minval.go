720a91c472d7c6c26e51f119675d1c0d65826d5b
Dmitry Filimonov <dmitry@pyroscope.io>
2022-10-10 15:28:05 -0700

    adds ability to set maxNodes to -1 to disable truncation

--- pkg/storage/tree/minval.go
+++ pkg/storage/tree/minval.go
@@ -1,8 +1,13 @@
 package tree
 
-import "github.com/pyroscope-io/pyroscope/pkg/structs/cappedarr"
+import (
+	"github.com/pyroscope-io/pyroscope/pkg/structs/cappedarr"
+)
 
 func (t *Tree) minValue(maxNodes int) uint64 {
+	if maxNodes == -1 {
+		return 0
+	}
 	c := cappedarr.New(maxNodes)
 	t.iterateWithTotal(func(total uint64) bool {
 		return c.Push(total)
