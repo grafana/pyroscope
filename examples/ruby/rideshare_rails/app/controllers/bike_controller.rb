46b06d6914edf15423340d1c66a486f78aaff1f8
Dmitry Filimonov <dmitry@pyroscope.io>
2022-10-09 21:39:21 -0700

    chore: adds spans to ruby example

--- examples/ruby/rideshare_rails/app/controllers/bike_controller.rb
+++ examples/ruby/rideshare_rails/app/controllers/bike_controller.rb
@@ -1,6 +1,8 @@
 class BikeController < ApplicationController
-    def index
-        helpers.find_nearest_vehicle( 0.4, "bike")
-        render html: "Bike ordered"
+  def index
+    OpenTelemetry.tracer_provider.tracer('my-tracer').in_span("BikeController") do |_|
+      helpers.find_nearest_vehicle( 0.4, "bike")
+      render html: "Bike ordered"
     end
+  end
 end
--- examples/ruby/rideshare_rails/app/controllers/car_controller.rb
+++ examples/ruby/rideshare_rails/app/controllers/car_controller.rb
@@ -1,6 +1,8 @@
 class CarController < ApplicationController
-    def index
-        helpers.find_nearest_vehicle( 0.5, "car")
-        render html: "Car ordered"
+  def index
+    OpenTelemetry.tracer_provider.tracer('my-tracer').in_span("CarController") do |_|
+      helpers.find_nearest_vehicle(0.5, "car")
+      render html: "Car ordered"
     end
+  end
 end
