aab7f9202dd341e5f08567f64f90d6b7ad854e9c
Dmitry Filimonov <dmitry@pyroscope.io>
2022-10-12 22:00:07 -0700

    refactor: tree serialization improvements (#1587)
    * shutdown improvement
    
    * deletes old unused functions
    
    * adds ability to set maxNodes to -1 to disable truncation
    
    * fix for dropped nodes
    
    * improvements for handling extra "other" nodes
    
    * adds proper "other" handling, adds tests
    
    * cleanup
    
    * final changes

--- pkg/storage/tree/serialize.go
+++ pkg/storage/tree/serialize.go
@@ -13,49 +13,9 @@ import (
 // serialization format version. it's not very useful right now, but it will be in the future
 const currentVersion = 1
 
-func (t *Tree) Serialize(d *dict.Dict, maxNodes int, w io.Writer) error {
-	t.RLock()
-	defer t.RUnlock()
+var lostDuringSerializationName = []byte("other")
 
-	varint.Write(w, currentVersion)
-
-	nodes := []*treeNode{t.root}
-	minVal := t.minValue(maxNodes)
-	j := 0
-
-	for len(nodes) > 0 {
-		j++
-		tn := nodes[0]
-		nodes = nodes[1:]
-
-		labelLink := d.Put([]byte(tn.Name))
-		_, err := varint.Write(w, uint64(len(labelLink)))
-		if err != nil {
-			return err
-		}
-		_, err = w.Write(labelLink)
-		if err != nil {
-			return err
-		}
-
-		val := tn.Self
-		_, err = varint.Write(w, uint64(val))
-		if err != nil {
-			return err
-		}
-		cnl := uint64(0)
-		if tn.Total > minVal {
-			cnl = uint64(len(tn.ChildrenNodes))
-			nodes = append(tn.ChildrenNodes, nodes...)
-		}
-		_, err = varint.Write(w, cnl)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
+// warning: this function modifies the tree
 func (t *Tree) SerializeTruncate(d *dict.Dict, maxNodes int, w io.Writer) error {
 	t.Lock()
 	defer t.Unlock()
@@ -79,64 +39,36 @@ func (t *Tree) SerializeTruncate(d *dict.Dict, maxNodes int, w io.Writer) error 
 		if _, err = w.Write(labelKey); err != nil {
 			return err
 		}
-		val := tn.Self
-		if _, err = vw.Write(w, val); err != nil {
+		if _, err = vw.Write(w, tn.Self); err != nil {
 			return err
 		}
 
 		cNodes := tn.ChildrenNodes
 		tn.ChildrenNodes = tn.ChildrenNodes[:0]
+
+		other := uint64(0)
 		for _, cn := range cNodes {
-			if cn.Total >= minVal {
+			isOtherNode := bytes.Equal(cn.Name, lostDuringSerializationName)
+			if cn.Total >= minVal || isOtherNode {
 				tn.ChildrenNodes = append(tn.ChildrenNodes, cn)
+			} else {
+				// Truncated children accounted as parent self.
+				other += cn.Total
 			}
 		}
+
+		if other > 0 {
+			otherNode := tn.insert(lostDuringSerializationName)
+			otherNode.Self += other
+			otherNode.Total += other
+		}
+
 		if len(tn.ChildrenNodes) > 0 {
 			nodes = append(tn.ChildrenNodes, nodes...)
 		} else {
 			tn.ChildrenNodes = nil // Just to make it eligible for GC.
 		}
 		if _, err = vw.Write(w, uint64(len(tn.ChildrenNodes))); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (t *Tree) SerializeNoDict(maxNodes int, w io.Writer) error {
-	t.RLock()
-	defer t.RUnlock()
-
-	nodes := []*treeNode{t.root}
-	minVal := t.minValue(maxNodes)
-	j := 0
-
-	for len(nodes) > 0 {
-		j++
-		tn := nodes[0]
-		nodes = nodes[1:]
-
-		_, err := varint.Write(w, uint64(len(tn.Name)))
-		if err != nil {
-			return err
-		}
-		_, err = w.Write(tn.Name)
-		if err != nil {
-			return err
-		}
-
-		val := tn.Self
-		_, err = varint.Write(w, val)
-		if err != nil {
-			return err
-		}
-		cnl := uint64(0)
-		if tn.Total > minVal {
-			cnl = uint64(len(tn.ChildrenNodes))
-			nodes = append(tn.ChildrenNodes, nodes...)
-		}
-		_, err = varint.Write(w, cnl)
-		if err != nil {
 			return err
 		}
 	}
@@ -223,6 +155,7 @@ func Deserialize(d *dict.Dict, r io.Reader) (*Tree, error) {
 	return t, nil
 }
 
+// used in the cloud
 func DeserializeNoDict(r io.Reader) (*Tree, error) {
 	t := New()
 	br := bufio.NewReader(r) // TODO if it's already a bytereader skip
@@ -273,55 +206,8 @@ func DeserializeNoDict(r io.Reader) (*Tree, error) {
 	return t, nil
 }
 
-func (t *Tree) Bytes(d *dict.Dict, maxNodes int) ([]byte, error) {
-	b := bytes.Buffer{}
-	if err := t.Serialize(d, maxNodes, &b); err != nil {
-		return nil, err
-	}
-	return b.Bytes(), nil
-}
-
-func FromBytes(d *dict.Dict, p []byte) (*Tree, error) {
-	return Deserialize(d, bytes.NewReader(p))
-}
-
-func (t *Tree) SerializeNoDictNoLimit(w io.Writer) error {
-	t.RLock()
-	defer t.RUnlock()
-
-	nodes := []*treeNode{t.root}
-	j := 0
-
-	for len(nodes) > 0 {
-		j++
-		tn := nodes[0]
-		nodes = nodes[1:]
-
-		_, err := varint.Write(w, uint64(len(tn.Name)))
-		if err != nil {
-			return err
-		}
-		_, err = w.Write(tn.Name)
-		if err != nil {
-			return err
-		}
-
-		val := tn.Self
-		_, err = varint.Write(w, val)
-		if err != nil {
-			return err
-		}
-		cnl := uint64(0)
-		cnl = uint64(len(tn.ChildrenNodes))
-		nodes = append(tn.ChildrenNodes, nodes...)
-		_, err = varint.Write(w, cnl)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
+// used in the cloud
+// warning: this function modifies the tree
 func (t *Tree) SerializeTruncateNoDict(maxNodes int, w io.Writer) error {
 	t.Lock()
 	defer t.Unlock()
@@ -340,19 +226,27 @@ func (t *Tree) SerializeTruncateNoDict(maxNodes int, w io.Writer) error {
 			return err
 		}
 
-		val := tn.Self
+		if _, err = vw.Write(w, tn.Self); err != nil {
+			return err
+		}
 		cNodes := tn.ChildrenNodes
 		tn.ChildrenNodes = tn.ChildrenNodes[:0]
+
+		other := uint64(0)
 		for _, cn := range cNodes {
-			if cn.Total >= minVal {
+			isOtherNode := bytes.Equal(cn.Name, lostDuringSerializationName)
+			if cn.Total >= minVal || isOtherNode {
 				tn.ChildrenNodes = append(tn.ChildrenNodes, cn)
 			} else {
 				// Truncated children accounted as parent self.
-				val += cn.Total
+				other += cn.Total
 			}
 		}
-		if _, err = vw.Write(w, val); err != nil {
-			return err
+
+		if other > 0 {
+			otherNode := tn.insert(lostDuringSerializationName)
+			otherNode.Self += other
+			otherNode.Total += other
 		}
 
 		if len(tn.ChildrenNodes) > 0 {
