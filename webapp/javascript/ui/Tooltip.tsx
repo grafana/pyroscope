dd2b680670541e512edda32b8048c2dedcf14a81
eduardo aleixo <eduardo@pyroscope.io>
2022-10-26 16:31:14 +0100

    make it look the same as the previous tooltip

--- webapp/javascript/ui/Tooltip.tsx
+++ webapp/javascript/ui/Tooltip.tsx
@@ -3,8 +3,7 @@ jsx-a11y/click-events-have-key-events,
 jsx-a11y/no-noninteractive-element-interactions, 
 css-modules/no-unused-class 
 */
-import React, { useRef } from 'react';
-import PopperUnstyled from '@mui/base/PopperUnstyled';
+import React from 'react';
 import MuiTooltip from '@mui/material/Tooltip';
 import styles from './Tooltip.module.scss';
 
@@ -29,38 +28,26 @@ const Tooltip = ({ title, visible, className, placement }: TooltipProps) => {
   );
 };
 
-//interface Tooltip2Props {
-//  title: string;
-//  placement: 'top' | 'left' | 'bottom' | 'right';
-//  children: JSX.Element;
-//}
+// Don't expose all props from the lib
+type AvailableProps = Pick<
+  React.ComponentProps<typeof MuiTooltip>,
+  'title' | 'children' | 'placement'
+>;
+function Tooltip2(props: AvailableProps) {
+  const defaultProps: Omit<
+    React.ComponentProps<typeof MuiTooltip>,
+    'title' | 'children'
+  > = {
+    arrow: true,
+    classes: {
+      tooltip: styles.muiTooltip,
+      arrow: styles.muiTooltipArrow,
+    },
+  };
 
-const Tooltip2 = MuiTooltip;
-// onMouseEnter={() => setRolesTooltipVisibility(true)}
-//                onMouseLeave={() => setRolesTooltipVisibility(false)}
-//function Tooltip2({ title, placement, children }: Tooltip2Props) {
-//  const id = 'simple-popper';
-//  const childRef = useRef();
-//  const [childNode, setChildNode] = React.useState<HTMLElement>();
-//
-//  const cloned = React.cloneElement(children, {
-//    ...children.props,
-//    ref: childRef,
-//  });
-//
-//  return (
-//    <>
-//      {cloned}
-//      <PopperUnstyled
-//        id={id}
-//        open={childNode ? open : false}
-//        anchorEl={childRef.current}
-//      >
-//        {title}
-//      </PopperUnstyled>
-//    </>
-//  );
-//}
-//
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  return <MuiTooltip {...defaultProps} {...props} />;
+}
+
 export default Tooltip;
 export { Tooltip2 };
