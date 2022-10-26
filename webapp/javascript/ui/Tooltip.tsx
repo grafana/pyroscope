2914f62c0f14f63d5ffb1414a6c9009d6b2d87ba
eduardo aleixo <eduardo@pyroscope.io>
2022-10-26 15:16:29 +0100

    wip

--- webapp/javascript/ui/Tooltip.tsx
+++ webapp/javascript/ui/Tooltip.tsx
@@ -3,7 +3,9 @@ jsx-a11y/click-events-have-key-events,
 jsx-a11y/no-noninteractive-element-interactions, 
 css-modules/no-unused-class 
 */
-import React from 'react';
+import React, { useRef } from 'react';
+import PopperUnstyled from '@mui/base/PopperUnstyled';
+import MuiTooltip from '@mui/material/Tooltip';
 import styles from './Tooltip.module.scss';
 
 interface TooltipProps {
@@ -27,4 +29,38 @@ const Tooltip = ({ title, visible, className, placement }: TooltipProps) => {
   );
 };
 
+//interface Tooltip2Props {
+//  title: string;
+//  placement: 'top' | 'left' | 'bottom' | 'right';
+//  children: JSX.Element;
+//}
+
+const Tooltip2 = MuiTooltip;
+// onMouseEnter={() => setRolesTooltipVisibility(true)}
+//                onMouseLeave={() => setRolesTooltipVisibility(false)}
+//function Tooltip2({ title, placement, children }: Tooltip2Props) {
+//  const id = 'simple-popper';
+//  const childRef = useRef();
+//  const [childNode, setChildNode] = React.useState<HTMLElement>();
+//
+//  const cloned = React.cloneElement(children, {
+//    ...children.props,
+//    ref: childRef,
+//  });
+//
+//  return (
+//    <>
+//      {cloned}
+//      <PopperUnstyled
+//        id={id}
+//        open={childNode ? open : false}
+//        anchorEl={childRef.current}
+//      >
+//        {title}
+//      </PopperUnstyled>
+//    </>
+//  );
+//}
+//
 export default Tooltip;
+export { Tooltip2 };
