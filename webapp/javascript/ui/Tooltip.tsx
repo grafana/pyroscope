e04a9a5b8abacbf6c3fad87b1a8aaf2ed1636053
eduardo aleixo <eh-am@users.noreply.github.com>
2022-10-27 17:26:03 +0100

    feat: add a generic Tooltip component (#1643)

--- webapp/javascript/ui/Tooltip.tsx
+++ webapp/javascript/ui/Tooltip.tsx
@@ -4,27 +4,28 @@ jsx-a11y/no-noninteractive-element-interactions,
 css-modules/no-unused-class 
 */
 import React from 'react';
+import MuiTooltip from '@mui/material/Tooltip';
 import styles from './Tooltip.module.scss';
 
-interface TooltipProps {
-  title: string;
-  visible: boolean;
-  className?: string;
-  placement: 'top' | 'left' | 'bottom' | 'right';
+// Don't expose all props from the lib
+type AvailableProps = Pick<
+  React.ComponentProps<typeof MuiTooltip>,
+  'title' | 'children' | 'placement'
+>;
+function Tooltip(props: AvailableProps) {
+  const defaultProps: Omit<
+    React.ComponentProps<typeof MuiTooltip>,
+    'title' | 'children'
+  > = {
+    arrow: true,
+    classes: {
+      tooltip: styles.muiTooltip,
+      arrow: styles.muiTooltipArrow,
+    },
+  };
+
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  return <MuiTooltip {...defaultProps} {...props} />;
 }
 
-const Tooltip = ({ title, visible, className, placement }: TooltipProps) => {
-  return (
-    <div
-      onClick={(e) => e.stopPropagation()}
-      className={`${styles.tooltip} ${visible ? styles.visible : ''} ${
-        styles?.[placement]
-      } ${className || ''} `}
-      role="tooltip"
-    >
-      {title}
-    </div>
-  );
-};
-
-export default Tooltip;
+export { Tooltip };
