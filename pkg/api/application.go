d183b9032e9aa776b0136361b64853f6468a759c
eduardo aleixo <eduardo@pyroscope.io>
2022-11-17 16:36:33 +0000

    delete unneeded file

--- pkg/api/application.go
+++
@@ -1,8 +0,0 @@
-package api
-
-//type ApplicationService interface {
-//	List(context.Context) ([]storage.ApplicationMetadata, error)               // GET /apps
-//	Get(ctx context.Context, name string) (storage.ApplicationMetadata, error) // GET /apps/{name}
-//	CreateOrUpdate(context.Context, storage.ApplicationMetadata) error         // PUT /apps // Should be idempotent.
-//	Delete(ctx context.Context, name string) error                             // DELETE /apps/{name}
-//}
--- pkg/api/applications.go
+++ pkg/api/applications.go
@@ -10,10 +10,10 @@ import (
 )
 
 type ApplicationService interface {
-	List(context.Context) ([]storage.ApplicationMetadata, error)               // GET /apps
-	Get(ctx context.Context, name string) (storage.ApplicationMetadata, error) // GET /apps/{name}
-	CreateOrUpdate(context.Context, storage.ApplicationMetadata) error         // PUT /apps // Should be idempotent.
-	Delete(ctx context.Context, name string) error                             // DELETE /apps/{name}
+	List(context.Context) ([]storage.ApplicationMetadata, error)
+	Get(ctx context.Context, name string) (storage.ApplicationMetadata, error)
+	CreateOrUpdate(context.Context, storage.ApplicationMetadata) error
+	Delete(ctx context.Context, name string) error
 }
 
 type ApplicationsHandler struct {
