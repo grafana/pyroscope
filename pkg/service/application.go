250a5988978647889c19cbc1b4b80ac6f7963f10
eduardo aleixo <eduardo@pyroscope.io>
2022-11-18 12:33:51 +0000

    use struct wrapping

--- pkg/service/application.go
+++ pkg/service/application.go
@@ -2,9 +2,6 @@ package service
 
 import (
 	"context"
-
-	"github.com/pyroscope-io/pyroscope/pkg/storage"
-	"golang.org/x/sync/errgroup"
 )
 
 type AppDeleter interface {
@@ -12,7 +9,7 @@ type AppDeleter interface {
 }
 
 type ApplicationService struct {
-	appMetadataSvc ApplicationMetadataService
+	ApplicationMetadataService
 	storageDeleter AppDeleter
 }
 
@@ -21,38 +18,18 @@ type ApplicationService struct {
 // Except when deleting, which is then forward to both ApplicationMetadataService and storageDeleter
 func NewApplicationService(appMetadataSvc ApplicationMetadataService, storageDeleter AppDeleter) ApplicationService {
 	return ApplicationService{
-		appMetadataSvc: appMetadataSvc,
-		storageDeleter: storageDeleter,
+		appMetadataSvc,
+		storageDeleter,
 	}
 }
 
-// List delegates to ApplicationMetadataService
-func (svc ApplicationService) List(ctx context.Context) (apps []storage.ApplicationMetadata, err error) {
-	return svc.appMetadataSvc.List(ctx)
-}
-
-// Get delegates to ApplicationMetadataService
-func (svc ApplicationService) Get(ctx context.Context, name string) (storage.ApplicationMetadata, error) {
-	return svc.appMetadataSvc.Get(ctx, name)
-}
-
-// CreateOrUpdate delegates to ApplicationMetadataService
-// For data ingestion, look for storage
-func (svc ApplicationService) CreateOrUpdate(ctx context.Context, application storage.ApplicationMetadata) error {
-	return svc.appMetadataSvc.CreateOrUpdate(ctx, application)
-}
-
 // Delete deletes apps from both storage and ApplicationMetadata
+// It first deletes from storage and only then deletes its metadata
 func (svc ApplicationService) Delete(ctx context.Context, name string) error {
-	g, ctx := errgroup.WithContext(ctx)
+	err := svc.storageDeleter.DeleteApp(ctx, name)
+	if err != nil {
+		return err
+	}
 
-	g.Go(func() error {
-		return svc.storageDeleter.DeleteApp(ctx, name)
-	})
-
-	g.Go(func() error {
-		return svc.appMetadataSvc.Delete(ctx, name)
-	})
-
-	return g.Wait()
+	return svc.ApplicationMetadataService.Delete(ctx, name)
 }
