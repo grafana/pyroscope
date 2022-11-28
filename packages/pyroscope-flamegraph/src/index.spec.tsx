e9eca9d2e7579739a008f006a795a46779b3e01c
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-11-28 17:19:27 +0100

    fix format

--- packages/pyroscope-flamegraph/src/index.spec.tsx
+++ packages/pyroscope-flamegraph/src/index.spec.tsx
@@ -322,7 +322,7 @@ describe.only('positions', () => {
         y: 0,
         bottom: 0,
         right: 0,
-        toJSON: () => { },
+        toJSON: () => {},
         height: 0,
         top: 0,
         left: 0,
