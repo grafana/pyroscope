1d1822fc67aed4179c4646508fb8886d3a90b172
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-12-05 16:22:43 +0300

    chore: upd color mode logic

--- webapp/javascript/hooks/colorMode.hook.ts
+++ webapp/javascript/hooks/colorMode.hook.ts
@@ -5,18 +5,49 @@ import { setColorMode, selectAppColorMode } from '@webapp/redux/reducers/ui';
 const useColorMode = () => {
   const dispatch = useAppDispatch();
   const colorMode = useAppSelector(selectAppColorMode);
+  const { body } = document;
+
+  const changeColorMode = (newColorMode: 'light' | 'dark') =>
+    dispatch(setColorMode(newColorMode));
 
   useEffect(() => {
-    // sync color mode from redux with DOM body attr
-    if (document.body.dataset.theme !== colorMode) {
-      document.body.dataset.theme = colorMode;
+    // sync data-theme attr with redux value
+    const observer = new MutationObserver((mutationsList) => {
+      const mutation = mutationsList[0];
+
+      if (mutation.type === 'attributes' && body.dataset.theme !== colorMode) {
+        changeColorMode(
+          body.dataset.theme === 'light' || body.dataset.theme === 'dark'
+            ? body.dataset.theme
+            : 'dark'
+        );
+      }
+    });
+
+    if (body) {
+      observer.observe(body, {
+        attributes: true,
+        attributeFilter: ['data-theme'],
+        childList: false,
+        subtree: false,
+      });
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    // sync redux value with DOM body attr
+    if (body.dataset.theme !== colorMode) {
+      body.dataset.theme = colorMode;
     }
   }, [colorMode]);
 
   return {
     colorMode,
-    changeColorMode: (newColorMode: 'light' | 'dark') =>
-      dispatch(setColorMode(newColorMode)),
+    changeColorMode,
   };
 };
 
