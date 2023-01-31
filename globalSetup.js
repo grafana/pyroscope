a6eab6f56cbb278d8fed2f44598a45aae2578363
eduardo aleixo <eduardo@pyroscope.io>
2023-01-31 10:58:06 +0000

    conditionally import jest-image-snapshot

--- globalSetup.js
+++ globalSetup.js
@@ -3,6 +3,6 @@ const Pyroscope = require('@pyroscope/nodejs');
 module.exports = (config) => {
   Pyroscope.init({ serverAddress: '_', appName: 'pyroscope-oss.frontend' });
   if (process.env.CI) {
-    Pyroscope.start();
+    Pyroscope.startCpuProfiling();
   }
 };
--- setupAfterEnv.ts
+++ setupAfterEnv.ts
@@ -1,32 +1,35 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
-import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 import type { MatchImageSnapshotOptions } from 'jest-image-snapshot';
 import nodeFetch from 'node-fetch';
 globalThis.fetch = nodeFetch as unknown as typeof fetch;
 
-expect.extend({
-  toMatchImageSnapshot(received: string, options: MatchImageSnapshotOptions) {
-    // If these checks pass, assume we're in a JSDOM environment with the 'canvas' package.
-    if (process.env.RUN_SNAPSHOTS) {
-      const toMatchImageSnapshot = configureToMatchImageSnapshot({
-        // Big enough threshold to account for different font rendering
-        // TODO: fix it
-        failureThreshold: 0.1,
-        failureThresholdType: 'percent',
-      }) as any;
-
-      // TODO
-      // for some reason it fails with
-      // Expected 1 arguments, but got 3.
-      // hence the any
-      return toMatchImageSnapshot.call(this, received, options);
-    }
-
-    return {
+async function getToMatchImageSnapshot() {
+  if (!process.env.RUN_SNAPSHOTS) {
+    return () => ({
       pass: true,
       message: () =>
         `Skipping 'toMatchImageSnapshot' assertion since env var 'RUN_SNAPSHOTS' is not set.`,
-    };
-  },
-});
+    });
+  }
+
+  return (await import('jest-image-snapshot')).configureToMatchImageSnapshot({
+    // Big enough threshold to account for different font rendering
+    // TODO: fix it
+    failureThreshold: 0.1,
+    failureThresholdType: 'percent',
+  });
+}
+
+async function setupExt() {
+  const toMatchImageSnapshot = await getToMatchImageSnapshot();
+
+  expect.extend({
+    toMatchImageSnapshot(received: string, options: MatchImageSnapshotOptions) {
+      // If these checks pass, assume we're in a JSDOM environment with the 'canvas' package.
+      return toMatchImageSnapshot.call(this, received, options);
+    },
+  });
+}
+
+setupExt();
