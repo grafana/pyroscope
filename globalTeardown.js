d5e907fd2d33239dde2485c64f1eccedfd1677c4
eduardo aleixo <eduardo@pyroscope.io>
2023-02-08 17:53:54 +0000

    debug logs

--- globalTeardown.js
+++ globalTeardown.js
@@ -2,7 +2,9 @@ const Pyroscope = require('@pyroscope/nodejs');
 
 module.exports = (config) => {
   if (process.env.CI) {
+    console.time('stopping');
     Pyroscope.stopCpuProfiling();
     Pyroscope.stopHeapProfiling();
+    console.timeEnd('stopping');
   }
 };
