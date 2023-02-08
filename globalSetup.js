eb0acdff376c698eec1d68240450deaa6cc1a9e6
eduardo aleixo <eduardo@pyroscope.io>
2023-02-08 17:40:19 +0000

    build: change ci profiling sampling duration to 1second

--- globalSetup.js
+++ globalSetup.js
@@ -1,8 +1,9 @@
 const Pyroscope = require('@pyroscope/nodejs');
 
 module.exports = (config) => {
+  process.env['PYROSCOPE_SAMPLING_DURATION'] = 1000;
   Pyroscope.init({ serverAddress: '_', appName: 'pyroscope-oss.frontend' });
   if (process.env.CI) {
-    Pyroscope.start();
+    Pyroscope.startCpuProfiling();
   }
 };
