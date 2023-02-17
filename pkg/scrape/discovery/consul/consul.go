72e2b6faefb177a94644640cc06506c615766202
eastfisher <eastfisher72@gmail.com>
2023-02-17 21:26:26 +0800

    Update pkg/scrape/discovery/consul/consul.go
    Co-authored-by: Anton Kolesnikov <anton.e.kolesnikov@gmail.com>

--- pkg/scrape/discovery/consul/consul.go
+++ pkg/scrape/discovery/consul/consul.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package consul
+package consul // revive:disable-line:import-shadowing package name is not referenced
 
 import (
 	"context"
