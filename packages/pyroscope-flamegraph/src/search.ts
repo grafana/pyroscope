3cb979b0bda638b87787e8278686ed414be8925a
eduardo aleixo <eduardo@pyroscope.io>
2023-02-23 10:51:07 +0000

    cache RegExp object

--- packages/pyroscope-flamegraph/src/search.ts
+++ packages/pyroscope-flamegraph/src/search.ts
@@ -1,9 +1,24 @@
-// Whether a query matches a node
-export function isMatch(query: string, nodeName: string) {
-  try {
-    const regex = new RegExp(query, 'i');
-    return regex.test(nodeName);
-  } catch (e) {
-    return false;
-  }
+function SearchRegex(pattern: string) {
+  return {
+    regex: new RegExp(pattern, 'i'),
+    pattern,
+  };
 }
+
+// Whether a query matches a node
+// It caches the RegExp object since the query most likely won't change between calls
+// Since it's the same node
+export const isMatch = (() => {
+  let regex = SearchRegex('noop');
+
+  return (query: string, nodeName: string) => {
+    try {
+      if (regex.pattern !== query) {
+        regex = SearchRegex(query);
+      }
+      return regex.regex.test(nodeName);
+    } catch (e) {
+      return false;
+    }
+  };
+})();
