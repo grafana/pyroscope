4129a30494a0842ac8db077c3938a689c4d85393
eduardo aleixo <eh-am@users.noreply.github.com>
2023-02-24 13:46:26 +0000

    chore: prune tree based on `MaxNodes` when converting json files (#1875)

--- pkg/structs/flamebearer/convert/convert_test.go
+++ pkg/structs/flamebearer/convert/convert_test.go
@@ -673,6 +673,26 @@ var _ = Describe("Convert", func() {
 		Expect(err).To(BeNil())
 		Expect(b).ToNot(BeNil())
 	})
+
+	Describe("JSON", func() {
+		It("prunes tree", func() {
+			m := ProfileFile{
+				Type: "json",
+				Data: readFile("./testdata/profile.json"),
+			}
+
+			f, err := converter(m)
+			Expect(err).To(BeNil())
+			Expect(f).ToNot(BeNil())
+
+			b, err := f(m.Data, "appname", 1)
+			Expect(err).To(BeNil())
+			Expect(b).ToNot(BeNil())
+
+			// 1 + total
+			Expect(len(b.FlamebearerProfileV1.Flamebearer.Levels)).To(Equal(2))
+		})
+	})
 })
 
 func readFile(path string) []byte {
