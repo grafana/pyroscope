9497b8f5d79029aeebf7430a4a95ff45913d6ef7
eduardo aleixo <eh-am@users.noreply.github.com>
2023-03-14 14:47:53 +0000

    build: improve ci profiling (#1847)
    * set isolatedModules: true
    
    * replace ts-jest for @swc/jest
    
    * move toMatchImageSnapshot closer to where it's used
    
    * group assertions
    
    * build: use latest version

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   // TypeScript files (.ts, .tsx) will be transformed by ts-jest to CommonJS syntax, and JavaScript files (.js, jsx) will be transformed by babel-jest.
-  preset: 'ts-jest/presets/js-with-babel',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: [path.join(__dirname, 'setupAfterEnv.ts')],
   testMatch: [
@@ -16,21 +15,13 @@ module.exports = {
     '\\.module\\.(css|scss)$': 'jest-css-modules-transform',
     '\\.(css|scss)$': 'jest-css-modules-transform',
     '\\.svg$': path.join(__dirname, 'svg-transform.js'),
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
   transformIgnorePatterns: [
     // force us to not transpile these dependencies
     // https://stackoverflow.com/a/69150188
     'node_modules/(?!(true-myth|d3|d3-array|internmap|d3-scale|react-notifications-component|graphviz-react))',
   ],
-  globals: {
-    'ts-jest': {
-      tsconfig: path.join(__dirname, `tsconfig.test.json`),
-      diagnostics: {
-        // https://github.com/kulshekhar/ts-jest/issues/1647#issuecomment-832577036
-        pathRegex: /\.(test)\.tsx$/,
-      },
-    },
-  },
   globalSetup: '<rootDir>/globalSetup.js',
   globalTeardown: '<rootDir>/globalTeardown.js',
 };
