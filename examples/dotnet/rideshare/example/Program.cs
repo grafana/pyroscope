4a0e1c0a0525d1d13e40d7ca335770d3f98bf3c8
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-04-03 21:08:05 +0700

    fix

--- examples/dotnet/rideshare/example/Program.cs
+++ examples/dotnet/rideshare/example/Program.cs
@@ -1,15 +1,19 @@
-namespace Example;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
 
+namespace Example;
 
 using System.Collections;
 using Microsoft.AspNetCore.Builder;
-
-
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        object _lock = new();
+
         var orderService = new OrderService();
         var bikeService = new BikeService(orderService);
         var scooterService = new ScooterService(orderService);
@@ -26,11 +30,64 @@ public static class Program
             scooterService.Order(2);
             return "Scooter ordered";
         });
-        
+
         app.MapGet("/car", () =>
         {
             carService.Order(3);
             return "Car ordered";
+        });
+        app.MapGet("/debug/sampler", (HttpRequest request) =>
+        {
+            bool enable = request.Query["enable"] == "true";
+            Pyroscope.Profiler.Instance.SetStackSamplerEnabled(enable);
+            return "OK";
+        });
+        app.MapGet("/debug/allocation", (HttpRequest request) =>
+        {
+            bool enable = request.Query["enable"] == "true";
+            Pyroscope.Profiler.Instance.SetAllocationTrackingEnabled(enable);
+            return "OK";
+        });
+        app.MapGet("/debug/contention", (HttpRequest request) =>
+        {
+            bool enable = request.Query["enable"] == "true";
+            Pyroscope.Profiler.Instance.SetContentionTrackingEnabled(enable);
+            return "OK";
+        });
+        app.MapGet("/pg/allocate", (HttpRequest request) =>
+        {
+            List<String> ss = new List<string>();
+            for (int i = 0; i < 10000; i++)
+            {
+                ss.Add("foobar" + i);
+            }
+
+            return "OK";
+        });
+        app.MapGet("/pg/exception", (HttpRequest request) =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    throw new Exception("foobar" + i);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+            return "OK";
+        });
+        app.MapGet("/pg/contention", (HttpRequest request) =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock (_lock)
+                {
+                    Thread.Sleep(10);
+                }
+            }
+            return "OK";
         });
         app.MapGet("/", () =>
         {
@@ -41,10 +98,7 @@ public static class Program
             }
             return env;
         });
-        
+
         app.Run();
     }
 }
-
-
-
