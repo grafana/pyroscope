adc98666da00215c7483600a58bdb42f8cc2a699
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-04 11:37:43 +0100

    add some css to make it look okayish

--- public/app/app.tsx
+++ public/app/app.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './jquery-import';
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store';
+import '@webapp/../sass/profile.scss';
+import '@szhsin/react-menu/dist/index.css';
+
 import { SingleView } from './pages/SingleView';
 
 const root = ReactDOM.createRoot(document.getElementById('reactRoot'));
-root.render(<SingleView />);
+root.render(
+  <Provider store={store}>
+    <SingleView />
+  </Provider>
+);
