34cd913868112aea631e60f9d91225cfe5406c0a
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-06 13:26:37 +0100

    fix type check

--- public/app/overrides/services/tags.ts
+++ public/app/overrides/services/tags.ts
@@ -1,7 +1,7 @@
 import { Result } from '@webapp/util/fp';
 
 export async function fetchTags(query: string, from: number, until: number) {
-  return Result.ok([]);
+  return Result.ok<string[], { message: string }>([]);
 }
 
 export async function fetchLabelValues(
