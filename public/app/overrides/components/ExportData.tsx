ead770720ff1d196a3db43b4759f824bd165d337
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-14 11:54:51 +0100

    fix type check

--- public/app/overrides/components/ExportData.tsx
+++ public/app/overrides/components/ExportData.tsx
@@ -1,11 +1,11 @@
 interface ExportDataProps {
-  flamebearer: unknown;
-  exportPNG: unknown;
-  exportJSON: unknown;
-  exportPprof: unknown;
-  exportHTML: unknown;
-  exportFlamegraphDotCom: unknown;
-  exportFlamegraphDotComFn: unknown;
+  flamebearer?: unknown;
+  exportPNG?: unknown;
+  exportJSON?: unknown;
+  exportPprof?: unknown;
+  exportHTML?: unknown;
+  exportFlamegraphDotCom?: unknown;
+  exportFlamegraphDotComFn?: unknown;
 }
 export default function (props: ExportDataProps) {
   return <></>;
