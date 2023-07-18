f1089a2d4a8f98be4de6f7273e3e0081bd30a3b7
Dmitry Filimonov <dmitry@pyroscope.io>
2023-07-18 16:04:26 -0700

    deletes old og packages

--- og/pkg/convert/pprof/streaming/labels_cache_arenas_disabled.go
+++
@@ -1,9 +0,0 @@
-//go:build !goexperiment.arenas
-
-package streaming
-
-import "github.com/pyroscope-io/pyroscope/pkg/storage/tree"
-
-func (c *LabelsCache) newCacheEntryA(l Labels) (int, *tree.Tree) {
-	return c.newCacheEntry(l)
-}
