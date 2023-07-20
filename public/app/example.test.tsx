8cfb9bd10b65c2b23237b02e8edbd3cea951987b
Darren Janeczek <darren.janeczek@grafana.com>
2023-07-20 17:58:19 -0400

    fix: prettier

--- public/app/example.test.tsx
+++ public/app/example.test.tsx
@@ -4,6 +4,6 @@ describe('noop', () => {
   });
 });
 
-// TS1208: 'example.test.tsx' cannot be compiled under '--isolatedModules' because it is considered a global script file. 
+// TS1208: 'example.test.tsx' cannot be compiled under '--isolatedModules' because it is considered a global script file.
 // Add an import, export, or an empty 'export {}' statement to make it a module.
 export {};
