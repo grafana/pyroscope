f507bd7e9eb04c364545f5adcb23126c742411b2
Cyril Tovena <cyril.tovena@gmail.com>
2023-07-24 11:20:52 +0200

    Adds push limits on profiles data (#2157)
    * Adds push limits on profiles data
    
    * generate cmd help
    
    * make reference-help
    
    * Tweak default based on review feedback
    
    * Tweak default based on review feedback

--- pkg/validation/testutil.go
+++ pkg/validation/testutil.go
@@ -10,6 +10,12 @@ type MockLimits struct {
 	MaxLabelNameLengthValue     int
 	MaxLabelValueLengthValue    int
 	MaxLabelNamesPerSeriesValue int
+
+	MaxProfileSizeBytesValue              int
+	MaxProfileStacktraceSamplesValue      int
+	MaxProfileStacktraceDepthValue        int
+	MaxProfileStacktraceSampleLabelsValue int
+	MaxProfileSymbolValueLengthValue      int
 }
 
 func (m MockLimits) QuerySplitDuration(string) time.Duration        { return m.QuerySplitDurationValue }
@@ -19,3 +25,19 @@ func (m MockLimits) MaxQueryLookback(tenantID string) time.Duration { return m.M
 func (m MockLimits) MaxLabelNameLength(userID string) int           { return m.MaxLabelNameLengthValue }
 func (m MockLimits) MaxLabelValueLength(userID string) int          { return m.MaxLabelValueLengthValue }
 func (m MockLimits) MaxLabelNamesPerSeries(userID string) int       { return m.MaxLabelNamesPerSeriesValue }
+func (m MockLimits) MaxProfileSizeBytes(userID string) int          { return m.MaxProfileSizeBytesValue }
+func (m MockLimits) MaxProfileStacktraceSamples(userID string) int {
+	return m.MaxProfileStacktraceSamplesValue
+}
+
+func (m MockLimits) MaxProfileStacktraceDepth(userID string) int {
+	return m.MaxProfileStacktraceDepthValue
+}
+
+func (m MockLimits) MaxProfileStacktraceSampleLabels(userID string) int {
+	return m.MaxProfileStacktraceSampleLabelsValue
+}
+
+func (m MockLimits) MaxProfileSymbolValueLength(userID string) int {
+	return m.MaxProfileSymbolValueLengthValue
+}
