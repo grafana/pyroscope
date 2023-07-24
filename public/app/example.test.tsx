b552ed555051f931078823047ef2fd20bc84d274
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2023-07-24 19:03:55 -0400

    fix: remove dependency on pyroscope-oss git branch (#2143)
    * fix: remove dependency on pyroscope-oss git branch
    
    also, fix repository entry in package.json
    
    * fix: update versions of node (lts) and oauth2-mock-server
    
    * fix: update dependencies
    
    * fix: type issue
    
    * fix: manually add css-loader dependency
    
    * fix: manually add sass-loader dependency
    
    * fix: type checks
    
    * fix: aliases and workspaces

--- public/app/example.test.tsx
+++ public/app/example.test.tsx
@@ -3,3 +3,7 @@ describe('noop', () => {
     expect(true).toBe(true);
   });
 });
+
+// TS1208: 'example.test.tsx' cannot be compiled under '--isolatedModules' because it is considered a global script file.
+// Add an import, export, or an empty 'export {}' statement to make it a module.
+export {};
