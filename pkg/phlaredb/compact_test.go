d3d7b9fca628a87365647ea2dce88f347adca24d
Cyril Tovena <cyril.tovena@gmail.com>
2023-07-26 09:00:47 +0200

    Remove bad tests

--- pkg/phlaredb/compact_test.go
+++ pkg/phlaredb/compact_test.go
@@ -3,139 +3,57 @@ package phlaredb
 import (
 	"context"
 	"fmt"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"path/filepath"
-	"sort"
-	"sync"
 	"testing"
-	"time"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/segmentio/parquet-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/sync/errgroup"
 
 	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
 	phlaremodel "github.com/grafana/pyroscope/pkg/model"
-	phlareobj "github.com/grafana/pyroscope/pkg/objstore"
-	"github.com/grafana/pyroscope/pkg/objstore/client"
-	"github.com/grafana/pyroscope/pkg/objstore/providers/filesystem"
-	"github.com/grafana/pyroscope/pkg/objstore/providers/gcs"
 	"github.com/grafana/pyroscope/pkg/phlaredb/block"
 	schemav1 "github.com/grafana/pyroscope/pkg/phlaredb/schemas/v1"
 	"github.com/grafana/pyroscope/pkg/phlaredb/tsdb/index"
 )
 
-func init() {
-	go func() {
-		_ = http.ListenAndServe("localhost:6060", nil)
-	}()
-}
+// func testCompact(t *testing.T, metas []*block.Meta, bkt phlareobj.Bucket, dst string) {
+// 	t.Helper()
+// 	g, ctx := errgroup.WithContext(context.Background())
+// 	var src []BlockReader
+// 	now := time.Now()
+// 	for i, m := range metas {
+// 		t.Log("src block(#", i, ")",
+// 			"ID", m.ULID.String(),
+// 			"minTime", m.MinTime.Time().Format(time.RFC3339Nano),
+// 			"maxTime", m.MaxTime.Time().Format(time.RFC3339Nano),
+// 			"numSeries", m.Stats.NumSeries,
+// 			"numProfiles", m.Stats.NumProfiles,
+// 			"numSamples", m.Stats.NumSamples)
+// 		b := NewSingleBlockQuerierFromMeta(ctx, bkt, m)
+// 		g.Go(func() error {
+// 			if err := b.Open(ctx); err != nil {
+// 				return err
+// 			}
+// 			return b.stacktraces.Load(ctx)
+// 		})
+// 		src = append(src, b)
+// 	}
 
-func TestCompact(t *testing.T) {
-	t.TempDir()
-	ctx := context.Background()
-	bkt, err := client.NewBucket(ctx, client.Config{
-		StorageBackendConfig: client.StorageBackendConfig{
-			Backend: client.GCS,
-			GCS: gcs.Config{
-				BucketName: "dev-us-central-0-profiles-dev-001-data",
-			},
-		},
-		StoragePrefix: "1218/phlaredb/",
-	}, "test")
-	require.NoError(t, err)
-	now := time.Now()
-	var (
-		meta []*block.Meta
-		mtx  sync.Mutex
-	)
+// 	require.NoError(t, g.Wait())
 
-	err = block.IterBlockMetas(ctx, bkt, now.Add(-24*time.Hour), now, func(m *block.Meta) {
-		mtx.Lock()
-		defer mtx.Unlock()
-		meta = append(meta, m)
-	})
-	require.NoError(t, err)
-	dst := t.TempDir()
-
-	sort.Slice(meta, func(i, j int) bool {
-		return meta[i].MinTime.Before(meta[j].MinTime)
-	})
-
-	// only test on the 4 latest blocks.
-	meta = meta[len(meta)-4:]
-	testCompact(t, meta, bkt, dst)
-}
-
-// to download the blocks:
-// gsutil -m cp -r \
-// "gs://dev-us-central-0-profiles-dev-001-data/1218/phlaredb/01H53WJEAB43S3GJ26XMSNRSJA" \
-// "gs://dev-us-central-0-profiles-dev-001-data/1218/phlaredb/01H5454JBEV80V2J7CKYHPCBG8" \
-// "gs://dev-us-central-0-profiles-dev-001-data/1218/phlaredb/01H54553SYKH43FNJN5BVR1M2H" \
-// "gs://dev-us-central-0-profiles-dev-001-data/1218/phlaredb/01H5457Q89WYYH9FCK8PZ6XG75" \
-// .
-func TestCompactLocal(t *testing.T) {
-	t.TempDir()
-	ctx := context.Background()
-	bkt, err := client.NewBucket(ctx, client.Config{
-		StorageBackendConfig: client.StorageBackendConfig{
-			Backend: client.Filesystem,
-			Filesystem: filesystem.Config{
-				Directory: "/Users/cyril/work/phlare-data/",
-			},
-		},
-		StoragePrefix: "",
-	}, "test")
-	require.NoError(t, err)
-	var metas []*block.Meta
-
-	metaMap, err := block.ListBlocks("/Users/cyril/work/phlare-data/", time.Time{})
-	require.NoError(t, err)
-	for _, m := range metaMap {
-		metas = append(metas, m)
-	}
-	dst := t.TempDir()
-	testCompact(t, metas, bkt, dst)
-}
-
-func testCompact(t *testing.T, metas []*block.Meta, bkt phlareobj.Bucket, dst string) {
-	t.Helper()
-	g, ctx := errgroup.WithContext(context.Background())
-	var src []BlockReader
-	now := time.Now()
-	for i, m := range metas {
-		t.Log("src block(#", i, ")",
-			"ID", m.ULID.String(),
-			"minTime", m.MinTime.Time().Format(time.RFC3339Nano),
-			"maxTime", m.MaxTime.Time().Format(time.RFC3339Nano),
-			"numSeries", m.Stats.NumSeries,
-			"numProfiles", m.Stats.NumProfiles,
-			"numSamples", m.Stats.NumSamples)
-		b := NewSingleBlockQuerierFromMeta(ctx, bkt, m)
-		g.Go(func() error {
-			if err := b.Open(ctx); err != nil {
-				return err
-			}
-			return b.stacktraces.Load(ctx)
-		})
-		src = append(src, b)
-	}
-
-	require.NoError(t, g.Wait())
-
-	new, err := Compact(context.Background(), src, dst)
-	require.NoError(t, err)
-	t.Log(new, dst)
-	t.Log("Compaction duration", time.Since(now))
-	t.Log("numSeries", new.Stats.NumSeries,
-		"numProfiles", new.Stats.NumProfiles,
-		"numSamples", new.Stats.NumSamples)
-}
+// 	new, err := Compact(context.Background(), src, dst)
+// 	require.NoError(t, err)
+// 	t.Log(new, dst)
+// 	t.Log("Compaction duration", time.Since(now))
+// 	t.Log("numSeries", new.Stats.NumSeries,
+// 		"numProfiles", new.Stats.NumProfiles,
+// 		"numSamples", new.Stats.NumSamples)
+// }
 
 type blockReaderMock struct {
 	BlockReader
