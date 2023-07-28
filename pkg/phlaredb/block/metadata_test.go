0a6d851c2b7a78bd6ac856e86c878532cbf33688
Cyril Tovena <cyril.tovena@gmail.com>
2023-07-28 16:53:21 +0200

    Adds a clone function for meta file

---
+++ pkg/phlaredb/block/metadata_test.go
@@ -0,0 +1,52 @@
+package block
+
+import (
+	"testing"
+
+	"github.com/oklog/ulid"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/tsdb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClone(t *testing.T) {
+	require.Equal(t, &Meta{}, (&Meta{}).Clone())
+	expected := &Meta{
+		ULID:    generateULID(),
+		MinTime: model.Time(1),
+		MaxTime: model.Time(2),
+		Labels:  map[string]string{"a": "b"},
+		Version: MetaVersion2,
+		Stats: BlockStats{
+			NumSamples:  1,
+			NumSeries:   2,
+			NumProfiles: 3,
+		},
+		Files: []File{
+			{
+				RelPath:   "a",
+				SizeBytes: 1,
+			},
+		},
+		Source: IngesterSource,
+		Compaction: tsdb.BlockMetaCompaction{
+			Level:     1,
+			Sources:   []ulid.ULID{generateULID()},
+			Deletable: true,
+			Parents: []tsdb.BlockDesc{
+				{
+					ULID:    generateULID(),
+					MinTime: 1,
+					MaxTime: 2,
+				},
+				{
+					ULID:    generateULID(),
+					MinTime: 2,
+					MaxTime: 3,
+				},
+			},
+			Hints: []string{"a", "b"},
+		},
+	}
+	require.Equal(t, expected, expected.Clone())
+}
