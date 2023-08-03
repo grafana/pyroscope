8321daaaab709ea54a0c0b7bcae535c41c4cba3d
Bryan Huhta <bryan.huhta@grafana.com>
2023-08-03 16:50:14 -0500

    Don't conditionally check to pad to end of window

--- pkg/querier/timeline/timeline.go
+++ pkg/querier/timeline/timeline.go
@@ -16,7 +16,7 @@ func New(series *v1.Series, startMs int64, endMs int64, durationDeltaSec int64) 
 	// ms to seconds
 	startSec := startMs / 1000
 	points := series.GetPoints()
-	res := make([]uint64, 0, sizeToBackfill(startMs, endMs, durationDeltaSec))
+	samples := make([]uint64, 0, sizeToBackfill(startMs, endMs, durationDeltaSec))
 
 	timeline := &flamebearer.FlamebearerTimelineV1{
 		StartTime:     startSec,
@@ -26,7 +26,7 @@ func New(series *v1.Series, startMs int64, endMs int64, durationDeltaSec int64) 
 
 	if len(points) < 1 {
 		if n := sizeToBackfill(startMs, endMs, durationDeltaSec); n > 0 {
-			timeline.Samples = res[:n]
+			timeline.Samples = samples[:n]
 		}
 		return timeline
 	}
@@ -34,32 +34,29 @@ func New(series *v1.Series, startMs int64, endMs int64, durationDeltaSec int64) 
 	// Backfill before the first data point.
 	firstAvailableData := points[0]
 	if n := sizeToBackfill(startMs, firstAvailableData.Timestamp, durationDeltaSec); n > 0 {
-		res = res[:len(res)+int(n)]
+		samples = samples[:len(samples)+int(n)]
 	}
 
 	prev := points[0]
 	for _, curr := range points {
 		if n := sizeToBackfill(prev.Timestamp, curr.Timestamp, durationDeltaSec) - 1; n > 0 {
 			// Insert backfill.
-			res = res[:len(res)+int(n)]
+			samples = samples[:len(samples)+int(n)]
 		}
 
-		res = append(res, uint64(curr.Value))
+		samples = append(samples, uint64(curr.Value))
 		prev = curr
 	}
 
-	// Backfill after the last data point.
-	lastAvailableData := points[len(points)-1]
-	if n := sizeToBackfill(lastAvailableData.Timestamp, endMs, durationDeltaSec) - 1; n > 0 {
-		res = res[:len(res)+int(n)]
-	}
+	// Backfill to the end of the samples window.
+	samples = samples[:cap(samples)]
 
-	timeline.Samples = res
+	timeline.Samples = samples
 	return timeline
 }
 
-// sizeToBackfill indicates how many items are needed to backfill
-// if none are needed, a negative value is returned
+// sizeToBackfill indicates how many items are needed to backfill in the range
+// [startMs, endMs).
 func sizeToBackfill(startMs int64, endMs int64, stepSec int64) int64 {
 	startSec := startMs / 1000
 	endSec := endMs / 1000
