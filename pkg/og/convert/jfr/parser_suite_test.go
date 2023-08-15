13fc8107b9666530d6b7a3c909488502e49f35e7
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-08-15 17:14:05 +0700

    Add test & data

--- pkg/og/convert/jfr/parser_suite_test.go
+++ pkg/og/convert/jfr/parser_suite_test.go
@@ -1,13 +1,60 @@
 package jfr
 
 import (
+	"bytes"
+	"context"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/grafana/pyroscope/pkg/og/convert/pprof/bench"
+	"github.com/grafana/pyroscope/pkg/og/storage"
+	"github.com/grafana/pyroscope/pkg/og/storage/segment"
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
 func TestParser(t *testing.T) {
 	RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "Java JFR Parser suite")
+}
+
+func TestParseCompareExpectedData(t *testing.T) {
+	testdata := []struct {
+		f string
+	}{
+		{"testdata/cortex-dev-01__kafka-0__cpu__0.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu__1.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu__2.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu__3.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu_lock_alloc__0.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu_lock_alloc__1.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu_lock_alloc__2.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu_lock_alloc__3.jfr.gz"},
+		{"testdata/cortex-dev-01__kafka-0__cpu_lock0_alloc0__0.jfr.gz"},
+	}
+	for _, td := range testdata {
+		t.Run(td.f, func(t *testing.T) {
+			jfr, err := bench.ReadGzipFile(td.f)
+			require.NoError(t, err)
+			putter := &bench.MockPutter{Keep: true}
+			k, err := segment.ParseKey("kafka.app")
+			require.NoError(t, err)
+
+			pi := &storage.PutInput{
+				StartTime:  time.UnixMilli(1000),
+				EndTime:    time.UnixMilli(2000),
+				Key:        k,
+				SpyName:    "java",
+				SampleRate: 100,
+			}
+			err = ParseJFR(context.TODO(), putter, bytes.NewBuffer(jfr), pi, nil)
+			require.NoError(t, err)
+			jsonFile := strings.TrimSuffix(td.f, ".jfr.gz") + ".json.gz"
+			//err = putter.DumpJson(jsonFile)
+			err = putter.CompareWithJson(jsonFile)
+			require.NoError(t, err)
+		})
+	}
 }
