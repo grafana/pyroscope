d19d7fee15ce2c14e91b44ce9c7dca597811c10e
Darren Janeczek <darren.janeczek@grafana.com>
2023-08-16 15:03:09 -0400

    fix: unremove deprecated

--- public/app/legacy/models/flamebearer.ts
+++ public/app/legacy/models/flamebearer.ts
@@ -1,3 +1,37 @@
+import { SpyName } from './spyName';
+import { Units } from './units';
+/**
+ * @deprecated one should use the Profile model
+ */
+export type Flamebearer = {
+  /**
+   * List of names
+   */
+  names: string[];
+  /**
+   * List of level
+   *
+   * This is NOT the same as in the flamebearer
+   * that we receive from the server.
+   * As in there are some transformations required
+   * (see deltaDiffWrapper)
+   */
+  levels: number[][];
+  numTicks: number;
+  maxSelf: number;
+
+  /**
+   * Sample Rate, used in text information
+   */
+  sampleRate: number;
+  units: Units;
+
+  spyName: SpyName;
+  // format: 'double' | 'single';
+  //  leftTicks?: number;
+  //  rightTicks?: number;
+} & addTicks;
+
 export type addTicks =
   | { format: 'double'; leftTicks: number; rightTicks: number }
   | { format: 'single' };
