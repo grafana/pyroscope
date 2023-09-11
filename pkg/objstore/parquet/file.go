7d266afaf7da9ada9c896c28fea209b33463741c
Christian Simon <simon@swine.de>
2023-09-11 17:47:56 +0100

    Implement speculative footer caching
    This will fetch a 1000th of the block size in footer size and will cache
    it until it is fully opened. This reduces the roundtrips required to
    open a parquet file from 6 to 1 RTTs.

--- pkg/objstore/parquet/file.go
+++ pkg/objstore/parquet/file.go
@@ -13,29 +13,31 @@ import (
 type File struct {
 	*parquet.File
 	reader objstore.ReaderAtCloser
-	path   string
-	size   int64
+	meta   block.File
 }
 
 func (f *File) Open(ctx context.Context, b objstore.BucketReader, meta block.File, options ...parquet.FileOption) error {
-	f.path = meta.RelPath
-	f.size = int64(meta.SizeBytes)
-
-	if f.size == 0 {
-		attrs, err := b.Attributes(ctx, f.path)
+	if meta.SizeBytes == 0 {
+		attrs, err := b.Attributes(ctx, meta.RelPath)
 		if err != nil {
 			return fmt.Errorf("getting attributes: %w", err)
 		}
-		f.size = attrs.Size
+		meta.SizeBytes = uint64(attrs.Size)
 	}
 	var err error
 	// the same reader is used to serve all requests, so we pass context.Background() here
-	if f.reader, err = OptimizedBucketReaderAt(b, context.Background(), f.path); err != nil {
+	ra, err := OptimizedBucketReaderAt(b, context.Background(), meta)
+	if err != nil {
 		return fmt.Errorf("creating reader: %w", err)
 	}
+	f.reader = ra
+	ora := ra.(*optimizedReaderAt)
+
+	// after finishing opening, clear footer cache
+	defer ora.clearFooterCache()
 
 	// first try to open file, this is required otherwise OpenFile panics
-	f.File, err = parquet.OpenFile(f.reader, f.size,
+	f.File, err = parquet.OpenFile(f.reader, int64(meta.SizeBytes),
 		parquet.SkipPageIndex(true),
 		parquet.SkipBloomFilters(true))
 	if err != nil {
@@ -43,7 +45,7 @@ func (f *File) Open(ctx context.Context, b objstore.BucketReader, meta block.Fil
 	}
 
 	// now open it for real
-	f.File, err = parquet.OpenFile(f.reader, f.size,
+	f.File, err = parquet.OpenFile(f.reader, int64(meta.SizeBytes),
 		options...,
 	)
 	return err
@@ -56,4 +58,4 @@ func (f *File) Close() (err error) {
 	return nil
 }
 
-func (f *File) Path() string { return f.path }
+func (f *File) Path() string { return f.meta.RelPath }
