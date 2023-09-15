9e1d40c2202884194df055932068068b51b0a640
Darren Janeczek <darren.janeczek@grafana.com>
2023-09-15 15:46:25 -0400

    fix: various bugs that occur when selection is too small

--- public/app/components/TimelineChart/TimelineChart.tsx
+++ public/app/components/TimelineChart/TimelineChart.tsx
@@ -24,10 +24,45 @@ class TimelineChart extends ReactFlot<TimelineChartProps> {
 
     // TODO: use ref
     $(`#${this.props.id}`).bind('plotselected', (event, ranges) => {
-      this.props.onSelect(
-        Math.round(ranges.xaxis.from / 1000).toString(),
-        Math.round(ranges.xaxis.to / 1000).toString()
-      );
+      // Before invoking the onselect, we ensure the selection is valid
+
+      if (ranges.xaxis == null) {
+        // Invalid range, do nothing.
+        return;
+      }
+
+      const {from, to} = ranges.xaxis;
+
+      let fromSeconds = Math.round(from / 1000);
+      let untilSeconds = Math.round(to / 1000);
+
+      if (fromSeconds === untilSeconds) {
+        // If the time ranges have a difference of zero seconds, we skip the zoom.
+        return;
+      }
+
+      let morePointsThanPixels = false;
+      let pointCount = 0;
+
+      for (const dataRange of this.props.data) {
+
+        if (morePointsThanPixels || (dataRange.data.length > event.currentTarget.clientWidth)) {
+          // We can assume there are some points in the selection, so we will short circuit out
+          morePointsThanPixels = true;
+          break;
+        }
+
+        dataRange.data.forEach(
+          (point: number[]) => pointCount += Number(point[0] > from && point[0] < to)
+        )
+      }
+
+      if (!morePointsThanPixels && pointCount < 3) {
+        // If we are zooming in and there are fewer than 3 points, we cancel the zoom.
+        return;
+      }
+
+      this.props.onSelect(fromSeconds.toString(), untilSeconds.toString());
     });
   }
 
