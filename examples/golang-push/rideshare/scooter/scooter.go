23552c2f2e414fb36ae6297029912ff14168a47c
Christian Simon <simon@swine.de>
2023-10-02 17:49:55 +0100

    Add otel logs to rideshare

--- examples/golang-push/rideshare/scooter/scooter.go
+++ examples/golang-push/rideshare/scooter/scooter.go
@@ -2,9 +2,11 @@ package scooter
 
 import (
 	"context"
+	"rideshare/rideshare"
 	"rideshare/utility"
 )
 
 func OrderScooter(ctx context.Context, searchRadius int64) {
+	rideshare.Log.Printf(ctx, "ordering scooter, with searchRadius=%d", searchRadius)
 	utility.FindNearestVehicle(ctx, searchRadius, "scooter")
 }
