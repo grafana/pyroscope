17159a668fab4ec448214f2fb6e4f028cbd4c3b0
Cyril Tovena <cyril.tovena@gmail.com>
2023-10-03 19:42:42 +0200

    Use stable time format for logs

--- pkg/compactor/split_merge_job.go
+++ pkg/compactor/split_merge_job.go
@@ -8,6 +8,7 @@ package compactor
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/prometheus/prometheus/model/labels"
 	"golang.org/x/exp/slices"
@@ -84,7 +85,7 @@ func (j *job) String() string {
 	for _, block := range j.blocks {
 		minT := block.MinTime.Time().UTC()
 		maxT := block.MaxTime.Time().UTC()
-		blocks = append(blocks, fmt.Sprintf("%s (min time: %s, max time: %s)", block.ULID.String(), minT.String(), maxT.String()))
+		blocks = append(blocks, fmt.Sprintf("%s (min time: %s, max time: %s)", block.ULID.String(), minT.Format(time.RFC3339), maxT.Format(time.RFC3339)))
 	}
 
 	// Keep the output stable for tests.
