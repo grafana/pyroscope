7ba342dec54cf500de986dd79fefbe77e8b75e5d
Bryan Huhta <bryan.huhta@grafana.com>
2023-10-04 14:02:19 -0500

    Add start/end validation to LabelNames frontend

--- pkg/frontend/frontend_label_names.go
+++ pkg/frontend/frontend_label_names.go
@@ -4,13 +4,38 @@ import (
 	"context"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/grafana/dskit/tenant"
+	"github.com/prometheus/common/model"
 
 	"github.com/grafana/pyroscope/api/gen/proto/go/querier/v1/querierv1connect"
 	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
 	"github.com/grafana/pyroscope/pkg/util/connectgrpc"
+	"github.com/grafana/pyroscope/pkg/validation"
 )
 
 func (f *Frontend) LabelNames(ctx context.Context, c *connect.Request[typesv1.LabelNamesRequest]) (*connect.Response[typesv1.LabelNamesResponse], error) {
 	ctx = connectgrpc.WithProcedure(ctx, querierv1connect.QuerierServiceLabelNamesProcedure)
+
+	tenantIDs, err := tenant.TenantIDs(ctx)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	if c.Msg.Start != 0 && c.Msg.End != 0 {
+		interval := model.Interval{
+			Start: model.Time(c.Msg.Start),
+			End:   model.Time(c.Msg.End),
+		}
+		validated, err := validation.ValidateRangeRequest(f.limits, tenantIDs, interval, model.Now())
+		if err != nil {
+			return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		}
+		if validated.IsEmpty {
+			return connect.NewResponse(&typesv1.LabelNamesResponse{}), nil
+		}
+		c.Msg.Start = int64(validated.Start)
+		c.Msg.End = int64(validated.End)
+	}
+
 	return connectgrpc.RoundTripUnary[typesv1.LabelNamesRequest, typesv1.LabelNamesResponse](ctx, f, c)
 }
