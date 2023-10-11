72510397a91daca8b093818ff52388e644699d6f
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2023-10-11 09:32:18 -0400

    chore: add some clarifying comments to `useColorMode` (#2501)

--- public/app/hooks/colorMode.hook.ts
+++ public/app/hooks/colorMode.hook.ts
@@ -2,18 +2,29 @@ import { useCallback, useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '@pyroscope/redux/hooks';
 import { setColorMode, selectAppColorMode } from '@pyroscope/redux/reducers/ui';
 
+/** Obtain the current color mode (light|dark) and ensure all representations of it are in sync
+ *
+ * Some elements of style rely on the `<body>` tag's `data-theme` attribute
+ * (accessibile via document.body.dataset.theme)
+ * while others must consult with the color mode set in the redux store.
+ *
+ * This hook ensures that any change in the redux store is reflected in the body theme,
+ * and vice versa.
+ */
 const useColorMode = () => {
   const dispatch = useAppDispatch();
   const colorMode = useAppSelector(selectAppColorMode);
   const { body } = document;
 
+  /** This callback updates the color mode in the redux store. */
   const changeColorMode = useCallback(
     (newColorMode: 'light' | 'dark') => dispatch(setColorMode(newColorMode)),
     [dispatch]
   );
 
+  // This effect sets up an observer for changes in the `<body>` `data-theme` attr
+  // which updates the redux value to keep them in sync.
   useEffect(() => {
-    // sync data-theme attr with redux value
     const observer = new MutationObserver((mutationsList) => {
       const mutation = mutationsList[0];
 
@@ -40,12 +51,14 @@ const useColorMode = () => {
     };
   }, [body, changeColorMode, colorMode]);
 
+  // This effect will update the `<body>` tag's `data-theme` attribute
+  // to ensure that it matches the color mode from the redux store.
   useEffect(() => {
     // sync redux value with DOM body attr
     if (body.dataset.theme !== colorMode) {
       body.dataset.theme = colorMode;
     }
-  }, [colorMode, body, body.dataset]);
+  }, [colorMode, body]);
 
   return {
     colorMode,
