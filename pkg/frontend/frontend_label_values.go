ec21725c5144f585ecbf9805c7b87a6d6cce1195
Bryan Huhta <bryan.huhta@grafana.com>
2023-10-11 10:37:39 -0500

    Add validation for start/end

--- pkg/frontend/frontend_label_values.go
+++ pkg/frontend/frontend_label_values.go
@@ -4,13 +4,39 @@ import (
 	"context"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/grafana/dskit/tenant"
+	"github.com/prometheus/common/model"
 
 	"github.com/grafana/pyroscope/api/gen/proto/go/querier/v1/querierv1connect"
 	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
+	"github.com/grafana/pyroscope/pkg/util"
 	"github.com/grafana/pyroscope/pkg/util/connectgrpc"
+	"github.com/grafana/pyroscope/pkg/validation"
 )
 
 func (f *Frontend) LabelValues(ctx context.Context, c *connect.Request[typesv1.LabelValuesRequest]) (*connect.Response[typesv1.LabelValuesResponse], error) {
 	ctx = connectgrpc.WithProcedure(ctx, querierv1connect.QuerierServiceLabelValuesProcedure)
+
+	tenantIDs, err := tenant.TenantIDs(ctx)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	if util.HasTimeRange(c.Msg) {
+		interval := model.Interval{
+			Start: model.Time(c.Msg.Start),
+			End:   model.Time(c.Msg.End),
+		}
+		validated, err := validation.ValidateRangeRequest(f.limits, tenantIDs, interval, model.Now())
+		if err != nil {
+			return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		}
+		if validated.IsEmpty {
+			return connect.NewResponse(&typesv1.LabelValuesResponse{}), nil
+		}
+		c.Msg.Start = int64(validated.Start)
+		c.Msg.End = int64(validated.End)
+	}
+
 	return connectgrpc.RoundTripUnary[typesv1.LabelValuesRequest, typesv1.LabelValuesResponse](ctx, f, c)
 }
