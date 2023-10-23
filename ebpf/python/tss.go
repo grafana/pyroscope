e475463ba4eca744518da2be4180f70b9fe7ff32
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-10-23 11:53:47 +0700

    check tss init

--- ebpf/python/tss.go
+++ ebpf/python/tss.go
@@ -12,27 +12,22 @@ func GetTSSKey(pid uint32, version Version, offsets *UserOffsets, autoTLSkeyAddr
 		return 0, fmt.Errorf("python memory open failed   %w", err)
 	}
 	defer fd.Close()
-	var key [4]byte
-	var pkey int64
 	if version.Compare(Py37) < 0 {
-		if autoTLSkeyAddr == 0 {
-			return 0, fmt.Errorf("python missing symbols autoTLSkey %d %v", pid, version)
-		}
-		pkey = int64(autoTLSkeyAddr)
-
+		return getAutoTLSKey(pid, version, autoTLSkeyAddr, fd)
 	} else {
-		if pyRuntime == 0 {
-			//should never happen
-			return 0, fmt.Errorf("python missing symbols pyRuntime %d %v", pid, version)
-		}
-		if offsets.PyRuntimeState_gilstate == -1 || offsets.Gilstate_runtime_state_autoTSSkey == -1 || offsets.PyTssT_key == -1 {
-			// should never happen
-			return 0, fmt.Errorf("python missing offsets PyRuntimeStateGilstate GilstateRuntimeStateAutoTSSkey PyTssT_key %d %v", pid, version)
-		}
-		pkey = int64(pyRuntime) + int64(offsets.PyRuntimeState_gilstate+offsets.Gilstate_runtime_state_autoTSSkey+offsets.PyTssT_key)
+		return getPyTssKey(pid, version, offsets, pyRuntime, fd)
 	}
+}
 
-	n, err := fd.ReadAt(key[:], int64(pkey))
+func getAutoTLSKey(pid uint32, version Version, autoTLSkeyAddr uint64, mem *os.File) (int32, error) {
+	var pkey int64
+	var key [4]byte
+	if autoTLSkeyAddr == 0 {
+		return 0, fmt.Errorf("python missing symbols autoTLSkey %d %v", pid, version)
+	}
+	pkey = int64(autoTLSkeyAddr)
+
+	n, err := mem.ReadAt(key[:], int64(pkey))
 	if err != nil {
 		return 0, fmt.Errorf("python failed to read key %d %d %v %w", pid, pkey, version, err)
 	}
@@ -40,5 +35,34 @@ func GetTSSKey(pid uint32, version Version, offsets *UserOffsets, autoTLSkeyAddr
 		return 0, fmt.Errorf("python failed to read key %d %d %v %w", pid, pkey, version, err)
 	}
 	res := int32(binary.LittleEndian.Uint32(key[:]))
+	if res == -1 {
+		return 0, fmt.Errorf("python not initialized %+v", version)
+	}
+	return res, nil
+}
+
+func getPyTssKey(pid uint32, version Version, offsets *UserOffsets, pyRuntime uint64, mem *os.File) (int32, error) {
+	var pkey int64
+	var key [4]byte //todo read the whole pytss includinig initialized flag
+	if pyRuntime == 0 {
+		//should never happen
+		return 0, fmt.Errorf("python missing symbols pyRuntime %d %v", pid, version)
+	}
+	if offsets.PyRuntimeState_gilstate == -1 || offsets.Gilstate_runtime_state_autoTSSkey == -1 || offsets.PyTssT_key == -1 {
+		// should never happen
+		return 0, fmt.Errorf("python missing offsets PyRuntimeStateGilstate GilstateRuntimeStateAutoTSSkey PyTssT_key %d %v", pid, version)
+	}
+	pkey = int64(pyRuntime) + int64(offsets.PyRuntimeState_gilstate+offsets.Gilstate_runtime_state_autoTSSkey+offsets.PyTssT_key)
+	n, err := mem.ReadAt(key[:], int64(pkey))
+	if err != nil {
+		return 0, fmt.Errorf("python failed to read key %d %d %v %w", pid, pkey, version, err)
+	}
+	if n != 4 {
+		return 0, fmt.Errorf("python failed to read key %d %d %v %w", pid, pkey, version, err)
+	}
+	res := int32(binary.LittleEndian.Uint32(key[:]))
+	if res == -1 {
+		return 0, fmt.Errorf("python not initialized %+v", version)
+	}
 	return res, nil
 }
