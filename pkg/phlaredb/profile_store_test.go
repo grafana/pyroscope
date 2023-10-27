9bb43ce79996889ce0a3ad10cf52befac83a1fbd
Cyril Tovena <cyril.tovena@gmail.com>
2023-10-27 23:33:38 +0200

    Uses a temp dir for the test to avoid left-over.

--- pkg/phlaredb/profile_store_test.go
+++ pkg/phlaredb/profile_store_test.go
@@ -598,9 +598,10 @@ func TestProfileStore_Querying(t *testing.T) {
 
 func TestRemoveFailedSegment(t *testing.T) {
 	store := newProfileStore(testContext(t))
-	require.NoError(t, store.Init("", defaultParquetConfig, contextHeadMetrics(context.Background())))
+	dir := t.TempDir()
+	require.NoError(t, store.Init(dir, defaultParquetConfig, contextHeadMetrics(context.Background())))
 	// fake a failed segment
-	_, err := os.Create("profiles.0.parquet")
+	_, err := os.Create(dir + "/profiles.0.parquet")
 	require.NoError(t, store.ingest(context.Background(), []schemav1.InMemoryProfile{{}}, phlaremodel.LabelsFromStrings(), "memory"))
 	require.NoError(t, err)
 	err = store.cutRowGroup(1)
