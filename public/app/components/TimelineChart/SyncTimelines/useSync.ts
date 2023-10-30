58df86d48d7e161c37a2b6a1f2559e66e1e5fc0d
Marc M <146180665+grafakus@users.noreply.github.com>
2023-10-30 16:17:33 +0100

    fix(SyncBanner): Add 10% buffer on both sides of the timeline (#2608)

--- public/app/components/TimelineChart/SyncTimelines/useSync.ts
+++ public/app/components/TimelineChart/SyncTimelines/useSync.ts
@@ -17,7 +17,6 @@ interface UseSyncParams {
   onSync: (from: string, until: string) => void;
 }
 
-const timeOffset = 5 * 60 * 1000;
 const selectionOffset = 5000;
 
 export const getTitle = (leftInRange: boolean, rightInRange: boolean) => {
@@ -36,6 +35,8 @@ const isInRange = (
   selectionFrom: number,
   selectionTo: number
 ) => {
+  const timeOffset = (to - from) * 0.1;
+
   return selectionFrom + timeOffset >= from && selectionTo - timeOffset <= to;
 };
 
@@ -48,10 +49,8 @@ export function useSync({
   const [isIgnoring, setIgnoring] = useState(false);
 
   useEffect(() => {
-    if (isIgnoring) {
-      setIgnoring(false);
-    }
-  }, [isIgnoring, leftSelection, rightSelection, timeline]);
+    setIgnoring(false);
+  }, [leftSelection, rightSelection, timeline]);
 
   const { from: leftFrom, to: leftTo } = getSelectionBoundaries(
     leftSelection as Selection
