5f1717f5deeb1cb8dba267027806f31273423a9c
Dmitry Filimonov <dmitry@pyroscope.io>
2023-11-06 14:27:19 -0600

    adds ruby example

---
+++ examples/golang-push/rideshare/rideshare-mem/Dockerfile
@@ -0,0 +1,12 @@
+FROM ruby:3.0.1
+
+WORKDIR /opt/app
+
+COPY Gemfile ./Gemfile
+COPY Gemfile.lock ./Gemfile.lock
+# RUN bundle config set --local deployment true
+RUN bundle install
+
+COPY lib ./lib
+
+CMD [ "ruby", "lib/server.rb" ]
---
+++ examples/golang-push/rideshare/rideshare-mem/lib/utility/utility.rb
@@ -0,0 +1,38 @@
+require "pyroscope"
+
+def mutex_lock(n)
+  i = 0
+  start_time = Time.new
+  while Time.new - start_time < n * 10 do
+    i += 1
+  end
+end
+
+def check_driver_availability(n)
+  i = 0
+  start_time = Time.new
+  while Time.new - start_time < n / 2 do
+    i += 1
+  end
+
+  # Every 4 minutes this will artificially create make requests in eu-north region slow
+  # this is just for demonstration purposes to show how performance impacts show up in the
+  # flamegraph
+  current_time = Time.now
+  current_minute = current_time.strftime('%M').to_i
+  force_mutex_lock = (current_minute * 4 % 8) == 0
+
+  mutex_lock(n) if ENV["REGION"] == "eu-north" and force_mutex_lock
+end
+
+def find_nearest_vehicle(n, vehicle)
+  Pyroscope.tag_wrapper({ "vehicle" => vehicle }) do
+    i = 0
+    start_time = Time.new
+    while Time.new - start_time < n do
+      i += 1
+    end
+
+    check_driver_availability(n) if vehicle == "car"
+  end
+end
