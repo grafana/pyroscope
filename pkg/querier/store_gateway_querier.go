ff1d77dde152036219be5ddbb49f45739f0f816d
Cyril Tovena <cyril.tovena@gmail.com>
2023-11-15 09:07:04 +0100

    Update pkg/querier/store_gateway_querier.go
    Co-authored-by: Anton Kolesnikov <anton.e.kolesnikov@gmail.com>

--- pkg/querier/store_gateway_querier.go
+++ pkg/querier/store_gateway_querier.go
@@ -267,7 +267,7 @@ func (q *Querier) selectSeriesFromStoreGateway(ctx context.Context, req *ingeste
 		g.Go(util.RecoverPanic(func() error {
 			req := req.CloneVT()
 			req.Request.Hints = &ingestv1.Hints{Block: hints}
-			return r.response.Send(req.CloneVT())
+			return r.response.Send(req)
 		}))
 	}
 	if err := g.Wait(); err != nil {
