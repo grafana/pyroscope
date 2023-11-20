5daf6af86c898e855c049106028b016ac9d8b039
Marc Mignonsin <marc.mignonsin@grafana.com>
2023-11-20 11:01:00 +0100

    fix(ReduxQuerySync): Do not unsubscribe

--- public/app/redux/useReduxQuerySync.ts
+++ public/app/redux/useReduxQuerySync.ts
@@ -5,7 +5,7 @@ import ReduxQuerySync from 'redux-query-sync';
 import { actions as continuousActions } from './reducers/continuous';
 import store, { RootState } from './store';
 
-function setupReduxQuerySync() {
+export function setupReduxQuerySync() {
   // This is a bi-directional sync between the query parameters and the redux store
   // It works as follows:
   // * When URL query changes, It will dispatch the action
@@ -99,8 +99,7 @@ function setupReduxQuerySync() {
   });
 }
 
-const unsubscribe = setupReduxQuerySync();
-
+// setup & unsubscribe on unmount
 export function useReduxQuerySync() {
-  useEffect(() => unsubscribe, []);
+  useEffect(setupReduxQuerySync, []);
 }
