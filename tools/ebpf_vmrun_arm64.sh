9a59f5f80b47e36768f82a149c535f97bae5e5e4
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-11-20 19:27:47 +0700

    arm64 qemu runner

--- tools/ebpf_vmrun_arm64.sh
+++ tools/ebpf_vmrun_arm64.sh
@@ -4,45 +4,53 @@
 
 set -ex
 
-IMG=$1
-BIN=$2
+
+KERNEL=$1
+INITRD=$2
+IMG=$3
+BIN=$4
 HOST_ROOT=$(pwd)
 
-if [ -z "$IMG"  ]; then
-  echo "Usage: $0 <image> <binary>"
+function usage() {
+  echo "Usage: $0 <kernel> <initrd> <image> <binary>"
   exit 1
+}
+if [ -z "$IMG"  ]; then
+  usage
+fi
+if [ -z "$KERNEL"  ]; then
+  usage
+fi
+if [ -z "$INITRD"  ]; then
+  usage
 fi
 if [ -z "${HOST_ROOT}/${BIN}" ]; then
-  echo "Usage: $0 <image> <binary>"
-  exit 1
+  usage
 fi
 
-if [[ -z "${GITHUB_ACTIONS}" && -e "/dev/kvm" ]]; then
-  KVM_ARGS="-enable-kvm -cpu kvm64"
-fi
 
-connect="ssh -p 2222 -o StrictHostKeyChecking=no root@localhost"
+connect="ssh -p 2222 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@localhost"
 
 kill_vm() {
-  pkill qemu-system-x86 || true
+  pkill qemu-system-aarch64 || true
 }
 
 run_vm() {
   kill_vm
-  qemu-system-x86_64 \
-                  -nodefaults \
-                  -display none \
-                  -no-reboot \
-                  -smp 4 \
-                  -m 2G \
-                  ${KVM_ARGS} \
-                  -hda ${IMG} \
-                  -netdev user,id=user.0,hostfwd=tcp::2222-:22 \
-                  -device virtio-net-pci,netdev=user.0 \
-                  -serial mon:stdio \
-                  -device virtio-serial-pci \
-                  -fsdev local,id=host_id,path=${HOST_ROOT},security_model=none \
-                  -device virtio-9p-pci,fsdev=host_id,mount_tag=host_mount >/dev/null 2>/dev/null &
+  qemu-system-aarch64 \
+    -no-reboot \
+    -M virt -cpu cortex-a57 -smp 4  -m 4G \
+    -initrd ${INITRD} \
+    -kernel ${KERNEL} \
+    -append "root=/dev/vda2 console=ttyAMA0" \
+    -drive if=virtio,file=${IMG},format=qcow2,id=hd \
+    -net user,hostfwd=tcp::2222-:22 -net nic \
+    -device intel-hda -device hda-duplex \
+    -nographic \
+    -fsdev local,id=host_id,path=${HOST_ROOT},security_model=none \
+    -device virtio-9p-pci,fsdev=host_id,mount_tag=host_mount >/dev/null 2>/dev/null &
+
+    sleep 15
 }
 
 wait_for_ssh() {
@@ -50,6 +58,7 @@ wait_for_ssh() {
   while ! (${connect} true); do
     if [[ "${retries}" -gt 30 ]]; then
       echo "SSH connection failed after 30 retries"
+      kill_vm
       exit 1
     fi
     retries=$((retries + 1))
