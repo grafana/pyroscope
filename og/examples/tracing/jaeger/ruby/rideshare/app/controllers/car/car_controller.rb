a4c1438edcb47ab25790420047428ea2f8e84975
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2023-12-05 20:13:40 +0800

    chore: remove old tracing integration examples (#2801)

--- og/examples/tracing/jaeger/ruby/rideshare/app/controllers/car/car_controller.rb
+++
@@ -1,7 +0,0 @@
-class Car::CarController < ActionController::Base
-  def show
-    OpenTelemetry.tracer_provider.tracer('my-tracer').in_span("CarController") do |_|
-      ApplicationHelper::find_nearest_vehicle(3, "car")
-    end
-  end
-end
