f148e139a90f73a3f709a6236074ad70b05a294b
Cyril Tovena <cyril.tovena@gmail.com>
2023-12-21 11:27:08 +0100

    bugfix: Warn only when deleting twice during compaction

--- pkg/phlaredb/block/block.go
+++ pkg/phlaredb/block/block.go
@@ -154,13 +154,13 @@ func cleanUp(logger log.Logger, bkt objstore.Bucket, id ulid.ULID, err error) er
 }
 
 // MarkForDeletion creates a file which stores information about when the block was marked for deletion.
-func MarkForDeletion(ctx context.Context, logger log.Logger, bkt objstore.Bucket, id ulid.ULID, details string, markedForDeletion prometheus.Counter) error {
+func MarkForDeletion(ctx context.Context, logger log.Logger, bkt objstore.Bucket, id ulid.ULID, details string, warnExist bool, markedForDeletion prometheus.Counter) error {
 	deletionMarkFile := path.Join(id.String(), DeletionMarkFilename)
 	deletionMarkExists, err := bkt.Exists(ctx, deletionMarkFile)
 	if err != nil {
 		return errors.Wrapf(err, "check exists %s in bucket", deletionMarkFile)
 	}
-	if deletionMarkExists {
+	if deletionMarkExists && warnExist {
 		level.Warn(logger).Log("msg", "requested to mark for deletion, but file already exists; this should not happen; investigate", "err", errors.Errorf("file %s already exists in bucket", deletionMarkFile))
 		return nil
 	}
