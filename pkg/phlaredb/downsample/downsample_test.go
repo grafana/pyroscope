f1e6d458359d98f61d497e600279c3e02c8228c2
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2023-12-29 16:08:14 -0400

    Add downsampling fingerprint test

--- pkg/phlaredb/downsample/downsample_test.go
+++ pkg/phlaredb/downsample/downsample_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/parquet-go/parquet-go"
+	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
 
 	phlareparquet "github.com/grafana/pyroscope/pkg/parquet"
@@ -14,7 +15,7 @@ import (
 	"github.com/grafana/pyroscope/pkg/pprof/testhelper"
 )
 
-func TestDownsampler_AddRow(t *testing.T) {
+func TestDownsampler_ProfileCounts(t *testing.T) {
 	outDir := t.TempDir()
 	d, err := NewDownsampler(outDir)
 	require.NoError(t, err)
@@ -25,7 +26,7 @@ func TestDownsampler_AddRow(t *testing.T) {
 		require.NoError(t, f.Close())
 	}()
 
-	reader := parquet.NewGenericReader[*schemav1.Profile](f, schemav1.ProfilesSchema)
+	reader := parquet.NewReader(f, schemav1.ProfilesSchema)
 	rows, err := phlareparquet.ReadAllWithBufferSize(reader, 1024)
 	require.NoError(t, err)
 
@@ -77,8 +78,7 @@ func TestDownsampler_Aggregation(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, row := range rows {
-		profileRow := schemav1.ProfileRow(row)
-		err = d.AddRow(profileRow, 1)
+		err = d.AddRow(schemav1.ProfileRow(row), 1)
 		require.NoError(t, err)
 	}
 
@@ -104,31 +104,36 @@ func TestDownsampler_Aggregation(t *testing.T) {
 	})
 }
 
-func readDownsampledRows(t *testing.T, path string, rowCount int) []parquet.Row {
-	stat, err := os.Stat(path)
-	require.NoError(t, err)
-	require.True(t, stat.Size() > 0)
+func TestDownsampler_VaryingFingerprints(t *testing.T) {
+	profiles := make([]schemav1.InMemoryProfile, 0)
+	for i := 0; i < 5; i++ {
+		builder := testhelper.NewProfileBuilder(1703853310000000000).CPUProfile() // 2023-12-29T12:35:10Z
+		builder.ForStacktraceString("a", "b", "c").AddSamples(30)
+		batch, _ := schemav1testhelper.NewProfileSchema(builder.Profile, "cpu")
+		profiles = append(profiles, batch...)
+	}
 
-	outFile, err := os.Open(path)
-	require.NoError(t, err)
-	defer func() {
-		require.NoError(t, outFile.Close())
-	}()
-
-	pf, err := parquet.OpenFile(outFile, stat.Size())
-	require.NoError(t, err)
-
-	outReader := parquet.NewReader(pf, schemav1.DownsampledProfilesSchema)
-	downsampledRows := make([]parquet.Row, 3)
-	count, err := outReader.ReadRows(downsampledRows)
+	reader := schemav1.NewInMemoryProfilesRowReader(profiles)
+	rows, err := phlareparquet.ReadAllWithBufferSize(reader, 1024)
 	require.NoError(t, err)
 
-	require.Equal(t, rowCount, count)
-	return downsampledRows
+	outDir := t.TempDir()
+	d, err := NewDownsampler(outDir)
+	require.NoError(t, err)
+
+	for i, row := range rows {
+		err = d.AddRow(schemav1.ProfileRow(row), model.Fingerprint(i))
+		require.NoError(t, err)
+	}
+
+	err = d.Close()
+	require.NoError(t, err)
+
+	verifyProfileCount(t, outDir, "profiles_5m_sum.parquet", 5)
+	verifyProfileCount(t, outDir, "profiles_1h_sum.parquet", 5)
 }
 
 func BenchmarkDownsampler_AddRow(b *testing.B) {
-
 	f, err := os.Open("../testdata/01HHYG6245NWHZWVP27V8WJRT7/profiles.parquet")
 	require.NoError(b, err)
 	defer func() {
@@ -172,4 +177,27 @@ func verifyProfileCount(t *testing.T, dir string, file string, expectedRows int)
 
 	outReader := parquet.NewReader(pf, schemav1.DownsampledProfilesSchema)
 	require.Equal(t, int64(expectedRows), outReader.NumRows())
+}
+
+func readDownsampledRows(t *testing.T, path string, expectedRowCount int) []parquet.Row {
+	stat, err := os.Stat(path)
+	require.NoError(t, err)
+	require.True(t, stat.Size() > 0)
+
+	outFile, err := os.Open(path)
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, outFile.Close())
+	}()
+
+	pf, err := parquet.OpenFile(outFile, stat.Size())
+	require.NoError(t, err)
+
+	reader := parquet.NewReader(pf, schemav1.DownsampledProfilesSchema)
+	downsampledRows := make([]parquet.Row, 1000)
+	rowCount, err := reader.ReadRows(downsampledRows)
+	require.NoError(t, err)
+
+	require.Equal(t, expectedRowCount, rowCount)
+	return downsampledRows
 }
