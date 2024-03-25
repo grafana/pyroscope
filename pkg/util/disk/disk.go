17d42a777fb824d3b77c108891bb46ef9d2b2022
Théo Crevon <oleiade@users.noreply.github.com>
2024-03-25 19:10:49 +0100

    Adapt disk util to FreeBSD
    On FreeBSD, the struct returned by the [fstat syscall](https://man.freebsd.org/cgi/man.cgi?statfs(2))'s `f_bavail` field is represented as an `int64`, rather than a `uint64` like on linux and macos platforms.
    
    As a result, comparisons such as ` stat.Bavail * uint64(stat.Bsize)` won't compile on FreeBSD, as it would lead to applying a multiplication between a signed and unsigned 64 bits integer.
    
    This commit refactors the logic of computing the disk available bytes and the percentage available to cater to that OS differences.

--- pkg/util/disk/disk.go
+++ pkg/util/disk/disk.go
@@ -24,14 +24,30 @@ func (v *volumeChecker) HasHighDiskUtilization(path string) (*VolumeStats, error
 		return nil, err
 	}
 
+  // Convert [stat.Bavail] to uint64 safely, considering it can be negative, and represented
+  // as an int64 on BSD systems.
+  //
+  // See https://cs.opensource.google/go/x/sys/+/refs/tags/v0.18.0:unix/ztypes_freebsd_arm64.go;l=96
+  var bytesAvailable uint64
+  if stat.Bavail < 0 {
+    bytesAvailable = 0
+  } else {
+    bytesAvailable = uint64(stat.Bavail) * uint64(stat.Bsize)
+  }
+
 	// available means accessible to the current user, while free means bytes
 	// for privileged users. (Linux sometimes reserves some space for root)
 	var (
 		stats = VolumeStats{
-			BytesAvailable: stat.Bavail * uint64(stat.Bsize),
+			BytesAvailable: bytesAvailable,
 		}
-		percentageAvailable = float64(stat.Bavail) / float64(stat.Blocks)
+		percentageAvailable = 0.0
 	)
+
+  // Ensure [stat.Blocks] is greater than zero to avoid division by zero.
+  if stat.Blocks > 0 {  
+    percentageAvailable = float64(bytesAvailable) / float64(uint64(stat.Blocks) * uint64(stat.Bsize))
+  }
 
 	// if bytes available is bigger than minFreeDisk => not in high disk utilization
 	if stats.BytesAvailable >= v.minFreeDisk {
