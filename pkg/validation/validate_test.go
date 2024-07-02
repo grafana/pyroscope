c697f0d0483bd33c8fb35032bd5f603e1a38ffed
Bryan Huhta <32787160+bryanhuhta@users.noreply.github.com>
2024-07-02 16:12:03 -0500

    Reject queries without a time range (#3395)

--- pkg/validation/validate_test.go
+++ pkg/validation/validate_test.go
@@ -200,6 +200,30 @@ func Test_ValidateRangeRequest(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "empty start",
+			in: model.Interval{
+				Start: 0,
+				End:   now,
+			},
+			expectedErr: NewErrorf(QueryMissingTimeRange, QueryMissingTimeRangeErrorMsg),
+		},
+		{
+			name: "empty end",
+			in: model.Interval{
+				Start: now,
+				End:   0,
+			},
+			expectedErr: NewErrorf(QueryMissingTimeRange, QueryMissingTimeRangeErrorMsg),
+		},
+		{
+			name: "empty start and end",
+			in: model.Interval{
+				Start: 0,
+				End:   0,
+			},
+			expectedErr: NewErrorf(QueryMissingTimeRange, QueryMissingTimeRangeErrorMsg),
+		},
 	} {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
