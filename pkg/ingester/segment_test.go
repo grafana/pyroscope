fb221381cb659ee5fb5018184296eb333eda17eb
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-07-10 14:38:40 +0200

    make fmt

--- pkg/ingester/segment_test.go
+++ pkg/ingester/segment_test.go
@@ -2,11 +2,13 @@ package ingester
 
 import (
 	"fmt"
-	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
 )
 
 func TestReadSegment(t *testing.T) {
