aec0a53b46b7d383863ed0f97cdfdcfd4fce2859
Bryan Huhta <bryan.huhta@grafana.com>
2024-07-17 12:49:47 -0500

    Add k6 middleware to querier

--- pkg/querier/grpc_handler.go
+++ pkg/querier/grpc_handler.go
@@ -3,6 +3,8 @@ package querier
 import (
 	"net/http"
 
+	"github.com/grafana/dskit/middleware"
+	"github.com/grafana/pyroscope-go/x/k6"
 	"github.com/grafana/pyroscope/api/gen/proto/go/querier/v1/querierv1connect"
 	vcsv1connect "github.com/grafana/pyroscope/api/gen/proto/go/vcs/v1/vcsv1connect"
 	connectapi "github.com/grafana/pyroscope/pkg/api/connect"
@@ -18,5 +20,13 @@ func NewGRPCHandler(svc QuerierSvc) connectgrpc.GRPCHandler {
 	mux := http.NewServeMux()
 	mux.Handle(querierv1connect.NewQuerierServiceHandler(svc, connectapi.DefaultHandlerOptions()...))
 	mux.Handle(vcsv1connect.NewVCSServiceHandler(svc, connectapi.DefaultHandlerOptions()...))
-	return connectgrpc.NewHandler(mux)
+
+	httpMiddleware := middleware.Func(func(h http.Handler) http.Handler {
+		next := k6.LabelsFromBaggageHandler(h)
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	return connectgrpc.NewHandler(httpMiddleware.Wrap(mux))
 }
