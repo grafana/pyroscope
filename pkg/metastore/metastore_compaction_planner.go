7c65d3ce59b7050ff1a7a0cbcd176f0f3501c671
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2024-07-18 16:22:57 -0300

    Fix metric names

--- pkg/metastore/metastore_compaction_planner.go
+++ pkg/metastore/metastore_compaction_planner.go
@@ -203,27 +203,27 @@ func newCompactionMetrics(reg prometheus.Registerer) *compactionMetrics {
 	m := &compactionMetrics{
 		addedBlocks: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Namespace: "pyroscope",
-			Name:      "metastore_compaction_added_blocks",
+			Name:      "metastore_compaction_added_blocks_count",
 			Help:      "The number of blocks added for compaction",
 		}, []string{"shard", "tenant", "level"}),
 		deletedBlocks: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Namespace: "pyroscope",
-			Name:      "metastore_compaction_deleted_blocks",
+			Name:      "metastore_compaction_deleted_blocks_count",
 			Help:      "The number of blocks deleted as a result of compaction",
 		}, []string{"shard", "tenant", "level"}),
 		addedJobs: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Namespace: "pyroscope",
-			Name:      "metastore_compaction_added_jobs",
+			Name:      "metastore_compaction_added_jobs_count",
 			Help:      "The number of created compaction jobs",
 		}, []string{"shard", "tenant", "level"}),
 		assignedJobs: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Namespace: "pyroscope",
-			Name:      "metastore_compaction_assigned_jobs",
+			Name:      "metastore_compaction_assigned_jobs_count",
 			Help:      "The number of assigned compaction jobs",
 		}, []string{"shard", "tenant", "level"}),
 		completedJobs: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Namespace: "pyroscope",
-			Name:      "metastore_compaction_completed_jobs",
+			Name:      "metastore_compaction_completed_jobs_count",
 			Help:      "The number of completed compaction jobs",
 		}, []string{"shard", "tenant", "level"}),
 	}
