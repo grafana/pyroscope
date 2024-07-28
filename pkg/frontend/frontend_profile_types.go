7872b48086d09c390c8817eacaa46fcf75676fa7
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-07-28 22:44:11 +0200

    Update frontend_profile_types.go

--- pkg/frontend/frontend_profile_types.go
+++ pkg/frontend/frontend_profile_types.go
@@ -2,17 +2,16 @@ package frontend
 
 import (
 	"context"
+	"github.com/go-kit/log/level"
+	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
+	"sort"
 
 	"connectrpc.com/connect"
-	"github.com/grafana/dskit/tenant"
 	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/common/model"
 
 	querierv1 "github.com/grafana/pyroscope/api/gen/proto/go/querier/v1"
-	"github.com/grafana/pyroscope/api/gen/proto/go/querier/v1/querierv1connect"
 	phlaremodel "github.com/grafana/pyroscope/pkg/model"
-	"github.com/grafana/pyroscope/pkg/util/connectgrpc"
-	"github.com/grafana/pyroscope/pkg/validation"
 )
 
 func (f *Frontend) ProfileTypes(ctx context.Context, c *connect.Request[querierv1.ProfileTypesRequest]) (*connect.Response[querierv1.ProfileTypesResponse], error) {
@@ -20,25 +19,30 @@ func (f *Frontend) ProfileTypes(ctx context.Context, c *connect.Request[querierv
 		SetTag("start", model.Time(c.Msg.Start).Time().String()).
 		SetTag("end", model.Time(c.Msg.End).Time().String())
 
-	ctx = connectgrpc.WithProcedure(ctx, querierv1connect.QuerierServiceProfileTypesProcedure)
-
-	interval, ok := phlaremodel.GetTimeRange(c.Msg)
-	if ok {
-		tenantIDs, err := tenant.TenantIDs(ctx)
-		if err != nil {
-			return nil, connect.NewError(connect.CodeInvalidArgument, err)
-		}
-
-		validated, err := validation.ValidateRangeRequest(f.limits, tenantIDs, interval, model.Now())
-		if err != nil {
-			return nil, connect.NewError(connect.CodeInvalidArgument, err)
-		}
-		if validated.IsEmpty {
-			return connect.NewResponse(&querierv1.ProfileTypesResponse{}), nil
-		}
-		c.Msg.Start = int64(validated.Start)
-		c.Msg.End = int64(validated.End)
+	lblRes, err := f.LabelValues(ctx, &connect.Request[typesv1.LabelValuesRequest]{
+		Msg: &typesv1.LabelValuesRequest{
+			Start:    c.Msg.Start,
+			End:      c.Msg.End,
+			Matchers: []string{"{}"},
+			Name:     phlaremodel.LabelNameProfileType,
+		},
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	return connectgrpc.RoundTripUnary[querierv1.ProfileTypesRequest, querierv1.ProfileTypesResponse](ctx, f, c)
+	var profileTypes []*typesv1.ProfileType
+	for _, profileTypeStr := range lblRes.Msg.Names {
+		profileType, err := phlaremodel.ParseProfileTypeSelector(profileTypeStr)
+		if err != nil {
+			level.Error(f.log).Log("msg", "ProfileTypes: failed to parse profile type", "profileType", profileTypeStr, "err", err)
+			continue
+		}
+		profileTypes = append(profileTypes, profileType)
+	}
+
+	sort.Slice(profileTypes, func(i, j int) bool {
+		return profileTypes[i].ID < profileTypes[j].ID
+	})
+	return connect.NewResponse(&querierv1.ProfileTypesResponse{ProfileTypes: profileTypes}), nil
 }
