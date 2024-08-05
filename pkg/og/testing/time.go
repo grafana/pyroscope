060b2b80032bbcd0b1cef726e68f0db1ff3fb496
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-08-05 11:55:31 +0200

    chore: cleanup pkg/og/storage/segment (#3453)
    * chore: cleanup pkg/og/storage/segment
    
    Remove some unused  pkg/og/storage/segment which are only referenced in
    tess. This saves 40s of `make test` on my laptop.
    
    * Update pkg/og/testing/time.go
    
    Co-authored-by: Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
    
    ---------
    
    Co-authored-by: Anton Kolesnikov <anton.e.kolesnikov@gmail.com>

--- pkg/og/testing/time.go
+++ pkg/og/testing/time.go
@@ -12,9 +12,6 @@ func ParseTime(str string) time.Time {
 	return r.UTC()
 }
 
-func SimpleTime(i int) time.Time {
-	return time.Time{}.Add(time.Duration(i) * time.Second).UTC()
-}
 
 func SimpleUTime(i int) time.Time {
 	return time.Unix(int64(i), 0)
