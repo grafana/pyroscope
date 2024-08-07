3ddd02297e16e091aa62387ba2f4a9708d3c08c3
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-08-07 13:17:20 +0200

    Revert "revert changes in tsdb/index.go"
    This reverts commit 2188cdee5fa5abcf88054ed78c2cbd70659c8d44.

--- pkg/experiment/querybackend/block/compaction.go
+++ pkg/experiment/querybackend/block/compaction.go
@@ -522,8 +522,11 @@ func (rw *indexRewriter) rewriteRow(e ProfileEntry) error {
 func (rw *indexRewriter) NumSeries() uint64 { return uint64(len(rw.series)) }
 
 func (rw *indexRewriter) Flush() error {
-	w, err := index.NewWriter(context.Background(),
-		filepath.Join(rw.path, block.IndexFilename))
+	w, err := index.NewWriterSize(context.Background(),
+		filepath.Join(rw.path, block.IndexFilename),
+		// There is no particular reason to use a buffer (bufio.Writer)
+		// larger than the default one when writing on disk
+		4<<10)
 	if err != nil {
 		return err
 	}
