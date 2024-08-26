d01d55a1b7d76344ce2f5f905584516b8dd377f6
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-08-26 08:43:24 +0200

    revert unneeded changes

--- pkg/pprof/testhelper/profile_builder.go
+++ pkg/pprof/testhelper/profile_builder.go
@@ -19,6 +19,14 @@ type ProfileBuilder struct {
 
 	uuid.UUID
 	Labels []*typesv1.LabelPair
+
+	externalFunctionID2LocationId map[uint32]uint64
+	externalSampleID2SampleIndex  map[sampleID]uint32
+}
+
+type sampleID struct {
+	locationsID uint64
+	labelsID    uint64
 }
 
 // NewProfileBuilder creates a new ProfileBuilder with the given nanoseconds timestamp.
@@ -43,6 +51,8 @@ func NewProfileBuilderWithLabels(ts int64, labels []*typesv1.LabelPair) *Profile
 		UUID:    uuid.New(),
 		Labels:  labels,
 		strings: map[string]int{},
+
+		externalFunctionID2LocationId: map[uint32]uint64{},
 	}
 	p.addString("")
 	return p
@@ -195,6 +205,66 @@ func (m *ProfileBuilder) addString(s string) int64 {
 		m.StringTable = append(m.StringTable, s)
 	}
 	return int64(i)
+}
+
+func (m *ProfileBuilder) FindLocationByExternalID(externalID uint32) (uint64, bool) {
+	loc, ok := m.externalFunctionID2LocationId[externalID]
+	return loc, ok
+}
+
+func (m *ProfileBuilder) AddExternalFunction(frame string, externalFunctionID uint32) uint64 {
+	fname := m.addString(frame)
+	funcID := uint64(len(m.Function)) + 1
+	m.Function = append(m.Function, &profilev1.Function{
+		Id:   funcID,
+		Name: fname,
+	})
+	locID := uint64(len(m.Location)) + 1
+	m.Location = append(m.Location, &profilev1.Location{
+		Id:        locID,
+		MappingId: uint64(1),
+		Line:      []*profilev1.Line{{FunctionId: funcID}},
+	})
+	m.externalFunctionID2LocationId[externalFunctionID] = locID
+	return locID
+}
+
+func (m *ProfileBuilder) AddExternalSample(locs []uint64, values []int64, externalSampleID uint32) {
+	m.AddExternalSampleWithLabels(locs, values, nil, uint64(externalSampleID), 0)
+}
+
+func (m *ProfileBuilder) FindExternalSample(externalSampleID uint32) *profilev1.Sample {
+	return m.FindExternalSampleWithLabels(uint64(externalSampleID), 0)
+}
+
+func (m *ProfileBuilder) AddExternalSampleWithLabels(locs []uint64, values []int64, labels phlaremodel.Labels, locationsID, labelsID uint64) {
+	sample := &profilev1.Sample{
+		LocationId: locs,
+		Value:      values,
+	}
+	if m.externalSampleID2SampleIndex == nil {
+		m.externalSampleID2SampleIndex = map[sampleID]uint32{}
+	}
+	m.externalSampleID2SampleIndex[sampleID{locationsID: locationsID, labelsID: labelsID}] = uint32(len(m.Profile.Sample))
+	m.Profile.Sample = append(m.Profile.Sample, sample)
+	if len(labels) > 0 {
+		sample.Label = make([]*profilev1.Label, 0, len(labels))
+		for _, label := range labels {
+			sample.Label = append(sample.Label, &profilev1.Label{
+				Key: m.addString(label.Name),
+				Str: m.addString(label.Value),
+			})
+		}
+	}
+}
+
+func (m *ProfileBuilder) FindExternalSampleWithLabels(locationsID, labelsID uint64) *profilev1.Sample {
+	sampleIndex, ok := m.externalSampleID2SampleIndex[sampleID{locationsID: locationsID, labelsID: labelsID}]
+	if !ok {
+		return nil
+	}
+	sample := m.Profile.Sample[sampleIndex]
+	return sample
 }
 
 type StacktraceBuilder struct {
