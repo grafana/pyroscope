a16807f96ffd4ec6605007fa88f4074e13afde52
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2024-09-04 17:22:40 -0300

    Retrieve profile types from block metadata instead

--- pkg/frontend/read_path/query_frontend/query_profile_types.go
+++ pkg/frontend/read_path/query_frontend/query_profile_types.go
@@ -5,10 +5,13 @@ import (
 	"sort"
 
 	"connectrpc.com/connect"
+	"github.com/grafana/dskit/tenant"
 
+	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
 	querierv1 "github.com/grafana/pyroscope/api/gen/proto/go/querier/v1"
 	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
 	phlaremodel "github.com/grafana/pyroscope/pkg/model"
+	"github.com/grafana/pyroscope/pkg/validation"
 )
 
 func (q *QueryFrontend) ProfileTypes(
@@ -16,26 +19,47 @@ func (q *QueryFrontend) ProfileTypes(
 	req *connect.Request[querierv1.ProfileTypesRequest],
 ) (*connect.Response[querierv1.ProfileTypesResponse], error) {
 
-	lblReq := connect.NewRequest(&typesv1.LabelValuesRequest{
-		Start:    req.Msg.Start,
-		End:      req.Msg.End,
-		Matchers: []string{"{}"},
-		Name:     phlaremodel.LabelNameProfileType,
+	tenants, err := tenant.TenantIDs(ctx)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+	empty, err := validation.SanitizeTimeRange(q.limits, tenants, &req.Msg.Start, &req.Msg.End)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+	if empty {
+		return connect.NewResponse(&querierv1.ProfileTypesResponse{}), nil
+	}
+
+	md, err := q.metastore.QueryMetadata(ctx, &metastorev1.QueryMetadataRequest{
+		TenantId:  tenants,
+		StartTime: req.Msg.Start,
+		EndTime:   req.Msg.End,
+		Query:     "{}",
 	})
 
-	lblRes, err := q.LabelValues(ctx, lblReq)
 	if err != nil {
 		return nil, err
 	}
 
-	var profileTypes []*typesv1.ProfileType
-
-	for _, profileTypeStr := range lblRes.Msg.Names {
-		profileType, err := phlaremodel.ParseProfileTypeSelector(profileTypeStr)
-		if err != nil {
-			return nil, err
+	pTypesFromMetadata := make(map[string]*typesv1.ProfileType)
+	for _, b := range md.Blocks {
+		for _, d := range b.Datasets {
+			for _, pType := range d.ProfileTypes {
+				if _, ok := pTypesFromMetadata[pType]; !ok {
+					profileType, err := phlaremodel.ParseProfileTypeSelector(pType)
+					if err != nil {
+						return nil, err
+					}
+					pTypesFromMetadata[pType] = profileType
+				}
+			}
 		}
-		profileTypes = append(profileTypes, profileType)
+	}
+
+	var profileTypes []*typesv1.ProfileType
+	for _, pType := range pTypesFromMetadata {
+		profileTypes = append(profileTypes, pType)
 	}
 
 	sort.Slice(profileTypes, func(i, j int) bool {
