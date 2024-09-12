8c82f6db6b1ccaa32c804e0c52dd73bdb7df6a3c
Bryan Huhta <bryan.huhta@grafana.com>
2024-09-11 17:51:47 -0700

    Refactor express-ts-inline example

--- examples/language-sdk-instrumentation/nodejs/express-ts-inline/index.ts
+++ examples/language-sdk-instrumentation/nodejs/express-ts-inline/index.ts
@@ -6,17 +6,18 @@ const Pyroscope = require('@pyroscope/nodejs');
 const SourceMapper = Pyroscope.default.SourceMapper;
 
 const port = process.env['PORT'] || 5000;
-
 const region = process.env['REGION'] || 'default';
+const appName = process.env['APP_NAME'] || 'express-ts-inline';
+const pyroscopeUrl = process.env['PYROSCOPE_URL'] || 'http://pyroscope:4040';
 
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Available routes are: /bike, /car, /scooter');
 });
 
-const genericSearchHandler = (p: number) => (req: any, res: any) => {
+const genericSearchHandler = (p: number) => (_: any, res: any) => {
   const time = +new Date() + p * 1000;
   let i = 0;
   while (+new Date() < time) {
@@ -30,11 +31,13 @@ app.get('/bike', function bikeSearchHandler(req, res) {
     genericSearchHandler(0.2)(req, res)
   );
 });
+
 app.get('/car', function carSearchHandler(req, res) {
   Pyroscope.wrapWithLabels({ vehicle: 'car' }, () =>
     genericSearchHandler(1)(req, res)
   );
 });
+
 app.get('/scooter', function scooterSearchHandler(req, res) {
   Pyroscope.wrapWithLabels({ vehicle: 'scooter' }, () =>
     genericSearchHandler(0.5)(req, res)
@@ -44,14 +47,14 @@ app.get('/scooter', function scooterSearchHandler(req, res) {
 SourceMapper.create(['.'])
   .then((sourceMapper) => {
     Pyroscope.init({
-      appName: 'nodejs',
-      serverAddress: 'http://pyroscope:4040',
+      appName: appName,
+      serverAddress: pyroscopeUrl,
       sourceMapper: sourceMapper,
       tags: { region },
     });
     Pyroscope.start();
   })
-  .catch((e) => {
+  .catch((e: any) => {
     console.error(e);
   });
 
