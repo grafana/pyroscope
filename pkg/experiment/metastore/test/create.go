792043490188818cee25de26d1e18ba43f513c7e
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-09-27 16:01:31 +0700

    refactor metastore creation to a package

--- pkg/experiment/metastore/test/create.go
+++ pkg/experiment/metastore/test/create.go
@@ -1,94 +1,29 @@
-package metastore
+package test
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
 	"github.com/go-kit/log"
-	"github.com/grafana/dskit/flagext"
 	"github.com/grafana/dskit/services"
 	compactorv1 "github.com/grafana/pyroscope/api/gen/proto/go/compactor/v1"
 	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
-	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
+	"github.com/grafana/pyroscope/pkg/experiment/metastore"
 	metastoreclient "github.com/grafana/pyroscope/pkg/experiment/metastore/client"
 	"github.com/grafana/pyroscope/pkg/experiment/metastore/discovery"
 	"github.com/grafana/pyroscope/pkg/test"
 	"github.com/grafana/pyroscope/pkg/test/mocks/mockdiscovery"
 	"github.com/hashicorp/raft"
-	"github.com/oklog/ulid"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"net"
 	"testing"
 	"time"
 )
 
-func TestRaftDetailsAddBlock(t *testing.T) {
-	cfg := new(Config)
-	flagext.DefaultValues(cfg)
-
-	ms := createMetastore(t, cfg, 3)
-	defer ms.Close()
-
-	errors := 0
-	m := &metastorev1.BlockMeta{
-		Id: ulid.MustNew(1, rand.Reader).String(),
-	}
-	for _, it := range ms.instances {
-		_, err := it.metastoreClient.AddBlock(context.Background(), &metastorev1.AddBlockRequest{
-			Block: m,
-		})
-		if err != nil {
-			requireRaftDetails(t, err)
-			errors++
-		}
-	}
-	require.Equal(t, 2, errors)
-}
-
-func TestRaftDetailsPullCompaction(t *testing.T) {
-	cfg := new(Config)
-	flagext.DefaultValues(cfg)
-
-	ms := createMetastore(t, cfg, 3)
-	defer ms.Close()
-
-	errors := 0
-	for _, it := range ms.instances {
-		_, err := it.compactorClient.PollCompactionJobs(context.Background(), &compactorv1.PollCompactionJobsRequest{})
-		if err != nil {
-			requireRaftDetails(t, err)
-			errors++
-		}
-	}
-	require.Equal(t, 2, errors)
-}
-
-func requireRaftDetails(t *testing.T, err error) {
-	t.Log("error", err)
-	s, ok := status.FromError(err)
-	detailsLeader := ""
-	if ok && s.Code() == codes.Unavailable {
-		ds := s.Details()
-		if len(ds) > 0 {
-			for _, d := range ds {
-				if rd, ok := d.(*typesv1.RaftDetails); ok {
-					detailsLeader = rd.Leader
-					break
-				}
-			}
-		}
-	}
-	t.Log("leader is", detailsLeader)
-	require.NotEmpty(t, detailsLeader)
-}
-
-func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
+func CreateMetastore(t *testing.T, cfg *metastore.Config, n int) MetastoreSet {
 	l := test.NewTestingLogger(t)
 
 	ports, err := test.GetFreePorts(2 * n)
@@ -103,7 +38,7 @@ func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
 		raftIds[i] = fmt.Sprintf("node-%d", i)
 	}
 	bootstrapPeers := make([]string, n)
-	configs := make([]Config, n)
+	configs := make([]metastore.Config, n)
 	servers := make([]discovery.Server, n)
 
 	for i := 0; i < n; i++ {
@@ -131,13 +66,13 @@ func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
 	}
 	require.NoError(t, err)
 
-	d := mockStaticDiscovery(t, servers)
+	d := MockStaticDiscovery(t, servers)
 	cl := metastoreclient.New(l, cfg.GRPCClientConfig, d)
 	err = cl.Service().StartAsync(context.Background())
 	require.NoError(t, err)
 
 	l.Log("grpcAddresses", fmt.Sprintf("%+v", grpcAddresses), "raftAddresses", fmt.Sprintf("%+v", raftAddresses))
-	res := metastoreSet{
+	res := MetastoreSet{
 		t: t,
 	}
 	for i := 0; i < n; i++ {
@@ -147,7 +82,7 @@ func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
 		require.NoError(t, err)
 		il := log.With(l, "idx", bootstrapPeers[i])
 		server := grpc.NewServer()
-		m, err := New(configs[i], nil, il, prometheus.NewRegistry(), cl)
+		m, err := metastore.New(configs[i], nil, il, prometheus.NewRegistry(), cl)
 		require.NoError(t, err)
 		metastorev1.RegisterMetastoreServiceServer(server, m)
 		compactorv1.RegisterCompactionPlannerServer(server, m)
@@ -157,13 +92,12 @@ func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
 			err := server.Serve(lis)
 			assert.NoError(t, err)
 		}()
-		res.instances = append(res.instances, metastoreInstance{
-			metastore:       m,
-			srv:             servers[i],
-			cc:              cc,
-			metastoreClient: metastorev1.NewMetastoreServiceClient(cc),
-			compactorClient: compactorv1.NewCompactionPlannerClient(cc),
-			server:          server,
+		res.Instances = append(res.Instances, MetastoreInstance{
+			Metastore:               m,
+			Connection:              cc,
+			MetastoreInstanceClient: metastorev1.NewMetastoreServiceClient(cc),
+			CompactorInstanceClient: compactorv1.NewCompactionPlannerClient(cc),
+			Server:                  server,
 		})
 		err = m.Service().StartAsync(context.Background())
 		il.Log("msg", "service started")
@@ -172,22 +106,22 @@ func createMetastore(t *testing.T, cfg *Config, n int) metastoreSet {
 
 	require.Eventually(t, func() bool {
 		for i := 0; i < n; i++ {
-			if res.instances[i].metastore.Service().State() != services.Running {
+			if res.Instances[i].Metastore.Service().State() != services.Running {
 				return false
 			}
-			if res.instances[i].metastore.CheckReady(context.Background()) != nil {
+			if res.Instances[i].Metastore.CheckReady(context.Background()) != nil {
 				return false
 			}
 		}
 		return true
 	}, 10*time.Second, 100*time.Millisecond)
 
-	res.client = cl
+	res.Client = cl
 
 	return res
 }
 
-func mockStaticDiscovery(t *testing.T, servers []discovery.Server) *mockdiscovery.MockDiscovery {
+func MockStaticDiscovery(t *testing.T, servers []discovery.Server) *mockdiscovery.MockDiscovery {
 	d := mockdiscovery.NewMockDiscovery(t)
 	d.On("Subscribe", mock.Anything).Run(func(args mock.Arguments) {
 		upd := args.Get(0).(discovery.Updates)
@@ -198,34 +132,33 @@ func mockStaticDiscovery(t *testing.T, servers []discovery.Server) *mockdiscover
 	return d
 }
 
-type metastoreInstance struct {
-	metastore       *Metastore
-	srv             discovery.Server
-	server          *grpc.Server
-	cc              *grpc.ClientConn
-	metastoreClient metastorev1.MetastoreServiceClient
-	compactorClient compactorv1.CompactionPlannerClient
+type MetastoreInstance struct {
+	Metastore               *metastore.Metastore
+	Server                  *grpc.Server
+	Connection              *grpc.ClientConn
+	MetastoreInstanceClient metastorev1.MetastoreServiceClient
+	CompactorInstanceClient compactorv1.CompactionPlannerClient
 }
 
-func (i *metastoreInstance) client() metastorev1.MetastoreServiceClient {
-	return i.metastoreClient
+func (i *MetastoreInstance) client() metastorev1.MetastoreServiceClient {
+	return i.MetastoreInstanceClient
 }
 
-type metastoreSet struct {
+type MetastoreSet struct {
 	t         *testing.T
-	instances []metastoreInstance
-	client    *metastoreclient.Client
+	Instances []MetastoreInstance
+	Client    *metastoreclient.Client
 }
 
-func (m *metastoreSet) Close() {
-	for _, i := range m.instances {
-		i.metastore.Service().StopAsync()
-		err := i.metastore.Service().AwaitTerminated(context.Background())
+func (m *MetastoreSet) Close() {
+	for _, i := range m.Instances {
+		i.Metastore.Service().StopAsync()
+		err := i.Metastore.Service().AwaitTerminated(context.Background())
 		require.NoError(m.t, err)
-		i.cc.Close()
-		i.server.Stop()
+		i.Connection.Close()
+		i.Server.Stop()
 	}
-	m.client.Service().StopAsync()
-	err := m.client.Service().AwaitTerminated(context.Background())
+	m.Client.Service().StopAsync()
+	err := m.Client.Service().AwaitTerminated(context.Background())
 	require.NoError(m.t, err)
 }
---
+++ pkg/experiment/metastore/test/metastore_leader_details_test.go
@@ -0,0 +1,76 @@
+package test
+
+import (
+	"context"
+	"crypto/rand"
+	"github.com/grafana/dskit/flagext"
+	compactorv1 "github.com/grafana/pyroscope/api/gen/proto/go/compactor/v1"
+	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
+	typesv1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
+	"github.com/grafana/pyroscope/pkg/experiment/metastore"
+	"github.com/oklog/ulid"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func TestRaftDetailsAddBlock(t *testing.T) {
+	cfg := new(metastore.Config)
+	flagext.DefaultValues(cfg)
+
+	ms := CreateMetastore(t, cfg, 3)
+	defer ms.Close()
+
+	errors := 0
+	m := &metastorev1.BlockMeta{
+		Id: ulid.MustNew(1, rand.Reader).String(),
+	}
+	for _, it := range ms.Instances {
+		_, err := it.MetastoreInstanceClient.AddBlock(context.Background(), &metastorev1.AddBlockRequest{
+			Block: m,
+		})
+		if err != nil {
+			requireRaftDetails(t, err)
+			errors++
+		}
+	}
+	require.Equal(t, 2, errors)
+}
+
+func TestRaftDetailsPullCompaction(t *testing.T) {
+	cfg := new(metastore.Config)
+	flagext.DefaultValues(cfg)
+
+	ms := CreateMetastore(t, cfg, 3)
+	defer ms.Close()
+
+	errors := 0
+	for _, it := range ms.Instances {
+		_, err := it.CompactorInstanceClient.PollCompactionJobs(context.Background(), &compactorv1.PollCompactionJobsRequest{})
+		if err != nil {
+			requireRaftDetails(t, err)
+			errors++
+		}
+	}
+	require.Equal(t, 2, errors)
+}
+
+func requireRaftDetails(t *testing.T, err error) {
+	t.Log("error", err)
+	s, ok := status.FromError(err)
+	detailsLeader := ""
+	if ok && s.Code() == codes.Unavailable {
+		ds := s.Details()
+		if len(ds) > 0 {
+			for _, d := range ds {
+				if rd, ok := d.(*typesv1.RaftDetails); ok {
+					detailsLeader = rd.Leader
+					break
+				}
+			}
+		}
+	}
+	t.Log("leader is", detailsLeader)
+	require.NotEmpty(t, detailsLeader)
+}
