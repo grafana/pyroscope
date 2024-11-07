8055672e368432d070bf1c2b7bbc09d32bd6018a
Xiaodong Li <xiaodoli@microsoft.com>
2024-11-07 09:56:34 -0800

    fix disk panic on windows

--- pkg/util/disk/disk_windows.go
+++ pkg/util/disk/disk_windows.go
@@ -2,6 +2,11 @@
 
 package diskutil
 
+import (
+	"syscall"
+	"unsafe"
+)
+
 type volumeChecker struct {
 	minFreeDisk                uint64
 	minDiskAvailablePercentage float64
@@ -15,5 +20,40 @@ func NewVolumeChecker(minFreeDisk uint64, minDiskAvailablePercentage float64) Vo
 }
 
 func (v *volumeChecker) HasHighDiskUtilization(path string) (*VolumeStats, error) {
-	panic("not implemented")
+	var freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes uint64
+
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	getDiskFreeSpaceEx := kernel32.NewProc("GetDiskFreeSpaceExW")
+
+	pathPtr, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return nil, err
+	}
+
+	r1, _, err := getDiskFreeSpaceEx.Call(
+		uintptr(unsafe.Pointer(pathPtr)),
+		uintptr(unsafe.Pointer(&freeBytesAvailable)),
+		uintptr(unsafe.Pointer(&totalNumberOfBytes)),
+		uintptr(unsafe.Pointer(&totalNumberOfFreeBytes)),
+	)
+	if r1 == 0 {
+		return nil, err
+	}
+
+	stats := &VolumeStats{
+		BytesAvailable: freeBytesAvailable,
+	}
+
+	percentageAvailable := float64(freeBytesAvailable) / float64(totalNumberOfBytes)
+
+	if stats.BytesAvailable >= v.minFreeDisk {
+		return stats, nil
+	}
+
+	if percentageAvailable > v.minDiskAvailablePercentage {
+		return stats, nil
+	}
+
+	stats.HighDiskUtilization = true
+	return stats, nil
 }
