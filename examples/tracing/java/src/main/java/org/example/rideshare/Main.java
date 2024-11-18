165e6e9f7c7adc8e8a10a857cedd07961fa91ce0
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2024-11-18 12:23:37 -0400

    examples: add a java span profiling example without the Pyroscope sdk (#3698)
    * examples: add a java span profiling example without the Pyroscope sdk
    
    * update java span profiles doc

---
+++ examples/tracing/java/src/main/java/org/example/rideshare/Main.java
@@ -0,0 +1,13 @@
+package org.example.rideshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
