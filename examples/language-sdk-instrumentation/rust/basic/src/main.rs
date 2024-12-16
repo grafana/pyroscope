98b1d054f70fd678642926e7a137f738af5fd101
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-12-16 17:48:16 +0700

    docs: update rust example and readme (#3775)
    Co-authored-by: Kim Nylander <104772500+knylander-grafana@users.noreply.github.com>

--- examples/language-sdk-instrumentation/rust/basic/src/main.rs
+++ examples/language-sdk-instrumentation/rust/basic/src/main.rs
@@ -2,36 +2,30 @@ use pyroscope::PyroscopeAgent;
 use pyroscope_pprofrs::{pprof_backend, PprofConfig};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create Pyroscope Agent
-    let agent = PyroscopeAgent::builder("http://localhost:4040", "rust-app")
+    std::env::set_var("RUST_LOG", "debug");
+    pretty_env_logger::init_timed();
+
+    let agent = PyroscopeAgent::builder("http://pyroscope:4040", "rust-app")
         .backend(pprof_backend(PprofConfig::new().sample_rate(100)))
         .tags(vec![("Hostname", "pyroscope")])
         .build()?;
 
-    // Start Agent
     let agent_running = agent.start()?;
 
     let (add_tag, _remove_tag) = agent_running.tag_wrapper();
 
-    // Add tag
     add_tag("Batch".to_string(), "first".to_string())?;
 
-    // Do some work for first batch.
     mutex_lock(2);
 
-    // Change Tag
     add_tag("Batch".to_string(), "second".to_string())?;
 
-    // Do some work for second batch.
     mutex_lock(5);
 
-    // Change Tag
     add_tag("Batch".to_string(), "third".to_string())?;
 
-    // Do some work for third batch.
     mutex_lock(12);
 
-    // Stop Agent
     let agent_ready = agent_running.stop()?;
     agent_ready.shutdown();
 
