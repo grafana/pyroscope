a261845f627367f4337b45bbda7e97bc43b9a55f
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2025-01-03 09:07:35 -0400

    Update java examples with latest SDKs (#3812)
    * Update java span-profiles example with latest SDKs
    
    * Bump java SDK version in a few more examples, improve docs further

--- examples/tracing/java/build.gradle.kts
+++ examples/tracing/java/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.pyroscope:agent:0.16.0")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
