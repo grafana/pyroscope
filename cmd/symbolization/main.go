86f1dc3dc8f3d7f7efab4a568fd884edbc6e51a4
Marc Sanmiquel <marcsanmiquel@gmail.com>
2025-02-10 11:23:24 +0100

    chore: move symbolizatoin into query frontend & add intermediate representation for lookups in dwarf

--- cmd/symbolization/main.go
+++ cmd/symbolization/main.go
@@ -5,93 +5,124 @@ import (
 	"fmt"
 	"log"
 
-	pprof "github.com/google/pprof/profile"
-
+	googlev1 "github.com/grafana/pyroscope/api/gen/proto/go/google/v1"
+	queryv1 "github.com/grafana/pyroscope/api/gen/proto/go/query/v1"
 	"github.com/grafana/pyroscope/pkg/experiment/symbolizer"
 )
 
 const (
 	debuginfodBaseURL = "https://debuginfod.elfutils.org"
-	buildID           = "2fa2055ef20fabc972d5751147e093275514b142"
+	// buildID           = "c047672cae7964324658491e7dee26748ae5d2f8"
+	buildID = "2fa2055ef20fabc972d5751147e093275514b142"
 )
 
 func main() {
-	client := symbolizer.NewDebuginfodClient(debuginfodBaseURL, nil)
+	client := symbolizer.NewDebuginfodClient(debuginfodBaseURL, symbolizer.NewMetrics(nil))
 
 	// Alternatively, use a local debug info file:
 	//client := &localDebuginfodClient{debugFilePath: "/path/to/your/debug/file"}
 
-	s := symbolizer.NewSymbolizer(client, nil, nil)
+	s := symbolizer.NewProfileSymbolizer(client, nil, symbolizer.NewMetrics(nil))
 	ctx := context.Background()
 
 	_, err := client.FetchDebuginfo(buildID)
 	if err != nil {
 		log.Fatalf("Failed to fetch debug info: %v", err)
 	}
-	//defer os.Remove(debugFilePath)
 
-	// Create a request to symbolize specific addresses
-	req := symbolizer.Request{
-		BuildID: buildID,
-		Mappings: []symbolizer.RequestMapping{
+	// {
+	// 	Address: 0x1500,
+	// 	Mapping: &pprof.Mapping{},
+	// },
+	// {
+	// 	Address: 0x3c5a,
+	// 	Mapping: &pprof.Mapping{},
+	// },
+	// {
+	// 	Address: 0x2745,
+	// 	Mapping: &pprof.Mapping{},
+	// },
+
+	// Create a profile with the address we want to symbolize
+	// c047672cae7964324658491e7dee26748ae5d2f8
+	// profile := &googlev1.Profile{
+	// 	Mapping: []*googlev1.Mapping{{
+	// 		BuildId:     1,
+	// 		MemoryStart: 0x0,
+	// 		MemoryLimit: 0x1000000,
+	// 		FileOffset:  0x0,
+	// 	}},
+	// 	Location: []*googlev1.Location{{
+	// 		MappingId: 1,
+	// 		Address:   0x11a230,
+	// 	}},
+	// 	StringTable: []string{"", buildID},
+	// }
+
+	profile := &googlev1.Profile{
+		Mapping: []*googlev1.Mapping{{
+			BuildId:     1,
+			MemoryStart: 0x0,
+			MemoryLimit: 0x1000000,
+			FileOffset:  0x0,
+		}},
+		Location: []*googlev1.Location{
 			{
-				Locations: []*symbolizer.Location{
-					{
-						Address: 0x1500,
-						Mapping: &pprof.Mapping{},
-					},
-					{
-						Address: 0x3c5a,
-						Mapping: &pprof.Mapping{},
-					},
-					{
-						Address: 0x2745,
-						Mapping: &pprof.Mapping{},
-					},
-				},
+				MappingId: 1,
+				Address:   0x1500,
+			},
+			{
+				MappingId: 1,
+				Address:   0x3c5a,
+			},
+			{
+				MappingId: 1,
+				Address:   0x2745,
 			},
 		},
+		StringTable: []string{"", buildID},
 	}
 
-	if err := s.Symbolize(ctx, req); err != nil {
+	// Marshal profile into tree report
+	data, err := profile.MarshalVT()
+	if err != nil {
+		log.Fatalf("Failed to marshal profile: %v", err)
+	}
+	report := &queryv1.TreeReport{Tree: data}
+
+	// Run symbolization
+	if err := s.SymbolizeTree(ctx, report); err != nil {
 		log.Fatalf("Failed to symbolize: %v", err)
 	}
 
-	fmt.Println("Symbolization Results:")
-	fmt.Printf("Build ID: %s\n", buildID)
-	fmt.Println("----------------------------------------")
-
-	for i, mapping := range req.Mappings {
-		fmt.Printf("Mapping #%d:\n", i+1)
-		for _, loc := range mapping.Locations {
-			fmt.Printf("\nAddress: 0x%x\n", loc.Address)
-			if len(loc.Lines) == 0 {
-				fmt.Println("  No symbolization information found")
-				continue
-			}
-
-			for j, line := range loc.Lines {
-				fmt.Printf("  Line %d:\n", j+1)
-				if line.Function != nil {
-					fmt.Printf("    Function:   %s\n", line.Function.Name)
-					fmt.Printf("    File:       %s\n", line.Function.Filename)
-					fmt.Printf("    Line:       %d\n", line.Line)
-					fmt.Printf("    StartLine:  %d\n", line.Function.StartLine)
-				} else {
-					fmt.Println("    No function information available")
-				}
-			}
-			fmt.Println("----------------------------------------")
-		}
+	// Unmarshal result for printing
+	result := &googlev1.Profile{}
+	if err := result.UnmarshalVT(report.Tree); err != nil {
+		log.Fatalf("Failed to unmarshal result: %v", err)
 	}
 
-	// Alternatively: Symbolize all addresses in the binary
-	// Note: Comment out the above specific symbolization when using this
-	// as it's a different approach meant for exploring all available symbols
-	//if err := symbolizer.SymbolizeAll(ctx, buildID); err != nil {
-	//	log.Fatalf("Failed to symbolize all addresses: %v", err)
-	//}
+	printResults(result)
+}
 
+func printResults(p *googlev1.Profile) {
+	fmt.Println("Symbolization Results:")
+	for _, loc := range p.Location {
+		fmt.Printf("\nAddress: 0x%x\n", loc.Address)
+		if len(loc.Line) == 0 {
+			fmt.Println("  No symbolization information found")
+			continue
+		}
+
+		for i, line := range loc.Line {
+			fmt.Printf("  Line %d:\n", i+1)
+			if fn := p.Function[line.FunctionId]; fn != nil {
+				fmt.Printf("    Function:   %s\n", p.StringTable[fn.Name])
+				fmt.Printf("    File:       %s\n", p.StringTable[fn.Filename])
+				fmt.Printf("    Line:       %d\n", line.Line)
+				fmt.Printf("    StartLine:  %d\n", fn.StartLine)
+			}
+		}
+	}
 	fmt.Println("\nSymbolization completed successfully.")
 }
 
