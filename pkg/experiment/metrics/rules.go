4775a2ed68e811fc35ae9592493b8ad0d27c882a
Alberto Soto <alberto.soto@grafana.com>
2025-02-10 09:38:42 +0100

    error handling

--- pkg/experiment/metrics/rules.go
+++ pkg/experiment/metrics/rules.go
@@ -13,40 +13,5 @@ type RecordingRule struct {
 
 func recordingRulesFromTenant(tenant string) []*RecordingRule {
 	// TODO
-	return []*RecordingRule{
-		{
-			profileType: "process_cpu:samples:count:cpu:nanoseconds",
-			metricName:  "ride_sharing_app_car_cpu_nanoseconds",
-			matchers: []*labels.Matcher{
-				{
-					Type:  labels.MatchEqual,
-					Name:  "service_name",
-					Value: "ride-sharing-app",
-				},
-				{
-					Type:  labels.MatchEqual,
-					Name:  "vehicle",
-					Value: "car",
-				},
-			},
-			keepLabels: []string{"region"},
-		},
-		{
-			profileType: "process_cpu:samples:count:cpu:nanoseconds",
-			metricName:  "ride_sharing_app_car_all_regions_cpu_nanoseconds",
-			matchers: []*labels.Matcher{
-				{
-					Type:  labels.MatchEqual,
-					Name:  "service_name",
-					Value: "ride-sharing-app",
-				},
-				{
-					Type:  labels.MatchEqual,
-					Name:  "vehicle",
-					Value: "car",
-				},
-			},
-			keepLabels: []string{},
-		},
-	}
+	return []*RecordingRule{}
 }
