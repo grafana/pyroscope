7c32355d1c0d00186444d2ab9967649b6c40be86
Alberto Soto <alberto.soto@grafana.com>
2025-02-11 16:57:34 +0100

    merging recorder into observer. renaming observer. static empty exporter

--- pkg/experiment/metrics/exporter.go
+++ pkg/experiment/metrics/exporter.go
@@ -13,10 +13,7 @@ import (
 	"github.com/prometheus/prometheus/storage/remote"
 )
 
-type Exporter struct {
-	client remote.WriteClient
-	data   map[AggregatedFingerprint]*Sample
-}
+type Exporter struct{}
 
 type Config struct {
 	url      string
@@ -24,56 +21,18 @@ type Config struct {
 	password config.Secret
 }
 
-func NewExporter(tenant string, recordings []*Recording) (*Exporter, error) {
-	exporter := &Exporter{
-		data: map[AggregatedFingerprint]*Sample{},
-	}
-	for _, r := range recordings {
-		for fp, ts := range r.data {
-			exporter.data[fp] = ts
-		}
-	}
-	if len(exporter.data) != 0 {
-		cfg := configFromTenant(tenant)
-		client, err := newClient(cfg)
-		if err != nil {
-			return nil, err
-		}
-		exporter.client = client
-	}
-	return exporter, nil
-}
-
-func (e *Exporter) Send() error {
-	if e.client == nil {
-		// no client = no data to send
-		return nil
-	}
-
-	p := &prompb.WriteRequest{Timeseries: make([]prompb.TimeSeries, 0, len(e.data))}
-	for _, sample := range e.data {
-		pts := prompb.TimeSeries{
-			Labels: make([]prompb.Label, 0, len(sample.Labels)),
-			Samples: []prompb.Sample{
-				{
-					Value:     sample.Value,
-					Timestamp: sample.Timestamp,
-				},
-			},
-		}
-		for _, l := range sample.Labels {
-			pts.Labels = append(pts.Labels, prompb.Label{
-				Name:  l.Name,
-				Value: l.Value,
-			})
-		}
-		p.Timeseries = append(p.Timeseries, pts)
-	}
+func (e *Exporter) Send(tenant string, data []prompb.TimeSeries) error {
+	p := &prompb.WriteRequest{Timeseries: data}
 	buf := proto.NewBuffer(nil)
 	if err := buf.Marshal(p); err != nil {
 		return err
 	}
-	return e.client.Store(context.Background(), snappy.Encode(nil, buf.Bytes()), 0)
+	cfg := configFromTenant(tenant)
+	client, err := newClient(cfg)
+	if err != nil {
+		return err
+	}
+	return client.Store(context.Background(), snappy.Encode(nil, buf.Bytes()), 0)
 }
 
 func newClient(cfg Config) (remote.WriteClient, error) {
@@ -104,7 +63,7 @@ func newClient(cfg Config) (remote.WriteClient, error) {
 	return c, nil
 }
 
-func configFromTenant(tenant string) Config {
+func configFromTenant(string) Config {
 	// TODO
 	return Config{
 		url:      "omitted",
--- pkg/experiment/metrics/observer.go
+++ pkg/experiment/metrics/observer.go
@@ -2,31 +2,50 @@ package metrics
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/cespare/xxhash/v2"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/oklog/ulid"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/prompb"
 
 	metastorev1 "github.com/grafana/pyroscope/api/gen/proto/go/metastore/v1"
 	"github.com/grafana/pyroscope/pkg/experiment/block"
+	phlaremodel "github.com/grafana/pyroscope/pkg/model"
 )
 
-type MetricsExporterSampleObserver struct {
+type SampleObserver struct {
+	exporter          *Exporter
 	tenant            string
-	recorder          *Recorder
+	Recordings        []*Recording
 	recordingTime     int64
 	pyroscopeInstance string
 	logger            log.Logger
 }
 
-func NewMetricsExporterSampleObserver(meta *metastorev1.BlockMeta, logger log.Logger) *MetricsExporterSampleObserver {
+type Recording struct {
+	rule  RecordingRule
+	data  map[AggregatedFingerprint]*Sample
+	state *recordingState
+}
+
+type recordingState struct {
+	fp      *model.Fingerprint
+	matches bool
+	sample  *Sample
+}
+
+func NewSampleObserver(meta *metastorev1.BlockMeta, logger log.Logger) *SampleObserver {
 	recordingTime := int64(ulid.MustParse(meta.Id).Time())
 	pyroscopeInstance := pyroscopeInstanceHash(meta.Shard, meta.CreatedBy)
-	return &MetricsExporterSampleObserver{
+	return &SampleObserver{
 		recordingTime:     recordingTime,
 		pyroscopeInstance: pyroscopeInstance,
 		logger:            logger,
+		exporter:          &Exporter{},
 	}
 }
 
@@ -37,28 +56,144 @@ func pyroscopeInstanceHash(shard uint32, createdBy int32) string {
 	return fmt.Sprintf("%x", xxhash.Sum64(buf))
 }
 
-func (o *MetricsExporterSampleObserver) Init(tenant string) {
+func (o *SampleObserver) Init(tenant string) {
 	o.tenant = tenant
-	o.recorder = NewRecorder(recordingRulesFromTenant(tenant), o.recordingTime, o.pyroscopeInstance)
+	recordingRules := recordingRulesFromTenant(o.tenant)
+	o.Recordings = make([]*Recording, len(recordingRules))
+	for i, rule := range recordingRules {
+		o.Recordings[i] = &Recording{
+			rule: *rule,
+			data: make(map[AggregatedFingerprint]*Sample),
+			state: &recordingState{
+				fp: nil,
+			},
+		}
+	}
+
 }
 
-func (o *MetricsExporterSampleObserver) Observe(row block.ProfileEntry) {
-	o.recorder.RecordRow(row.Fingerprint, row.Labels, row.Row.TotalValue())
+func (o *SampleObserver) Observe(row block.ProfileEntry) {
+	for _, recording := range o.Recordings {
+		if recording.state.fp == nil || *recording.state.fp != row.Fingerprint {
+			recording.InitState(row.Fingerprint, row.Labels, o.pyroscopeInstance, o.recordingTime)
+		}
+		if !recording.state.matches {
+			continue
+		}
+		recording.state.sample.Value += float64(row.Row.TotalValue())
+	}
 }
 
-func (o *MetricsExporterSampleObserver) Flush() error {
-	rec := o.recorder
-	o.recorder = nil
-	go func(tenant string, recorder *Recorder) {
-		exporter, err := NewExporter(tenant, recorder.Recordings)
-		if err != nil {
-			level.Error(o.logger).Log("msg", "error creating metrics exporter", "err", err)
-			return
+func (o *SampleObserver) Flush() error {
+	recs := o.Recordings
+	o.Recordings = nil
+	go func(tenant string, recordings []*Recording) {
+		timeSeries := make([]prompb.TimeSeries, 0)
+		for _, recording := range recordings {
+			for _, sample := range recording.data {
+				ts := prompb.TimeSeries{
+					Labels: make([]prompb.Label, 0, len(sample.Labels)),
+					Samples: []prompb.Sample{
+						{
+							Value:     sample.Value,
+							Timestamp: sample.Timestamp,
+						},
+					},
+				}
+				for _, l := range sample.Labels {
+					ts.Labels = append(ts.Labels, prompb.Label{
+						Name:  l.Name,
+						Value: l.Value,
+					})
+				}
+				timeSeries = append(timeSeries, ts)
+			}
 		}
 
-		if err = exporter.Send(); err != nil {
+		if err := o.exporter.Send(tenant, timeSeries); err != nil {
 			level.Error(o.logger).Log("msg", "error sending recording metrics", "err", err)
 		}
-	}(o.tenant, rec)
+	}(o.tenant, recs)
 	return nil
+}
+
+func (r *Recording) InitState(fp model.Fingerprint, lbls phlaremodel.Labels, pyroscopeInstance string, recordingTime int64) {
+	r.state.fp = &fp
+	labelsMap := map[string]string{}
+	for _, label := range lbls {
+		labelsMap[label.Name] = label.Value
+	}
+	r.state.matches = r.matches(labelsMap)
+	if !r.state.matches {
+		return
+	}
+
+	exportedLabels := generateExportedLabels(labelsMap, r, pyroscopeInstance)
+	sort.Sort(exportedLabels)
+	aggregatedFp := AggregatedFingerprint(exportedLabels.Hash())
+	sample, ok := r.data[aggregatedFp]
+	if !ok {
+		sample = newSample(exportedLabels, recordingTime)
+		r.data[aggregatedFp] = sample
+	}
+	r.state.sample = sample
+}
+
+type AggregatedFingerprint model.Fingerprint
+
+type Sample struct {
+	Labels    labels.Labels
+	Value     float64
+	Timestamp int64
+}
+
+func newSample(exportedLabels labels.Labels, time int64) *Sample {
+	return &Sample{
+		Labels:    exportedLabels,
+		Value:     float64(0),
+		Timestamp: time,
+	}
+}
+
+func generateExportedLabels(labelsMap map[string]string, rec *Recording, pyroscopeInstance string) labels.Labels {
+	exportedLabels := labels.Labels{
+		labels.Label{
+			Name:  "__name__",
+			Value: rec.rule.metricName,
+		},
+		labels.Label{
+			Name:  "pyroscope_instance",
+			Value: pyroscopeInstance,
+		},
+	}
+	// Add filters as exported labels
+	for _, matcher := range rec.rule.matchers {
+		exportedLabels = append(exportedLabels, labels.Label{
+			Name:  matcher.Name,
+			Value: matcher.Value,
+		})
+	}
+	// Keep the expected labels
+	for _, label := range rec.rule.keepLabels {
+		labelValue, ok := labelsMap[label]
+		if ok {
+			exportedLabels = append(exportedLabels, labels.Label{
+				Name:  label,
+				Value: labelValue,
+			})
+		}
+	}
+	return exportedLabels
+}
+
+func (r *Recording) matches(labelsMap map[string]string) bool {
+	if r.rule.profileType != labelsMap["__profile_type__"] {
+		return false
+	}
+	for _, matcher := range r.rule.matchers {
+		if !matcher.Matches(labelsMap[matcher.Name]) {
+			return false
+		}
+	}
+	return true
 }
