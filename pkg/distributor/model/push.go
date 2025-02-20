b18f22b08bd2631bb76fe005ba69885e5f1a42a6
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2025-02-20 13:37:11 -0400

    Add profile annotations on the write path

--- pkg/distributor/model/push.go
+++ pkg/distributor/model/push.go
@@ -1,7 +1,11 @@
 package model
 
 import (
+	"encoding/json"
+	"time"
+
 	v1 "github.com/grafana/pyroscope/api/gen/proto/go/types/v1"
+	"github.com/grafana/pyroscope/pkg/distributor/ingest_limits"
 	phlaremodel "github.com/grafana/pyroscope/pkg/model"
 	"github.com/grafana/pyroscope/pkg/pprof"
 )
@@ -33,6 +37,8 @@ type ProfileSeries struct {
 	Labels   []*v1.LabelPair
 	Samples  []*ProfileSample
 	Language string
+
+	Annotations []string
 }
 
 func (p *ProfileSeries) GetLanguage() string {
@@ -93,4 +99,50 @@ func (req *PushRequest) Clone() *PushRequest {
 		}
 	}
 	return c
+}
+
+func (req *PushRequest) MarkThrottledTenant(l *ingest_limits.Config) error {
+	annotation := &ThrottledAnnotation{
+		PeriodType:       l.PeriodType,
+		PeriodLimitMb:    l.PeriodLimitMb,
+		LimitResetTime:   l.LimitResetTime,
+		SamplingPeriod:   l.Sampling.Period,
+		SamplingRequests: l.Sampling.NumRequests,
+	}
+	bytes, err := json.Marshal(annotation)
+	if err != nil {
+		return err
+	}
+	for _, series := range req.Series {
+		series.Annotations = append(series.Annotations, string(bytes))
+	}
+	return nil
+}
+
+func (req *PushRequest) MarkThrottledUsageGroup(l *ingest_limits.Config, usageGroup string) error {
+	annotation := &ThrottledAnnotation{
+		PeriodType:       l.PeriodType,
+		PeriodLimitMb:    l.PeriodLimitMb,
+		LimitResetTime:   l.LimitResetTime,
+		SamplingPeriod:   l.Sampling.Period,
+		SamplingRequests: l.Sampling.NumRequests,
+		UsageGroup:       usageGroup,
+	}
+	bytes, err := json.Marshal(annotation)
+	if err != nil {
+		return err
+	}
+	for _, series := range req.Series {
+		series.Annotations = append(series.Annotations, string(bytes))
+	}
+	return nil
+}
+
+type ThrottledAnnotation struct {
+	PeriodType       string        `json:"period_type"`
+	PeriodLimitMb    int           `json:"period_limit_mb"`
+	LimitResetTime   int64         `json:"limit_reset_time"`
+	SamplingPeriod   time.Duration `json:"sampling_period"`
+	SamplingRequests int           `yaml:"sampling_requests"`
+	UsageGroup       string        `json:"usage_group"`
 }
