9bea45df23d8094404ecdaa0500f61971e16520f
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2025-02-27 08:30:05 -0400

    Fix test

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -527,6 +527,12 @@ func Test_IngestLimits(t *testing.T) {
 						Samples: []*distributormodel.ProfileSample{
 							{
 								RawProfile: collectTestProfileBytes(t),
+								Profile: pprof2.RawFromProto(&profilev1.Profile{
+									Sample: []*profilev1.Sample{{
+										Value: []int64{1},
+									}},
+									StringTable: []string{""},
+								}),
 							},
 						},
 					},
