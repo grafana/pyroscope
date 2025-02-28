aa4f0c15b7e65778f6444eb2f92f501786de7098
bryanhuhta <bryan.huhta@grafana.com>
2025-02-28 10:19:08 -0600

    Only register settings services when they're enabled

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -208,8 +208,14 @@ func (a *API) RegisterRuntimeConfig(runtimeConfigHandler http.HandlerFunc, userL
 
 func (a *API) RegisterTenantSettings(ts *settings.TenantSettings) {
 	settingsv1connect.RegisterSettingsServiceHandler(a.server.HTTP, ts, a.connectOptionsAuthRecovery()...)
-	settingsv1connect.RegisterCollectionRulesServiceHandler(a.server.HTTP, ts, a.connectOptionsAuthRecovery()...)
-	settingsv1connect.RegisterRecordingRulesServiceHandler(a.server.HTTP, ts, a.connectOptionsAuthRecovery()...)
+
+	if ts.CollectionRulesServiceHandler != nil {
+		settingsv1connect.RegisterCollectionRulesServiceHandler(a.server.HTTP, ts, a.connectOptionsAuthRecovery()...)
+	}
+
+	if ts.RecordingRulesServiceHandler != nil {
+		settingsv1connect.RegisterRecordingRulesServiceHandler(a.server.HTTP, ts, a.connectOptionsAuthRecovery()...)
+	}
 }
 
 // RegisterOverridesExporter registers the endpoints associated with the overrides exporter.
