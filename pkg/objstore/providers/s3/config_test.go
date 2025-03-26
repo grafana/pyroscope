30782771e943d5d40bf77f1f6ea8ae1b1a508cbb
Christian Simon <simon@swine.de>
2025-03-26 19:47:55 +0000

    fix: Update minio-go to fix bug in AWS STS auth
    With the upgrade of objstore libraries from #3954 we introduced bug
    
    This fixes the underlying problem and adds a tests to validate that form
    of auth.

--- pkg/objstore/providers/s3/config_test.go
+++ pkg/objstore/providers/s3/config_test.go
@@ -6,9 +6,16 @@
 package s3
 
 import (
+	"context"
 	"encoding/base64"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/go-kit/log"
 
 	"github.com/grafana/dskit/flagext"
 	"github.com/stretchr/testify/assert"
@@ -157,4 +164,125 @@ func TestConfig_Validate(t *testing.T) {
 			assert.Equal(t, testData.expected, testData.setup().Validate())
 		})
 	}
+}
+
+type testRoundTripper struct {
+	roundTrip func(r *http.Request) (*http.Response, error)
+}
+
+func (t *testRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	return t.roundTrip(r)
+}
+
+func handleSTSRequest(t *testing.T, r *http.Request, w http.ResponseWriter) {
+	body, err := io.ReadAll(r.Body)
+	require.NoError(t, err)
+
+	require.Contains(t, string(body), "RoleArn=arn%3Ahello-world")
+	require.Contains(t, string(body), "WebIdentityToken=my-web-token")
+	require.Contains(t, string(body), "Action=AssumeRoleWithWebIdentity")
+
+	w.WriteHeader(200)
+	w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>
+				<AssumeRoleWithWebIdentityResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
+				  <AssumeRoleWithWebIdentityResult>
+				    <Credentials>
+				      <AccessKeyId>test-key</AccessKeyId>
+				      <SecretAccessKey>test-secret</SecretAccessKey>
+				      <SessionToken>test-token</SessionToken>
+				      <Expiration>` + time.Now().Add(time.Hour).Format(time.RFC3339) + `</Expiration>
+				    </Credentials>
+				  </AssumeRoleWithWebIdentityResult>
+				  <ResponseMetadata>
+				    <RequestId>test-request-id</RequestId>
+				  </ResponseMetadata>
+				</AssumeRoleWithWebIdentityResponse>`))
+
+}
+
+func overrideEnv(t testing.TB, kv ...string) {
+	old := make([]string, len(kv))
+	for i := 0; i < len(kv); i += 2 {
+		k := kv[i]
+		v := kv[i+1]
+		old[i] = k
+		old[i+1] = os.Getenv(k)
+		os.Setenv(k, v)
+	}
+	t.Cleanup(func() {
+		for i := 0; i < len(old); i += 2 {
+			os.Setenv(old[i], old[i+1])
+		}
+	})
+}
+
+func TestAWSSTSWebIdentity(t *testing.T) {
+	logger := log.NewNopLogger()
+	tmpDir := t.TempDir()
+
+	// override env variables, will be cleaned up by t.Cleanup
+	overrideEnv(t,
+		"AWS_WEB_IDENTITY_TOKEN_FILE", tmpDir+"/token",
+		"AWS_ROLE_ARN", "arn:hello-world",
+		"AWS_DEFAULT_REGION", "eu-central-1",
+		"AWS_CONFIG_FILE", "/dev/null", // dont accidentally use real config
+		"AWS_ACCESS_KEY_ID", "", // dont use real credentials
+		"AWS_SECRET_ACCESS_KEY", "", // dont use real credentials
+	)
+
+	rt := &testRoundTripper{
+		roundTrip: func(r *http.Request) (*http.Response, error) {
+			w := httptest.NewRecorder()
+			if r.Body != nil {
+				defer r.Body.Close()
+			}
+			switch r.URL.String() {
+			case "https://sts.amazonaws.com":
+				handleSTSRequest(t, r, w)
+			case "https://eu-central-1.amazonaws.com/pyroscope-test-bucket/test":
+				assert.Equal(t, "GET", r.Method)
+				assert.Contains(t, r.Header.Get("Authorization"), "AWS4-HMAC-SHA256 Credential=test-key")
+				w.Header().Set("Last-Modified", time.Now().Format(time.RFC1123))
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte("test"))
+			default:
+				w.WriteHeader(404)
+				_, _ = w.Write([]byte("unexpected"))
+				t.Errorf("unexpected request: %s", r.URL.Host)
+				t.FailNow()
+			}
+			return w.Result(), nil
+		},
+	}
+	oldDefaultTransport := http.DefaultTransport
+	oldDefaultClient := http.DefaultClient
+	http.DefaultTransport = rt
+	http.DefaultClient = &http.Client{
+		Transport: rt,
+	}
+	// restore default transport and client
+	t.Cleanup(func() {
+		http.DefaultTransport = oldDefaultTransport
+		http.DefaultClient = oldDefaultClient
+	})
+
+	// mock a web token
+	err := os.WriteFile(tmpDir+"/token", []byte("my-web-token"), 0644)
+	require.NoError(t, err)
+
+	cfg := Config{
+		SignatureVersion: SignatureVersionV4,
+		BucketName:       "pyroscope-test-bucket",
+		Region:           "eu-central-1",
+		Endpoint:         "eu-central-1.amazonaws.com",
+		BucketLookupType: AutoLookup,
+	}
+
+	cfg.HTTP.Transport = rt
+	r, err := NewBucketClient(cfg, "test", logger)
+	require.NoError(t, err)
+
+	_, err = r.Get(context.Background(), "test")
+	require.NoError(t, err)
+
 }
