c54e6f8c5de3b9eab484aae77b7c73771e89504a
bryanhuhta <bryan.huhta@grafana.com>
2025-03-27 09:18:08 -0500

    Clean up scaffolded project

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4040',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
 });
