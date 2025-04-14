96974a7d404efbd6ebe1779707a06d290539978b
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-04-14 17:28:44 +0700

    Revert "Refactor workflows to streamline Go version update (#4101)" (#4103)
    This reverts commit 1f7ec2db230553de428c2b4c24b8baec144f9b9a.

--- tools/upgrade-go-version.sh
+++ tools/upgrade-go-version.sh
@@ -2,39 +2,22 @@
 
 set -euo pipefail
 
-# If no arguments provided, determine the version from go.mod and fetch the latest patch version
-if [ $# -eq 0 ]; then
-    # Use the get-latest-go-version.sh script to get the latest patch version
-    SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-    VERSION=$("$SCRIPT_DIR/get-latest-go-version.sh")
-
-    if [ -z "$VERSION" ]; then
-        echo "Error: Could not determine latest patch version"
-        exit 1
-    fi
-else
-    # Use the provided version
-    if [ $# -ne 1 ]; then
-        echo "Usage: $0 [version]"
-        echo "If version is not provided, the latest patch version matching the current go.mod minor version will be used."
-        exit 1
-    fi
-    VERSION="$1"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
 fi
 
-echo "Updating Go version to $VERSION"
-
 # update ci/cd versions
-git ls-files .github/workflows | xargs sed -i 's/go-version:\([ \["]\+\)\([0-9\.\=<>]\+\)/go-version:\1'$VERSION'/g' 
+git ls-files .github/workflows | xargs sed -i 's/go-version:\([ \["]\+\)\([0-9\.\=<>]\+\)/go-version:\1'$1'/g' 
 
 # update goreleaser check
-sed -i 's/go version go[0-9\.]\+/go version go'$VERSION'/g' .goreleaser.yaml
+sed -i 's/go version go[0-9\.]\+/go version go'$1'/g' .goreleaser.yaml
 
 # update all dockerfile versions, skips the elf tests from ebpf
 DOCKER_FILES=$(git ls-files '**/Dockerfile*' | grep -v ebpf/symtab/elf/testdata/Dockerfile)
-sed -i 's/golang:[0-9\.]\+/golang:'$VERSION'/g' $DOCKER_FILES
+sed -i 's/golang:[0-9\.]\+/golang:'$1'/g' $DOCKER_FILES
 git add -u $DOCKER_FILES
 
 # add changes
 git add -u .github/workflows .goreleaser.yaml
-git commit -m "Update golang version to $VERSION"
+git commit -m "Update golang version to $1"
