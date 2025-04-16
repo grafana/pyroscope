368e60351fecdde46289b99dc95ef95822cdd5e4
Christian Simon <simon@swine.de>
2025-04-16 15:54:47 +0100

    Add the novelty calculation, based on function selfs

--- cmd/profilecli/query-novelty.go
+++ cmd/profilecli/query-novelty.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"sort"
 	"strings"
 	"time"
 
 	"connectrpc.com/connect"
+	"github.com/dustin/go-humanize"
 	"github.com/go-kit/log/level"
 	"github.com/pkg/errors"
 
@@ -18,9 +17,11 @@ import (
 
 type queryNoveltyParams struct {
 	*queryParams
-	TopN        uint64        // TOPk stacktraces
-	StepSize    time.Duration // Step size for the novelty query
-	ProfileType string
+	TopN           uint64        // TOPk stacktraces
+	StepSize       time.Duration // Step size for the novelty query
+	ProfileType    string
+	Dimension      string
+	MergeThreshold float64
 }
 
 func addQueryNoveltyParams(queryCmd commander) *queryNoveltyParams {
@@ -28,7 +29,9 @@ func addQueryNoveltyParams(queryCmd commander) *queryNoveltyParams {
 	params.queryParams = addQueryParams(queryCmd)
 	queryCmd.Flag("profile-type", "Profile type to query.").Default("process_cpu:cpu:nanoseconds:cpu:nanoseconds").StringVar(&params.ProfileType)
 	queryCmd.Flag("step-size", "Show the top N stacktraces").Default("15s").DurationVar(&params.StepSize)
-	queryCmd.Flag("top-n", "Show the top N stacktraces").Default("20").Uint64Var(&params.TopN)
+	queryCmd.Flag("top-n", "Show the top N").Default("20").Uint64Var(&params.TopN)
+	queryCmd.Flag("dimension", "Aggregate stacktrace-self or function-self").Default("function-self").StringVar(&params.Dimension)
+	queryCmd.Flag("merge-threshold", "Threshold when to consider profiles simliar enough").Default("0.10").Float64Var(&params.MergeThreshold)
 	return params
 }
 
@@ -38,7 +41,7 @@ func queryNovelty(ctx context.Context, params *queryNoveltyParams) (err error) {
 		return err
 	}
 
-	samples := novelty.NewSamples(0, 0.05)
+	samples := novelty.NewSamples(0, params.MergeThreshold)
 
 	pos := from
 
@@ -64,43 +67,81 @@ func queryNovelty(ctx context.Context, params *queryNoveltyParams) (err error) {
 		if err != nil {
 			return errors.Wrap(err, "failed to query")
 		}
-
-		// sort the samples by the first type
+		var stacks []string
+		var values []int64
 		profileTypeIdx := 0
-		sort.Slice(resp.Msg.Sample, func(i, j int) bool {
-			return resp.Msg.Sample[i].Value[profileTypeIdx] > resp.Msg.Sample[j].Value[profileTypeIdx]
-		})
 
-		// append the first N samples to the noveltySamples
-		end := len(resp.Msg.Sample)
-		if end > int(params.TopN) {
-			end = int(params.TopN)
-		}
-		stacks := make([]string, 0, end)
-		values := make([]int64, 0, end)
-		for _, sample := range resp.Msg.Sample[:end] {
-			stackParts := make([]string, len(sample.LocationId))
-			for idx := range sample.LocationId {
-				loc := resp.Msg.Location[sample.LocationId[idx]-1]
-				if len(loc.Line) == 0 {
-					panic("no line")
-				}
-				functionID := loc.Line[0].FunctionId
-				function := resp.Msg.Function[functionID-1]
-				stackParts[idx] = resp.Msg.StringTable[function.Name]
+		if params.Dimension == "stacktrace-self" {
+			// sort the samples by the first type
+			sort.Slice(resp.Msg.Sample, func(i, j int) bool {
+				return resp.Msg.Sample[i].Value[profileTypeIdx] > resp.Msg.Sample[j].Value[profileTypeIdx]
+			})
+			// append the first N samples to the noveltySamples
+			end := len(resp.Msg.Sample)
+			if end > int(params.TopN) {
+				end = int(params.TopN)
 			}
-			stacks = append(stacks, strings.Join(stackParts, "|"))
-			values = append(values, sample.Value[profileTypeIdx])
+			stacks = make([]string, 0, end)
+			values = make([]int64, 0, end)
+			for _, sample := range resp.Msg.Sample[:end] {
+				stackParts := make([]string, len(sample.LocationId))
+				for idx := range sample.LocationId {
+					loc := resp.Msg.Location[sample.LocationId[idx]-1]
+					if len(loc.Line) == 0 {
+						panic("no line")
+					}
+					functionID := loc.Line[0].FunctionId
+					function := resp.Msg.Function[functionID-1]
+					stackParts[idx] = resp.Msg.StringTable[function.Name]
+				}
+				stacks = append(stacks, strings.Join(stackParts, "|"))
+				values = append(values, sample.Value[profileTypeIdx])
+			}
+		} else if params.Dimension == "function-self" {
+			values = make([]int64, len(resp.Msg.Function))
+
+			// add the leaf function to the values
+			for _, sample := range resp.Msg.Sample {
+				if len(sample.LocationId) == 0 {
+					continue
+				}
+
+				leafLoc := resp.Msg.Location[sample.LocationId[0]-1]
+				functionID := leafLoc.Line[0].FunctionId
+				values[functionID-1] += sample.Value[profileTypeIdx]
+			}
+
+			// sort the functions and values by the biggest
+			sort.Slice(resp.Msg.Function, func(i, j int) bool {
+				return values[i] > values[j]
+			})
+			sort.Slice(values, func(i, j int) bool {
+				return values[i] > values[j]
+			})
+
+			// append the first N samples to the noveltySamples
+			end := len(resp.Msg.Function)
+			if end > int(params.TopN) {
+				end = int(params.TopN)
+			}
+
+			// resovle to func names
+			stacks = make([]string, end)
+			for i := range stacks {
+				stacks[i] = resp.Msg.StringTable[resp.Msg.Function[i].Name]
+			}
+			values = values[:end]
+		} else {
+			return errors.New("invalid dimension: " + params.Dimension)
 		}
 
-		for idx, name := range stacks {
-			fmt.Printf("%d: %s\n", idx, name)
-			fmt.Printf("%d: %s\n", idx, values[idx])
+		for i, stack := range stacks {
+			level.Debug(logger).Log("stack", stack, "value", humanize.FormatInteger("", int(values[i])))
 		}
 
 		noveltyScore := samples.Add(stacks, values)
 
-		log.Printf("novelty score: %f", noveltyScore)
+		level.Info(logger).Log("novelty score", noveltyScore)
 	}
 
 	return nil
