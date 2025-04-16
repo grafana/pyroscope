1fe627798462edbc61ee82aba793b84f3dd85e38
Christian Simon <simon@swine.de>
2025-04-16 14:28:26 +0100

    Use correct retry config
    This change is a consequence by the grpc-go change in v1.70.0, which got
    merged here: https://github.com/grpc/grpc-go/pull/7905
    
    As we do call `WithDisableRetry()`, it should be a no-op.

--- pkg/experiment/ingester/client/client.go
+++ pkg/experiment/ingester/client/client.go
@@ -127,16 +127,30 @@ func shouldBeHandledByCaller(err error) bool {
 	return true
 }
 
-// The default gRPC service config is explicitly set to
-// not retry and load balance between instances.
+// The default gRPC service config is explicitly set to balance between
+// instances.
 const grpcServiceConfig = `{
-    "methodConfig": [{
-        "name": [{"service": ""}],
-        "retryPolicy": {}
-    }],
-	"healthCheckConfig": {
-		"serviceName": "pyroscope.segment-writer"
-	}
+  "methodConfig": [
+    {
+      "name": [
+        {
+          "service": ""
+        }
+      ],
+      "retryPolicy": {
+        "maxAttempts": 4,
+        "initialBackoff": "0.1s",
+        "maxBackoff": "1s",
+        "backoffMultiplier": 2,
+        "retryableStatusCodes": [
+          "UNAVAILABLE"
+        ]
+      }
+    }
+  ],
+  "healthCheckConfig": {
+    "serviceName": "pyroscope.segment-writer"
+  }
 }`
 
 type Client struct {
