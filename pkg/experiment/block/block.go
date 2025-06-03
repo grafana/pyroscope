f979ab6edf43cdd113c46777075625b34097ab39
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2025-06-03 11:46:12 +0800

    Revert "perf(v2): static parquet page buffer size (#4208)"
    This reverts commit 77754daf95b1fc93e53916af7bc49cc53c62348a.

--- pkg/experiment/block/block.go
+++ pkg/experiment/block/block.go
@@ -23,11 +23,29 @@ const (
 	symbolsPrefetchSize = 32 << 10
 )
 
-const (
-	// Each 2MB translates to an I/O read op.
-	parquetReadBufferSize      = 2 << 20
-	parquetPageWriteBufferSize = 1 << 20
-)
+func estimateReadBufferSize(s int64) int {
+	const minSize = 64 << 10
+	const maxSize = 1 << 20
+	// Parquet has global buffer map, where buffer size is key,
+	// so we want a low cardinality here.
+	e := nextPowerOfTwo(uint32(s / 10))
+	if e < minSize {
+		return minSize
+	}
+	return int(min(e, maxSize))
+}
+
+// This is a verbatim copy of estimateReadBufferSize.
+// It's kept for the sake of clarity and to avoid confusion.
+func estimatePageBufferSize(s int64) int {
+	const minSize = 64 << 10
+	const maxSize = 1 << 20
+	e := nextPowerOfTwo(uint32(s / 10))
+	if e < minSize {
+		return minSize
+	}
+	return int(min(e, maxSize))
+}
 
 func estimateFooterSize(size int64) int64 {
 	var s int64
@@ -48,4 +66,18 @@ func estimateFooterSize(size int64) int64 {
 		s = size
 	}
 	return s
+}
+
+func nextPowerOfTwo(n uint32) uint32 {
+	if n == 0 {
+		return 1
+	}
+	n--
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	n++
+	return n
 }
