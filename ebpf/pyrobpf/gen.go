67ff53c61863954e35850f244deaa39bfdff3faf
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-07-29 10:58:17 +0700

    chore(ebpf): remove ebpf module

--- ebpf/pyrobpf/gen.go
+++
@@ -1,4 +0,0 @@
-package pyrobpf
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go   -target amd64 -cc clang-17 -cflags "-O2 -Wall -Werror -fpie -Wno-unused-variable -Wno-unused-function" Profile ../bpf/profile.bpf.c -- -I../bpf/libbpf -I../bpf/vmlinux/
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go   -target arm64 -cc clang-17 -cflags "-O2 -Wall -Werror -fpie -Wno-unused-variable -Wno-unused-function" Profile ../bpf/profile.bpf.c -- -I../bpf/libbpf -I../bpf/vmlinux/
