ba4ba29e219af2b48f744bc63bc8a32f08adc676
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-08-23 23:04:17 +0000

    profile id

--- pkg/ingester/otlp/convert.go
+++ pkg/ingester/otlp/convert.go
@@ -28,8 +28,19 @@ func at[T any](arr []T, i int32) (T, error) {
 	return zero, fmt.Errorf("index %d out of bounds", i)
 }
 
+func allZeros(arr []byte) bool {
+	for _, val := range arr {
+		if val != 0 {
+			return false
+		}
+	}
+	return true
+}
+
 // ConvertOtelToGoogle converts an OpenTelemetry profile to a Google profile.
 func ConvertOtelToGoogle(src *otelProfile.Profile, dictionary *otelProfile.ProfilesDictionary) (map[string]convertedProfile, error) {
+	applyProfileId := src.GetProfileId() != nil && len(src.GetProfileId()) == 16 && !allZeros(src.GetProfileId())
+
 	svc2Profile := make(map[string]*profileBuilder)
 	for _, sample := range src.Sample {
 		svc, err := serviceNameFromSample(sample, dictionary)
@@ -45,8 +56,13 @@ func ConvertOtelToGoogle(src *otelProfile.Profile, dictionary *otelProfile.Profi
 			}
 			svc2Profile[svc] = p
 		}
-		if _, err := p.convertSampleBack(sample, dictionary); err != nil {
+		if gs, err := p.convertSampleBack(sample, dictionary); err != nil {
 			return nil, err
+		} else if applyProfileId {
+			gs.Label = append(gs.Label, &googleProfile.Label{
+				Key: p.addstr(pprof.ProfileIDLabelName),
+				Str: p.addstr(hex.EncodeToString(src.GetProfileId())),
+			})
 		}
 	}
 
