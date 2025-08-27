b545b71ce4a84998ac37d3110126585914400264
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-08-27 12:50:36 +0700

    remove legacy frontend

--- public/app/components/FlameGraphWrapper.tsx
+++ public/app/components/FlameGraphWrapper.tsx
@@ -4,16 +4,11 @@ import { FlameGraph } from '@grafana/flamegraph';
 import { Button, Tooltip } from '@grafana/ui';
 
 import useColorMode from '@pyroscope/hooks/colorMode.hook';
-import { FlamegraphRenderer } from '@pyroscope/legacy/flamegraph/FlamegraphRenderer';
 import ExportData from '@pyroscope/components/ExportData';
 import type { Profile } from '@pyroscope/legacy/models';
 import useExportToFlamegraphDotCom from '@pyroscope/components/exportToFlamegraphDotCom.hook';
-import { SharedQuery } from '@pyroscope/legacy/flamegraph/FlameGraph/FlameGraphRenderer';
 
-import {
-  isExportToFlamegraphDotComEnabled,
-  isGrafanaFlamegraphEnabled,
-} from '@pyroscope/util/features';
+import { isExportToFlamegraphDotComEnabled } from '@pyroscope/util/features';
 
 import { flamebearerToDataFrameDTO } from '@pyroscope/util/flamebearer';
 
@@ -21,7 +16,6 @@ type Props = {
   profile?: Profile;
   dataTestId?: string;
   vertical?: boolean;
-  sharedQuery?: SharedQuery;
   timelineEl?: React.ReactNode;
   diff?: boolean;
 };
@@ -30,68 +24,21 @@ export function FlameGraphWrapper(props: Props) {
   const { colorMode } = useColorMode();
   const exportToFlamegraphDotComFn = useExportToFlamegraphDotCom(props.profile);
 
-  if (isGrafanaFlamegraphEnabled) {
-    const dataFrame = props.profile
-      ? flamebearerToDataFrameDTO(
-          props.profile.flamebearer.levels,
-          props.profile.flamebearer.names,
-          props.profile.metadata.units,
-          Boolean(props.diff)
-        )
-      : undefined;
+  const dataFrame = props.profile
+    ? flamebearerToDataFrameDTO(
+        props.profile.flamebearer.levels,
+        props.profile.flamebearer.names,
+        props.profile.metadata.units,
+        Boolean(props.diff)
+      )
+    : undefined;
 
-    let extraEl = <></>;
+  let extraEl = <></>;
 
-    // This is a bit weird but the typing is not great. It seems like flamegraph assumed profile can be undefined
-    // but then ExportData won't work so not sure if the profile === undefined could actually happen.
-    if (props.profile) {
-      extraEl = (
-        <ExportData
-          flamebearer={props.profile}
-          exportPNG
-          exportJSON
-          exportPprof
-          exportHTML
-          exportFlamegraphDotCom={isExportToFlamegraphDotComEnabled}
-          exportFlamegraphDotComFn={exportToFlamegraphDotComFn}
-          buttonEl={({ onClick }) => {
-            return (
-              <Tooltip content={'Export Data'}>
-                <Button
-                  // Ugly hack to go around globally defined line height messing up sizing of the button.
-                  // Not sure why it happens even if everything is display: Block. To override it would
-                  // need changes in Flamegraph which would be weird so this seems relatively sensible.
-                  style={{ marginTop: -7 }}
-                  icon={'download-alt'}
-                  size={'sm'}
-                  variant={'secondary'}
-                  fill={'outline'}
-                  onClick={onClick}
-                />
-              </Tooltip>
-            );
-          }}
-        />
-      );
-    }
-
-    return (
-      <>
-        {props.timelineEl}
-        <FlameGraph
-          getTheme={() => createTheme({ colors: { mode: colorMode } })}
-          data={dataFrame}
-          extraHeaderElements={extraEl}
-          vertical={props.vertical}
-          disableCollapsing={true}
-        />
-      </>
-    );
-  }
-
-  let exportData = undefined;
+  // This is a bit weird but the typing is not great. It seems like flamegraph assumed profile can be undefined
+  // but then ExportData won't work so not sure if the profile === undefined could actually happen.
   if (props.profile) {
-    exportData = (
+    extraEl = (
       <ExportData
         flamebearer={props.profile}
         exportPNG
@@ -100,20 +47,37 @@ export function FlameGraphWrapper(props: Props) {
         exportHTML
         exportFlamegraphDotCom={isExportToFlamegraphDotComEnabled}
         exportFlamegraphDotComFn={exportToFlamegraphDotComFn}
+        buttonEl={({ onClick }) => {
+          return (
+            <Tooltip content={'Export Data'}>
+              <Button
+                // Ugly hack to go around globally defined line height messing up sizing of the button.
+                // Not sure why it happens even if everything is display: Block. To override it would
+                // need changes in Flamegraph which would be weird so this seems relatively sensible.
+                style={{ marginTop: -7 }}
+                icon={'download-alt'}
+                size={'sm'}
+                variant={'secondary'}
+                fill={'outline'}
+                onClick={onClick}
+              />
+            </Tooltip>
+          );
+        }}
       />
     );
   }
 
   return (
-    <FlamegraphRenderer
-      showCredit={false}
-      profile={props.profile}
-      colorMode={colorMode}
-      ExportData={exportData}
-      data-testid={props.dataTestId}
-      sharedQuery={props.sharedQuery}
-    >
+    <>
       {props.timelineEl}
-    </FlamegraphRenderer>
+      <FlameGraph
+        getTheme={() => createTheme({ colors: { mode: colorMode } })}
+        data={dataFrame}
+        extraHeaderElements={extraEl}
+        vertical={props.vertical}
+        disableCollapsing={true}
+      />
+    </>
   );
 }
--- public/app/util/features.ts
+++ public/app/util/features.ts
@@ -1,12 +1,6 @@
 import { z } from 'zod';
 
 const featuresSchema = z.object({
-  googleEnabled: z.boolean().default(false),
-  gitlabEnabled: z.boolean().default(false),
-  githubEnabled: z.boolean().default(false),
-  internalAuthEnabled: z.boolean().default(false),
-  signupEnabled: z.boolean().default(false),
-  isAuthRequired: z.boolean().default(false),
   exportToFlamegraphDotComEnabled: z.boolean().default(true),
 });
 
@@ -22,19 +16,9 @@ function hasFeatures(
   return false;
 }
 
-// Parse features at startup
 const features = featuresSchema.parse(
   hasFeatures(window) ? window.features : {}
 );
 
-// Re-export with more friendly names
-export const isGoogleEnabled = features.googleEnabled;
-export const isGitlabEnabled = features.gitlabEnabled;
-export const isGithubEnabled = features.githubEnabled;
-export const isInternalAuthEnabled = features.internalAuthEnabled;
-export const isSignupEnabled = features.signupEnabled;
 export const isExportToFlamegraphDotComEnabled =
   features.exportToFlamegraphDotComEnabled;
-export const isAuthRequired = features.isAuthRequired;
-
-export const isGrafanaFlamegraphEnabled = true;
