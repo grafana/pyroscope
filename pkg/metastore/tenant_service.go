1130f427d35b72bb132eb3fc56f7a52eab85ba00
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2025-10-23 14:07:13 -0300

    Add basic tracing to metastore endpoints

--- pkg/metastore/tenant_service.go
+++ pkg/metastore/tenant_service.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/go-kit/log"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	otlog "github.com/opentracing/opentracing-go/log"
 	"go.etcd.io/bbolt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -41,12 +44,22 @@ func (svc *TenantService) GetTenants(
 	ctx context.Context,
 	_ *metastorev1.GetTenantsRequest,
 ) (resp *metastorev1.GetTenantsResponse, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "metastore.TenantService.GetTenants")
+	defer func() {
+		if err != nil {
+			ext.Error.Set(span, true)
+			span.LogFields(otlog.Error(err))
+		}
+		span.Finish()
+	}()
+
 	read := func(tx *bbolt.Tx, _ raftnode.ReadIndex) {
 		resp = &metastorev1.GetTenantsResponse{TenantIds: svc.index.GetTenants(tx)}
 	}
 	if readErr := svc.state.ConsistentRead(ctx, read); readErr != nil {
 		return nil, status.Error(codes.Unavailable, readErr.Error())
 	}
+	span.SetTag("tenant_count", len(resp.GetTenantIds()))
 	return resp, err
 }
 
@@ -54,11 +67,29 @@ func (svc *TenantService) GetTenant(
 	ctx context.Context,
 	req *metastorev1.GetTenantRequest,
 ) (resp *metastorev1.GetTenantResponse, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "metastore.TenantService.GetTenant")
+	defer func() {
+		if err != nil {
+			ext.Error.Set(span, true)
+			span.LogFields(otlog.Error(err))
+		}
+		span.Finish()
+	}()
+
+	span.SetTag("tenant_id", req.GetTenantId())
+
 	read := func(tx *bbolt.Tx, _ raftnode.ReadIndex) {
 		resp = &metastorev1.GetTenantResponse{Stats: svc.index.GetTenantStats(tx, req.TenantId)}
 	}
 	if readErr := svc.state.ConsistentRead(ctx, read); readErr != nil {
 		return nil, status.Error(codes.Unavailable, readErr.Error())
+	}
+	if stats := resp.GetStats(); stats != nil {
+		span.SetTag("data_ingested", stats.GetDataIngested())
+		span.SetTag("oldest_profile_time", stats.GetOldestProfileTime())
+		span.SetTag("newest_profile_time", stats.GetNewestProfileTime())
+	} else {
+		span.SetTag("data_ingested", false)
 	}
 	return resp, err
 }
