c0e90517044232ce56eb13b77c9dcb2095cb1972
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2025-10-24 12:32:15 -0300

    Fix linter errors

--- pkg/metastore/tracing.go
+++ pkg/metastore/tracing.go
@@ -10,9 +10,9 @@ var noopTracer = opentracing.NoopTracer{}
 
 // startSpanFromContextIfTracing starts a span only if there's a parent span in the context.
 // Otherwise, it returns a noop span. To be used in places where we might not have access to the original context.
-func startSpanFromContext(ctx context.Context, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
+func startSpanFromContext(ctx context.Context, operationName string) (opentracing.Span, context.Context) {
 	if opentracing.SpanFromContext(ctx) != nil {
-		return opentracing.StartSpanFromContext(ctx, operationName, opts...)
+		return opentracing.StartSpanFromContext(ctx, operationName)
 	}
-	return noopTracer.StartSpan(operationName, opts...), ctx
+	return noopTracer.StartSpan(operationName), ctx
 }
