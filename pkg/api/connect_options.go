8237c7f1dc05c9d262450a78eaa51bd19baa9aae
Tolya Korniltsev <anatoly.korniltsev@grafana.com>
2025-10-31 12:56:19 +0800

    fix(api): add ingest limits to connect push requests

--- pkg/api/connect_options.go
+++ pkg/api/connect_options.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"connectrpc.com/connect"
+	"github.com/grafana/pyroscope/pkg/tenant"
+	"github.com/grafana/pyroscope/pkg/validation"
 
 	connectapi "github.com/grafana/pyroscope/pkg/api/connect"
 	"github.com/grafana/pyroscope/pkg/util"
@@ -32,8 +34,15 @@ func (a *API) connectOptionsAuthRecovery() []connect.HandlerOption {
 	return append(connectapi.DefaultHandlerOptions(), a.connectInterceptorAuth(), connectInterceptorRecovery())
 }
 
-func (a *API) connectOptionsAuthDelayRecovery(limits delayhandler.Limits) []connect.HandlerOption {
-	return append(connectapi.DefaultHandlerOptions(), a.connectInterceptorAuth(), a.connectInterceptorDelay(limits), connectInterceptorRecovery())
+func (a *API) connectOptionsAuthDelayRecovery(limits *validation.Overrides) []connect.HandlerOption {
+	// no per tenant overrides implemented for connect requests
+	messageLimit := int(limits.IngestionBodyLimitBytes(tenant.DefaultTenantID))
+	return append(connectapi.DefaultHandlerOptions(),
+		a.connectInterceptorAuth(),
+		connect.WithReadMaxBytes(messageLimit),
+		a.connectInterceptorDelay(limits),
+		connectInterceptorRecovery(),
+	)
 }
 
 func (a *API) connectOptionsAuthLogRecovery() []connect.HandlerOption {
