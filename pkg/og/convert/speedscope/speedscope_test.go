f71678395af62f2153e9025056266922a5ab1d34
Jake Kramer <899428+jake-kramer@users.noreply.github.com>
2025-11-03 15:25:34 -0500

    Update test to account for different order

--- pkg/og/convert/speedscope/speedscope_test.go
+++ pkg/og/convert/speedscope/speedscope_test.go
@@ -21,6 +21,15 @@ func (m *mockIngester) Put(_ context.Context, p *storage.PutInput) error {
 	return nil
 }
 
+func findInputByLabel(inputs []*storage.PutInput, normalizedLabel string) *storage.PutInput {
+	for _, in := range inputs {
+		if in.LabelSet.Normalized() == normalizedLabel {
+			return in
+		}
+	}
+	return nil
+}
+
 var _ = Describe("Speedscope", func() {
 	It("Can parse an event-format profile", func() {
 		data, err := os.ReadFile("testdata/simple.speedscope.json")
@@ -65,7 +74,8 @@ a;b;d 400
 
 		Expect(ingester.actual).To(HaveLen(2))
 
-		input := ingester.actual[0]
+		input := findInputByLabel(ingester.actual, "foo.seconds{profile_name=one,x=y}")
+		Expect(input).ToNot(BeNil())
 		Expect(input.Units).To(Equal(metadata.SamplesUnits))
 		Expect(input.LabelSet.Normalized()).To(Equal("foo.seconds{profile_name=one,x=y}"))
 		expectedResult := `a;b 500
@@ -75,7 +85,8 @@ a;b;d 400
 		Expect(input.Val.String()).To(Equal(expectedResult))
 		Expect(input.SampleRate).To(Equal(uint32(100)))
 
-		input2 := ingester.actual[1]
+		input2 := findInputByLabel(ingester.actual, "foo.seconds{profile_name=two,x=y}")
+		Expect(input2).ToNot(BeNil())
 		Expect(input2.Units).To(Equal(metadata.SamplesUnits))
 		Expect(input2.LabelSet.Normalized()).To(Equal("foo.seconds{profile_name=two,x=y}"))
 		Expect(input2.Val.String()).To(Equal(expectedResult))
