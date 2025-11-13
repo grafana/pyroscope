0ceb01b1614630369a6bfce2a75660cb4c27e749
Christian Simon <simon@swine.de>
2025-11-13 12:10:33 +0000

    Fix some bugs

--- pkg/frontend/vcs/source/find_java.go
+++ pkg/frontend/vcs/source/find_java.go
@@ -14,6 +14,35 @@ const (
 	ExtJava = ".java"
 )
 
+func convertJavaFunctionNameToPath(functionName string) string {
+	pathSegments := strings.Split(functionName, "/")
+	last := len(pathSegments) - 1
+
+	// pos to cut from
+	pos := -1
+	updatePos := func(v int) {
+		if v != -1 {
+			return
+		}
+		if pos == -1 || pos > v {
+			pos = v
+		}
+	}
+
+	// find first dot in last segment
+	updatePos(strings.Index(pathSegments[last], "."))
+
+	// find first $ in last segment
+	updatePos(strings.Index(pathSegments[last], "$"))
+
+	if pos > 0 {
+		pathSegments[last] = pathSegments[last][:pos]
+	}
+
+	pathSegments[last] = pathSegments[last] + ExtJava
+	return strings.Join(pathSegments, "/")
+}
+
 // findJavaFile finds a java file in a vcs repository.
 func (ff FileFinder) findJavaFile(ctx context.Context, mappings ...*config.MappingConfig) (*vcsv1.GetFileResponse, error) {
 	sp, ctx := opentracing.StartSpanFromContext(ctx, "findJavaFile")
@@ -21,20 +50,7 @@ func (ff FileFinder) findJavaFile(ctx context.Context, mappings ...*config.Mappi
 	sp.SetTag("file.function_name", ff.file.FunctionName)
 	sp.SetTag("file.path", ff.file.Path)
 
-	pathSegments := strings.Split(ff.file.FunctionName, "/")
-	last := len(pathSegments) - 1
-
-	// find first dot in last segment
-	posDot := strings.Index(pathSegments[last], ".")
-	if posDot > 0 {
-		pathSegments[last] = pathSegments[last][:posDot]
-	}
-	// TODO: treat $$
-
-	pathSegments[last] = pathSegments[last] + ExtJava
-
-	javaPath := strings.Join(pathSegments, "/")
-
+	javaPath := convertJavaFunctionNameToPath(ff.file.FunctionName)
 	for _, m := range mappings {
 		return ff.fetchMappingFile(ctx, m, javaPath)
 	}
