bc3636f7d023a595b7bdf2cd0bb22df65314871f
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2026-02-26 15:21:26 -0400

    examples(java): add traces-to-profiles example using wall profiles

---
+++ examples/tracing/java-wall/src/main/java/org/example/rideshare/OrderService.java
@@ -0,0 +1,85 @@
+package org.example.rideshare;
+
+import io.pyroscope.labels.v2.LabelsSet;
+import io.pyroscope.labels.v2.Pyroscope;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class OrderService {
+
+    public static final Duration OP_DURATION = Duration.of(200, ChronoUnit.MILLIS);
+
+    public synchronized void findNearestVehicle(int searchRadius, String vehicle) {
+        Pyroscope.LabelsWrapper.run(new LabelsSet("vehicle", vehicle), () -> {
+            AtomicLong i = new AtomicLong();
+            Instant end = Instant.now()
+                    .plus(OP_DURATION.multipliedBy(searchRadius));
+            while (Instant.now().compareTo(end) <= 0) {
+                i.incrementAndGet();
+            }
+            waitForVehicleResponse(searchRadius);
+
+            if (vehicle.equals("car")) {
+                checkDriverAvailability(searchRadius);
+            }
+        });
+    }
+
+    private void waitForVehicleResponse(int searchRadius) {
+        try {
+            Thread.sleep(OP_DURATION.multipliedBy(searchRadius).toMillis());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void checkDriverAvailability(int searchRadius) {
+        AtomicLong i = new AtomicLong();
+        Instant end = Instant.now()
+                .plus(OP_DURATION.multipliedBy(searchRadius));
+        while (Instant.now().compareTo(end) <= 0) {
+            i.incrementAndGet();
+        }
+        waitForDriverConfirmation(searchRadius);
+        // Every other minute this will artificially create make requests in eu-north region slow
+        // this is just for demonstration purposes to show how performance impacts show up in the
+        // flamegraph
+        boolean force_mutex_lock = Instant.now().atZone(ZoneOffset.UTC).getMinute() % 2 == 0;
+        if (System.getenv("REGION").equals("eu-north") && force_mutex_lock) {
+            mutexLock(searchRadius);
+        }
+    }
+
+    private void waitForDriverConfirmation(int searchRadius) {
+        try {
+            Thread.sleep(OP_DURATION.multipliedBy(searchRadius).toMillis());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void mutexLock(int searchRadius) {
+        AtomicLong i = new AtomicLong();
+        Instant end = Instant.now()
+                .plus(OP_DURATION.multipliedBy(30L * searchRadius));
+        while (Instant.now().compareTo(end) <= 0) {
+            i.incrementAndGet();
+        }
+        waitForMutexRelease(searchRadius);
+    }
+
+    private void waitForMutexRelease(int searchRadius) {
+        try {
+            Thread.sleep(OP_DURATION.multipliedBy(30L * searchRadius).toMillis());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
