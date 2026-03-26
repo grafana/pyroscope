8355676a58c7a93bbdf089d166ed966dab32c2be
bryanhuhta <bryan.huhta@grafana.com>
2026-03-26 12:09:58 -0700

    Remove old ui

--- public/app/pages/PageContentWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export function PageContentWrapper({ children }: { children: ReactNode }) {
-  return <div className="main-wrapper">{children}</div>;
-}
--- public/app/ui/Menu.tsx
+++
@@ -1,9 +0,0 @@
-import {
-  Menu,
-  MenuItem,
-  ControlledMenu,
-  useMenuState,
-} from '@szhsin/react-menu';
-import type { ClickEvent } from '@szhsin/react-menu';
-
-export { ControlledMenu, Menu, MenuItem, useMenuState, ClickEvent };
