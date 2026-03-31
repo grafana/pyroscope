d8f39d126cb1038a546dfce0aa2dd9a9b71e845d
Alberto Soto <alberto.soto@grafana.com>
2026-03-31 11:47:04 +0200

    v1 / v2 tests

--- cmd/profilecli/canary_exporter.go
+++ cmd/profilecli/canary_exporter.go
@@ -34,7 +34,7 @@ type canaryExporterParams struct {
 	ListenAddress string
 	TestFrequency time.Duration
 	TestDelay     time.Duration
-	QueryProbeSet string
+	QueryProbeSet []string
 }
 
 func addCanaryExporterParams(ceCmd commander) *canaryExporterParams {
@@ -44,7 +44,7 @@ func addCanaryExporterParams(ceCmd commander) *canaryExporterParams {
 	ceCmd.Flag("listen-address", "Listen address for the canary exporter.").Default(":4101").StringVar(&params.ListenAddress)
 	ceCmd.Flag("test-frequency", "How often the specified Pyroscope cell should be tested.").Default("15s").DurationVar(&params.TestFrequency)
 	ceCmd.Flag("test-delay", "The delay between ingest and query requests.").Default("2s").DurationVar(&params.TestDelay)
-	ceCmd.Flag("query-probe-set", "Which set of probes to use for query requests. Available sets are \"default\" and \"all\".").Default("default").EnumVar(&params.QueryProbeSet, "default", "all")
+	ceCmd.Flag("query-probe-set", "Which set of probes to use for query requests. Repeatable. Available sets are \"default\", \"extended\", \"v2\", and \"all\" (extended + v2).").Default("default").EnumsVar(&params.QueryProbeSet, "default", "extended", "v2", "all")
 	params.phlareClient = addPhlareClient(ceCmd)
 
 	return params
@@ -159,10 +159,19 @@ func newCanaryExporter(params *canaryExporterParams) *canaryExporter {
 		queryProbes: make([]*queryProbe, 0),
 	}
 
+	probeSetEnabled := func(name string) bool {
+		for _, s := range params.QueryProbeSet {
+			if s == name {
+				return true
+			}
+		}
+		return false
+	}
+
 	ce.queryProbes = append(ce.queryProbes, &queryProbe{name: "query-select-merge-profile", f: ce.testSelectMergeProfile})
 	ce.queryProbes = append(ce.queryProbes, &queryProbe{name: "query-select-merge-otlp-profile", f: ce.testSelectMergeOTLPProfile})
 
-	if params.QueryProbeSet == "all" {
+	if probeSetEnabled("extended") || probeSetEnabled("all") {
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-profile-types", ce.testProfileTypes})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-series", ce.testSeries})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-label-names", ce.testLabelNames})
@@ -171,9 +180,12 @@ func newCanaryExporter(params *canaryExporterParams) *canaryExporter {
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-select-merge-stacktraces", ce.testSelectMergeStacktraces})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-select-merge-span-profile", ce.testSelectMergeSpanProfile})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-get-profile-stats", ce.testGetProfileStats})
-		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-exemplars", ce.testQueryExemplars})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"render", ce.testRender})
 		ce.queryProbes = append(ce.queryProbes, &queryProbe{"render-diff", ce.testRenderDiff})
+	}
+
+	if probeSetEnabled("v2") || probeSetEnabled("all") {
+		ce.queryProbes = append(ce.queryProbes, &queryProbe{"query-exemplars", ce.testQueryExemplars})
 	}
 
 	metricsPath := "/metrics"
